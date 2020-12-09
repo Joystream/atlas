847310a11437d0094fb77e9d2016b117b61a3182
Francesco <gamarantor@gmail.com>
2020-12-09 10:24:07 +0100

    scroll to top on navigation (#112)
    * Scroll To Top After Navigation
    
    * Scroll To Top Of Category Picker After Re-selecting
    
    * Code Review Fixes
    
    * Code Review Fixes
    
    * Code Review Fixes
    
    * Code Review Fixes
    
    * Code Review Fixes

--- src/components/LayoutWithRouting.tsx
+++ src/components/LayoutWithRouting.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from '@emotion/styled'
-import { RouteComponentProps, Router, navigate } from '@reach/router'
+import { RouteComponentProps, Router, navigate, globalHistory } from '@reach/router'
 import { ErrorBoundary } from '@sentry/react'
 
 import { GlobalStyle } from '@/shared/components'
@@ -24,21 +24,31 @@ const Route: React.FC<RouteProps> = ({ Component, ...pathProps }) => {
   )
 }
 
-const LayoutWithRouting: React.FC = () => (
-  <>
-    <GlobalStyle />
-    <Navbar default />
-    <MainContainer>
-      <Router primary={false}>
-        <Route default Component={HomeView} />
-        <Route path={routes.video()} Component={VideoView} />
-        <Route path={routes.search()} Component={SearchView} />
-        <Route Component={BrowseView} path={routes.browse()} />
-        <Route Component={ChannelView} path={routes.channel()} />
-      </Router>
-    </MainContainer>
-  </>
-)
+const LayoutWithRouting: React.FC = () => {
+  useEffect(() => {
+    const unsubscribeFromHistory = globalHistory.listen(({ action }) => {
+      if (action === 'PUSH') {
+        window.scrollTo(0, 0)
+      }
+    })
+    return unsubscribeFromHistory
+  }, [])
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar default />
+      <MainContainer>
+        <Router primary={false}>
+          <Route default Component={HomeView} />
+          <Route path={routes.video()} Component={VideoView} />
+          <Route path={routes.search()} Component={SearchView} />
+          <Route Component={BrowseView} path={routes.browse()} />
+          <Route Component={ChannelView} path={routes.channel()} />
+        </Router>
+      </MainContainer>
+    </>
+  )
+}
 
 const MainContainer = styled.main`
   padding: 0 var(--global-horizontal-padding);
