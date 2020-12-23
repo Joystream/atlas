4794b87c7806c543c676b1174f6116b579a06a28
Klaudiusz Dembler <dev@kdembler.com>
2020-12-23 22:32:41 +0100

    update URLs for Babylon

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -13,7 +13,7 @@ export const QUERY_NODE_GRAPHQL_URL = envQueryNodeUrl || MOCK_QUERY_NODE_GRAPHQL
 export const MOCK_ORION_GRAPHQL_URL = '/orion-graphql'
 export const ORION_GRAPHQL_URL = envOrionUrl || MOCK_ORION_GRAPHQL_URL
 
-export const MOCK_STORAGE_NODE_URL = 'https://staging-3.joystream.app/storage/asset/v0/'
+export const MOCK_STORAGE_NODE_URL = 'https://rome-rpc-endpoint.joystream.org/asset/v0/'
 export const STORAGE_NODE_URL = envStorageNodeUrl || MOCK_STORAGE_NODE_URL
 
 export const SENTRY_DSN = envSentryDSN
