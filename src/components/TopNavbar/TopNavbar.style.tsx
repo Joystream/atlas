21c5e4160767b594cf50a3284d7b74510d3c94af
Bartosz Dryl <drylbartosz@gmail.com>
2021-01-18 15:48:13 +0100

    introduce sidenav (#209)
    * refactor HamburgerButton and SideNav
    
    * remove most links from navbar and add navbar stylings
    
    * create useWindowSize hook
    
    * update styles for mobile devices
    
    * add bars icons
    
    * fix issue with not working styled Icon
    
    * update Layout styles, add sidenav items
    
    * remove redundant code
    
    * remove unnecessary icons from navbar and sidenav
    
    * remove react-spring animations and react-spring itself
    
    * fix searchbar height on mobile devices
    
    * fix issue with undefined component
    
    * fix hamburger button position and DrawerOverlay z-index
    
    * rename Navbar to TopNavbar
    
    * rename Sidenav to SideNavbar

--- src/components/TopNavbar/TopNavbar.style.tsx
+++ src/components/TopNavbar/TopNavbar.style.tsx
@@ -1,38 +1,51 @@
 import styled from '@emotion/styled'
 
-import { Icon, Searchbar } from '@/shared/components'
+import { Searchbar } from '@/shared/components'
 import { breakpoints, colors, sizes, transitions, zIndex } from '@/shared/theme'
 import { ReactComponent as UnstyledShortLogo } from '@/assets/logo.svg'
 import { ReactComponent as UnstyledFullLogo } from '@/assets/full-logo.svg'
 import { Link } from '@reach/router'
 
-type NavbarStyleProps = {
+type TopNavbarStyleProps = {
   hasFocus: boolean
 }
 
 export const StyledSearchbar = styled(Searchbar)`
   transition: max-width ${transitions.timings.regular} ${transitions.easing};
   will-change: max-width;
+  height: 42px;
+  @media screen and (min-width: ${breakpoints.small}) {
+    height: initial;
+  }
 `
-export const NAVBAR_HEIGHT = 81
-export const Header = styled.header<NavbarStyleProps>`
+export const TOP_NAVBAR_HEIGHT = 81
+export const Header = styled.header<TopNavbarStyleProps>`
+  width: 100%;
   position: sticky;
   top: 0;
   z-index: ${zIndex.header};
-  min-height: ${NAVBAR_HEIGHT}px;
-  max-height: ${NAVBAR_HEIGHT}px;
-  display: grid;
-  width: 100%;
+  min-height: ${TOP_NAVBAR_HEIGHT}px;
+  max-height: ${TOP_NAVBAR_HEIGHT}px;
 
-  grid-template-columns: 1fr 2fr;
+  display: flex;
+  justify-content: space-between;
+
   border-bottom: 1px solid ${colors.gray[800]};
 
-  padding: ${sizes(2)} ${sizes(3)};
-  @media screen and (min-width: ${breakpoints.small}) {
-    padding: ${sizes(3)} ${sizes(3)};
-  }
   background-color: ${(props) => (props.hasFocus ? colors.gray[900] : colors.black)};
   transition: all 0.4s ${transitions.easing};
+  padding: ${sizes(3)} ${sizes(3)};
+
+  @media screen and (min-width: ${breakpoints.small}) {
+    z-index: ${zIndex.header};
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;
+    column-gap: ${sizes(2)};
+  }
+  @media screen and (min-width: ${breakpoints.medium}) {
+    margin-left: ${sizes(14)};
+    width: calc(100% - ${sizes(14)});
+  }
 
   ${StyledSearchbar} {
     width: 100%;
@@ -42,7 +55,7 @@ export const Header = styled.header<NavbarStyleProps>`
 
 export const LogoLink = styled(Link)`
   padding: 0 ${sizes(5)};
-  margin-right: ${sizes(2)};
+  margin-right: ${sizes(1)};
   @media screen and (min-width: ${breakpoints.medium}) {
     margin-right: ${sizes(5)};
   }
@@ -65,74 +78,29 @@ export const FullLogo = styled(UnstyledFullLogo)`
 `
 
 export const NavigationContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: ${sizes(3)};
+  margin-left: ${sizes(10)};
+  margin-top: ${sizes(1)};
+  display: none;
+  align-items: flex-start;
+  @media screen and (min-width: ${breakpoints.small}) {
+    align-items: center;
+    display: flex;
+    margin: 0 ${sizes(3)} 0 ${sizes(12)};
+  }
+  @media screen and (min-width: ${breakpoints.medium}) {
+    margin-left: 0;
+  }
 `
 
 export const SearchbarContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-
-  justify-self: end;
-
-  width: 100%;
   max-width: 1156px;
-`
-export const ActiveIcon = styled(Icon)`
-  display: none;
-  color: ${colors.gray[300]};
-`
-
-export const InactiveIcon = styled(Icon)`
-  display: block;
-`
-
-export const StyledLink = styled(Link)`
-  display: inline-flex;
+  width: 100%;
+  display: flex;
+  justify-content: center;
   align-items: center;
-  color: ${colors.gray[300]};
-  font-weight: 500;
+  margin-left: ${sizes(13)};
 
-  text-decoration: none;
-
-  span {
-    display: none;
-    margin-left: ${sizes(3)};
-  }
   @media screen and (min-width: ${breakpoints.small}) {
-    span {
-      display: inline-block;
-    }
-  }
-
-  &[data-active='true'] {
-    ${ActiveIcon} {
-      display: block;
-      color: ${colors.gray[100]};
-    }
-    ${InactiveIcon} {
-      display: none;
-    }
-    color: ${colors.gray[100]};
-  }
-
-  &:hover {
-    ${ActiveIcon} {
-      display: block;
-      color: ${colors.white};
-    }
-    ${InactiveIcon} {
-      display: none;
-    }
-    color: ${colors.white};
-  }
-
-  & + & {
-    margin-left: ${sizes(3)};
-    @media screen and (min-width: ${breakpoints.small}) {
-      margin-left: ${sizes(6)};
-    }
+    margin: 0;
   }
 `
---
+++ src/components/TopNavbar/index.ts
@@ -0,0 +1,3 @@
+import TopNavbar from './TopNavbar'
+export { TOP_NAVBAR_HEIGHT } from './TopNavbar.style'
+export default TopNavbar
