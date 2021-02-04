83a358fc48d8fa80753117f9fdb157e63a1419bc
Mikolaj <mikkiodev@gmail.com>
2021-02-04 10:54:47 +0100

    remove text header, edit styled input

--- src/shared/components/HeaderTextField/HeaderTextField.style.tsx
+++ src/shared/components/HeaderTextField/HeaderTextField.style.tsx
@@ -3,21 +3,21 @@ import { fluidRange } from 'polished'
 import { Text } from '@/shared/components'
 import { colors, sizes, typography, breakpoints } from '@/shared/theme'
 
-export const Title = styled(Text)`
+export const StyledInput = styled.input`
   line-height: 1;
   padding: ${sizes(1)} ${sizes(2)} ${sizes(2)};
   ${fluidRange({ prop: 'fontSize', fromSize: '32px', toSize: '40px' })};
+  color: white;
   background-color: ${colors.gray[800]};
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
-  width: fit-content;
+  font-family: ${typography.fonts.headers};
+  font-weight: ${typography.weights.bold};
+  border: none;
+  min-width: 100px;
   max-width: 100%;
-  transition: 0.2s;
-  cursor: pointer;
-  &:hover {
-    filter: brightness(80%);
-  }
+  height: ${sizes(13)};
   @media screen and (min-width: ${breakpoints.small}) {
     max-width: 600px;
   }
@@ -31,24 +31,12 @@ export const WarningText = styled(Text)`
   ${fluidRange({ prop: 'fontSize', fromSize: '10px', toSize: '16px' })};
 `
 
-export const StyledInput = styled.input`
-  line-height: 1;
-  padding: ${sizes(1)} ${sizes(2)} ${sizes(2)};
-  ${fluidRange({ prop: 'fontSize', fromSize: '32px', toSize: '40px' })};
-  color: white;
-  background-color: ${colors.gray[800]};
-  font-family: ${typography.fonts.headers};
-  font-weight: ${typography.weights.bold};
-  border: none;
-  width: 100%;
-  max-width: 600px;
-  max-height: 50px;
-`
-
 export const StyledTooltip = styled.span`
-  position: relative;
+  position: absolute;
   opacity: 0;
   transition: 0.2s;
+  left: 0;
+  bottom: -${sizes(4)};
   &:before {
     display: inline-block;
     content: attr(data-text);
@@ -74,7 +62,7 @@ export const StyledTooltip = styled.span`
 
 export const Container = styled.div`
   position: relative;
-  & ${Title}:hover ~ ${StyledTooltip} {
+  & ${StyledInput}:hover ~ ${StyledTooltip} {
     opacity: 1;
   }
 `
--- src/shared/components/HeaderTextField/HeaderTextField.tsx
+++ src/shared/components/HeaderTextField/HeaderTextField.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Container, Title, WarningText, StyledTooltip, StyledInput } from './HeaderTextField.style'
+import React, { useEffect } from 'react'
+import { Container, WarningText, StyledTooltip, StyledInput } from './HeaderTextField.style'
 
 export type HeaderTextFieldProps = {
   value: string
@@ -11,34 +11,18 @@ export type HeaderTextFieldProps = {
 
 const HeaderTextField = React.forwardRef<HTMLInputElement, HeaderTextFieldProps>(
   ({ value, helperText, errorText, onChange, variant = 'default' }, ref) => {
-    const [isInEditMode, setEditMode] = useState(false)
-
-    const handleKeyDown = (e: React.KeyboardEvent) => {
-      if (e.key !== 'Enter' || ref === null) {
+    useEffect(() => {
+      if (ref === null) {
         return
       }
-      setEditMode(false)
-    }
+      ref.current.style.width = value.length + 'ch'
+    }, [ref, value, value.length])
 
     return (
       <Container>
-        {isInEditMode ? (
-          <StyledInput
-            ref={ref}
-            type="text"
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            onKeyDown={handleKeyDown}
-            onBlur={() => setEditMode(false)}
-            autoFocus
-          />
-        ) : (
-          <Title variant="h1" onClick={() => setEditMode(true)}>
-            {value}
-            {variant === 'error' && <WarningText variant="body1">{errorText}</WarningText>}
-          </Title>
-        )}
-        {!isInEditMode && value && <StyledTooltip data-text={helperText} />}
+        <StyledInput ref={ref} type="text" value={value} onChange={(e) => onChange(e.target.value)} />
+        {variant === 'error' && <WarningText variant="body1">{errorText}</WarningText>}
+        {value && <StyledTooltip data-text={helperText} />}
       </Container>
     )
   }
