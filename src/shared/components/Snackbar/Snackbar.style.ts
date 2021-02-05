e70406ee7d87b461594ef50fd7d6273684778641
drillprop <drylbartosz@gmail.com>
2021-02-05 14:13:29 +0100

    update Snackbar styles

--- src/shared/components/Snackbar/Snackbar.style.ts
+++ src/shared/components/Snackbar/Snackbar.style.ts
@@ -1,5 +1,7 @@
-import { colors, sizes, typography } from '@/shared/theme'
+import { colors, sizes, typography, zIndex } from '@/shared/theme'
 import styled from '@emotion/styled'
+
+const SNACKBAR_POSITION = 34
 
 export const SnackbarWrapper = styled.div`
   background-color: ${colors.gray[800]};
@@ -8,6 +10,10 @@ export const SnackbarWrapper = styled.div`
   align-items: center;
   padding: ${sizes(3)} ${sizes(3)} ${sizes(3)} ${sizes(6)};
   max-width: 430px;
+  position: fixed;
+  bottom: ${SNACKBAR_POSITION}px;
+  left: ${SNACKBAR_POSITION}px;
+  z-index: ${zIndex.overlay};
 `
 
 export const SnackbarButton = styled.button`
@@ -24,7 +30,7 @@ export const SnackbarParagraph = styled.p`
   color: ${colors.white};
   display: flex;
   align-items: center;
-
+  word-break: break-all;
   svg {
     margin-right: ${sizes(4)};
     width: ${sizes(6)};
