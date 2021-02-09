afbe17073ca594f18c9068828ff1bb75f4b6d084
Bartosz Dryl <drylbartosz@gmail.com>
2021-02-09 14:56:10 +0100

    add TextField and Select components (#297)
    * install downshift
    
    * create InputBase component
    
    * update colors
    
    * create Select and TextField component
    
    * fix imports
    
    * update Select props
    
    * fix Select for CR
    
    * fix TextField for CR
    
    * fix label transition in text field
    
    * fix helperText logic
    
    * refactor TextField
    
    * add story with uncontrolled input
    
    * minor improvements to TextField stories
    
    * styling updates for inputbase and select
    
    * cr fixes

---
+++ src/shared/components/Select/Select.tsx
@@ -0,0 +1,68 @@
+import React, { forwardRef } from 'react'
+import InputBase, { InputBaseProps } from '../InputBase'
+import { SelectButton, SelectMenu, SelectOption, SelectWrapper, StyledLabelText } from './Select.style'
+import { useSelect, UseSelectStateChange } from 'downshift'
+import Icon from '../Icon'
+
+export type SelectedItem = {
+  value: string
+  name: string
+}
+
+export type SelectProps = {
+  onChange?: (changes: UseSelectStateChange<SelectedItem>) => void
+  value?: SelectedItem | null
+  items?: SelectedItem[]
+  placeholder?: string
+} & InputBaseProps
+
+const SelectComponent: React.ForwardRefRenderFunction<HTMLDivElement, SelectProps> = (
+  { label = '', items, placeholder = 'Select option', error, value, disabled, onChange, ...inputBaseProps },
+  ref
+) => {
+  const {
+    isOpen,
+    selectedItem,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    highlightedIndex,
+    getItemProps,
+  } = useSelect({ items: items || [], selectedItem: value, onSelectedItemChange: onChange })
+
+  return (
+    <InputBase error={error} disabled={disabled} {...inputBaseProps}>
+      <SelectWrapper ref={ref}>
+        <SelectButton
+          disabled={disabled}
+          error={error}
+          filled={!!selectedItem}
+          isOpen={isOpen}
+          type="button"
+          {...getToggleButtonProps()}
+          tabIndex={disabled ? -1 : 0}
+        >
+          {selectedItem?.name || placeholder}
+          <Icon name="chevron-down" />
+        </SelectButton>
+        <label {...getLabelProps()} tabIndex={disabled ? -1 : 0}>
+          <StyledLabelText>{label}</StyledLabelText>
+        </label>
+        <SelectMenu isOpen={isOpen} {...getMenuProps()}>
+          {isOpen &&
+            items &&
+            items.map((item, index) => (
+              <SelectOption isSelected={highlightedIndex === index} key={index} {...getItemProps({ item, index })}>
+                {item?.name}
+              </SelectOption>
+            ))}
+        </SelectMenu>
+      </SelectWrapper>
+    </InputBase>
+  )
+}
+const Select = forwardRef(SelectComponent)
+
+Select.displayName = 'Select'
+
+export default Select
---
+++ src/shared/components/TextField/TextField.tsx
@@ -0,0 +1,45 @@
+import React, { forwardRef } from 'react'
+import InputBase, { InputBaseProps, LabelText } from '../InputBase'
+import { TextInput } from './TextField.style'
+
+export type TextFieldProps = {
+  type?: 'text' | 'email' | 'password' | 'search'
+  value?: string
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void
+  onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void
+  required?: boolean
+  className?: string
+} & InputBaseProps
+
+const TextFieldComponent: React.ForwardRefRenderFunction<HTMLInputElement, TextFieldProps> = (
+  { type = 'text', label, value, onChange, onBlur, onFocus, error, disabled, required, ...inputBaseProps },
+  ref
+) => {
+  return (
+    <InputBase error={error} disabled={disabled} {...inputBaseProps}>
+      <label>
+        <TextInput
+          ref={ref}
+          value={value}
+          disabled={disabled}
+          error={error}
+          onChange={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          placeholder={label}
+          type={type}
+          required={required}
+          tabIndex={disabled ? -1 : 0}
+        />
+        <LabelText>{label}</LabelText>
+      </label>
+    </InputBase>
+  )
+}
+
+const TextField = forwardRef(TextFieldComponent)
+
+TextField.displayName = 'TextField'
+
+export default TextField
