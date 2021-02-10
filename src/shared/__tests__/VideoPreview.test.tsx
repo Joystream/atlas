a539c83ec3fc32ceb45018ff26315b927a97c768
Diego Cardenas <cardenasrdiego@gmail.com>
2021-02-10 12:54:50 +0100

    Fix test

--- src/shared/__tests__/VideoPreview.test.tsx
+++ src/shared/__tests__/VideoPreview.test.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { VideoPreviewBase } from '@/shared/components/VideoPreviewBase'
+import { BrowserRouter } from 'react-router-dom'
 
 describe('VideoPreviewBase component', () => {
   ;(global as any).ResizeObserver = class {
     disconnect() {}
     observe(element: any, initObject: any) {}
   }
-  it('Should render Video Preview correctly', () => {
+  it.only('Should render Video Preview correctly', () => {
     expect(
       mount(
-        <VideoPreviewBase
-          title="Some Video Title"
-          channelHandle="some channel"
-          thumbnailUrl=""
-          views={1000}
-          createdAt={new Date()}
-        />
+        <BrowserRouter>
+          <VideoPreviewBase
+            title="Some Video Title"
+            channelHandle="some channel"
+            thumbnailUrl=""
+            views={1000}
+            createdAt={new Date()}
+          />
+        </BrowserRouter>
       )
     ).toBeDefined()
   })
