7c5fab326875de2ad464bf9b677339b7222b89c7
drillprop <drylbartosz@gmail.com>
2021-02-10 13:56:10 +0100

    create Avatar.stories

---
+++ src/shared/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, Story } from '@storybook/react'
+import Avatar, { AvatarProps } from './Avatar'
+import React from 'react'
+
+export default {
+  title: 'Shared/Avatar',
+  component: Avatar,
+} as Meta
+
+const Template: Story<AvatarProps> = (args) => <Avatar {...args} />
+
+export const Default = Template.bind({})
+
+export const WithAvatar = Template.bind({})
+WithAvatar.args = {
+  imageUrl: 'https://picsum.photos/200/300',
+}
