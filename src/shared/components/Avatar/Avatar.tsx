69b487fff9e8cb536867b4d2a75df481b4d5b2b1
drillprop <drylbartosz@gmail.com>
2021-02-10 14:01:17 +0100

    add EditButton to avatar

--- src/shared/components/Avatar/Avatar.tsx
+++ src/shared/components/Avatar/Avatar.tsx
@@ -1,39 +1,52 @@
 import React from 'react'
 import { CSSTransition } from 'react-transition-group'
-import { AvatarSize, Container, StyledImage, StyledTransitionGroup, StyledPlaceholder } from './Avatar.style'
+import {
+  AvatarSize,
+  Container,
+  StyledImage,
+  StyledTransitionGroup,
+  StyledPlaceholder,
+  SilhouetteAvatar,
+  EditButton,
+} from './Avatar.style'
 import { transitions } from '@/shared/theme'
+import Icon from '../Icon'
 
 export type AvatarProps = {
   onClick?: (e: React.MouseEvent<HTMLElement>) => void
+  onEditClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
   imageUrl?: string | null
   loading?: boolean
   className?: string
   size?: AvatarSize
+  editable?: boolean
 }
 
-const initialsFromHandle = (rawHandle: string | null | undefined): string => {
-  const handle = rawHandle || 'Unknown'
-  const vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-  const [first = '', second = ''] = handle.split('')
-  return vowels.includes(second) ? first : `${first}${second}`
-}
-
-const Avatar: React.FC<AvatarProps> = ({ imageUrl, handle, loading = false, size = 'default', onClick, className }) => {
+const Avatar: React.FC<AvatarProps> = ({
+  imageUrl,
+  loading = false,
+  size = 'default',
+  onClick,
+  onEditClick,
+  className,
+  editable,
+}) => {
+  const isEditable = !loading && editable && size !== 'default'
   return (
     <Container onClick={onClick} size={size} className={className}>
       <StyledTransitionGroup>
+        {isEditable && (
+          <EditButton size={size} withAvatar={!!imageUrl} onClick={onEditClick}>
+            <Icon name="camera" />
+            <span>{imageUrl ? 'Edit' : 'Add avatar'}</span>
+          </EditButton>
+        )}
         <CSSTransition
           key={loading ? 'placeholder' : 'content'}
           timeout={parseInt(transitions.timings.loading)}
           classNames={transitions.names.fade}
         >
-          {loading ? (
-            <StyledPlaceholder rounded />
-          ) : imageUrl ? (
-            <StyledImage src={imageUrl} />
-          ) : (
-            <span>{initialsFromHandle(handle)}</span>
-          )}
+          {loading ? <StyledPlaceholder rounded /> : imageUrl ? <StyledImage src={imageUrl} /> : <SilhouetteAvatar />}
         </CSSTransition>
       </StyledTransitionGroup>
     </Container>
