9351a48ffec9f20e975ad1578773e0a324174f53
Mikolaj <mikkiodev@gmail.com>
2021-02-18 15:08:31 +0100

    change EventListener

--- src/hooks/useContextMenu.tsx
+++ src/hooks/useContextMenu.tsx
@@ -4,13 +4,15 @@ export const useContextMenu = () => {
   const [position, setPosition] = useState({ x: 0, y: 0, left: true })
   const [isActive, setMenuActive] = useState(false)
 
+  // Adds eventListener only when the menu is active, removes listener after click
+  // To make it work properly menu should not be binded to more than one component
   useEffect(() => {
-    const closeContextMenu = () => setMenuActive(false)
-    document.addEventListener('click', closeContextMenu, true)
-    return () => {
-      document.removeEventListener('click', closeContextMenu, true)
+    if (!isActive) {
+      return
     }
-  }, [])
+    const closeContextMenu = () => setMenuActive(false)
+    document.addEventListener('click', closeContextMenu, { once: true })
+  }, [isActive])
 
   const openContextMenu = useCallback((event: React.MouseEvent, menuWidth: number) => {
     const clickPositionFromRight = document.body.clientWidth - event.pageX
