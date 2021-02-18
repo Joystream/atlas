5e5d10cb702c7c7772a9d7e52db9acb61c55f6f4
mikkio <mikkiodev@gmail.com>
2021-02-18 15:50:29 +0100

    add ContextMenu component (#321)
    * add context menu
    
    * edit hook
    
    * add portal and container
    
    * edit styles
    
    * edit styles
    
    * cr fixes, add css transition
    
    * change IconType import
    
    * fix hook dependency, remove second menu in stories
    
    * change EventListener
    
    * fix index
    
    * add capture to EventListener

---
+++ src/shared/components/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Portal } from '@/components'
+import { useOverlayManager } from '@/hooks'
+import { CSSTransition } from 'react-transition-group'
+import { StyledContainer, StyledMenuItem, StyledIcon, StyledText, menuTransitions } from './ContextMenu.style'
+import type { IconType } from '../Icon'
+
+type MenuItemProps = {
+  iconName: IconType
+  onClick: () => void
+}
+
+type ContextMenuProps = {
+  contextMenuOpts: {
+    isActive?: boolean
+    position: { x: number; y: number; left: boolean }
+  }
+}
+
+export const ContextMenuItem: React.FC<MenuItemProps> = ({ iconName, children, onClick }) => {
+  return (
+    <StyledMenuItem onClick={onClick}>
+      <StyledIcon name={iconName} />
+      <StyledText>{children}</StyledText>
+    </StyledMenuItem>
+  )
+}
+
+const ContextMenu: React.FC<ContextMenuProps> = ({ contextMenuOpts: { isActive, position }, children }) => {
+  const { contextMenuContainerRef } = useOverlayManager()
+  return (
+    <Portal containerRef={contextMenuContainerRef}>
+      <CSSTransition in={isActive} timeout={150} classNames="menu" css={menuTransitions} unmountOnExit>
+        <StyledContainer className="menu" position={position}>
+          {children}
+        </StyledContainer>
+      </CSSTransition>
+    </Portal>
+  )
+}
+
+export default ContextMenu
