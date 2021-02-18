943dba3e8e2c63747de17e1ec8ab3ac47bc5a31b
mikkio <mikkiodev@gmail.com>
2021-02-18 14:34:34 +0100

    add Multistepper component (#317)
    * add multistepper
    
    * edit styles
    
    * cr fixes
    
    * cr fixes
    
    * add StyledDialog
    
    * fix css variables, edit styling for header, move transitionStyles to Global

---
+++ src/components/Dialogs/Multistepper/Multistepper.stories.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import Multistepper from './Multistepper'
+import { Story, Meta } from '@storybook/react'
+import { OverlayManagerProvider } from '@/hooks/useOverlayManager'
+import { Button } from '@/shared/components'
+
+export default {
+  title: 'General components/Multistepper',
+  component: Multistepper,
+  argTypes: {
+    exitButton: { defaultValue: true },
+  },
+  decorators: [
+    (Story) => (
+      <OverlayManagerProvider>
+        <Story />
+      </OverlayManagerProvider>
+    ),
+  ],
+} as Meta
+type ElementType = {
+  step: string
+  handleStep: (idx: number) => void
+  currentStepIdx: number
+}
+const Element: React.FC<ElementType> = ({ step, handleStep, currentStepIdx }) => {
+  return (
+    <>
+      <h1>Hello! This is a {step} step!</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Button onClick={() => handleStep(currentStepIdx - 1)}>Previous step</Button>
+        <Button onClick={() => handleStep(currentStepIdx + 1)}>Next step</Button>
+      </div>
+    </>
+  )
+}
+
+const RegularTemplate: Story = (args) => {
+  const [currentStepIdx, setCurrentStep] = useState(0)
+  const handleStepChange = (idx: number) => {
+    if (idx < 0 || idx > steps.length - 1) {
+      return
+    }
+    setCurrentStep(idx)
+  }
+  const steps = [
+    {
+      title: 'Add Polkadot plugin',
+      element: <Element step="first" handleStep={handleStepChange} currentStepIdx={currentStepIdx} />,
+    },
+    {
+      title: 'Create or select a polkadot account',
+      element: <Element step="second" handleStep={handleStepChange} currentStepIdx={currentStepIdx} />,
+    },
+    {
+      title: 'Get FREE tokens and start a channel',
+      element: <Element step="third" handleStep={handleStepChange} currentStepIdx={currentStepIdx} />,
+    },
+  ]
+
+  return <Multistepper showDialog={true} steps={steps} currentStepIdx={currentStepIdx} {...args} />
+}
+
+export const Regular = RegularTemplate.bind({})
