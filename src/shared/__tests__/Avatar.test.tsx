eca54ace56011b9c2c8e14f16a76c32e777ee1e5
Klaudiusz Dembler <dev@kdembler.com>
2021-02-18 16:32:06 +0100

    remove tests

--- src/shared/__tests__/Avatar.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import Avatar from '@/shared/components/Avatar'
-describe('Avatar component', () => {
-  it('Should render avatar correctly', () => {
-    expect(mount(<Avatar imageUrl="https://source.unsplash.com/WLUHO9A_xik/1600x900" />)).toBeDefined()
-  })
-})
