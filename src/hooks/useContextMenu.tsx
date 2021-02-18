d9fb71ac7d32f5b3b8eee7905ec2c6000e62dbd0
Mikolaj <mikkiodev@gmail.com>
2021-02-18 15:44:08 +0100

    add capture to EventListener

--- src/hooks/useContextMenu.tsx
+++ src/hooks/useContextMenu.tsx
@@ -4,14 +4,12 @@ export const useContextMenu = () => {
   const [position, setPosition] = useState({ x: 0, y: 0, left: true })
   const [isActive, setMenuActive] = useState(false)
 
-  // Adds eventListener only when the menu is active, removes listener after click
-  // To make it work properly menu should not be binded to more than one component
   useEffect(() => {
     if (!isActive) {
       return
     }
     const closeContextMenu = () => setMenuActive(false)
-    document.addEventListener('click', closeContextMenu, { once: true })
+    document.addEventListener('click', closeContextMenu, { once: true, capture: true })
   }, [isActive])
 
   const openContextMenu = useCallback((event: React.MouseEvent, menuWidth: number) => {
