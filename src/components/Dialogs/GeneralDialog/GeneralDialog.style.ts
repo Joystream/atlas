7463d6ec8e22feb50c4f87f9f8e56bee42813eb3
mikkio <mikkiodev@gmail.com>
2021-02-18 14:34:34 +0100

    add Multistepper component (#317)
    * add multistepper
    
    * edit styles
    
    * cr fixes
    
    * cr fixes
    
    * add StyledDialog
    
    * fix css variables, edit styling for header, move transitionStyles to Global

--- src/components/Dialogs/GeneralDialog/GeneralDialog.style.ts
+++ src/components/Dialogs/GeneralDialog/GeneralDialog.style.ts
@@ -1,20 +1,20 @@
 import styled from '@emotion/styled'
-import { css } from '@emotion/react'
 import { Button } from '@/shared/components'
 import { colors, sizes, breakpoints } from '@/shared/theme'
 
 export const StyledContainer = styled.div`
+  --dialog-padding: ${sizes(4)};
+  @media screen and (min-width: ${breakpoints.small}) {
+    --dialog-padding: ${sizes(6)};
+  }
   position: relative;
   width: 90%;
-  max-width: ${sizes(110)};
+  max-width: 440px;
   min-height: 150px;
   margin: ${sizes(32)} auto;
   background-color: ${colors.gray[600]};
-  padding: ${sizes(4)};
+  padding: var(--dialog-padding);
   box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.12), 0px 24px 40px rgba(0, 0, 0, 0.16);
-  @media screen and (min-width: ${breakpoints.small}) {
-    padding: ${sizes(6)};
-  }
 `
 
 export const StyledExitButton = styled(Button)`
@@ -25,29 +25,5 @@ export const StyledExitButton = styled(Button)`
   border: none;
   &:hover {
     background-color: ${colors.transparent};
-  }
-`
-
-export const dialogTransitions = css`
-  &.dialog-enter {
-    opacity: 0;
-    transform: scale(0.88);
-  }
-
-  &.dialog-enter-active {
-    opacity: 1;
-    transform: scale(1);
-    transition: 150ms cubic-bezier(0.25, 0.01, 0.25, 1);
-  }
-
-  &.dialog-exit {
-    opacity: 1;
-    transform: scale(1);
-  }
-
-  &.dialog-exit-active {
-    opacity: 0;
-    transform: scale(0.88);
-    transition: 100ms cubic-bezier(0.25, 0.01, 0.25, 1);
   }
 `
--- src/shared/components/GlobalStyle/transitionStyles.tsx
+++ src/shared/components/GlobalStyle/transitionStyles.tsx
@@ -21,4 +21,26 @@ export const transitionStyles = css`
   .${transitions.names.fade}-enter-active, .${transitions.names.fade}-exit-active {
     transition: opacity ${transitions.timings.loading} ${transitions.easing};
   }
+
+  .${transitions.names.dialog}-enter {
+    opacity: 0;
+    transform: scale(0.88);
+  }
+
+  .${transitions.names.dialog}-enter-active {
+    opacity: 1;
+    transform: scale(1);
+    transition: 150ms cubic-bezier(0.25, 0.01, 0.25, 1);
+  }
+
+  .${transitions.names.dialog}-exit {
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  .${transitions.names.dialog}-exit-active {
+    opacity: 0;
+    transform: scale(0.88);
+    transition: 100ms cubic-bezier(0.25, 0.01, 0.25, 1);
+  }
 `
