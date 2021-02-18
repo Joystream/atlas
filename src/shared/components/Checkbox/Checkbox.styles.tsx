426b9d146e6a4f0e86933d36e5d2613631130c70
Diego Cardenas <cardenasrdiego@gmail.com>
2021-02-18 13:36:55 +0100

    add Checkbox component (#320)
    * Checkbox component
    
    * Update src/shared/components/Checkbox/Checkbox.tsx
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>
    
    * Update src/shared/components/Checkbox/Checkbox.tsx
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>
    
    * Update src/shared/components/Checkbox/Checkbox.styles.tsx
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>
    
    * Feedback
    
    * Feedback
    
    * Using the new design check
    
    * Error hover
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>

---
+++ src/shared/components/Checkbox/Checkbox.styles.tsx
@@ -0,0 +1,94 @@
+import { darken } from 'polished'
+import { sizes, colors, transitions } from '@/shared/theme'
+import { css } from '@emotion/react'
+import styled from '@emotion/styled/'
+
+export const Container = styled.div<CheckboxStateProps>`
+  position: relative;
+  width: max-content;
+  padding: ${sizes(2)};
+  margin: -${sizes(2)};
+  cursor: pointer;
+  border-radius: 100%;
+  color: ${colors.gray[300]};
+  transition: background ${transitions.timings.loading} ${transitions.easing};
+  ${({ isFocused, selected, disabled }) =>
+    isFocused && !disabled && `background: ${selected ? colors.blue[900] : colors.gray[800]};`}
+  &:hover {
+    ${({ selected, disabled, error }) => [
+      !disabled && `background: ${selected ? colors.blue[900] : colors.gray[800]};`,
+      error && `background: ${darken(0.5, colors.error)};`,
+    ]}
+  }
+`
+
+const selectedStyles = (props: CheckboxStateProps) =>
+  props.selected
+    ? css`
+        background-color: ${props.selected ? colors.blue[500] : 'transparent'};
+        border: 1px solid ${colors.blue[500]};
+      `
+    : null
+const disabledStyles = (props: CheckboxStateProps) =>
+  props.disabled
+    ? [
+        css`
+          cursor: not-allowed;
+        `,
+        props.selected &&
+          css`
+            background-color: ${props.disabled ? colors.gray[700] : 'transparent'};
+            border: 1px solid ${colors.gray[700]};
+            color: ${colors.gray[400]};
+          `,
+      ]
+    : null
+const errorStyles = (props: CheckboxStateProps) =>
+  props.error
+    ? [
+        css`
+          border: 1px solid ${colors.error};
+        `,
+        props.selected &&
+          css`
+            background-color: ${colors.error};
+          `,
+      ]
+    : null
+export type CheckboxStateProps = {
+  selected: boolean
+  disabled: boolean
+  error: boolean
+  isFocused: boolean
+}
+export const InnerContainer = styled.div<CheckboxStateProps>`
+  transition: all 0.125s ease;
+  color: ${colors.white};
+  border: 1px solid ${colors.gray[300]};
+  ${selectedStyles}
+  ${errorStyles}
+  ${disabledStyles}
+  &:active {
+    border: 1px solid ${colors.gray[100]};
+  }
+`
+
+export const Input = styled.input`
+  top: 0;
+  left: 0;
+  margin: 0;
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  cursor: inherit;
+`
+
+export const Checkmark = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 18px;
+  height: 18px;
+  border-radius: 1px;
+`
---
+++ src/shared/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import Icon from '../Icon'
+import { Checkmark, Container, InnerContainer, Input } from './Checkbox.styles'
+
+type HTMLCheckboxProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+export interface CheckboxProps extends Omit<HTMLCheckboxProps, 'value' | 'onChange' | 'checked' | 'multiple' | 'ref'> {
+  value: boolean
+  disabled?: boolean
+  indeterminate?: boolean
+  error?: boolean
+  className?: string
+  onChange?: (value: boolean) => void
+  onFocus?: (event: React.FocusEvent<HTMLDivElement>) => void
+  onBlur?: (event: React.FocusEvent<HTMLDivElement>) => void
+}
+
+export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ value, indeterminate, onChange, disabled = false, onFocus, onBlur, error = false, className, ...props }, ref) => {
+    const isIndeterminate = indeterminate || false
+    const isSelected = !!value
+    const [isFocused, setIsFocused] = useState(false)
+
+    const onChangeHandler = (e: React.FocusEvent<HTMLInputElement>) => {
+      if (!disabled && onChange) {
+        onChange(!value)
+      }
+    }
+    const onFocusHandler = (e: React.FocusEvent<HTMLInputElement>) => {
+      if (!disabled) {
+        setIsFocused(true)
+        if (onFocus) {
+          onFocus(e)
+        }
+      }
+    }
+    const onBlurHandler = (e: React.FocusEvent<HTMLInputElement>) => {
+      if (!disabled) {
+        setIsFocused(false)
+        if (onBlur) {
+          onBlur(e)
+        }
+      }
+    }
+    return (
+      <Container selected={value} disabled={disabled} isFocused={isFocused} error={error}>
+        <InnerContainer selected={value} disabled={disabled} error={error} isFocused={isFocused}>
+          <Input
+            ref={ref}
+            type="checkbox"
+            data-multiple="false"
+            checked={isSelected}
+            disabled={disabled}
+            onChange={onChangeHandler}
+            onFocus={onFocusHandler}
+            onBlur={onBlurHandler}
+            {...props}
+          />
+          <Checkmark>{isSelected ? <Icon name={isIndeterminate ? 'dash' : 'check'} /> : null}</Checkmark>
+        </InnerContainer>
+      </Container>
+    )
+  }
+)
+
+Checkbox.displayName = 'Checkbox'
+
+export default Checkbox
