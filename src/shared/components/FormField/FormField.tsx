dc229a2a06c090fcf8f255e16a1c02e7c6903fe3
Bartosz Dryl <drylbartosz@gmail.com>
2021-02-18 14:39:15 +0100

    add FormField component (#322)
    * create FormField component with corresponding stories
    
    * cr fixes
    
    * add index file
    
    * set consistent width and max-width to all input elements
    
    * cr fix
    
    * fix typo
    
    * remove global max-width from input fields

---
+++ src/shared/components/FormField/FormField.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { ChildrenWrapper, FormFieldDescription, FormFieldTitle, FormFieldWrapper } from './FormField.style'
+
+export type FormFieldProps = {
+  title: string
+  description?: string | string[]
+  className?: string
+}
+
+const FormField: React.FC<FormFieldProps> = ({ children, title, description, className }) => {
+  return (
+    <FormFieldWrapper className={className}>
+      <FormFieldTitle variant="h6">{title}</FormFieldTitle>
+      {description &&
+        (description instanceof Array ? (
+          description.map((p, idx) => (
+            <FormFieldDescription key={idx} variant="body2">
+              {p}
+            </FormFieldDescription>
+          ))
+        ) : (
+          <FormFieldDescription variant="body2">{description}</FormFieldDescription>
+        ))}
+      <ChildrenWrapper>{children}</ChildrenWrapper>
+    </FormFieldWrapper>
+  )
+}
+
+export default FormField
