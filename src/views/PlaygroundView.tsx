b8b2a637241a3baef1b945177747eab25646d43d
Mikolaj <mikkiodev@gmail.com>
2021-02-23 12:35:41 +0100

    move form playground to Playgrounds

--- src/views/PlaygroundView.tsx
+++ src/views/PlaygroundView.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import styled from '@emotion/styled'
-import PlaygroundForm from './PlaygroundForm'
+import { PlaygroundForm } from './Playgrounds'
 import { Text } from '@/shared/components'
 import Link from '@/components/Link'
 
---
+++ src/views/Playgrounds/index.ts
@@ -0,0 +1,1 @@
+export { default as PlaygroundForm } from './PlaygroundForm'
