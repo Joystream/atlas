a7487393fa6fafcf3a9dcf5041d9c0c462364e59
mikkio <mikkiodev@gmail.com>
2021-02-24 10:22:46 +0100

    add Datepicker component (#325)
    * add datepicker
    
    * add handler to component file
    
    * add index
    
    * remove useEffect from stories
    
    * add react-number-format
    
    * move handlers outside
    
    * move parsing date inside of datepicker component
    
    * add null to date handler

---
+++ src/shared/components/Datepicker/Datepicker.tsx
@@ -0,0 +1,48 @@
+import React, { forwardRef } from 'react'
+import { parse } from 'date-fns'
+import NumberFormat from 'react-number-format'
+import TextField from '../TextField'
+
+export type DatepickerProps = {
+  required?: boolean
+  error?: boolean
+  disabled?: boolean
+  helperText?: string
+  onChange: (date: Date | null) => void
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void
+}
+
+const DatepickerComponent: React.ForwardRefRenderFunction<HTMLInputElement, DatepickerProps> = (
+  { required, error, disabled, helperText, onChange, onBlur },
+  ref
+) => {
+  const handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void = (e) => {
+    if (!e.target.value) {
+      onChange(null)
+    } else {
+      const date = parse(e.target.value, 'dd/MM/yyyy', new Date())
+      onChange(date)
+    }
+  }
+  return (
+    <NumberFormat
+      getInputRef={ref}
+      customInput={TextField}
+      format="##/##/####"
+      label="DD / MM / YYYY"
+      mask={['D', 'D', 'M', 'M', 'Y', 'Y', 'Y', 'Y']}
+      helperText={helperText}
+      error={error}
+      required={required}
+      disabled={disabled}
+      onChange={handleChange}
+      onBlur={onBlur}
+    />
+  )
+}
+
+const Datepicker = forwardRef(DatepickerComponent)
+
+Datepicker.displayName = 'Datepicker'
+
+export default Datepicker
