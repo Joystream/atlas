c5c10a646c6dc9f253d603c3c8e6323d314d8016
Mikolaj <mikkiodev@gmail.com>
2021-02-24 10:42:25 +0100

    add negative margin to icon

--- src/shared/components/Button/Button.style.ts
+++ src/shared/components/Button/Button.style.ts
@@ -76,18 +76,18 @@ const sizeFromProps = ({ size = 'medium', full, hasText }: ButtonStyleProps) => 
   let padding, fontSize
   switch (size) {
     case 'small': {
-      padding = '9px 13px'
+      padding = '8px 12px'
       fontSize = typography.sizes.button.small
       break
     }
     case 'medium': {
-      padding = hasText ? `13px 17px` : '13px'
+      padding = hasText ? `12px 16px` : '12px'
       fontSize = typography.sizes.button.medium
       break
     }
     case 'large':
     default: {
-      padding = hasText ? `12px 20px` : '12px'
+      padding = hasText ? `14px 20px` : '14px'
       fontSize = typography.sizes.button.large
       break
     }
@@ -111,10 +111,9 @@ const disabled = ({ disabled }: ButtonStyleProps) =>
 export const StyledIcon = styled(Icon)<IconStyleProps>`
   display: block;
   flex-shrink: 0;
-  position: relative;
-  top: 0.09em;
   width: ${({ size }) => (size === 'large' ? '24px' : '14px')};
   height: ${({ size }) => (size === 'large' ? '24px' : '14px')};
+  margin: ${({ size }) => (size === 'large' ? '-14px 0' : size === 'medium' ? '-12px 0' : '-8px 0')};
   & + * {
     margin-left: 10px;
   }
