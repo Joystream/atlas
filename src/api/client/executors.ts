8935b90a6b55ba2d56c2ce2bdd4da47d943e1ae9
Klaudiusz Dembler <dev@kdembler.com>
2021-03-01 13:40:47 +0100

    import mocking code only in staging

--- src/api/client/executors.ts
+++ src/api/client/executors.ts
@@ -2,8 +2,14 @@ import { ORION_GRAPHQL_URL, QUERY_NODE_GRAPHQL_URL } from '@/config/urls'
 import { HttpLink } from '@apollo/client'
 import { linkToExecutor } from '@graphql-tools/links'
 
-const queryNodeLink = new HttpLink({ uri: QUERY_NODE_GRAPHQL_URL })
-const orionLink = new HttpLink({ uri: ORION_GRAPHQL_URL })
+const createExecutors = () => {
+  const queryNodeLink = new HttpLink({ uri: QUERY_NODE_GRAPHQL_URL })
+  const orionLink = new HttpLink({ uri: ORION_GRAPHQL_URL })
 
-export const queryNodeExecutor = linkToExecutor(queryNodeLink)
-export const orionExecutor = linkToExecutor(orionLink)
+  const queryNodeExecutor = linkToExecutor(queryNodeLink)
+  const orionExecutor = linkToExecutor(orionLink)
+
+  return { queryNodeExecutor, orionExecutor }
+}
+
+export { createExecutors }
--- src/config/urls.ts
+++ src/config/urls.ts
@@ -2,18 +2,9 @@ const readEnv = (name: string): string | undefined => {
   return process.env[name]
 }
 
-const envQueryNodeUrl = readEnv('REACT_APP_QUERY_NODE_URL')
-const envOrionUrl = readEnv('REACT_APP_ORION_URL')
-const envStorageNodeUrl = readEnv('REACT_APP_STORAGE_NODE_URL')
-const envSentryDSN = readEnv('REACT_APP_SENTRY_DSN')
-
-export const MOCK_QUERY_NODE_GRAPHQL_URL = '/query-node-graphql'
-export const QUERY_NODE_GRAPHQL_URL = envQueryNodeUrl || MOCK_QUERY_NODE_GRAPHQL_URL
-
-export const MOCK_ORION_GRAPHQL_URL = '/orion-graphql'
-export const ORION_GRAPHQL_URL = envOrionUrl || MOCK_ORION_GRAPHQL_URL
-
-export const MOCK_STORAGE_NODE_URL = 'https://rome-rpc-endpoint.joystream.org/asset/v0/'
-export const STORAGE_NODE_URL = envStorageNodeUrl || MOCK_STORAGE_NODE_URL
-
-export const SENTRY_DSN = envSentryDSN
+export const QUERY_NODE_GRAPHQL_URL = readEnv('REACT_APP_QUERY_NODE_URL')
+export const MOCK_QUERY_NODE_GRAPHQL_URL = '/mocked-query-node'
+export const ORION_GRAPHQL_URL = readEnv('REACT_APP_ORION_URL')
+export const MOCK_ORION_GRAPHQL_URL = '/mocked-orion'
+export const STORAGE_NODE_URL = readEnv('REACT_APP_STORAGE_NODE_URL')
+export const SENTRY_DSN = readEnv('REACT_APP_SENTRY_DSN')
