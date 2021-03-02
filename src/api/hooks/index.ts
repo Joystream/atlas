545a70f5468c400e19a85f3f160d408cca4d64bb
Klaudiusz Dembler <dev@kdembler.com>
2021-03-02 15:01:39 +0100

    clean up queries

--- src/api/hooks/index.ts
+++ src/api/hooks/index.ts
@@ -1,22 +1,10 @@
 import useCoverVideo from './coverVideo'
 import useFeaturedVideos from './featuredVideos'
 import useCategories from './categories'
-import { useChannel, useChannels, useFollowChannel, useUnfollowChannel } from './channel'
 import useSearch from './search'
-import useVideo, { useVideos, useAddVideoView } from './video'
 import useVideosConnection from './videosConnection'
 
-export {
-  useCoverVideo,
-  useVideosConnection,
-  useFeaturedVideos,
-  useCategories,
-  useChannel,
-  useChannels,
-  useFollowChannel,
-  useUnfollowChannel,
-  useSearch,
-  useVideo,
-  useVideos,
-  useAddVideoView,
-}
+export { useCoverVideo, useVideosConnection, useFeaturedVideos, useCategories, useSearch }
+
+export * from './channel'
+export * from './video'
