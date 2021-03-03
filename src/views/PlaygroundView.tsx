f89089655624086f6a7c81e3354de06540c38ee7
Diego Cardenas <cardenasrdiego@gmail.com>
2021-03-03 18:39:00 +0100

    GetVideoMetadata utility fn (#349)
    * Add useVideoMetadata hook
    
    * Update src/views/PlaygroundView.tsx
    
    Co-authored-by: Klaudiusz Dembler <accounts@kdembler.com>
    
    * VideoMetadata asyn fn
    
    * feedback
    
    * CR fixes
    
    * extra
    
    Co-authored-by: Klaudiusz Dembler <accounts@kdembler.com>

--- src/views/PlaygroundView.tsx
+++ src/views/PlaygroundView.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import styled from '@emotion/styled'
-import { PlaygroundValidationForm, PlaygroundDrafts } from './Playgrounds'
+import { PlaygroundValidationForm, PlaygroundDrafts, VideoMetaData } from './Playgrounds'
 import { Text } from '@/shared/components'
 import Link from '@/components/Link'
 import { DraftsProvider } from '@/hooks'
@@ -14,12 +14,12 @@ export const PlaygroundView = () => {
         <LinksContainer>
           <Link to="./validation-form">Validation Form</Link>
           <Link to="./drafts">Drafts</Link>
-          <Link to="./third">Third</Link>
+          <Link to="./video-metadata">Video Metadata</Link>
         </LinksContainer>
         <Routes>
           <Route key="validation-form" path="/validation-form" element={<PlaygroundValidationForm />} />
           <Route key="drafts" path="/drafts" element={<PlaygroundDrafts />} />
-          <Route key="third" path="/third" element={<p>Third</p>} />
+          <Route key="metadata" path="/video-metadata" element={<VideoMetaData />} />
         </Routes>
       </Container>
     </DraftsProvider>
