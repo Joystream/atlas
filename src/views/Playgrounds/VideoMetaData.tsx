518e0b6af494af7d6d93dbd037de9fd865696c5d
Diego Cardenas <cardenasrdiego@gmail.com>
2021-03-03 18:39:00 +0100

    GetVideoMetadata utility fn (#349)
    * Add useVideoMetadata hook
    
    * Update src/views/PlaygroundView.tsx
    
    Co-authored-by: Klaudiusz Dembler <accounts@kdembler.com>
    
    * VideoMetadata asyn fn
    
    * feedback
    
    * CR fixes
    
    * extra
    
    Co-authored-by: Klaudiusz Dembler <accounts@kdembler.com>

---
+++ src/views/Playgrounds/VideoMetaData.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Text } from '@/shared/components'
+import { getVideoMetadata, VideoMetadata as Metadata } from '@/utils/video'
+
+export const VideoMetaData = () => {
+  const [metadata, setmetadata] = useState<Metadata>()
+  const [error, seterror] = useState<Error>()
+  return (
+    <div>
+      <input
+        type="file"
+        accept="video/*,.mkv"
+        onChange={async (e) => {
+          const file = e.target.files?.[0]
+          if (file) {
+            try {
+              seterror(undefined)
+              const metadata = await getVideoMetadata(file)
+              setmetadata(metadata)
+            } catch (err) {
+              seterror(err)
+            }
+          }
+        }}
+      ></input>
+      <Text>{error?.message}</Text>
+      <h2>Metadata:</h2>
+      <p>type: {metadata?.mimeType}</p>
+      <p>sizeInBytes: {metadata?.sizeInBytes}</p>
+      <p>duration: {metadata?.duration}</p>
+      <p>width: {metadata?.width}</p>
+      <p>height: {metadata?.height}</p>
+    </div>
+  )
+}
+
+export default VideoMetaData
--- src/views/Playgrounds/index.ts
+++ src/views/Playgrounds/index.ts
@@ -1,2 +1,3 @@
 export { default as PlaygroundValidationForm } from './PlaygroundValidationForm'
 export { default as PlaygroundDrafts } from './PlaygroundDrafts'
+export { default as VideoMetaData } from './VideoMetaData'
