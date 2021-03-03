75a8609a691fa83efb3d0e6b3c54a087c5a22f6a
Diego Cardenas <cardenasrdiego@gmail.com>
2021-03-03 18:39:00 +0100

    GetVideoMetadata utility fn (#349)
    * Add useVideoMetadata hook
    
    * Update src/views/PlaygroundView.tsx
    
    Co-authored-by: Klaudiusz Dembler <accounts@kdembler.com>
    
    * VideoMetadata asyn fn
    
    * feedback
    
    * CR fixes
    
    * extra
    
    Co-authored-by: Klaudiusz Dembler <accounts@kdembler.com>

--- src/utils/video.ts
+++ src/utils/video.ts
@@ -10,3 +10,39 @@ export const formatVideoViewsAndDate = (
   const formattedViews = views !== null && (fullViews ? formatNumber(views) : formatNumberShort(views))
   return formattedViews ? `${formattedDate} • ${formattedViews} views` : formattedDate
 }
+
+export type VideoMetadata = {
+  mimeType: string
+  sizeInBytes: number
+  height: number
+  width: number
+  duration: number
+}
+export const getVideoMetadata = async (file: File): Promise<VideoMetadata> => {
+  const videoEl = document.createElement('video')
+  const mimeType = file?.type ?? ''
+  const sizeInBytes = file?.size ?? 0
+  const canPlay = videoEl.canPlayType(mimeType)
+
+  return new Promise((resolve, reject) => {
+    const handleLoadedData = (e: Event) => {
+      // Video should now be loaded but we can add a second check
+      if (videoEl.readyState >= 3) {
+        const height = videoEl.videoHeight
+        const width = videoEl.videoWidth
+        const duration = videoEl.duration
+        resolve({ mimeType, sizeInBytes, height, width, duration })
+      } else {
+        reject(new Error('There was an error loading the video please try again'))
+      }
+    }
+
+    if (canPlay) {
+      const fileURL = URL.createObjectURL(file)
+      videoEl.src = fileURL
+      videoEl.addEventListener('loadeddata', handleLoadedData, { once: true })
+    } else {
+      reject(new Error("Can't play video file"))
+    }
+  })
+}
