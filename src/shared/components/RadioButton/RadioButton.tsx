79b363354fc9fa8323ec86ce02bfde66493c8bf3
mikkio <mikkiodev@gmail.com>
2021-03-05 14:38:45 +0100

    update RadioButton component (#350)
    * edit RadioButton
    
    * add radioGroup to hook form playground
    
    * add validation in form for radio button
    
    * back to initial design
    
    * add LabelText
    
    * cr fixes
    
    * cr fixes
    
    * fix pointer for label and button
    
    * change name for selectedValue

--- src/shared/components/RadioButton/RadioButton.tsx
+++ src/shared/components/RadioButton/RadioButton.tsx
@@ -1,35 +1,29 @@
 import React from 'react'
-import { Label, Input, StyledInput, RadioButtonStyleProps } from './RadioButton.style'
+import { Label, Input, StyledInput, StyledLabelText, RadioButtonStyleProps } from './RadioButton.style'
 
 type RadioButtonProps = Partial<{
+  selectedValue: string | number
   label: string
 }> &
   Omit<RadioButtonStyleProps, 'clickable'> &
-  React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+  React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> &
+  Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onClick'>
 
-const RadioButton: React.FC<RadioButtonProps> = ({
-  label,
-  position = 'end',
-  disabled,
-  error,
-  onClick,
-  checked,
-  ...props
-}) => {
-  const clickable = !!onClick
-  const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
-    if (onClick) {
-      onClick(e)
-    }
+const RadioButton = React.forwardRef<HTMLInputElement, RadioButtonProps>(
+  ({ value, selectedValue, label, disabled, error, ...props }, ref) => {
+    const isSelected = value === selectedValue
+
+    return (
+      <Label disabled={disabled}>
+        <StyledInput checked={isSelected} error={error} disabled={disabled}>
+          <Input ref={ref} value={value} type="radio" disabled={disabled} {...props} checked={isSelected} />
+        </StyledInput>
+        {label && <StyledLabelText>{label}</StyledLabelText>}
+      </Label>
+    )
   }
-  return (
-    <Label position={position} clickable={clickable} disabled={disabled}>
-      <StyledInput checked={checked} error={error} disabled={disabled}>
-        <Input type="radio" onClick={handleClick} disabled={disabled} {...props} checked={checked} />
-      </StyledInput>
-      {label && <span>{label}</span>}
-    </Label>
-  )
-}
+)
+
+RadioButton.displayName = 'RadioButton'
 
 export default RadioButton
