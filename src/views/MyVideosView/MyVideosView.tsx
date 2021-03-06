85a9864755ca4f924fbaab8dd820a8473b7d47e6
Diego Cardenas <cardenasrdiego@gmail.com>
2021-03-06 23:29:13 +0100

    Draft message

--- src/views/MyVideosView/MyVideosView.tsx
+++ src/views/MyVideosView/MyVideosView.tsx
@@ -1,6 +1,6 @@
 import { useVideos, useVideosConnection } from '@/api/hooks'
-import { PlaceholderVideoGrid, VideoGrid, VideoPreview } from '@/components'
-import { Grid, Icon, Pagination, Tabs } from '@/shared/components'
+import { VideoPreview } from '@/components'
+import { Grid, Pagination, Tabs, DismissibleMessage } from '@/shared/components'
 import { colors, sizes } from '@/shared/theme'
 import styled from '@emotion/styled'
 import React, { useState } from 'react'
@@ -8,13 +8,22 @@ import { StyledText, ViewContainer } from './MyVideos.styles'
 
 const tabs = ['All Videos', 'Published', 'Drafts', 'Unlisted']
 
-const testChannelId = '0d75be46-9e81-4d79-a38f-753fbec0adf6'
+const testChannelId = '100'
 const videosPerPage = 8
+// TODO: No videos screen
+// TODO: Unlisted videos
 // TODO: Channel dependant drafts
+// TODO: Draft text box
+// TODO: video selection / deletion
+// TODO: dynamic channels (not hardcoded)
+// TODO: context menu callbacks
+// TODO: Responsive
+// TODO: OnCoverResize support
 export const MyVideosView = () => {
+  // const [currentPageForUseVideos, setCurrentPageForUseVideos] = useState(0)
   const [currentPage, setCurrentPage] = useState(1)
   const [currentTab, setCurrentTab] = useState(0)
-  const tab = tabs[currentTab]
+  const currentTabName = tabs[currentTab]
 
   // we need the total video count from somewhere
   const { videosConnection, loading: loadingVideosConnection, error: errorVideosConnection } = useVideosConnection({
@@ -27,51 +36,71 @@ export const MyVideosView = () => {
       channelId: testChannelId,
     },
     {
-      notifyOnNetworkStatusChange: true,
+      // notifyOnNetworkStatusChange: true,
       nextFetchPolicy: 'cache-first',
     }
   )
 
-  const placeholderItems = Array.from({ length: loading ? videosPerPage : 0 }, () => ({
+  const placeholderItems = Array.from({ length: loading || videos?.length === 0 ? videosPerPage : 0 }, () => ({
     id: undefined,
     progress: undefined,
   }))
-  // console.log({ videos, loading, error, called })
+
+  const videosWPlaceholders = [...(videos || []), ...placeholderItems]
+  console.log({ videosWPlaceholders, placeholderItems, videos, loading, error, currentTabName, currentPage })
   return (
     <ViewContainer>
       <StyledText variant="h2">My Videos</StyledText>
       <TabsContainer>
         <Tabs initialIndex={0} tabs={tabs} onSelectTab={setCurrentTab} />
       </TabsContainer>
-
+      {currentTabName === 'Drafts' && (
+        // Should this really be dismissable?
+        <StyledDismissibleMessage
+          id="video-draft-saved-locally-warning"
+          title={'Video Drafts are saved locally'}
+          description={
+            'This mean you can only access one on the device you used to create it. Clearing your browser history will delete all your drafts.'
+          }
+        />
+      )}
       <Grid>
-        {[...(videos || []), ...placeholderItems]
+        {videosWPlaceholders
           // this makes for a smoother transition between pages
           .slice(0, videosPerPage)
-          .map((v, idx) => (
+          .map((video, idx) => (
             <VideoPreview
-              key={idx + '-' + currentTab + '-' + currentPage}
-              id={v.id}
+              key={idx + '-' + currentTabName + '-' + currentPage}
+              id={video.id}
+              showChannel={false}
               isLoading={loading}
               publisherMode
               isSelected={false}
               isAnyVideoSelected={false}
               onSelectClick={() => {}}
-              contextMenuCallbacks={{}}
             />
           ))}
       </Grid>
       <PaginationContainer>
         <Pagination
-          onChangePage={async (page) => {
-            const res = await fetchMore({
+          onChangePage={(page) => {
+            setCurrentPage(page)
+            // setCurrentPageForUseVideos(page - 1)
+            // fetchMore({
+            //   variables: {
+            //     offset: videosPerPage * (page - 1),
+            //     limit: videosPerPage,
+            //   },
+            // })
+          }}
+          onMouseEnterPage={(page) => {
+            fetchMore({
               variables: {
+                // substract 1 coz offset index starts at 0
                 offset: videosPerPage * (page - 1),
                 limit: videosPerPage,
               },
             })
-            setCurrentPage(page)
-            console.log(res)
           }}
           page={currentPage}
           itemsPerPage={videosPerPage}
@@ -91,9 +120,13 @@ const TabsContainer = styled.div`
 `
 
 const PaginationContainer = styled.div`
-  padding-top: ${sizes(20)};
+  padding-top: ${sizes(6)};
   padding-bottom: ${sizes(16)};
   display: flex;
   align-items: center;
   justify-content: center;
+`
+
+const StyledDismissibleMessage = styled(DismissibleMessage)`
+  margin-bottom: ${sizes(8)};
 `
