39f1d6813f46bfc410ac68bc1a8224d9e8abb4f2
Bartosz Dryl <drylbartosz@gmail.com>
2021-03-12 13:03:10 +0100

    add MultiFileSelect component (#355)
    * add drag n drop icons
    
    * install react-dropzone
    
    * create MultiFileSelect component, stories and styles
    
    * add play-smal and trash-fill icon
    
    * add simple stepper and progress bar
    
    * add possibility to open image crop dialog without file select
    
    * add error-second icon
    
    * extract FileDrop and FileStep from MultifileSelect
    
    * fix types in ImageCropDialog
    
    * fix props in CircularProgressBar
    
    * update icons
    
    * add animations, progress, refactor stylings
    
    * lift state up
    
    * add FileStep to storybook
    
    * add FileDrop to storybook
    
    * add index files, fix imports
    
    * add mobile styles
    
    * fix issue with thumbnail, improve responsivness
    
    * add padlock icon
    
    * add disabled state to FileStep
    
    * update icons
    
    * add better error handling
    
    * refactor upload button
    
    * update delete button
    
    * cr fixes
    
    * improve mobile view
    
    * cr fixes
    
    * fix drop area border, rename FileDrop to FileSelect
    
    * fix issue with InnerContainer height

---
+++ src/shared/components/FileStep/FileStep.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+import FileStep, { FileStepProps } from './FileStep'
+
+export default {
+  title: 'Shared/FileStep',
+  component: FileStep,
+  argTypes: {
+    step: {
+      defaultValue: 'video',
+    },
+    overhead: {
+      defaultValue: 'Video File',
+    },
+    subtitle: {
+      defaultValue: 'Select Video File',
+    },
+  },
+} as Meta
+
+const Template: Story<FileStepProps> = (args) => {
+  return <FileStep {...args} />
+}
+
+export const Default = Template.bind({})
