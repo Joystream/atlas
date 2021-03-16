1ce8f597e3a417bf5d83fd7087d7bf37d882dcba
drillprop <drylbartosz@gmail.com>
2021-03-16 13:31:35 +0100

    create SelectMemberDialog

---
+++ src/views/studio/SelectMemberDialog/SelectMemberDialog.style.ts
@@ -0,0 +1,21 @@
+import { BaseDialog } from '@/components/Dialogs'
+import { Text } from '@/shared/components'
+import { colors, sizes } from '@/shared/theme'
+import styled from '@emotion/styled'
+
+export const StyledDialog = styled(BaseDialog)`
+  background: ${colors.gray[700]};
+`
+export const DialogSubtitle = styled(Text)`
+  margin-top: ${sizes(2)};
+  color: ${colors.gray[200]};
+`
+
+export const Underline = styled.div`
+  margin-top: ${sizes(4)};
+  background: ${colors.gray[600]};
+  position: absolute;
+  left: 0;
+  height: 1px;
+  width: 100%;
+`
---
+++ src/views/studio/SelectMemberDialog/SelectMemberDialog.tsx
@@ -0,0 +1,39 @@
+import { AccountBar, Text } from '@/shared/components'
+import React from 'react'
+import { Membership } from '../StudioView'
+import { Underline, StyledDialog, DialogSubtitle } from './SelectMemberDialog.style'
+
+type SelectMemberDialogProps = {
+  memberships: Membership[]
+  onExit?: () => void
+  showDialog?: boolean
+  onSelectMember?: (member: Membership) => void
+  onCreateNewMember?: () => void
+}
+
+const SelectMemberDialog: React.FC<SelectMemberDialogProps> = ({
+  memberships,
+  onExit,
+  showDialog,
+  onSelectMember,
+  onCreateNewMember,
+}) => {
+  return (
+    <StyledDialog showDialog={showDialog} onExitClick={onExit}>
+      <Text variant="h4">Select Membership</Text>
+      <DialogSubtitle variant="body2">Select your membership account</DialogSubtitle>
+      <Underline />
+      <AccountBar blank name="New Member" secondary="Create New Member" onClick={onCreateNewMember} />
+      {memberships.map((member) => (
+        <AccountBar
+          avatarUrl={member.avatarUri}
+          name={member.handle}
+          onClick={() => onSelectMember?.(member)}
+          key={member.id}
+        />
+      ))}
+    </StyledDialog>
+  )
+}
+
+export default SelectMemberDialog
