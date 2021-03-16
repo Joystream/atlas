3cc4db9943580fd9d447933484e888f3b4b0a31b
Diego Cardenas <cardenasrdiego@gmail.com>
2021-03-16 16:07:41 +0100

    Layout for studio routes

--- src/views/studio/StudioRouter.tsx
+++ src/views/studio/StudioRouter.tsx
@@ -1,8 +1,22 @@
+import styled from '@emotion/styled'
 import React from 'react'
 import { Routes } from 'react-router'
 
 const StudioRouter = () => {
-  return <Routes>{/* routes for studio here */}</Routes>
+  return (
+    <>
+      <Container>
+        <h1>Studio view here</h1>
+        <Routes></Routes>
+      </Container>
+    </>
+  )
 }
 
 export default StudioRouter
+
+const Container = styled.div`
+  --max-inner-width: calc(1440px);
+  max-width: var(--max-inner-width);
+  margin: 0 auto;
+`
