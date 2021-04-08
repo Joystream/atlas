1ea0f4877ebef69305b33fcde9a984ad1612ba44
drillprop <drylbartosz@gmail.com>
2021-04-08 17:19:12 +0200

    cleanup SelectMembershipView

--- src/views/studio/SelectMembershipView/SelectMembershipView.tsx
+++ src/views/studio/SelectMembershipView/SelectMembershipView.tsx
@@ -1,26 +1,16 @@
-import { Multistepper } from '@/components/Dialogs'
 import { absoluteRoutes } from '@/config/routes'
 import { useActiveUser, useCheckBrowser } from '@/hooks'
-import { promisify } from '@/utils/data'
-import { readFromLocalStorage, writeToLocalStorage } from '@/utils/localStorage'
 import React, { useCallback, useEffect, useState } from 'react'
-import { checkPolkadot, getAccountMemberships } from '../SignInView/fakeUtils'
-import { Membership } from '../SignInView/SignInView'
-import { MemberChannelGrid, Wrapper, Header, Hero, SubTitle } from './SelectMembershipView.style'
-import { AccountStep, ExtensionStep, TermsStep } from './Steps'
+import { getAccountMemberships } from '../fakeUtils'
+import { Membership } from '../InitialStudioView/InitialStudioView'
+import { Header, Hero, MemberChannelGrid, SubTitle, Wrapper } from './SelectMembershipView.style'
 import StudioCard from './StudioCard'
-
-type DialogStep = {
-  step: number
-  isActive?: boolean
-}
 
 const SelectMembershipView = () => {
   const [memberships, setMemberships] = useState<Membership[]>()
 
   const browser = useCheckBrowser()
   const { activeUser } = useActiveUser()
-  const { dialogVisible, openDialog, closeDialog, changeDialogStep, step } = useSteps()
 
   // temporary
   const getMemberShips = useCallback(async () => {
@@ -31,33 +21,9 @@ const SelectMembershipView = () => {
     setMemberships(memberships)
   }, [activeUser?.accountId])
 
-  // temporary
-  const checkIfExtensionIsIntalled = useCallback(async () => {
-    const isInstalled = await checkPolkadot()
-    if (isInstalled && step < 1) {
-      changeDialogStep(1)
-    }
-  }, [changeDialogStep, step])
-
   useEffect(() => {
-    checkIfExtensionIsIntalled()
     getMemberShips()
-  }, [checkIfExtensionIsIntalled, getMemberShips])
-
-  const steps = [
-    {
-      title: 'Add Polkadot plugin',
-      element: <ExtensionStep browser={browser} />,
-    },
-    {
-      title: 'Create or select a polkadot account',
-      element: <AccountStep onStepChange={changeDialogStep} currentStepIdx={step} />,
-    },
-    {
-      title: 'Accept terms and conditions',
-      element: <TermsStep onStepChange={changeDialogStep} />,
-    },
-  ]
+  }, [getMemberShips])
 
   const hasMemberships = memberships?.length
 
@@ -80,55 +46,7 @@ const SelectMembershipView = () => {
             handle={membership.handle}
           />
         ))}
-        <StudioCard empty handle="Create a membership" onClick={openDialog} />
       </MemberChannelGrid>
-      <Multistepper currentStepIdx={step} steps={steps} showDialog={dialogVisible} onExitClick={closeDialog} />
     </Wrapper>
   )
 }
-
-const getDialogState = promisify(() => readFromLocalStorage<DialogStep>('dialogStep') || { step: 0, isActive: false })
-
-const updateDialogState = async (dialogStep: DialogStep) => {
-  writeToLocalStorage('dialogStep', dialogStep)
-}
-
-const useSteps = () => {
-  const [step, setCurrentStep] = useState(0)
-  const [dialogVisible, setDialogVisible] = useState(false)
-
-  const fetchDialogState = useCallback(async () => {
-    const { step, isActive } = await getDialogState()
-    setCurrentStep(step)
-    setDialogVisible(isActive || false)
-  }, [])
-
-  useEffect(() => {
-    fetchDialogState()
-  }, [fetchDialogState])
-
-  const openDialog = () => {
-    setDialogVisible(true)
-    updateDialogState({ step: step, isActive: true })
-  }
-
-  const closeDialog = () => {
-    setDialogVisible(false)
-    updateDialogState({ step: 0, isActive: false })
-  }
-
-  const changeDialogStep = useCallback((newStep: number) => {
-    setCurrentStep(newStep)
-    updateDialogState({ step: newStep })
-  }, [])
-
-  return {
-    openDialog,
-    closeDialog,
-    changeDialogStep,
-    dialogVisible,
-    step,
-  }
-}
-
-export default SelectMembershipView
--- src/views/studio/SelectMembershipView/Steps/index.ts
+++
@@ -1,5 +0,0 @@
-import AccountStep from './AccountStep'
-import ExtensionStep from './ExtensionStep'
-import TermsStep from './TermsStep'
-
-export { AccountStep, ExtensionStep, TermsStep }
