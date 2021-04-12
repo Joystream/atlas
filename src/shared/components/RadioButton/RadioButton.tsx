a88fd2bf6204fb9b7965cba040756f7ae5d9940d
mikkio <mikkiodev@gmail.com>
2021-04-12 12:32:27 +0200

    Radio button update (#431)
    * add radio button update
    
    * change Text import in styles
    
    * change caption prop name to helperText
    
    * cr fixes, add color transition
    
    * revert ::before, style fixes, add HelperText
    
    * change margin
    
    * fix spacing between radio button and container
    
    * change LabelText to Text
    
    * add margin to helperText, fix color
    
    * change display to grid, add className prop
    
    * cleaning

--- src/shared/components/RadioButton/RadioButton.tsx
+++ src/shared/components/RadioButton/RadioButton.tsx
@@ -1,25 +1,38 @@
 import React from 'react'
-import { Label, Input, StyledInput, StyledLabelText, RadioButtonStyleProps } from './RadioButton.style'
+import {
+  RadioButtonLabel,
+  RadioButtonContainer,
+  Input,
+  StyledInput,
+  StyledLabelText,
+  StyledHelperText,
+  RadioButtonStyleProps,
+} from './RadioButton.style'
 
 type RadioButtonProps = Partial<{
   selectedValue: string | number
   label: string
+  helperText?: string
+  className?: string
 }> &
   Omit<RadioButtonStyleProps, 'clickable'> &
   React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> &
   Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onClick'>
 
 const RadioButton = React.forwardRef<HTMLInputElement, RadioButtonProps>(
-  ({ value, selectedValue, label, disabled, error, ...props }, ref) => {
+  ({ value, selectedValue, label, helperText, className, disabled, error, ...props }, ref) => {
     const isSelected = value === selectedValue
 
     return (
-      <Label disabled={disabled}>
-        <StyledInput checked={isSelected} error={error} disabled={disabled}>
-          <Input ref={ref} value={value} type="radio" disabled={disabled} {...props} checked={isSelected} />
-        </StyledInput>
-        {label && <StyledLabelText>{label}</StyledLabelText>}
-      </Label>
+      <RadioButtonLabel disabled={disabled} className={className}>
+        <RadioButtonContainer>
+          <StyledInput checked={isSelected} error={error} disabled={disabled}>
+            <Input ref={ref} value={value} type="radio" disabled={disabled} {...props} checked={isSelected} />
+          </StyledInput>
+        </RadioButtonContainer>
+        {label && <StyledLabelText variant="body1">{label}</StyledLabelText>}
+        {helperText && <StyledHelperText helperText={helperText} error={error} />}
+      </RadioButtonLabel>
     )
   }
 )
