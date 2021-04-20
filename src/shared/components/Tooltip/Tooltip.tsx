4b20ae8ece6ae807c977b76d21c3d4ee8787b47f
mikkio <mikkiodev@gmail.com>
2021-04-20 15:38:13 +0200

    Tooltip improvement (#481)
    * add tooltip improvement
    
    * change color
    
    * add offsetX, offsetY, cr fixes
    
    * rebase, change tooltip position in video sheet action bar
    
    * remove className from Tooltip
    
    Co-authored-by: Klaudiusz Dembler <accounts@kdembler.com>
    
    * add className to span
    
    Co-authored-by: Klaudiusz Dembler <accounts@kdembler.com>
    
    * fix error
    
    Co-authored-by: Klaudiusz Dembler <accounts@kdembler.com>

--- src/shared/components/Tooltip/Tooltip.tsx
+++ src/shared/components/Tooltip/Tooltip.tsx
@@ -1,49 +1,61 @@
 import React, { useState } from 'react'
-import { StyledTooltip, ChildrenContainer } from './Tooltip.style'
+import Tippy from '@tippyjs/react/headless'
+import { CSSTransition } from 'react-transition-group'
+import { transitions } from '@/shared/theme'
+import { StyledTooltip, TooltipText, IconWrapper, Arrow } from './Tooltip.style'
+import { SvgGlyphInfo } from '@/shared/icons'
 
+type Placement = 'top-start' | 'top-end' | 'bottom-start' | 'bottom-end'
 export type TooltipProps = {
   text: string
-  above?: boolean
-  right?: boolean
+  icon?: boolean
+  placement?: Placement
+  offsetX?: number
   offsetY?: number
   arrowDisabled?: boolean
-  darkenContent?: boolean
   className?: string
 }
 
 const Tooltip: React.FC<TooltipProps> = ({
-  children,
   text,
-  above,
-  right,
-  offsetY,
+  icon,
+  children,
+  placement = 'bottom-start',
+  offsetX = 0,
+  offsetY = 8,
   arrowDisabled,
-  darkenContent = true,
   className,
 }) => {
-  const [isActive, setActive] = useState(false)
-
+  const [isVisible, setIsVisible] = useState(false)
   return (
-    <StyledTooltip
-      data-text={text}
-      isActive={isActive}
-      arrowDisabled={arrowDisabled}
-      above={above}
-      right={right}
-      offsetY={offsetY}
-      className={className}
+    <Tippy
+      onMount={() => setIsVisible(true)}
+      onHide={() => setIsVisible(false)}
+      placement={placement}
+      offset={[offsetX, offsetY]}
+      render={(attrs) => (
+        <CSSTransition
+          in={isVisible}
+          timeout={parseInt(transitions.timings.sharp)}
+          classNames={transitions.names.fade}
+          unmountOnExit
+        >
+          <StyledTooltip {...attrs}>
+            {icon && (
+              <IconWrapper>
+                <SvgGlyphInfo />
+              </IconWrapper>
+            )}
+            <TooltipText variant="caption">{text}</TooltipText>
+            {!arrowDisabled && <Arrow />}
+          </StyledTooltip>
+        </CSSTransition>
+      )}
     >
-      <ChildrenContainer
-        onMouseEnter={() => setActive(true)}
-        onMouseLeave={() => setActive(false)}
-        onFocus={() => setActive(false)}
-        onBlur={() => setActive(false)}
-        darkenContent={darkenContent}
-        tabIndex={0}
-      >
+      <span tabIndex={0} className={className}>
         {children}
-      </ChildrenContainer>
-    </StyledTooltip>
+      </span>
+    </Tippy>
   )
 }
 
