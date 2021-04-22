231f0a463f3411243fa3968327716f9ba6eff547
mikkio <mikkiodev@gmail.com>
2021-04-22 19:05:48 +0200

    Connection indicator update (#495)
    * add indicator update
    
    * cleaning, add rwd

--- src/components/NoConnectionIndicator/NoConnectionIndicator.style.ts
+++ src/components/NoConnectionIndicator/NoConnectionIndicator.style.ts
@@ -1,30 +1,35 @@
-import { Text } from '@/shared/components'
-import { colors, sizes, zIndex } from '@/shared/theme'
+import { colors, sizes, zIndex, media } from '@/shared/theme'
+import { TOP_NAVBAR_HEIGHT } from '@/components'
 import styled from '@emotion/styled'
 
 export const TextWrapper = styled.div`
-  margin-left: ${sizes(4)};
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 `
 
-export const StyledTitle = styled(Text)`
-  color: ${colors.gray[50]};
-`
-export const StyledSubTitle = styled(Text)`
-  color: ${colors.gray[300]};
+export const IconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${sizes(6)};
+  height: ${sizes(6)};
+  margin-right: ${sizes(2)};
 `
 
 export const IndicatorWrapper = styled.div`
-  align-items: center;
-  width: 100%;
-  max-width: 500px;
   display: flex;
   position: fixed;
-  top: 90px;
-  z-index: ${zIndex.nearOverlay};
-  background-color: ${colors.gray[900]};
-  border-left: 7px solid ${colors.error};
-  padding: 14px 24px;
-  color: white;
-  left: var(--sidenav-collapsed-width);
-  margin-left: 12px;
+  top: calc(${TOP_NAVBAR_HEIGHT}px + ${sizes(4)});
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+  max-width: 500px;
+  align-items: center;
+  z-index: ${zIndex.header};
+  background-color: ${colors.gray[600]};
+  padding: ${sizes(3)} ${sizes(5)};
+  ${media.medium} {
+    margin-left: var(--sidenav-collapsed-width);
+  }
 `
