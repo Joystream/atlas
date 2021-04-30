92d79b58ac51205605d9813a830545e01999fb81
Bartosz Dryl <drylbartosz@gmail.com>
2021-04-30 12:03:06 +0200

    Add checkout component to create channel view (#520)
    * update Checkout component look
    
    * set Checkout component in CreateEditChannelView
    
    * cr fixes
    
    * cleaning
    
    * cr fixes

--- src/shared/components/Checkout/Checkout.tsx
+++ src/shared/components/Checkout/Checkout.tsx
@@ -1,49 +1,63 @@
-import React from 'react'
-import CircularProgressbar from '../CircularProgressbar'
+import React, { useState } from 'react'
 import Text from '../Text'
 import {
   Container,
-  TopRowContainer,
+  BottomRowContainer,
   StepsProgressContainer,
-  ProgressbarContainer,
   StepsContainer,
   Step,
   StepInnerContainer,
   StepState,
   StepsCompletedText,
+  StyledCircularProgressbar,
+  CircularProgresaBarContainer,
+  StyledSvgGlyphChevronDown,
 } from './Checkout.styles'
-import { SvgGlyphCheck, SvgGlyphChevronDown } from '@/shared/icons'
+import { SvgGlyphCheck, SvgGlyphChevronRight } from '@/shared/icons'
+import IconButton from '../IconButton'
+import { CSSTransition } from 'react-transition-group'
+import { transitions } from '@/shared/theme'
 
 export type Step = { title: string; onClick: () => void; completed: boolean }
 export type CheckoutProps = {
   steps: Step[]
+  className?: string
 }
-export const Checkout: React.FC<CheckoutProps> = ({ steps }) => {
+export const Checkout: React.FC<CheckoutProps> = ({ steps, className }) => {
   const stepsCompletedNumber = steps.filter(({ completed }) => completed).length
+  const [isHidden, setIsHidden] = useState(false)
   return (
-    <Container>
-      <TopRowContainer>
-        <Text variant="h6">Progress</Text>
-        <StepsProgressContainer>
-          <ProgressbarContainer>
-            <CircularProgressbar value={stepsCompletedNumber} maxValue={steps.length} />
-          </ProgressbarContainer>
-          <StepsCompletedText variant="body2">
-            {stepsCompletedNumber}/{steps.length}
-          </StepsCompletedText>
-        </StepsProgressContainer>
-      </TopRowContainer>
-      <StepsContainer>
+    <Container className={className}>
+      <StepsContainer isHidden={isHidden}>
         {steps.map((step, idx) => (
-          <Step key={step.title + idx} onClick={step.onClick}>
+          <Step completed={step.completed} key={step.title + idx} onClick={() => !step.completed && step.onClick()}>
             <StepInnerContainer>
-              <StepState completed={step.completed}>{step.completed && <SvgGlyphCheck />}</StepState>{' '}
+              <StepState completed={step.completed}>
+                <CSSTransition in={step.completed} timeout={100} classNames={transitions.names.fade} unmountOnExit>
+                  <SvgGlyphCheck />
+                </CSSTransition>
+              </StepState>
               <Text variant="body2">{step.title}</Text>
             </StepInnerContainer>
-            <SvgGlyphChevronDown />
+            <CSSTransition in={!step.completed} timeout={100} classNames={transitions.names.fade} unmountOnExit>
+              <SvgGlyphChevronRight />
+            </CSSTransition>
           </Step>
         ))}
       </StepsContainer>
+      <BottomRowContainer>
+        <StepsProgressContainer>
+          <CircularProgresaBarContainer>
+            <StyledCircularProgressbar value={stepsCompletedNumber} maxValue={steps.length} />
+            <StepsCompletedText variant="body2">
+              {stepsCompletedNumber}/{steps.length}
+            </StepsCompletedText>
+          </CircularProgresaBarContainer>
+          <IconButton variant="secondary" onClick={() => setIsHidden(!isHidden)} size="small">
+            <StyledSvgGlyphChevronDown isHidden={isHidden} />
+          </IconButton>
+        </StepsProgressContainer>
+      </BottomRowContainer>
     </Container>
   )
 }
