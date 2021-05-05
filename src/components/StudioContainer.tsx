b0b48ede0f1bc02eed85932bd53536a2e1562e79
drillprop <drylbartosz@gmail.com>
2021-05-05 11:33:11 +0200

    add padding-bottom to EditVideoForm

--- src/components/StudioContainer.tsx
+++ src/components/StudioContainer.tsx
@@ -5,7 +5,7 @@ export const studioContainerStyle = css`
   --max-inner-width: calc(1440px - var(--sidenav-collapsed-width) - calc(2 * var(--global-horizontal-padding)));
   max-width: var(--max-inner-width);
   position: relative;
-  margin: 0 auto 100px;
+  margin: 0 auto;
 `
 const StudioContainer = styled.div`
   ${studioContainerStyle}
