c82695e3ea7181243f918cb4ae3a324bdc49fef0
Bartosz Dryl <drylbartosz@gmail.com>
2021-05-10 10:36:50 +0200

    Fix video drop animation on firefox (#585)
    * refactor progress in MultiFileSelect
    
    * refactor FileStep
    
    * get rid of progress, refactor

--- src/shared/components/MultiFileSelect/MultiFileSelect.tsx
+++ src/shared/components/MultiFileSelect/MultiFileSelect.tsx
@@ -55,7 +55,6 @@ const MultiFileSelect: React.FC<MultiFileSelectProps> = ({
 }) => {
   const dialogRef = useRef<ImageCropDialogImperativeHandle>(null)
   const [step, setStep] = useState<FileType>('video')
-  const [progress, setProgress] = useState(0)
   const [isLoading, setIsLoading] = useState(false)
   const [rawImageFile, setRawImageFile] = useState<File | null>(null)
 
@@ -76,17 +75,14 @@ const MultiFileSelect: React.FC<MultiFileSelectProps> = ({
       return
     }
     const timeout = setTimeout(() => {
-      if (progress < 100) {
-        setProgress(progress + 1)
-      } else {
+      if (isLoading) {
         setIsLoading(false)
-        setProgress(0)
         setStep('image')
       }
-    }, 5)
+    }, 1000)
 
     return () => clearTimeout(timeout)
-  }, [error, isLoading, progress])
+  }, [error, isLoading])
 
   const updateVideoFile = async (file: File) => {
     try {
@@ -144,7 +140,6 @@ const MultiFileSelect: React.FC<MultiFileSelectProps> = ({
       onThumbnailChange(null)
     }
     setIsLoading(false)
-    setProgress(0)
   }
 
   const handleFileRejections = async (fileRejections: FileRejection[]) => {
@@ -167,7 +162,7 @@ const MultiFileSelect: React.FC<MultiFileSelectProps> = ({
         maxSize={step === 'video' ? maxVideoSize : maxImageSize}
         onUploadFile={handleUploadFile}
         onReAdjustThumbnail={handleReAdjustThumbnail}
-        progress={progress}
+        isLoading={isLoading}
         fileType={step}
         title={step === 'video' ? 'Select video file' : 'Add thumbnail image'}
         thumbnailUrl={files.thumbnail?.url}
@@ -189,7 +184,7 @@ const MultiFileSelect: React.FC<MultiFileSelectProps> = ({
           type="video"
           onDelete={() => handleDeleteFile('video')}
           onSelect={handleChangeStep}
-          progress={progress}
+          isLoading={isLoading}
         />
         <StepDivider>
           <SvgGlyphChevronRight />
