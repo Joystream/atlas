732e272a239e026077009e748f1c0849cb167998
Klaudiusz Dembler <dev@kdembler.com>
2021-05-13 16:40:19 +0200

    fix avatar url regex

--- src/config/regex.ts
+++ src/config/regex.ts
@@ -1,2 +1,2 @@
 export const MEMBERSHIP_NAME_PATTERN = /^[a-z0-9_]+$/
-export const URL_PATTERN = /[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)?/gi
+export const URL_PATTERN = /^https?:\/\/[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)?$/i
