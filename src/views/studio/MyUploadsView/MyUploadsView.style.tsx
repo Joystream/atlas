49e74fb4900c4e8e9276d28dd817d182fe2f22ab
mikkio-j <mikkiodev@gmail.com>
2021-05-19 12:34:00 +0200

    add padding to uploads view

--- src/views/studio/MyUploadsView/MyUploadsView.style.tsx
+++ src/views/studio/MyUploadsView/MyUploadsView.style.tsx
@@ -1,6 +1,11 @@
 import styled from '@emotion/styled'
 import { sizes } from '@/shared/theme'
 import { Text } from '@/shared/components'
+import { StudioContainer } from '@/components'
+
+export const UploadsContainer = styled(StudioContainer)`
+  padding-bottom: 120px;
+`
 
 export const StyledText = styled(Text)`
   margin: ${sizes(8)} 0;
