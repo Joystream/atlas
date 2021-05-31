2d5f3f1a33cf9891f300b3cb7cd6d8102044a208
drillprop <drylbartosz@gmail.com>
2021-05-31 16:10:50 +0200

    rebase

--- src/views/studio/MyUploadsView/AssetLine/AssetLine.tsx
+++ src/views/studio/MyUploadsView/AssetLine/AssetLine.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useState, useRef } from 'react'
 import { DropzoneOptions, useDropzone } from 'react-dropzone'
 import { useNavigate } from 'react-router'
-import { useUploadsManager, useAuthorizedUser } from '@/hooks'
+import { useUploadsManager, useAuthorizedUser, useDialog } from '@/hooks'
 import { useRandomStorageProviderUrl } from '@/api/hooks'
 import { absoluteRoutes } from '@/config/routes'
 import { formatBytes } from '@/utils/size'
@@ -17,7 +17,7 @@ import {
   ProgressbarContainer,
 } from './AssetLine.style'
 import { AssetUploadWithProgress } from '@/hooks/useUploadsManager/types'
-import { MessageDialog, ImageCropDialog, ImageCropDialogImperativeHandle } from '@/components'
+import { ImageCropDialog, ImageCropDialogImperativeHandle } from '@/components'
 import { Text, CircularProgressbar, Button } from '@/shared/components'
 import { SvgAlertError, SvgAlertSuccess, SvgGlyphFileImage, SvgGlyphFileVideo, SvgGlyphUpload } from '@/shared/icons'
 import { LiaisonJudgement } from '@/api/queries'
@@ -37,12 +37,36 @@ const AssetLine: React.FC<AssetLineProps> = ({ isLast = false, asset }) => {
   const avatarDialogRef = useRef<ImageCropDialogImperativeHandle>(null)
   const coverDialogRef = useRef<ImageCropDialogImperativeHandle>(null)
 
+  const [openDifferentFileDialog, closeDifferentFileDialog] = useDialog({
+    title: 'Different file was selected!',
+    description: `We detected that you selected a different file than the one you uploaded previously. Select the same file to continue the upload or edit ${
+      asset.parentObject.type === 'channel' ? 'your channel' : 'the video'
+    } to use the new file.`,
+    variant: 'warning',
+    onSecondaryButtonClick: () => {
+      if (asset.parentObject.type === 'video') {
+        navigate(absoluteRoutes.studio.editVideo())
+      }
+      if (asset.parentObject.type === 'channel') {
+        navigate(absoluteRoutes.studio.editChannel())
+      }
+      closeDifferentFileDialog()
+    },
+    onPrimaryButtonClick: () => {
+      openFileSelect()
+      closeDifferentFileDialog()
+    },
+    primaryButtonText: 'Reselect file',
+    secondaryButtonText: `Edit ${asset.parentObject.type === 'channel' ? 'channel' : 'video'}`,
+    exitButton: false,
+  })
+
   const onDrop: DropzoneOptions['onDrop'] = useCallback(
     async (acceptedFiles) => {
       const [file] = acceptedFiles
       const fileHash = await computeFileHash(file)
       if (fileHash !== asset.ipfsContentId) {
-        setShowDialog(true)
+        openDifferentFileDialog()
       } else {
         const randomStorageProviderUrl = getRandomStorageProviderUrl()
         if (!randomStorageProviderUrl) {
@@ -66,7 +90,17 @@ const AssetLine: React.FC<AssetLineProps> = ({ isLast = false, asset }) => {
         )
       }
     },
-    [asset, getRandomStorageProviderUrl, startFileUpload]
+    [
+      asset.contentId,
+      asset.ipfsContentId,
+      asset.owner,
+      asset.parentObject.id,
+      asset.parentObject.type,
+      asset.type,
+      getRandomStorageProviderUrl,
+      openDifferentFileDialog,
+      startFileUpload,
+    ]
   )
 
   const { getRootProps, getInputProps, open: openFileSelect } = useDropzone({
@@ -76,8 +110,6 @@ const AssetLine: React.FC<AssetLineProps> = ({ isLast = false, asset }) => {
     noClick: true,
     noKeyboard: true,
   })
-
-  const [showDialog, setShowDialog] = useState(false)
 
   const isVideo = asset.type === 'video'
   const fileTypeText = isVideo ? 'Video file' : `${asset.type.charAt(0).toUpperCase() + asset.type.slice(1)} image`
@@ -108,7 +140,7 @@ const AssetLine: React.FC<AssetLineProps> = ({ isLast = false, asset }) => {
   const handleCropConfirm = async (croppedBlob: Blob) => {
     const fileHash = await computeFileHash(croppedBlob)
     if (fileHash !== asset.ipfsContentId) {
-      setShowDialog(true)
+      openDifferentFileDialog()
     } else {
       const randomStorageProviderUrl = getRandomStorageProviderUrl()
       if (!randomStorageProviderUrl) {
@@ -190,30 +222,6 @@ const AssetLine: React.FC<AssetLineProps> = ({ isLast = false, asset }) => {
 
   return (
     <>
-      <MessageDialog
-        title="Different file was selected!"
-        description={`We detected that you selected a different file than the one you uploaded previously. Select the same file to continue the upload or edit ${
-          asset.parentObject.type === 'channel' ? 'your channel' : 'the video'
-        } to use the new file.`}
-        showDialog={showDialog}
-        variant="warning"
-        onSecondaryButtonClick={() => {
-          setShowDialog(false)
-          if (asset.parentObject.type === 'video') {
-            navigate(absoluteRoutes.studio.editVideo())
-          }
-          if (asset.parentObject.type === 'channel') {
-            navigate(absoluteRoutes.studio.editChannel())
-          }
-        }}
-        primaryButtonText="Reselect file"
-        onPrimaryButtonClick={() => {
-          setShowDialog(false)
-          reselectFile()
-        }}
-        secondaryButtonText={`Edit ${asset.parentObject.type === 'channel' ? 'channel' : 'video'}`}
-        exitButton={false}
-      />
       <FileLineContainer isLast={isLast}>
         {isLast ? <FileLineLastPoint /> : <FileLinePoint />}
         <FileStatusContainer>{renderStatusIndicator(asset)}</FileStatusContainer>
