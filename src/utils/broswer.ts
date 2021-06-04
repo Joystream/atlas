df33276eb5cd84a50d31cb7c9e3c1c09bbd4d2e6
Diego Cardenas <cardenasrdiego@gmail.com>
2021-06-04 12:50:21 +0200

    Unsupported Browser dialog

--- src/utils/broswer.ts
+++ src/utils/broswer.ts
@@ -1,3 +1,10 @@
 export const copyToClipboard = (text: string) => {
   navigator.clipboard.writeText(text)
 }
+
+// @ts-ignore not worth typing
+export const isFirefox = (): boolean => typeof InstallTrigger !== 'undefined'
+// @ts-ignore not worth typing
+export const isChromiumBased = (): boolean => !!window.chrome
+
+export const isAllowedBrowser = () => isFirefox() || isChromiumBased()
