1ad9ecf0fcf030fd84e5b3aab83cee79f68e9cd4
mikkio-j <mikkiodev@gmail.com>
2021-06-04 15:22:14 +0200

    edit styles

--- src/components/SignInSteps/ExtensionStep.style.ts
+++ src/components/SignInSteps/ExtensionStep.style.ts
@@ -19,5 +19,8 @@ export const StyledListItem = styled(Text)`
 `
 
 export const PolkadotExtensionRejectedWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
 `
