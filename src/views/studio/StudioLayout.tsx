9b8b2ad71b042ae3d3c805468755846324ac4846
Diego Cardenas <cardenasrdiego@gmail.com>
2021-06-07 12:22:23 +0200

    Cr

--- src/views/studio/StudioLayout.tsx
+++ src/views/studio/StudioLayout.tsx
@@ -70,7 +70,12 @@ const StudioLayout = () => {
       openUnsupportedBrowserDialog({
         variant: 'warning',
         title: 'Unsupported browser detected',
-        description: 'It seems the browser you are using is not fully supported by Joystream Studio. Some of the features may not be accessible. For the best experience, please use a recent version of Chrome, Firefox or Edge.',
+        description:
+          'It seems the browser you are using is not fully supported by Joystream Studio. Some of the features may not be accessible. For the best experience, please use a recent version of Chrome, Firefox or Edge.',
+        primaryButtonText: 'I understand',
+        onPrimaryButtonClick: () => {
+          closeUnsupportedBrowserDialog()
+        },
         onExitClick: () => {
           closeUnsupportedBrowserDialog()
         },
