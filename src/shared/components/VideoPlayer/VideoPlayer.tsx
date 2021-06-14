d7c45985e27df062bb8c716b76fd4fed85ce13ca
drillprop <drylbartosz@gmail.com>
2021-06-14 13:28:47 +0200

    revert previous change, update useEffects in videoJsPlayer

--- src/shared/components/VideoPlayer/VideoPlayer.tsx
+++ src/shared/components/VideoPlayer/VideoPlayer.tsx
@@ -52,21 +52,25 @@ const VideoPlayer: React.ForwardRefRenderFunction<HTMLVideoElement, VideoPlayerP
   }, [player, initialized, autoplay])
 
   useEffect(() => {
-    if (!player || !playing) {
+    if (!player) {
       return
     }
 
-    const playPromise = player.play()
-    if (playPromise) {
-      playPromise.catch((e) => {
-        if (e.name === 'NotAllowedError') {
-          console.warn('Video play failed:', e)
-        } else {
-          console.error('Video play failed:', e)
+    if (playing != null) {
+      if (playing) {
+        const playPromise = player.play()
+        if (playPromise) {
+          playPromise.catch((e) => {
+            if (e.name === 'NotAllowedError') {
+              console.warn('Video play failed:', e)
+            } else {
+              console.error('Video play failed:', e)
+            }
+          })
         }
-      })
-    } else {
-      player.pause()
+      } else {
+        player.pause()
+      }
     }
   }, [player, playing])
 
