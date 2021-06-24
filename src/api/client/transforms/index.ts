dbdf5dc4b51627891c7d37f7e86492d4028ca8ea
drillprop <drylbartosz@gmail.com>
2021-06-24 10:45:49 +0200

    remove redundant transform

--- src/api/client/transforms/index.ts
+++ src/api/client/transforms/index.ts
@@ -1,4 +1,4 @@
-export { ORION_VIEWS_QUERY_NAME, TransformOrionViewsField, TransformBatchedOrionViewsField } from './orionViews'
+export { TransformBatchedOrionViewsField } from './orionViews'
 export { ORION_FOLLOWS_QUERY_NAME, TransformOrionFollowsField } from './orionFollows'
 export { RemoveQueryNodeViewsField } from './queryNodeViews'
 export { RemoveQueryNodeFollowsField } from './queryNodeFollows'
--- src/api/client/transforms/orionViews.ts
+++ src/api/client/transforms/orionViews.ts
@@ -30,51 +30,6 @@ const VIDEO_INFO_SELECTION_SET: SelectionSetNode = {
   ],
 }
 
-export const ORION_VIEWS_QUERY_NAME = 'videoViews'
-
-// Transform a request to expect VideoViewsInfo return type instead of an Int
-export const TransformOrionViewsField: Transform = {
-  transformRequest(request) {
-    request.document = {
-      ...request.document,
-      definitions: request.document.definitions.map((definition) => {
-        if (definition.kind === 'OperationDefinition') {
-          return {
-            ...definition,
-            selectionSet: {
-              ...definition.selectionSet,
-              selections: definition.selectionSet.selections.map((selection) => {
-                if (selection.kind === 'Field' && selection.name.value === ORION_VIEWS_QUERY_NAME) {
-                  return {
-                    ...selection,
-                    selectionSet: VIDEO_INFO_SELECTION_SET,
-                  }
-                }
-                return selection
-              }),
-            },
-          }
-        }
-        return definition
-      }),
-    }
-
-    return request
-  },
-  transformResult(result) {
-    if (result.errors) {
-      throw new OrionError(result.errors)
-    }
-
-    const views = result?.data?.[ORION_VIEWS_QUERY_NAME]?.views || 0
-    const data = {
-      videoViews: views,
-    }
-
-    return { data }
-  },
-}
-
 export const BATCHED_ORION_VIEWS_QUERY_NAME = 'batchedVideoViews'
 
 export const TransformBatchedOrionViewsField: Transform = {
