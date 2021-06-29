3db9a30fb1039cca8b52fb44fe3ddd092636f136
drillprop <drylbartosz@gmail.com>
2021-06-29 11:19:39 +0200

    replace details with blank string when there is no details

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,18 +1,18 @@
 /* eslint-disable no-console */
 export const Logger = {
   log: (message: string, details?: unknown) => {
-    console.log(message, details)
+    console.log(message, details || '')
   },
 
   warn: (message: string, details?: unknown) => {
-    console.warn(message, details)
+    console.warn(message, details || '')
   },
 
   error: (message: string, details?: unknown) => {
-    console.error(message, details)
+    console.error(message, details || '')
   },
 
   debug: (message: string, details?: unknown) => {
-    console.debug(message, details)
+    console.debug(message, details || '')
   },
 }
