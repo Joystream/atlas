6cca78f65083c0094254039c4f5bc2550ffafa58
mikkio-j <mikkiodev@gmail.com>
2021-06-29 14:37:30 +0200

    cr fix

--- src/views/admin/AdminView.tsx
+++ src/views/admin/AdminView.tsx
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom'
 
 import { TARGET_DEV_ENV, availableEnvs, setEnvInLocalStorage } from '@/config/envs'
 import { absoluteRoutes } from '@/config/routes'
+import { useSnackbar } from '@/providers'
 import { Button, Select, Text } from '@/shared/components'
+import { Logger } from '@/utils/logger'
 
 const items = availableEnvs().map((item) => ({ name: item, value: item }))
 
 export const AdminView = () => {
   const env = availableEnvs().includes(TARGET_DEV_ENV) ? TARGET_DEV_ENV : null
   const [value, setValue] = useState<null | string>(env)
+  const { displaySnackbar } = useSnackbar()
 
   const handleEnvironmentChange = (value?: string | null | undefined) => {
     if (!value) {
@@ -37,21 +40,34 @@ export const AdminView = () => {
 
     const linkElement = document.createElement('a')
     linkElement.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(jsonStorage))
-    linkElement.setAttribute('download', 'atlas-export.json')
+    linkElement.setAttribute('download', `atlas-export-${new Date().toLocaleString()}.json`)
     linkElement.click()
   }
 
   const handleFileChange = async (e: Event) => {
-    const file = (e.target as HTMLInputElement).files?.[0]
-    if (!file) return
-    const fileText = await file.text()
-    const storage = JSON.parse(fileText)
-    Object.keys(storage).forEach((key) => {
-      window.localStorage.setItem(key, JSON.stringify(storage[key]))
-    })
+    try {
+      const file = (e.target as HTMLInputElement).files?.[0]
+      if (!file) return
+      const fileText = await file.text()
+      const storage = JSON.parse(fileText)
+      Object.keys(storage).forEach((key) => {
+        window.localStorage.setItem(key, JSON.stringify(storage[key]))
+      })
+      displaySnackbar({
+        title: 'Local state updated',
+        iconType: 'success',
+      })
+    } catch (error) {
+      Logger.error(error)
+      displaySnackbar({
+        title: 'JSON file seems to be corrupted',
+        description: 'Please try again with different file',
+        iconType: 'error',
+      })
+    }
   }
 
-  const handleImportClick = async () => {
+  const handleImportClick = () => {
     const inputElement = document.createElement('input')
     inputElement.setAttribute('type', 'file')
     inputElement.onchange = handleFileChange
