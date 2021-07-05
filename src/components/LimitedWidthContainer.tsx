bf89d0cb85e64f8605938a421f562c37e4917466
Diego Cardenas <cardenasrdiego@gmail.com>
2021-07-05 21:11:17 +0200

    Update src/components/LimitedWidthContainer.tsx
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>

--- src/components/LimitedWidthContainer.tsx
+++ src/components/LimitedWidthContainer.tsx
@@ -9,9 +9,5 @@ export const limitedWidthContainerStyle = css`
   margin: 0 auto;
 `
 export const LimitedWidthContainer = styled.div`
-  --max-inner-width: calc(1440px - var(--sidenav-collapsed-width) - calc(2 * var(--global-horizontal-padding)));
-
-  max-width: var(--max-inner-width);
-  position: relative;
-  margin: 0 auto;
+  ${limitedWidthContainerStyle}
 `
