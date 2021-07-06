a6964399fd098eec4298efc82fb86bb7204fbf36
Bartosz Dryl <drylbartosz@gmail.com>
2021-07-06 07:57:58 +0200

    Update src/views/viewer/VideoView/VideoView.tsx
    Co-authored-by: Rafał Pawłow <pawlow.rafal@gmail.com>

--- src/views/viewer/VideoView/VideoView.tsx
+++ src/views/viewer/VideoView/VideoView.tsx
@@ -132,7 +132,7 @@ export const VideoView: React.FC = () => {
         <PlayerContainer>
           {video ? (
             <VideoPlayer
-              autoplay={true}
+              autoplay
               src={mediaUrl}
               fill
               posterUrl={thumbnailPhotoUrl}
