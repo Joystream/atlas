1c6ba1cace2ba9f88676b5745007f649a5b97455
Diego Cardenas <cardenasrdiego@gmail.com>
2021-07-22 13:26:45 +0200

    Channel view layout (#976)
    * fix weird charactr bug on channels with no uploads
    
    * layout wip
    
    * Update src/components/LimitedWidthContainer.tsx
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>
    
    * Update src/views/viewer/ChannelView/ChannelView.tsx
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>
    
    * Update src/views/viewer/ChannelView/ChannelView.style.tsx
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>
    
    * why is lint not doing this automatically
    
    * wip
    
    * Tabs
    
    * about wip
    
    * about wip
    
    * wip
    
    * layout
    
    * Update src/views/viewer/ChannelView/ChannelAbout.tsx
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>
    
    * cr
    
    * cr
    
    * cr
    
    * trying to get channel views but failed
    
    * channel views
    
    * Revert "trying to get channel views but failed" and
    "channel views"
    
    This reverts commit 8da6e6fa17bd0bc614a8b989e76e3735d993ef7b.
    bd61c620a876568c08a5370893901dee9ec85537.
    
    * cr
    
    * fix weird charactr bug on channels with no uploads
    
    * layout wip
    
    * Update src/components/LimitedWidthContainer.tsx
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>
    
    * Update src/views/viewer/ChannelView/ChannelView.tsx
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>
    
    * Update src/views/viewer/ChannelView/ChannelView.style.tsx
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>
    
    * why is lint not doing this automatically
    
    * wip
    
    * Tabs
    
    * about wip
    
    * about wip
    
    * wip
    
    * layout
    
    * cr
    
    * cr
    
    * cr
    
    * trying to get channel views but failed
    
    * channel views
    
    * Revert "trying to get channel views but failed" and
    "channel views"
    
    This reverts commit 8da6e6fa17bd0bc614a8b989e76e3735d993ef7b.
    bd61c620a876568c08a5370893901dee9ec85537.
    
    * cr
    
    * cr
    
    * visual cr
    
    * information
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>

---
+++ src/views/viewer/ChannelView/ChannelAbout.style.tsx
@@ -0,0 +1,79 @@
+import styled from '@emotion/styled'
+
+import { Avatar, Text } from '@/shared/components'
+import { colors, media, sizes } from '@/shared/theme'
+
+export const TextContainer = styled.div`
+  display: grid;
+  grid-gap: ${sizes(4)};
+  padding-bottom: ${sizes(8)};
+  margin-bottom: ${sizes(8)};
+  border-bottom: 1px solid ${colors.gray[600]};
+`
+
+export const Container = styled.div`
+  display: grid;
+  gap: 142px;
+  grid-template-columns: minmax(0, 1fr) minmax(206px, 326px);
+  margin-bottom: 50px;
+  ${media.base} {
+    gap: 32px;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto 1fr;
+  }
+  ${media.small} {
+    gap: 79px;
+    grid-template-rows: initial;
+    grid-template-columns: minmax(0, 1fr) minmax(206px, 228px);
+  }
+  ${media.medium} {
+    gap: 94px;
+    grid-template-columns: minmax(0, 1fr) minmax(206px, 260px);
+  }
+`
+
+export const LinksContainer = styled.div`
+  display: grid;
+  grid-gap: ${sizes(6)};
+`
+
+export const Links = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+
+  > a {
+    margin-right: ${sizes(12)};
+    margin-bottom: ${sizes(6)};
+  }
+`
+
+export const DetailsContainer = styled.div`
+  ${media.base} {
+    grid-row: 1;
+  }
+  ${media.small} {
+    grid-row: initial;
+  }
+`
+
+export const Details = styled.div`
+  display: grid;
+  gap: ${sizes(2)};
+  padding-bottom: ${sizes(4)};
+  border-bottom: 1px solid ${colors.gray[600]};
+  margin-bottom: ${sizes(4)};
+`
+
+export const DetailsText = styled(Text)`
+  margin-bottom: ${sizes(4)};
+`
+
+export const AvatarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: ${sizes(2)};
+`
+
+export const StyledAvatar = styled(Avatar)`
+  margin-right: ${sizes(2)};
+`
