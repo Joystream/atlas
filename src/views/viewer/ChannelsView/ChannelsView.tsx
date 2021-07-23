6865874b933be695aa0677b73ed7b9852cadfcf4
Rafał Pawłow <pawlow.rafal@gmail.com>
2021-07-23 14:20:15 +0200

    Add CTA buttons to all video content pages

--- src/views/viewer/ChannelsView/ChannelsView.tsx
+++ src/views/viewer/ChannelsView/ChannelsView.tsx
@@ -2,7 +2,9 @@ import styled from '@emotion/styled'
 import React from 'react'
 
 import { InfiniteChannelWithVideosGrid, ViewWrapper } from '@/components'
-import { Text } from '@/shared/components'
+import { absoluteRoutes } from '@/config/routes'
+import { CallToActionButton, CallToActionWrapper, Text } from '@/shared/components'
+import { SvgNavChannels, SvgNavHome, SvgNavNew, SvgNavPopular } from '@/shared/icons'
 import { sizes } from '@/shared/theme'
 
 export const ChannelsView = () => {
@@ -10,6 +12,26 @@ export const ChannelsView = () => {
     <StyledViewWrapper>
       <Header variant="h2">Browse channels</Header>
       <InfiniteChannelWithVideosGrid title="Channels in your language:" languageSelector onDemand />
+      <CallToActionWrapper>
+        <CallToActionButton
+          label="Popular on Joystream"
+          to={absoluteRoutes.viewer.popular()}
+          colorVariant="red"
+          icon={<SvgNavPopular />}
+        />
+        <CallToActionButton
+          label="New & Noteworthy"
+          to={absoluteRoutes.viewer.new()}
+          colorVariant="green"
+          icon={<SvgNavNew />}
+        />
+        <CallToActionButton
+          label="Home"
+          to={absoluteRoutes.viewer.index()}
+          colorVariant="yellow"
+          icon={<SvgNavHome />}
+        />
+      </CallToActionWrapper>
     </StyledViewWrapper>
   )
 }
