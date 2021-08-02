af798de58499148cb152b28caadc467c5bb0c79f
drillprop <drylbartosz@gmail.com>
2021-08-02 08:26:47 +0200

    update channels and videos resolvers

--- src/api/client/resolvers.ts
+++ src/api/client/resolvers.ts
@@ -6,16 +6,19 @@ import { createLookup } from '@/utils/data'
 import { Logger } from '@/utils/logger'
 
 import {
+  ORION_BATCHED_FOLLOWS_QUERY_NAME,
   ORION_BATCHED_VIEWS_QUERY_NAME,
   ORION_FOLLOWS_QUERY_NAME,
+  ORION_VIEWS_QUERY_NAME,
   RemoveQueryNodeFollowsField,
   RemoveQueryNodeViewsField,
+  TransformBatchedOrionFollowsField,
   TransformBatchedOrionViewsField,
   TransformOrionFollowsField,
+  TransformOrionViewsField,
 } from './transforms'
-import { ORION_VIEWS_QUERY_NAME, TransformOrionViewsField } from './transforms/orionViews'
 
-import { VideoEdge } from '../queries'
+import { Channel, ChannelEdge, Video, VideoEdge } from '../queries'
 
 const createResolverWithTransforms = (
   schema: GraphQLSchema,
@@ -45,13 +48,65 @@ export const queryNodeStitchingResolvers = (
     videoByUniqueInput: createResolverWithTransforms(queryNodeSchema, 'videoByUniqueInput', [
       RemoveQueryNodeViewsField,
     ]),
-    videos: createResolverWithTransforms(queryNodeSchema, 'videos', [RemoveQueryNodeViewsField]),
+    videos: async (parent, args, context, info) => {
+      try {
+        const videosResolver = createResolverWithTransforms(queryNodeSchema, 'videos', [RemoveQueryNodeViewsField])
+        const videos = await videosResolver(parent, args, context, info)
+
+        const batchedVideoViews = await delegateToSchema({
+          schema: orionSchema,
+          operation: 'query',
+          // operationName has to be manually kept in sync with the query name used
+          operationName: 'GetBatchedVideoViews',
+          fieldName: ORION_BATCHED_VIEWS_QUERY_NAME,
+          args: {
+            videoIdList: videos.map((video: Video) => video.id),
+          },
+          context,
+          info,
+          transforms: [TransformBatchedOrionViewsField],
+        })
+
+        const viewsLookup = createLookup<{ id: string; views: number }>(batchedVideoViews || [])
+        return videos.map((video: Video) => ({ ...video, views: viewsLookup[video.id]?.views || 0 }))
+      } catch (error) {
+        Logger.warn('Failed to resolve videos field', { error })
+        return null
+      }
+    },
     videosConnection: createResolverWithTransforms(queryNodeSchema, 'videosConnection', [RemoveQueryNodeViewsField]),
     // channel queries
     channelByUniqueInput: createResolverWithTransforms(queryNodeSchema, 'channelByUniqueInput', [
       RemoveQueryNodeFollowsField,
     ]),
-    channels: createResolverWithTransforms(queryNodeSchema, 'channels', [RemoveQueryNodeFollowsField]),
+    channels: async (parent, args, context, info) => {
+      try {
+        const channelsResolver = createResolverWithTransforms(queryNodeSchema, 'channels', [
+          RemoveQueryNodeFollowsField,
+        ])
+        const channels = await channelsResolver(parent, args, context, info)
+
+        const batchedChannelFollows = await delegateToSchema({
+          schema: orionSchema,
+          operation: 'query',
+          // operationName has to be manually kept in sync with the query name used
+          operationName: 'GetBatchedChannelFollows',
+          fieldName: ORION_BATCHED_FOLLOWS_QUERY_NAME,
+          args: {
+            channelIdList: channels.map((channel: Channel) => channel.id),
+          },
+          context,
+          info,
+          transforms: [TransformBatchedOrionFollowsField],
+        })
+
+        const followsLookup = createLookup<{ id: string; follows: number }>(batchedChannelFollows || [])
+        return channels.map((channel: Channel) => ({ ...channel, follows: followsLookup[channel.id]?.follows || 0 }))
+      } catch (error) {
+        Logger.warn('Failed to resolve channels field', { error })
+        return null
+      }
+    },
     channelsConnection: createResolverWithTransforms(queryNodeSchema, 'channelsConnection', [
       RemoveQueryNodeFollowsField,
     ]),
@@ -114,8 +169,38 @@ export const queryNodeStitchingResolvers = (
     },
   },
 
+  ChannelConnection: {
+    edges: async (parent, args, context, info) => {
+      const batchedChannelFollows = await delegateToSchema({
+        schema: orionSchema,
+        operation: 'query',
+        // operationName has to be manually kept in sync with the query name used
+        operationName: 'GetBatchedChannelFollows',
+        fieldName: ORION_BATCHED_FOLLOWS_QUERY_NAME,
+        args: {
+          channelIdList: parent.edges.map((edge: ChannelEdge) => edge.node.id),
+        },
+        context,
+        info,
+        transforms: [TransformBatchedOrionFollowsField],
+      })
+
+      const followsLookup = createLookup<{ id: string; views: number }>(batchedChannelFollows || [])
+
+      return parent.edges.map((edge: ChannelEdge) => ({
+        ...edge,
+        node: {
+          ...edge.node,
+          follows: followsLookup[edge.node.id]?.views || 0,
+        },
+      }))
+    },
+  },
   Channel: {
     follows: async (parent, args, context, info) => {
+      if (parent.follows != null) {
+        return parent.follows
+      }
       try {
         return await delegateToSchema({
           schema: orionSchema,
--- src/api/client/transforms/orionFollows.ts
+++ src/api/client/transforms/orionFollows.ts
@@ -74,3 +74,41 @@ export const TransformOrionFollowsField: Transform = {
     return { data }
   },
 }
+
+export const ORION_BATCHED_FOLLOWS_QUERY_NAME = 'batchedChannelFollows'
+
+export const TransformBatchedOrionFollowsField: Transform = {
+  transformRequest(request) {
+    request.document = {
+      ...request.document,
+      definitions: request.document.definitions.map((definition) => {
+        if (definition.kind === 'OperationDefinition') {
+          return {
+            ...definition,
+            selectionSet: {
+              ...definition.selectionSet,
+              selections: definition.selectionSet.selections.map((selection) => {
+                if (selection.kind === 'Field' && selection.name.value === ORION_BATCHED_FOLLOWS_QUERY_NAME) {
+                  return {
+                    ...selection,
+                    selectionSet: CHANNEL_INFO_SELECTION_SET,
+                  }
+                }
+                return selection
+              }),
+            },
+          }
+        }
+        return definition
+      }),
+    }
+
+    return request
+  },
+  transformResult(result) {
+    if (result.errors) {
+      throw new OrionError(result.errors)
+    }
+    return result
+  },
+}
