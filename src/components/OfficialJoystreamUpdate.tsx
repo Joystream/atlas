e7e9e1863d643be3f72b76a33d08879590647ab7
Rafał Pawłow <pawlow.rafal@gmail.com>
2021-08-02 10:30:01 +0200

    Add ranking number to carousel (#1046)

--- src/components/OfficialJoystreamUpdate.tsx
+++ src/components/OfficialJoystreamUpdate.tsx
@@ -2,7 +2,6 @@ import styled from '@emotion/styled'
 import React from 'react'
 
 import { useVideos } from '@/api/hooks'
-import { VideoOrderByInput } from '@/api/queries'
 import { VideoGallery } from '@/components'
 import { readEnv } from '@/config/envs'
 import { sizes } from '@/shared/theme'
@@ -15,7 +14,6 @@ export const OfficialJoystreamUpdate = () => {
     where: {
       channelId_eq: channelId,
     },
-    orderBy: VideoOrderByInput.CreatedAtDesc,
     limit: MAX_VIDEOS,
   })
 
