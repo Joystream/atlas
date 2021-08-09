73c9a013396bc6871a464b04fe8b028d2931cd66
Rafał Pawłow <pawlow.rafal@gmail.com>
2021-08-09 15:06:22 +0200

    Add CTA buttons to all video content pages (#1136)

--- src/views/viewer/NewView/NewView.tsx
+++ src/views/viewer/NewView/NewView.tsx
@@ -1,17 +1,33 @@
 import styled from '@emotion/styled'
-import React from 'react'
+import React, { FC } from 'react'
 
 import { ViewWrapper } from '@/components'
+import { absoluteRoutes } from '@/config/routes'
+import { CallToActionButton, CallToActionWrapper } from '@/shared/components'
 import { Text } from '@/shared/components'
+import { SvgNavChannels, SvgNavHome, SvgNavPopular } from '@/shared/icons'
 import { sizes } from '@/shared/theme'
 
-export const NewView = () => {
-  return (
-    <ViewWrapper>
-      <Header variant="h2">New & Noteworthy</Header>
-    </ViewWrapper>
-  )
-}
+export const NewView: FC = () => (
+  <ViewWrapper>
+    <Header variant="h2">New & Noteworthy</Header>
+    <CallToActionWrapper>
+      <CallToActionButton label="Home" to={absoluteRoutes.viewer.index()} colorVariant="yellow" icon={<SvgNavHome />} />
+      <CallToActionButton
+        label="Browse channels"
+        to={absoluteRoutes.viewer.channels()}
+        colorVariant="blue"
+        icon={<SvgNavChannels />}
+      />
+      <CallToActionButton
+        label="Popular on Joystream"
+        to={absoluteRoutes.viewer.popular()}
+        colorVariant="red"
+        icon={<SvgNavPopular />}
+      />
+    </CallToActionWrapper>
+  </ViewWrapper>
+)
 
 const Header = styled(Text)`
   margin-top: ${sizes(16)};
