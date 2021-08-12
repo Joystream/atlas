d676a59f9d549df9606fffbc1cdf804fc1959b97
Rafal Pawlow <pawlow.rafal@gmail.com>
2021-08-12 14:44:07 +0200

    PR FIX

--- src/shared/components/Carousel/Carousel.tsx
+++ src/shared/components/Carousel/Carousel.tsx
@@ -1,4 +1,13 @@
-import React, { ComponentPropsWithoutRef, ReactNode, RefObject, forwardRef, useImperativeHandle, useRef } from 'react'
+import Glider from 'glider-js'
+import React, {
+  ComponentPropsWithoutRef,
+  ReactNode,
+  RefObject,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from 'react'
 
 import { Container, Dots, GliderContainer, Track } from './Carousel.style'
 
@@ -35,6 +44,23 @@ export const Carousel = forwardRef<
     ref
   ) => {
     const dotsRef = useRef<HTMLDivElement>(null)
+    const gliderInstanceRef = useRef<Glider.Static<HTMLElement>>()
+
+    const onAnimated = () => {
+      if (gliderInstanceRef.current && gliderOptions.responsive) {
+        const breakpointIndex = gliderOptions.responsive.findIndex(
+          (item) => item.breakpoint === gliderInstanceRef.current?.breakpoint
+        )
+        const slidesToScroll = gliderOptions.responsive[breakpointIndex].settings.slidesToScroll as number
+        const itemsRemainder = gliderInstanceRef.current.slides.length % slidesToScroll
+        if (nextArrowRef.current && nextArrowRef.current?.classList.contains('disabled') && itemsRemainder) {
+          gliderInstanceRef.current.setOption({ slidesToScroll: 2 }, false)
+        } else {
+          gliderInstanceRef.current.setOption({ skipTrack: true, ...gliderOptions }, false)
+        }
+      }
+    }
+
     const {
       ref: gliderRef,
       getContainerProps,
@@ -43,12 +69,19 @@ export const Carousel = forwardRef<
       getPrevArrowProps,
       getNextArrowProps,
       getDotsProps,
+      glider,
     } = useGlider<HTMLDivElement>({
       slidesToShow,
+      onAnimated,
       arrows: { prev: prevArrowRef.current, next: nextArrowRef.current },
       dots: dotsRef.current,
       ...gliderOptions,
     })
+
+    useEffect(() => {
+      if (!glider) return
+      gliderInstanceRef.current = glider
+    }, [glider])
 
     useImperativeHandle(ref, () => ({
       getPrevArrowProps,
