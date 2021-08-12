a7231369ddd7a0fcf151435816ca81b752c16771
drillprop <drylbartosz@gmail.com>
2021-08-12 14:51:30 +0200

    remove ChannelCard from ChannelWithVideos

--- src/components/ChannelWithVideos.tsx
+++ src/components/ChannelWithVideos.tsx
@@ -1,13 +1,17 @@
 import styled from '@emotion/styled'
 import React, { FC, useState } from 'react'
+import { Link } from 'react-router-dom'
 
+import { useChannel } from '@/api/hooks'
 import { GetVideosConnectionDocument, GetVideosConnectionQuery, GetVideosConnectionQueryVariables } from '@/api/queries'
 import { useInfiniteGrid } from '@/components/InfiniteGrids/useInfiniteGrid'
 import { VideoTile } from '@/components/VideoTile'
-import { Grid } from '@/shared/components'
+import { absoluteRoutes } from '@/config/routes'
+import { useHandleFollowChannel } from '@/hooks'
+import { AssetType, useAsset } from '@/providers'
+import { Avatar, Button, Grid, SkeletonLoader, Text } from '@/shared/components'
 import { sizes } from '@/shared/theme'
-
-import { ChannelCard } from './ChannelCard'
+import { formatNumberShort } from '@/utils/number'
 
 type ChannelWithVideosProps = {
   channelId?: string
@@ -18,6 +22,10 @@ const INITAL_ROWS = 1
 
 export const ChannelWithVideos: FC<ChannelWithVideosProps> = ({ channelId }) => {
   const [videosPerRow, setVideosPerRow] = useState(INITIAL_VIDEOS_PER_ROW)
+  const { channel, loading } = useChannel(channelId || '')
+
+  const { url: avatarUrl } = useAsset({ entity: channel, assetType: AssetType.AVATAR })
+  const { toggleFollowing, isFollowing } = useHandleFollowChannel(channelId)
   const { displayedItems, placeholdersCount } = useInfiniteGrid<
     GetVideosConnectionQuery,
     GetVideosConnectionQuery['videosConnection'],
@@ -47,14 +55,72 @@ export const ChannelWithVideos: FC<ChannelWithVideosProps> = ({ channelId }) => 
     </>
   )
 
+  const isLoading = !channelId || loading
+
   return (
     <>
-      <StyledChannelCard id={channelId} variant="secondary" />
+      <ChannelCardAnchor to={absoluteRoutes.viewer.channel(channelId)}>
+        <StyledAvatar size="channel" loading={isLoading} assetUrl={avatarUrl} />
+        <InfoWrapper>
+          {isLoading ? (
+            <SkeletonLoader width="120px" height="20px" bottomSpace="4px" />
+          ) : (
+            <ChannelTitle variant="h6">{channel?.title}</ChannelTitle>
+          )}
+          {isLoading ? (
+            <SkeletonLoader width="80px" height="20px" bottomSpace="8px" />
+          ) : (
+            <ChannelFollows variant="body2" secondary>
+              {formatNumberShort(channel?.follows || 0)} followers
+            </ChannelFollows>
+          )}
+          {isLoading ? (
+            <SkeletonLoader width="90px" height="40px" />
+          ) : (
+            <FollowButton variant="secondary" size={'medium'} onClick={() => toggleFollowing()}>
+              {isFollowing ? 'Unfollow' : 'Follow'}
+            </FollowButton>
+          )}
+        </InfoWrapper>
+      </ChannelCardAnchor>
       <Grid onResize={(sizes) => setVideosPerRow(sizes.length)}>{gridContent}</Grid>
     </>
   )
 }
 
-export const StyledChannelCard = styled(ChannelCard)`
+const ChannelCardAnchor = styled(Link)`
+  text-decoration: none;
+  align-items: center;
+  transition: transform, box-shadow;
+  display: inline-flex;
+  justify-content: unset;
   margin-bottom: ${sizes(10)};
+`
+
+const StyledAvatar = styled(Avatar)`
+  margin-left: ${sizes(6)};
+`
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const ChannelTitle = styled(Text)`
+  max-width: 200px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+const ChannelFollows = styled(Text)`
+  margin-top: ${sizes(1)};
+  max-width: 200px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+const FollowButton = styled(Button)`
+  margin-top: ${sizes(2)};
 `
