9257ccc95716e3c28b98e6f98d57779ecc22d70e
drillprop <drylbartosz@gmail.com>
2021-08-20 08:41:15 +0200

    add CircularDependencyPlugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
 const StylelintPlugin = require('stylelint-webpack-plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin')
 const { override, addBabelPlugin, addWebpackAlias, addWebpackModuleRule, addWebpackPlugin } = require('customize-cra')
 
 module.exports = {
   webpack: override(
     addBabelPlugin('@emotion/babel-plugin'),
     addWebpackPlugin(new StylelintPlugin({ files: './src/**/*.{tsx,ts}' })),
+    addWebpackPlugin(
+      new CircularDependencyPlugin({
+        exclude: /a\.js|node_modules/,
+      })
+    ),
     addWebpackAlias({
       '@': path.resolve(__dirname, 'src/'),
     }),
