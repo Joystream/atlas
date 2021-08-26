80b340fc0792e69b7517d7c610ff33c63d49d893
eldiegod <cardenasrdiego@gmail.com>
2021-08-26 11:54:56 +0200

    Pie chart

--- src/shared/components/VideoCategoryCard/VideoCategoryCard.style.ts
+++ src/shared/components/VideoCategoryCard/VideoCategoryCard.style.ts
@@ -146,3 +146,55 @@ export const VideosNumberContainer = styled.div`
   display: flex;
   align-items: center;
 `
+
+// ref https://codeburst.io/how-to-pure-css-pie-charts-w-css-variables-38287aea161e
+export const PieChart = styled.div`
+  margin-right: ${sizes(2)};
+  background: ${transparentize(0.8, colors.gray[300])};
+  border-radius: 100%;
+  height: ${sizes(4)};
+  width: ${sizes(4)};
+  overflow: hidden;
+  position: relative;
+`
+
+export const PieSegment = styled.div<{ value: number }>`
+  --a: calc(var(--over50, 0) * -100%);
+  --b: calc((1 + var(--over50, 0)) * 100%);
+  --degrees: calc((var(--offset, 0) / 100) * 360);
+  --over50: ${({ value }) => (value > 50 ? 1 : 0)};
+  --pieChartValue: ${({ value }) => value};
+
+  -webkit-clip-path: polygon(var(--a) var(--a), var(--b) var(--a), var(--b) var(--b), var(--a) var(--b));
+  clip-path: polygon(var(--a) var(--a), var(--b) var(--a), var(--b) var(--b), var(--a) var(--b));
+  height: 100%;
+  position: absolute;
+  transform: translate(0, -50%) rotate(90deg) rotate(calc(var(--degrees) * 1deg));
+  transform-origin: 50% 100%;
+  width: 100%;
+  z-index: calc(1 + var(--over50));
+
+  &::after,
+  &::before {
+    background: var(--bg, #7b8a95);
+    content: '';
+    height: 100%;
+    position: absolute;
+    width: 100%;
+  }
+
+  &::before {
+    --degrees: calc((var(--pieChartValue, 0) / 100) * 360);
+
+    transform: translate(0, 100%) rotate(calc(var(--degrees) * 1deg));
+    transform-origin: 50% 0%;
+  }
+
+  &::after {
+    opacity: var(--over50, 0);
+  }
+`
+
+export const VideosNumberContainer = styled.div`
+  display: flex;
+`
