77872529a641290af75d0461e73e646b37af8d6a
Rafał Pawłow <pawlow.rafal@gmail.com>
2021-09-03 09:11:56 +0200

    Center dialogs vertically (#1314)

--- src/components/Dialogs/BaseDialog/BaseDialog.style.ts
+++ src/components/Dialogs/BaseDialog/BaseDialog.style.ts
@@ -24,21 +24,17 @@ export const StyledContainer = styled.div`
   z-index: ${zIndex.globalOverlay};
   position: fixed;
   left: 50%;
-  transform: translateX(-50%);
+  top: 50%;
+  transform: translate(-50%, -50%);
   width: 90%;
   max-width: 440px;
   min-height: 150px;
   max-height: 75vh;
   overflow: auto;
-  margin: ${sizes(16)} auto;
   color: ${colors.white};
   background-color: ${colors.gray[700]};
   padding: var(--dialog-padding);
   box-shadow: 0 8px 8px rgba(0, 0, 0, 0.12), 0 24px 40px rgba(0, 0, 0, 0.16);
-
-  ${media.md} {
-    margin: ${sizes(32)} auto;
-  }
 `
 
 export const StyledExitButton = styled(IconButton)`
