dd7ac39d411215e5ff9f34e8126a1d75dc3539b2
drillprop <drylbartosz@gmail.com>
2021-09-13 08:26:54 +0200

    generate new chevrons

--- src/shared/icons/GlyphChevronUp.tsx
+++ src/shared/icons/GlyphChevronUp.tsx
@@ -3,6 +3,11 @@ import * as React from 'react'
 
 export const SvgGlyphChevronUp = (props: React.SVGProps<SVGSVGElement>) => (
   <svg width={16} height={16} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M2 11l6-6 6 6" stroke="#F4F6F8" strokeWidth={2} />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8 6.414l-5.293 5.293-1.414-1.414L8 3.586l6.707 6.707-1.414 1.414L8 6.414z"
+      fill="#F4F6F8"
+    />
   </svg>
 )
