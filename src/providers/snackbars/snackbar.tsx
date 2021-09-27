3020bacc477bdc3f36a78b9ad5c0990b59d90cba
Rafał Pawłow <pawlow.rafal@gmail.com>
2021-09-27 10:37:33 +0200

    Update upload snackbars (#1373)
    * Update upload snackbard
    
    * Fix displaying many snackbars
    
    * PR FIX
    
    * update SnackbarActionButton
    
    * PR FIX 2
    
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>

--- src/providers/snackbars/snackbar.tsx
+++ src/providers/snackbars/snackbar.tsx
@@ -3,18 +3,17 @@ import React, { ReactNode, useEffect } from 'react'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import { Snackbar } from '@/shared/components/Snackbar'
-import { SvgAlertError, SvgAlertInfo, SvgAlertSuccess, SvgAlertWarning } from '@/shared/icons'
+import { SvgAlertError, SvgAlertInfo, SvgAlertSuccess, SvgAlertWarning, SvgGlyphUpload } from '@/shared/icons'
 import { sizes, transitions, zIndex } from '@/shared/theme'
 
-import { useSnackbarStore } from './store'
-
-type SnackbarIconType = 'success' | 'error' | 'info' | 'warning'
+import { SnackbarIconType, useSnackbarStore } from './store'
 
 const ICON_TYPE_TO_ICON: Record<SnackbarIconType, ReactNode> = {
   info: <SvgAlertInfo />,
   success: <SvgAlertSuccess />,
   error: <SvgAlertError />,
   warning: <SvgAlertWarning />,
+  uploading: <SvgGlyphUpload />,
 }
 
 const SNACKBARS_LIMIT = 3
