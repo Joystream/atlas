afcc13a563643e97ea0eab8e21e1dcbe9f1a4027
drillprop <drylbartosz@gmail.com>
2021-09-28 10:07:31 +0200

    fix overflow issue

--- src/views/studio/EditVideoSheet/EditVideoForm/EditVideoForm.style.ts
+++ src/views/studio/EditVideoSheet/EditVideoForm/EditVideoForm.style.ts
@@ -86,17 +86,9 @@ export const MoreSettingsDescription = styled(Text)<MoreSettingsDescriptionProps
 export const MoreSettingsSection = styled.div<MoreSettingsSectionProps>`
   visibility: ${({ expanded }) => (expanded ? 'visible' : 'hidden')};
   max-height: ${({ expanded }) => (expanded ? '1200px' : '0px')};
-  overflow: hidden;
-  transition: max-height 300ms ease-out;
-
-  svg {
-    /*
-    Workaround. 
-    For some reason chevron on Select element hides with little delay. This is causing layout shift. 
-    To fix that let's just set 'display: none' on all svgs when section is not expanded.
-    */
-    display: ${({ expanded }) => (expanded ? 'unset' : 'none')};
-  }
+  overflow: ${({ expanded }) => (expanded ? 'unset' : 'hidden')};
+  opacity: ${({ expanded }) => (expanded ? 1 : 0)};
+  transition: opacity 150ms ease-out, max-height 150ms ease-out 150ms;
 `
 
 export const DeleteVideoButton = styled(Button)`
