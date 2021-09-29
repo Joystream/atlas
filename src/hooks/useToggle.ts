284768a5f3ddb51ad7dd0102734fa96e13528fdf
eldiegod <cardenasrdiego@gmail.com>
2021-09-29 17:06:58 +0200

    cr

--- src/hooks/useToggle.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from 'react'
-
-export const useToggle = (initialState = false) => {
-  const [state, setState] = useState(initialState)
-
-  const toggle = useCallback(() => setState((state) => !state), [])
-
-  return [state, toggle] as const
-}
