d976423267e1505ac0b593548ea7b40d4a445d3e
Bartosz Dryl <drylbartosz@gmail.com>
2021-09-30 10:08:56 +0200

    Create BackgroundVideoPlayer component (#1430)
    * create BackgroundVideoPlayer
    
    * add autoplay warning, set default value to playsinline
    
    * cr fixes
    
    * remove isInBackground prop

--- src/components/VideoHero/VideoHero.tsx
+++ src/components/VideoHero/VideoHero.tsx
@@ -5,7 +5,6 @@ import { absoluteRoutes } from '@/config/routes'
 import { AssetType, useAsset } from '@/providers/assets'
 import { Button } from '@/shared/components/Button'
 import { GridItem, LayoutGrid } from '@/shared/components/LayoutGrid'
-import { VideoPlayer } from '@/shared/components/VideoPlayer'
 import { SvgActionPlay } from '@/shared/icons/ActionPlay'
 import { SvgActionSoundOff } from '@/shared/icons/ActionSoundOff'
 import { SvgActionSoundOn } from '@/shared/icons/ActionSoundOn'
@@ -26,6 +25,8 @@ import {
   TitleSkeletonLoader,
 } from './VideoHero.style'
 import { useVideoHero } from './VideoHeroData'
+
+import { BackgroundVideoPlayer } from '../BackgroundVideoPlayer'
 
 const VIDEO_PLAYBACK_DELAY = 1250
 
@@ -55,17 +56,14 @@ export const VideoHero: React.FC = () => {
         <Media>
           <PlayerContainer>
             {coverVideo && (
-              <VideoPlayer
-                videoStyle={{ objectFit: 'cover' }}
-                fluid
-                isInBackground
+              <BackgroundVideoPlayer
                 muted={soundMuted}
                 playing={videoPlaying}
-                posterUrl={thumbnailPhotoUrl}
-                onDataLoaded={handlePlaybackDataLoaded}
+                poster={thumbnailPhotoUrl || ''}
+                onLoadedData={handlePlaybackDataLoaded}
                 onPlay={() => setVideoPlaying(true)}
                 onPause={() => setVideoPlaying(false)}
-                onEnd={() => setVideoPlaying(false)}
+                onEnded={() => setVideoPlaying(false)}
                 src={coverVideo?.coverCutMediaUrl}
               />
             )}
