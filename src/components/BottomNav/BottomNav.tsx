b396fb82559dc61e98596727d3a487fa3c8875e4
Rafal Pawlow <pawlow.rafal@gmail.com>
2021-10-01 16:42:32 +0200

    PR FIX 2

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -7,20 +7,31 @@ import { transitions } from '@/shared/theme'
 
 import { Container, NavLink, NavTitle } from './BottomNav.style'
 
+const Link: React.FC<typeof viewerNavItems[number]> = ({ to, icon, name }) => {
+  const match = useMatch(to)
+  return (
+    <NavLink to={to} active={match}>
+      {icon}
+      <NavTitle variant="button3">{name}</NavTitle>
+    </NavLink>
+  )
+}
+
+const OPENING_MARGIN = 24
+
 export const BottomNav = () => {
   const [open, setOpen] = useState(true)
-  const match = useMatch
   const pageYOffsetRef = useRef<number | null>(null)
 
   const toggleNavbar = () => {
     const scrollRange = pageYOffsetRef.current !== null ? window.scrollY - pageYOffsetRef.current : 0
-    if (scrollRange >= 64) {
+    if (scrollRange >= OPENING_MARGIN) {
       setOpen(false)
     }
-    if (scrollRange <= -24) {
+    if (scrollRange <= -OPENING_MARGIN) {
       setOpen(true)
     }
-    if (Math.abs(scrollRange) >= 64 || pageYOffsetRef.current === null) {
+    if (Math.abs(scrollRange) >= OPENING_MARGIN || pageYOffsetRef.current === null) {
       pageYOffsetRef.current = window.scrollY
     }
   }
@@ -43,10 +54,7 @@ export const BottomNav = () => {
     >
       <Container>
         {viewerNavItems.map((item) => (
-          <NavLink to={item.to} key={`bottomLink-${item.to}`} active={match(item.to)}>
-            {item.icon}
-            <NavTitle variant="button3">{item.name}</NavTitle>
-          </NavLink>
+          <Link {...item} key={`bottomLink-${item.to}`} />
         ))}
       </Container>
     </CSSTransition>
