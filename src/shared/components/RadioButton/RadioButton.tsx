1b8b1af058e0e7ef4e678066a2dee277c016b606
drillprop <drylbartosz@gmail.com>
2021-10-06 14:09:44 +0200

    refactor RadioButton and RadioInput

--- src/shared/components/RadioButton/RadioButton.tsx
+++ src/shared/components/RadioButton/RadioButton.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 
-import { RadioButtonLabel, RadioButtonStyleProps, StyledHelperText } from './RadioButton.style'
-
+import { RadioAndCheckboxBase } from '../RadioAndCheckboxBase'
 import { RadioInput } from '../RadioInput'
-import { Text } from '../Text'
 
 type RadioButtonProps = Partial<{
   selectedValue: string | number
   label: string
   helperText?: string
   className?: string
+  error?: boolean
 }> &
-  RadioButtonStyleProps &
   React.InputHTMLAttributes<HTMLInputElement>
 
 export const RadioButton = React.forwardRef<HTMLInputElement, RadioButtonProps>(
   ({ value, selectedValue, label, helperText, className, disabled, error, onChange, ...props }, ref) => {
     return (
-      <RadioButtonLabel disabled={disabled} className={className}>
+      <RadioAndCheckboxBase
+        disabled={disabled}
+        className={className}
+        label={label}
+        error={error}
+        helperText={helperText}
+      >
         <RadioInput
           {...props}
           ref={ref}
@@ -27,9 +31,7 @@ export const RadioButton = React.forwardRef<HTMLInputElement, RadioButtonProps>(
           value={value}
           selectedValue={selectedValue}
         />
-        {label && <Text variant="body1">{label}</Text>}
-        {helperText && <StyledHelperText helperText={helperText} error={error} />}
-      </RadioButtonLabel>
+      </RadioAndCheckboxBase>
     )
   }
 )
