240be9e538f81e31ece324417254b2065f28b0d1
eldiegod <cardenasrdiego@gmail.com>
2021-10-18 20:13:52 +0200

    Category page layout
    fixes:  #1359

---
+++ src/views/viewer/CategoryView/CategoryView.style.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+
+import { LayoutGrid } from '@/shared/components/LayoutGrid'
+import { colors, sizes } from '@/shared/theme'
+
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: ${sizes(21)} 0 ${sizes(0)} 0;
+  padding-bottom: ${sizes(6)};
+  border-bottom: 1px solid ${colors.gray[700]};
+`
+
+export const CategoriesContainer = styled(LayoutGrid)`
+  margin-top: ${sizes(12)};
+`
