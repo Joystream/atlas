1d6e1b0e63fff6df49f35bae0f7766e12d0866f9
Diego Cardenas <cardenasrdiego@gmail.com>
2021-10-19 12:37:06 +0200

    Category infinite grid (#1490)
    * Mobile filters
    
    * videoWhereInput
    
    * Category infinite grid
    
    * Cr
    
    * z-indexes
    
    * remove null helperText

--- src/views/viewer/VideoView/VideoView.tsx
+++ src/views/viewer/VideoView/VideoView.tsx
@@ -195,7 +195,7 @@ export const VideoView: React.FC = () => {
             title={`More from ${video?.channel.title}`}
             titleLoader
             ready={!loading}
-            channelId={channelId}
+            videoWhereInput={{ channelId_eq: channelId }}
             showChannel={false}
             currentlyWatchedVideoId={video?.id}
           />
