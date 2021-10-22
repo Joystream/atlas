79626a57357e46440263bb7f5e895030615662cc
Klaudiusz Dembler <dev@kdembler.com>
2021-10-22 14:43:59 +0200

    introduce featured content fetched from Orion (#1509)

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -12,8 +12,6 @@ export const SENTRY_DSN = readEnv('SENTRY_DSN', false)
 export const WEB3_APP_NAME = 'Joystream Atlas'
 export const STORAGE_URL_PATH = 'asset/v0'
 
-export const VIDEO_HERO_DATA_URL = 'https://eu-central-1.linodeobjects.com/atlas-hero/hero-info.json'
-
 export const JOYSTREAM_DISCORD_URL = 'https://discord.gg/DE9UN3YpRP'
 export const JOYSTREAM_STORAGE_DISCORD_URL = 'https://discord.gg/WUb7XwW72a'
 
