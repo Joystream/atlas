5a00e8fe2bd59766dd9988d7dbb4a3702ff99c37
Klaudiusz Dembler <dev@kdembler.com>
2021-10-27 14:03:06 +0200

    add comments with new colors references

--- src/shared/theme/colors.ts
+++ src/shared/theme/colors.ts
@@ -3,69 +3,69 @@ export default {
   black: '#000000',
   transparent: 'transparent',
   gray: {
-    900: '#0B0C0F',
-    800: '#181C20',
-    700: '#272D33',
-    600: '#343D44',
-    500: '#424E57',
-    400: '#52616B',
-    300: '#7B8A95',
-    200: '#B5C1C9',
-    100: '#DAE2EB',
-    50: '#F4F6F8',
+    900: '#0B0C0F', // core.neutral.900.default
+    800: '#181C20', // core.neutral.800.default
+    700: '#272D33', // core.neutral.700.default
+    600: '#343D44', // core.neutral.600.default
+    500: '#424E57', // core.neutral.500.default
+    400: '#52616B', // core.neutral.400.default
+    300: '#7B8A95', // core.neutral.300.default
+    200: '#B5C1C9', // core.neutral.200.default
+    100: '#DAE2EB', // core.neutral.100.default
+    50: '#F4F6F8', // core.neutral.50.default
   },
   blue: {
-    900: '#08071E',
-    800: '#1B186C',
-    700: '#2823A0',
-    600: '#342ECF',
-    500: '#4038FF',
-    400: '#5A58FF',
-    300: '#7174FF',
-    200: '#8890FF',
-    100: '#9FACFF',
-    50: '#BECAFF',
+    900: '#08071E', // core.blue.900.default
+    800: '#1B186C', // core.blue.800.default
+    700: '#2823A0', // core.blue.700.default
+    600: '#342ECF', // core.blue.600.default
+    500: '#4038FF', // core.blue.500.default
+    400: '#5A58FF', // core.blue.400.default
+    300: '#7174FF', // core.blue.300.default
+    200: '#8890FF', // core.blue.200.default
+    100: '#9FACFF', // core.blue.100.default
+    50: '#BECAFF', // core.blue.50.default
   },
   secondary: {
     alert: {
-      300: '#6D1E18',
-      200: '#8D261F',
-      100: '#CF382D',
-      50: '#FF695F',
-      5: '#FF948D',
+      300: '#6D1E18', // core.red.700
+      200: '#8D261F', // core.red.600
+      100: '#CF382D', // core.red.400
+      50: '#FF695F', // core.red.200
+      5: '#FF948D', // core.red.100
     },
     warning: {
-      300: '#917901',
-      200: '#AC9002',
-      100: '#CAA802',
-      50: '#ECC502',
-      5: '#FEE879',
+      300: '#917901', // core.yellow.400
+      200: '#AC9002', // core.yellow.300
+      100: '#CAA802', // core.yellow.200
+      50: '#ECC502', // core.yellow.100
+      5: '#FEE879', // core.yellow.50
     },
     success: {
-      300: '#0A823C',
-      200: '#0C9846',
-      100: '#0EB152',
-      50: '#12CC60',
-      5: '#75E1A2',
+      300: '#0A823C', // core.green.400
+      200: '#0C9846', // core.green.300
+      100: '#0EB152', // core.green.200
+      50: '#12CC60', // core.green.100
+      5: '#75E1A2', // core.green.50
     },
   },
   transparentWhite: {
-    6: 'rgba(183, 200, 250, 0.06)',
-    32: 'rgba(220, 225, 229, 0.42)',
+    6: 'rgba(183, 200, 250, 0.06)', // core.neutral.500.darken
+    32: 'rgba(220, 225, 229, 0.42)', // core.neutral.400.lighten
   },
   transparentBlack: {
-    24: 'rgba(8, 47, 74, 0.3)',
-    32: 'rgba(0, 0, 0, 0.32)',
-    54: 'rgba(16, 18, 20, 0.75)',
-    86: 'rgba(7, 8, 8, 0.85)',
+    24: 'rgba(8, 47, 74, 0.3)', // core.neutral.200.darken
+    32: 'rgba(0, 0, 0, 0.32)', // core.neutral.200.darken
+    54: 'rgba(16, 18, 20, 0.75)', // core.neutral.500.darken
+    86: 'rgba(7, 8, 8, 0.85)', // core.neutral.700.darken
   },
   transparentGray: {
-    54: 'rgba(10, 28, 41, 0.52)',
+    54: 'rgba(10, 28, 41, 0.52)', // core.neutral.300.darken
   },
   transparentPrimary: {
-    6: 'rgba(180, 187, 255, 0.06)',
-    10: 'rgba(187, 217, 246, 0.13)',
-    12: 'rgba(180, 187, 255, 0.12)',
-    18: 'rgba(194, 224, 255, 0.2)',
+    6: 'rgba(180, 187, 255, 0.06)', // core.neutral.900.lighten
+    10: 'rgba(187, 217, 246, 0.13)', // core.neutral.800.lighten
+    12: 'rgba(180, 187, 255, 0.12)', // core.neutral.100.darken
+    18: 'rgba(194, 224, 255, 0.2)', // core.neutral.700.lighten
   },
 }
