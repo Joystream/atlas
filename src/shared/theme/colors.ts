1011f4f1ce57b6c7efa0dbd29fbffd5c2b386188
Bartosz Dryl <drylbartosz@gmail.com>
2021-11-05 11:41:15 +0100

    Apply generated variables to color theme (#1548)
    * apply variables to theme
    
    * update colors in skeleton loader
    
    * remove comments
    
    * remove another comment

--- src/shared/theme/colors.ts
+++ src/shared/theme/colors.ts
@@ -1,71 +1,73 @@
+import { cVar } from '@/styles/generated/variables'
+
 export default {
   white: '#FFFFFF',
   black: '#000000',
   transparent: 'transparent',
   gray: {
-    900: '#0B0C0F', // core.neutral.900.default
-    800: '#181C20', // core.neutral.800.default
-    700: '#272D33', // core.neutral.700.default
-    600: '#343D44', // core.neutral.600.default
-    500: '#424E57', // core.neutral.500.default
-    400: '#52616B', // core.neutral.400.default
-    300: '#7B8A95', // core.neutral.300.default
-    200: '#B5C1C9', // core.neutral.200.default
-    100: '#DAE2EB', // core.neutral.100.default
-    50: '#F4F6F8', // core.neutral.50.default
+    900: cVar('colorCoreNeutral900'),
+    800: cVar('colorCoreNeutral800'),
+    700: cVar('colorCoreNeutral700'),
+    600: cVar('colorCoreNeutral600'),
+    500: cVar('colorCoreNeutral500'),
+    400: cVar('colorCoreNeutral400'),
+    300: cVar('colorCoreNeutral300'),
+    200: cVar('colorCoreNeutral200'),
+    100: cVar('colorCoreNeutral100'),
+    50: cVar('colorCoreNeutral50'),
   },
   blue: {
-    900: '#08071E', // core.blue.900.default
-    800: '#1B186C', // core.blue.800.default
-    700: '#2823A0', // core.blue.700.default
-    600: '#342ECF', // core.blue.600.default
-    500: '#4038FF', // core.blue.500.default
-    400: '#5A58FF', // core.blue.400.default
-    300: '#7174FF', // core.blue.300.default
-    200: '#8890FF', // core.blue.200.default
-    100: '#9FACFF', // core.blue.100.default
-    50: '#BECAFF', // core.blue.50.default
+    900: cVar('colorCoreBlue900'),
+    800: cVar('colorCoreBlue800'),
+    700: cVar('colorCoreBlue700'),
+    600: cVar('colorCoreBlue600'),
+    500: cVar('colorCoreBlue500'),
+    400: cVar('colorCoreBlue400'),
+    300: cVar('colorCoreBlue300'),
+    200: cVar('colorCoreBlue200'),
+    100: cVar('colorCoreBlue100'),
+    50: cVar('colorCoreBlue50'),
   },
   secondary: {
     alert: {
-      300: '#6D1E18', // core.red.700
-      200: '#8D261F', // core.red.600
-      100: '#CF382D', // core.red.400
-      50: '#FF695F', // core.red.200
-      5: '#FF948D', // core.red.100
+      300: cVar('colorCoreRed700'),
+      200: cVar('colorCoreRed600'),
+      100: cVar('colorCoreRed400'),
+      50: cVar('colorCoreRed200'),
+      5: cVar('colorCoreRed100'),
     },
     warning: {
-      300: '#917901', // core.yellow.400
-      200: '#AC9002', // core.yellow.300
-      100: '#CAA802', // core.yellow.200
-      50: '#ECC502', // core.yellow.100
-      5: '#FEE879', // core.yellow.50
+      300: cVar('colorCoreYellow400'),
+      200: cVar('colorCoreYellow300'),
+      100: cVar('colorCoreYellow200'),
+      50: cVar('colorCoreYellow100'),
+      5: cVar('colorCoreYellow50'),
     },
     success: {
-      300: '#0A823C', // core.green.400
-      200: '#0C9846', // core.green.300
-      100: '#0EB152', // core.green.200
-      50: '#12CC60', // core.green.100
-      5: '#75E1A2', // core.green.50
+      300: cVar('colorCoreGreen400'),
+      200: cVar('colorCoreGreen300'),
+      100: cVar('colorCoreGreen200'),
+      50: cVar('colorCoreGreen100'),
+      5: cVar('colorCoreGreen50'),
     },
   },
   transparentWhite: {
-    6: 'rgba(183, 200, 250, 0.06)', // core.neutral.500.darken
-    32: 'rgba(220, 225, 229, 0.42)', // core.neutral.400.lighten
+    6: cVar('colorCoreNeutral500Darken'),
+    32: cVar('colorCoreNeutral400Lighten'),
   },
   transparentBlack: {
-    24: 'rgba(8, 47, 74, 0.3)', // core.neutral.200.darken
-    32: 'rgba(0, 0, 0, 0.32)', // core.neutral.200.darken
-    54: 'rgba(16, 18, 20, 0.75)', // core.neutral.500.darken
-    86: 'rgba(7, 8, 8, 0.85)', // core.neutral.700.darken
+    24: cVar('colorCoreNeutral200Darken'),
+    32: 'rgba(0, 0, 0, 0.32)',
+    54: cVar('colorCoreNeutral500Darken'),
+    86: cVar('colorCoreNeutral700Darken'),
   },
   transparentGray: {
-    54: 'rgba(10, 28, 41, 0.52)', // core.neutral.300.darken
+    54: cVar('colorCoreNeutral300Darken'),
   },
   transparentPrimary: {
-    6: 'rgba(180, 187, 255, 0.06)', // core.neutral.900.lighten
-    10: 'rgba(187, 217, 246, 0.13)', // core.neutral.800.lighten
-    12: 'rgba(180, 187, 255, 0.12)', // core.neutral.100.darken
-    18: 'rgba(194, 224, 255, 0.2)', // core.neutral.700.lighten
+    6: cVar('colorCoreNeutral900Lighten'),
+    10: cVar('colorCoreNeutral800Lighten'),
+    12: cVar('colorCoreNeutral100Darken'),
+    18: cVar('colorCoreNeutral700Lighten'),
   },
 }
