aa28ca9bcc6007c3ee795fc025606e03d59d6f8c
Klaudiusz Dembler <dev@kdembler.com>
2021-11-17 11:28:42 +0100

    add livesession support

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ApolloProvider } from '@apollo/client'
+import ls from '@livesession/sdk'
 import React, { useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import useHotjar from 'react-use-hotjar'
@@ -24,7 +25,9 @@ export const App = () => {
   useEffect(() => {
     if (BUILD_ENV === 'production') {
       // eslint-disable-next-line no-console
-      initHotjar(parseInt(readEnv('HOTJAR_KEY')), 6, false, console.info)
+      initHotjar(parseInt(readEnv('HOTJAR_ID')), 6, false, console.info)
+      ls.init(readEnv('LIVESESSION_ID'))
+      ls.newPageView()
     }
   }, [initHotjar])
 
