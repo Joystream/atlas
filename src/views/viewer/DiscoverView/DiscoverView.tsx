5eaddd17298b5b0da6a2182a3ab2cb1a8744dfe8
eldiegod <cardenasrdiego@gmail.com>
2021-11-22 22:01:14 +0100

    Discovery community content fixes
    fixes: #1616 and some other details found along the way

--- src/views/viewer/DiscoverView/DiscoverView.tsx
+++ src/views/viewer/DiscoverView/DiscoverView.tsx
@@ -1,3 +1,4 @@
+import { sampleSize } from 'lodash-es'
 import React from 'react'
 
 import { useCategoriesFeaturedVideos } from '@/api/featured/categoriesFeaturedVideos'
@@ -25,22 +26,28 @@ export const DiscoverView: React.FC = () => {
   }))
 
   const categoriesFeaturedVideos = useCategoriesFeaturedVideos()
-  const featuredVideoCategoryCardsData = categories
-    ?.map((category) => {
-      const video =
-        categoriesFeaturedVideos?.[category.id]?.find((video) => !!video.videoCutUrl) ??
-        categoriesFeaturedVideos?.[category.id]?.[0] ??
-        undefined
-      return video === undefined
-        ? undefined
-        : {
-            videoTitle: video?.title ?? '',
-            videoUrl: video?.videoCutUrl ?? '',
-            ...mappedVideoCategories?.find((cat) => cat.id === category.id),
-          }
-    })
-    .filter((cat) => !!cat) ?? [null, null, null]
+  const featuredVideoCategoryCardsData = React.useMemo(() => {
+    const _featuredVideoCategoryCardsData = sampleSize(
+      categories
+        ?.map((category) => {
+          const video =
+            categoriesFeaturedVideos?.[category.id]?.find((video) => !!video.videoCutUrl) ??
+            categoriesFeaturedVideos?.[category.id]?.[0] ??
+            undefined
+          return video === undefined
+            ? undefined
+            : {
+                videoTitle: video?.title ?? '',
+                videoUrl: video?.videoCutUrl ?? '',
+                ...mappedVideoCategories?.find((cat) => cat.id === category.id),
+              }
+        })
+        .filter((cat) => !!cat),
+      3
+    )
 
+    return _featuredVideoCategoryCardsData.length > 0 ? _featuredVideoCategoryCardsData : [null, null, null]
+  }, [categories, categoriesFeaturedVideos, mappedVideoCategories])
   const { videoCount, error } = useVideoCount(
     {},
     {
@@ -58,7 +65,7 @@ export const DiscoverView: React.FC = () => {
       <Text variant="h2">Discover</Text>
       <FeaturedCategoriesContainer>
         {featuredVideoCategoryCardsData?.map((category, i) => (
-          <GridItem key={i} colSpan={{ base: 12, sm: i === 0 ? 12 : 6, xl: 4 }}>
+          <GridItem key={category?.id ?? i} colSpan={{ base: 12, sm: i === 0 ? 12 : 6, xl: 4 }}>
             <FeaturedVideoCategoryCard
               variant={isMdBreakpoint ? 'default' : 'compact'}
               title={category?.name ?? ''}
@@ -66,7 +73,7 @@ export const DiscoverView: React.FC = () => {
               videoUrl={category?.videoUrl ?? ''}
               color={category?.color ?? 'white'}
               icon={category?.icon}
-              id={category?.id ?? i.toString()}
+              id={category?.id}
             />
           </GridItem>
         ))}
@@ -75,17 +82,18 @@ export const DiscoverView: React.FC = () => {
         <Text variant="h4">All categories</Text>
       </BorderTextContainer>
       <CategoriesContainer>
-        {mappedVideoCategories?.map((category, i) => (
+        {(mappedVideoCategories ?? new Array(15).fill(null))?.map((category, i) => (
           <GridItem key={i} colSpan={{ base: 6, lg: 4, xl: 3 }}>
             <VideoCategoryCard
-              title={category.name ?? ''}
+              isLoading={category === null}
+              title={category?.name ?? ''}
               coverImg={category?.coverImg ?? ''}
-              categoryId={category.id}
-              color={category.color ?? 'white'}
-              icon={category.icon}
+              categoryId={category?.id}
+              color={category?.color ?? 'white'}
+              icon={category?.icon}
               videosTotalCount={videoCount}
               variant={isMdBreakpoint ? 'default' : 'compact'}
-              id={category.id}
+              id={category?.id}
             />
           </GridItem>
         ))}
