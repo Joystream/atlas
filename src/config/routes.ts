b7a8d7b7d0b29cbaaa94ebd069d2cf4f4c381cd1
Diego Cardenas <cardenasrdiego@gmail.com>
2021-11-24 18:53:51 +0100

    Embedded view (#1606)
    * Embedded view
    
    Attempt at a workaround for embeddable video
    
    fixes: #1591
    
    * fix overflow
    
    * End state for embedded videos
    
    * overflow and embeded prop

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,6 +1,7 @@
 export const BASE_PATHS = {
   // must be empty so we don't get double '/' after joining paths
   viewer: '',
+  embedded: '/embedded',
   legal: '/legal',
   studio: '/studio',
   playground: '/playground',
@@ -16,6 +17,9 @@ const withQueryParameters = (basePath: string, query: Record<string, string> = {
 }
 
 export const relativeRoutes = {
+  embedded: {
+    video: (id = ':id') => `video/${id}`,
+  },
   viewer: {
     index: () => '',
     new: () => 'new',
