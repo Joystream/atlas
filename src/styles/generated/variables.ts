20b56013857983092b854dde7be377db639a5d63
Bartosz Dryl <drylbartosz@gmail.com>
2021-11-30 16:11:38 +0100

    Add effects tokens (#1679)
    * bring back parser, remove referencedValueTransform, add effectsTransform
    
    * generate new variables, small refactor
    
    * add inset when divider
    
    * update tokens playground

--- src/styles/generated/variables.ts
+++ src/styles/generated/variables.ts
@@ -112,6 +112,16 @@ export const variables = css`
     --color-background-success-muted: var(--color-core-green-600);
     --color-background-success: var(--color-core-green-500);
     --color-background-success-strong: var(--color-core-green-400);
+    --effect-dividers-top: inset 0 1px 0 0 var(--color-border-muted-alpha);
+    --effect-dividers-bottom: inset 0 -1px 0 0 var(--color-border-muted-alpha);
+    --effect-dividers-left: inset 1px 0 0 0 var(--color-border-muted-alpha);
+    --effect-dividers-right: inset -1px 0 0 0 var(--color-border-muted-alpha);
+    --effect-elevation-8-layer1: 0 8px 16px 0 #0000001f;
+    --effect-elevation-8-layer2: 0 4px 4px 0 #0000001a;
+    --effect-elevation-16-layer1: 0 16px 32px 0 #00000029;
+    --effect-elevation-16-layer2: 0 4px 8px 0 #0000001a;
+    --effect-elevation-24-layer1: 0 24px 40px 0 #00000029;
+    --effect-elevation-24-layer2: 0 8px 8px 0 #0000001f;
     --typography-fonts-primary: IBM Plex Sans, -apple-system, BlinkMacSystemFont, Avenir Next, Avenir, Segoe UI,
       Helvetica Neue, Helvetica, Ubuntu, Roboto, Noto, Arial, sans-serif;
     --typography-fonts-secondary: Inter, -apple-system, BlinkMacSystemFont, Avenir Next, Avenir, Segoe UI,
@@ -368,6 +378,16 @@ export const theme = {
   colorBackgroundSuccessMuted: 'var(--color-background-success-muted)',
   colorBackgroundSuccess: 'var(--color-background-success)',
   colorBackgroundSuccessStrong: 'var(--color-background-success-strong)',
+  effectDividersTop: 'var(--effect-dividers-top)',
+  effectDividersBottom: 'var(--effect-dividers-bottom)',
+  effectDividersLeft: 'var(--effect-dividers-left)',
+  effectDividersRight: 'var(--effect-dividers-right)',
+  effectElevation8Layer1: 'var(--effect-elevation-8-layer1)',
+  effectElevation8Layer2: 'var(--effect-elevation-8-layer2)',
+  effectElevation16Layer1: 'var(--effect-elevation-16-layer1)',
+  effectElevation16Layer2: 'var(--effect-elevation-16-layer2)',
+  effectElevation24Layer1: 'var(--effect-elevation-24-layer1)',
+  effectElevation24Layer2: 'var(--effect-elevation-24-layer2)',
   typographyFontsPrimary: 'var(--typography-fonts-primary)',
   typographyFontsSecondary: 'var(--typography-fonts-secondary)',
   typographyFontSizes1: 'var(--typography-font-sizes-1)',
