11486bf839da19e15d63c93ce825ede5a38649cf
eldiegod <cardenasrdiego@gmail.com>
2021-12-08 13:40:46 +0100

    Icon Wrapper component
    Fixes: #1656
    
    https://www.figma.com/file/Pf31tuYpozYmpq163U2ho8/Web-Components?node-id=3201%3A109979

---
+++ src/components/_icons/IconWrapper/IconWrapper.tsx
@@ -0,0 +1,42 @@
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import React from 'react'
+
+import { cVar, sizes } from '@/styles'
+
+export type IconWrapperProps = {
+  size?: 'small' | 'medium' | 'large'
+  icon: React.ReactNode
+}
+
+export const IconWrapper: React.FC<IconWrapperProps> = ({ size = 'medium', icon }) => {
+  return <IconContainer size={size}>{icon}</IconContainer>
+}
+
+const getIconContainerPaddingStyles = ({ size }: IconContainerProps) => {
+  switch (size) {
+    case 'large':
+      return css`
+        padding: ${sizes(3)};
+      `
+    case 'medium':
+      return css`
+        padding: ${sizes(2)};
+      `
+    case 'small':
+      return css`
+        padding: ${sizes(1)};
+      `
+  }
+}
+type IconContainerProps = Omit<IconWrapperProps, 'icon'>
+const IconContainer = styled.div<IconContainerProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: min-content;
+  border-radius: 9999px;
+  background: ${cVar('colorBackgroundAlpha')};
+
+  ${getIconContainerPaddingStyles}
+`
