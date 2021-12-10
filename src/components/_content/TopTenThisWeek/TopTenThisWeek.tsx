3c3af4dc2ef03b4b0d2462dc8ccbcfe1defc902a
drillprop <drylbartosz@gmail.com>
2021-12-10 15:03:32 +0100

    fix top ten this week

--- src/components/_content/TopTenThisWeek/TopTenThisWeek.tsx
+++ src/components/_content/TopTenThisWeek/TopTenThisWeek.tsx
@@ -6,9 +6,11 @@ import { VideoGallery } from '@/components/_video/VideoGallery'
 export const TopTenThisWeek = () => {
   const { videos, loading } = useMostViewedVideos({ limit: 10, timePeriodDays: 7 })
 
+  const sortedTopTenVideos = videos && [...videos]?.sort((a, b) => (b.views || 0) - (a.views || 0))
+
   return (
     <section>
-      <VideoGallery title="Top 10 this week" videos={videos} loading={loading} hasRanking />
+      <VideoGallery title="Top 10 this week" videos={sortedTopTenVideos} loading={loading} hasRanking />
     </section>
   )
 }
