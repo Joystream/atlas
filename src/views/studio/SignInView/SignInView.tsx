887dd874e5ced8a01d688f76acbf776c64bc62b6
drillprop <drylbartosz@gmail.com>
2021-12-22 12:45:26 +0100

    handle sign in in SignInView

--- src/views/studio/SignInView/SignInView.tsx
+++ src/views/studio/SignInView/SignInView.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
 
 import { Text } from '@/components/Text'
-import { CreateMemberModal } from '@/components/_auth/CreateMemberModal'
-import { SignInStepsStepper } from '@/components/_auth/SignInSteps'
 import { Button } from '@/components/_buttons/Button'
 import { SvgActionChannel, SvgActionChevronL, SvgActionInformative } from '@/components/_icons'
 import { SvgJoystreamLogoFull } from '@/components/_illustrations'
+import { Loader } from '@/components/_loaders/Loader'
+import { Modal } from '@/components/_overlays/Modal'
 import { absoluteRoutes } from '@/config/routes'
+import { useUser } from '@/providers/user'
 
 import {
   BackLink,
@@ -28,34 +30,50 @@ export type Membership = {
 }
 
 export const SignInView: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const { activeAccountId, extensionConnected, activeMemberId, signIn } = useUser()
+  const isLoggedIn = !!activeAccountId && !!activeMemberId && !!extensionConnected
+  const navigate = useNavigate()
+
+  const handleSignIn = async () => {
+    setIsLoading(true)
+    await signIn()
+    setIsLoading(false)
+    if (!isLoggedIn) {
+      navigate(`?step=1`)
+    }
+  }
   return (
-    <StyledContainer>
-      <Header>
-        <LogoContainer>
-          <SvgJoystreamLogoFull />
-        </LogoContainer>
-        <StyledHero variant="h900">Welcome to Joystream Studio</StyledHero>
-        <SubTitle variant="t300">
-          Start your journey as a Video Publisher. Publish and manage your channel and video content.
-        </SubTitle>
-        <ButtonGroup>
-          <SignInButton icon={<SvgActionChannel />} size="large" to={absoluteRoutes.studio.signIn({ step: '1' })}>
-            Sign in
-          </SignInButton>
-          <Button variant="secondary" icon={<SvgActionInformative />} size="large" to="https://www.joystream.org/">
-            How it works?
-          </Button>
-        </ButtonGroup>
-        <BackLink to={absoluteRoutes.viewer.index()}>
-          <SvgActionChevronL />
-          <Text variant="t200" secondary>
-            Go back
-          </Text>
-        </BackLink>
-      </Header>
-      <StyledSignInIllustrationSVG />
-      <SignInStepsStepper />
-      <CreateMemberModal />
-    </StyledContainer>
+    <>
+      <Modal show={isLoading} noBoxShadow>
+        <Loader variant="xlarge" />
+      </Modal>
+      <StyledContainer>
+        <Header>
+          <LogoContainer>
+            <SvgJoystreamLogoFull />
+          </LogoContainer>
+          <StyledHero variant="h900">Welcome to Joystream Studio</StyledHero>
+          <SubTitle variant="t300">
+            Start your journey as a Video Publisher. Publish and manage your channel and video content.
+          </SubTitle>
+          <ButtonGroup>
+            <SignInButton icon={<SvgActionChannel />} size="large" onClick={handleSignIn}>
+              Sign in
+            </SignInButton>
+            <Button variant="secondary" icon={<SvgActionInformative />} size="large" to="https://www.joystream.org/">
+              How it works?
+            </Button>
+          </ButtonGroup>
+          <BackLink to={absoluteRoutes.viewer.index()}>
+            <SvgActionChevronL />
+            <Text variant="t200" secondary>
+              Go back
+            </Text>
+          </BackLink>
+        </Header>
+        <StyledSignInIllustrationSVG />
+      </StyledContainer>
+    </>
   )
 }
