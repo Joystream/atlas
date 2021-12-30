b30e2c434f8b2be18fd553b67b6a46fbd548647d
Bartosz Dryl <drylbartosz@gmail.com>
2021-12-30 12:49:17 +0100

    Video thumbnail (#1838)
    * video thumbnail groundwork
    
    * add slots
    
    * update stories, remove margin from Pill
    
    * improve api, refactor
    
    * clean storybook
    
    * more refactoring and cleaning
    
    * change hover overlay background when loading
    
    * cr fixes and slot improvements
    
    * refactor slots, add ContentContainer, add custom content template
    
    * image improvements, set default slot type

---
+++ src/components/_video/VideoThumbnail/VideoThumbnail.stories.tsx
@@ -0,0 +1,150 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+
+import { Pill, PillGroup } from '@/components/Pill'
+import { UploadProgressBar } from '@/components/UploadProgressBar'
+import { IconButton } from '@/components/_buttons/IconButton'
+import {
+  SvgActionBid,
+  SvgActionEdit,
+  SvgActionJoyToken,
+  SvgAlertsWarning24,
+  SvgIllustrativePlay,
+  SvgIllustrativeReupload,
+} from '@/components/_icons'
+import { cVar } from '@/styles'
+
+import { VideoThumbnail, VideoThumbnailProps } from './VideoThumbnail'
+
+export default {
+  title: 'video/VideoThumbnail',
+  component: VideoThumbnail,
+  args: {
+    clickable: true,
+    loading: false,
+    thumbnailUrl: 'https://thispersondoesnotexist.com/image',
+    thumbnailAlt: 'This person does not exist',
+    slots: {
+      bottomLeft: {
+        element: (
+          <PillGroup
+            size="small"
+            items={[
+              {
+                label: 'NFT',
+              },
+              {
+                icon: <SvgActionJoyToken />,
+                label: '24K tJOY',
+              },
+            ]}
+          />
+        ),
+        type: 'default',
+      },
+      topRight: {
+        element: (
+          <IconButton size="small">
+            <SvgActionBid />
+          </IconButton>
+        ),
+        type: 'default',
+        clickable: true,
+      },
+      center: {
+        element: <SvgIllustrativePlay />,
+        type: 'hover',
+      },
+    },
+  },
+  argTypes: {
+    slots: { table: { disable: true } },
+    contentSlot: { table: { disable: true } },
+    onClick: { action: 'clicked' },
+  },
+} as Meta<VideoThumbnailProps>
+
+const Template: Story<VideoThumbnailProps> = (args) => <VideoThumbnail {...args} />
+export const Default = Template.bind({})
+
+export const UploadProgress = Template.bind({})
+UploadProgress.args = {
+  ...UploadProgress.args,
+  slots: {},
+  clickable: false,
+  contentSlot: <UploadProgressBar progress={40} lastStatus="processing" withLoadingIndicator />,
+}
+
+export const FailedUpload = Template.bind({})
+FailedUpload.args = {
+  ...FailedUpload.args,
+  slots: {
+    bottomRight: {
+      element: <Pill label="Failed upload" variant="danger" icon={<SvgAlertsWarning24 />} size="medium" />,
+      type: 'default',
+    },
+    center: {
+      element: <SvgIllustrativeReupload />,
+      type: 'hover',
+    },
+  },
+}
+export const Draft = Template.bind({})
+Draft.args = {
+  ...Draft.args,
+  slots: {
+    topRight: {
+      element: (
+        <IconButton size="small" variant="tertiary">
+          <SvgActionEdit />
+        </IconButton>
+      ),
+      type: 'hover',
+      clickable: true,
+    },
+    bottomRight: {
+      element: <Pill label="8:24" />,
+      type: 'default',
+    },
+    bottomLeft: {
+      element: <Pill label="Draft" />,
+      type: 'default',
+    },
+    center: {
+      element: <SvgIllustrativePlay />,
+      type: 'hover',
+    },
+  },
+}
+
+export const CustomContent = Template.bind({})
+CustomContent.args = {
+  ...CustomContent.args,
+  contentSlot: (
+    <div
+      style={{
+        backgroundColor: cVar('colorCoreGreen700'),
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      Hello world!
+    </div>
+  ),
+  slots: {
+    bottomRight: {
+      element: <Pill label="8:24" />,
+      type: 'default',
+    },
+    bottomLeft: {
+      element: <Pill label="Draft" />,
+      type: 'default',
+    },
+    center: {
+      element: <SvgIllustrativePlay />,
+      type: 'hover',
+    },
+  },
+}
