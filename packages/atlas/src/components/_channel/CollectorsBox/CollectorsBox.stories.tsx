8fca75738db8d8f3de63bb2e360d8f393795f7ca
drillprop <drylbartosz@gmail.com>
2022-01-18 10:42:09 +0100

    update stories

--- packages/atlas/src/components/_channel/CollectorsBox/CollectorsBox.stories.tsx
+++ packages/atlas/src/components/_channel/CollectorsBox/CollectorsBox.stories.tsx
@@ -5,6 +5,8 @@ import { OverlayManagerProvider } from '@/providers/overlayManager'
 
 import { CollectorsBox, CollectorsBoxProps } from './'
 
+import { ChannelCover } from '../ChannelCover'
+
 export default {
   title: 'channel/CollectorsBox',
   component: CollectorsBox,
@@ -25,6 +27,9 @@ export default {
       { assetUrl: 'https://thispersondoesnotexist.com/image', tooltipText: 'Someone else', nftsAmount: 7 },
     ],
   },
+  argTypes: {
+    collectors: { table: { disable: true } },
+  },
   decorators: [
     (Story) => (
       <OverlayManagerProvider>
@@ -37,3 +42,26 @@ export default {
 const Template: Story<CollectorsBoxProps> = (args) => <CollectorsBox {...args} />
 
 export const Default = Template.bind({})
+export const WithLessThan5Collectors = Template.bind({})
+WithLessThan5Collectors.args = {
+  collectors: [
+    { assetUrl: 'https://thispersondoesnotexist.com/image', tooltipText: 'William', nftsAmount: 6 },
+    {
+      assetUrl: 'https://eu-central-1.linodeobjects.com/atlas-assets/channel-posters/2.jpg',
+      tooltipText: 'Someone',
+      nftsAmount: 1,
+    },
+    { assetUrl: 'https://thispersondoesnotexist.com/image', tooltipText: 'Someone else', nftsAmount: 7 },
+  ],
+}
+
+const TemplateWithChannelCover: Story<CollectorsBoxProps> = (args) => (
+  <div style={{ position: 'relative' }}>
+    <ChannelCover assetUrl="https://eu-central-1.linodeobjects.com/atlas-assets/channel-posters/2.jpg" />
+    <div style={{ position: 'absolute', bottom: -32, right: 0 }}>
+      <CollectorsBox {...args} />
+    </div>
+  </div>
+)
+
+export const WithChannelCover = TemplateWithChannelCover.bind({})
