9a8360f43880b44a856dfe359450ffccfaf138cd
Loic Coenen <lcoenenbwu@gmail.com>
2022-01-25 16:39:58 +0100

    feat: update sidenav flow

--- src/components/_navigation/SidenavViewer/SidenavViewer.tsx
+++ src/components/_navigation/SidenavViewer/SidenavViewer.tsx
@@ -1,26 +1,58 @@
 import React, { useState } from 'react'
 
 import { Button } from '@/components/_buttons/Button'
-import { SvgActionNewTab } from '@/components/_icons'
+import { SvgActionMember, SvgActionNewTab } from '@/components/_icons'
 import { SvgJoystreamLogoFull } from '@/components/_illustrations'
 import { viewerNavItems } from '@/config/nav'
 import { absoluteRoutes } from '@/config/routes'
+import { useConnectionStatusStore } from '@/providers/connectionStatus'
 import { usePersonalDataStore } from '@/providers/personalData'
+import { useUser } from '@/providers/user'
 import { ConsoleLogger } from '@/utils/logs'
 
 import { FollowedChannels } from './FollowedChannels'
 
 import { SidenavBase } from '../SidenavBase'
 
+//import { absoluteRoutes } from '@/config/routes'
+
 export const SidenavViewer: React.FC = () => {
   const [expanded, setExpanded] = useState(false)
   const followedChannels = usePersonalDataStore((state) => state.followedChannels)
   const updateChannelFollowing = usePersonalDataStore((state) => state.actions.updateChannelFollowing)
+  const nodeConnectionStatus = useConnectionStatusStore((state) => state.nodeConnectionStatus)
+  const internetConnectionStatus = useConnectionStatusStore((state) => state.internetConnectionStatus)
 
   const handleChannelNotFound = (id: string) => {
     ConsoleLogger.warn(`Followed channel not found, removing id: ${id}`)
     updateChannelFollowing(id, false)
   }
+
+  // TODO how do I chose the right button?
+  // TODO how is it signed in?
+  //    const signedIn = nodeConnectionStatus === 'connected' || internetConnectionStatus === 'connected';
+
+  console.log('nodeConnectionStatus', nodeConnectionStatus)
+  console.log('internetConnectionStatus', internetConnectionStatus)
+
+  const { signIn, activeMemberId, activeAccountId, extensionConnected, activeChannelId } = useUser()
+
+  const memberSet = activeMemberId && activeAccountId && extensionConnected && !activeChannelId
+
+  const buttonsContent = !memberSet ? (
+    <Button size="large" icon={<SvgActionMember />} onClick={signIn}>
+      Sign in
+    </Button>
+  ) : (
+    <Button
+      variant="secondary"
+      to={absoluteRoutes.studio.index()}
+      onClick={() => setExpanded(false)}
+      icon={<SvgActionNewTab />}
+    >
+      Go to Studio
+    </Button>
+  )
 
   return (
     <SidenavBase
@@ -39,18 +71,7 @@ export const SidenavViewer: React.FC = () => {
           />
         ) : null
       }
-      buttonsContent={
-        <>
-          <Button
-            variant="secondary"
-            to={absoluteRoutes.studio.index()}
-            onClick={() => setExpanded(false)}
-            icon={<SvgActionNewTab />}
-          >
-            Joystream Studio
-          </Button>
-        </>
-      }
+      buttonsContent={buttonsContent}
     />
   )
 }
