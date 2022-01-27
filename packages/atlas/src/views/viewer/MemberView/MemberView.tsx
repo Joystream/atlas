480e3a569a7766075689b30a82a28a67a85f85ee
eldiegod <cardenasrdiego@gmail.com>
2022-01-27 12:22:22 +0100

    wip

--- packages/atlas/src/views/viewer/MemberView/MemberView.tsx
+++ packages/atlas/src/views/viewer/MemberView/MemberView.tsx
@@ -3,26 +3,36 @@ import { useParams } from 'react-router'
 import { useSearchParams } from 'react-router-dom'
 
 import { useMemberships } from '@/api/hooks'
+import { VideoOrderByInput } from '@/api/queries'
 import { EmptyFallback } from '@/components/EmptyFallback'
 import { LimitedWidthContainer } from '@/components/LimitedWidthContainer'
 import { ViewErrorFallback } from '@/components/ViewErrorFallback'
 import { ViewWrapper } from '@/components/ViewWrapper'
 import { Button } from '@/components/_buttons/Button'
+import { Select } from '@/components/_inputs/Select'
 import { absoluteRoutes } from '@/config/routes'
+import { SORT_OPTIONS } from '@/config/sorting'
 import { useUser } from '@/providers/user'
 import { SentryLogger } from '@/utils/logs'
 
 import { MemberAbout } from './MemberAbout'
-import { NotFoundMemberContainer, StyledMembershipInfo, StyledTabs, TabsContainer } from './MemberView.styles'
+import {
+  NotFoundMemberContainer,
+  SortContainer,
+  StyledMembershipInfo,
+  StyledTabs,
+  TabsContainer,
+} from './MemberView.styles'
 
 const TABS = [
   // 'NFTs',
-  // 'Activity',
+  'Activity',
   'About',
 ] as const
 
 export const MemberView: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
+  const [sortActivityBy, setSortActivityBy] = useState<VideoOrderByInput>(VideoOrderByInput.CreatedAtDesc)
   const currentTabName = searchParams.get('tab') as typeof TABS[number] | null
   const [currentTab, setCurrentTab] = useState<typeof TABS[number] | null>(null)
   const { activeMemberId } = useUser()
@@ -42,10 +52,16 @@ export const MemberView: React.FC = () => {
   const handleSetCurrentTab = async (tab: number) => {
     setSearchParams({ 'tab': TABS[tab] }, { replace: true })
   }
-
+  const handleSorting = (value?: unknown) => {
+    if (value) {
+      setSortActivityBy(value as VideoOrderByInput)
+    }
+  }
   const mappedTabs = TABS.map((tab) => ({ name: tab, badgeNumber: 0 }))
   const tabContent = React.useMemo(() => {
     switch (currentTab) {
+      case 'Activity':
+        return 'Activity'
       case 'About':
         return <MemberAbout />
     }
@@ -101,6 +117,18 @@ export const MemberView: React.FC = () => {
             tabs={mappedTabs}
             onSelectTab={handleSetCurrentTab}
           />
+
+          {currentTab === 'Activity' && (
+            <SortContainer>
+              <Select
+                size="small"
+                labelPosition="left"
+                value={sortActivityBy}
+                items={SORT_OPTIONS}
+                onChange={handleSorting}
+              />
+            </SortContainer>
+          )}
         </TabsContainer>
         {tabContent}
       </LimitedWidthContainer>
