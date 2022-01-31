f85f56fb83d246427a26a416347ced909c8133b4
Rafał Pawłow <pawlow.rafal@gmail.com>
2022-01-31 12:49:56 +0100

    implement dropdown account balance (#2022)
    * implement dropdown account balance
    
    * change account balance subscription to actually be a subscription
    
    * PR FIX

--- packages/atlas/src/joystream-lib/lib.ts
+++ packages/atlas/src/joystream-lib/lib.ts
@@ -83,4 +83,12 @@ export class JoystreamLib {
 
     return new BN(balance.freeBalance).toNumber()
   }
+
+  async subscribeAccountBalance(accountId: AccountId, callback: (balance: number) => void) {
+    await this.ensureApi()
+
+    return this.api.query.system.account(accountId, ({ data: { free } }) => {
+      callback(new BN(free).toNumber())
+    })
+  }
 }
