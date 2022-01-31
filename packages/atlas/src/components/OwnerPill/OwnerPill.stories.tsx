34b1862fce5c3af51355e1bfb1c08ceda028a7d9
drillprop <drylbartosz@gmail.com>
2022-01-31 14:19:47 +0100

    cr fixes

--- packages/atlas/src/components/OwnerPill/OwnerPill.stories.tsx
+++ packages/atlas/src/components/OwnerPill/OwnerPill.stories.tsx
@@ -1,6 +1,8 @@
 import { Meta, Story } from '@storybook/react'
 import React from 'react'
 
+import { cVar } from '@/styles'
+
 import { OwnerPill, OwnerPillProps } from '.'
 
 export default {
@@ -13,9 +15,46 @@ export default {
 } as Meta
 
 const Template: Story<OwnerPillProps> = (args) => (
-  <div style={{ maxWidth: '180px' }}>
-    <OwnerPill {...args} />
-  </div>
+  <>
+    <div
+      style={{
+        width: '320px',
+        height: '180px',
+        padding: '8px',
+        background: 'black',
+        marginBottom: '8px',
+      }}
+    >
+      <div style={{ maxWidth: '50%' }}>
+        <OwnerPill {...args} />
+      </div>
+    </div>
+    <div
+      style={{
+        width: '320px',
+        height: '180px',
+        padding: '8px',
+        background: cVar('colorCoreBlue500'),
+        marginBottom: '8px',
+      }}
+    >
+      <div style={{ maxWidth: '50%' }}>
+        <OwnerPill {...args} />
+      </div>
+    </div>
+    <div
+      style={{
+        width: '320px',
+        height: '180px',
+        padding: '8px',
+        background: 'url(https://picsum.photos/320/180)',
+      }}
+    >
+      <div style={{ maxWidth: '50%' }}>
+        <OwnerPill {...args} />
+      </div>
+    </div>
+  </>
 )
 
 export const Default = Template.bind({})
