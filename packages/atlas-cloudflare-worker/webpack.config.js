3a69834bef41d7c7b0f4279b7112f8946d108a73
Klaudiusz Dembler <accounts@kdembler.com>
2022-02-07 16:22:09 +0100

    pr review

--- packages/atlas-cloudflare-worker/webpack.config.js
+++ packages/atlas-cloudflare-worker/webpack.config.js
@@ -17,10 +17,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
-        options: {
-          // transpileOnly is useful to skip typescript checks occasionally:
-          // transpileOnly: true,
-        },
+        options: {},
       },
     ],
   },
