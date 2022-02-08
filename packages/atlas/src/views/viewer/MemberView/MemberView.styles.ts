9576d89f01ad83bdcf962879cad1378687abb8c5
eldiegod <cardenasrdiego@gmail.com>
2022-02-08 21:59:50 +0100

    Member nfts tab
    fixes: #1752

--- packages/atlas/src/views/viewer/MemberView/MemberView.styles.ts
+++ packages/atlas/src/views/viewer/MemberView/MemberView.styles.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 
 import { MembershipInfo } from '@/components/MembershipInfo'
 import { Tabs } from '@/components/Tabs'
-import { media, oldColors, sizes } from '@/styles'
+import { cVar, media, oldColors, sizes, transitions } from '@/styles'
 
 export const NotFoundMemberContainer = styled.div`
   display: flex;
@@ -31,23 +31,38 @@ export const StyledTabs = styled(Tabs)`
 
 export const TabsContainer = styled.div`
   display: grid;
-  margin-bottom: ${sizes(8)};
+  padding-top: ${sizes(8)};
   gap: ${sizes(2)};
-  grid-template: 'tabs tabs tabs' 1fr 'search search search' auto 'sort sort sort' auto / 1fr 1fr;
+  grid-template: 'tabs tabs tabs' 1fr 'search search search' auto 'sort sort sort' auto / 1fr 1fr 'filter filter filter' auto / 1fr 1fr;
   align-items: baseline;
-
-  ${media.xs} {
-    padding-top: ${sizes(8)};
-  }
+  background-color: #000;
 
   ${media.sm} {
     align-items: center;
-    border-bottom: solid 1px ${oldColors.gray[700]};
-    gap: ${sizes(8)};
-    grid-template: 1fr / 1fr 160px;
+    box-shadow: ${cVar('effectDividersBottom')};
+    gap: ${sizes(4)};
+    grid-template: 1fr / auto 1fr 160px 99px;
   }
 `
 
 export const StyledMembershipInfo = styled(MembershipInfo)`
   margin-top: ${sizes(8)};
+`
+
+export const FilterButtonContainer = styled.div`
+  grid-area: filter;
+  grid-gap: 8px;
+  ${media.sm} {
+    grid-area: initial;
+  }
+`
+
+export const TabsWrapper = styled.div<{ isFiltersOpen: boolean }>`
+  z-index: 99;
+  position: relative;
+  margin-bottom: ${sizes(8)};
+  ${media.sm} {
+    margin-bottom: ${({ isFiltersOpen }) => sizes(isFiltersOpen ? 30 : 12)};
+    transition: margin-bottom ${transitions.timings.routing} ${transitions.easing};
+  }
 `
