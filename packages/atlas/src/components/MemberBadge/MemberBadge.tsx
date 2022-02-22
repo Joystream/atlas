487cf961f489e19b3d3930a3d567a863499e9e7b
drillprop <drylbartosz@gmail.com>
2022-02-22 13:27:45 +0100

    create MemberBadge component

---
+++ packages/atlas/src/components/MemberBadge/MemberBadge.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { MemberBadgeWrapper, RemoveButton, StyledHandleText, StyledSVGCloseIcon } from './MemberBadge.styles'
+
+import { Avatar } from '../Avatar'
+
+export type MemberBadgeProps = {
+  avatarUri?: string | null
+  handle: string | null
+  onDeleteClick?: () => void
+  className?: string
+}
+export const MemberBadge: React.FC<MemberBadgeProps> = ({ avatarUri, handle, onDeleteClick, className }) => {
+  return (
+    <MemberBadgeWrapper className={className}>
+      <Avatar size="bid" assetUrl={avatarUri} />
+      <StyledHandleText variant="t100" as="p">
+        {handle}
+      </StyledHandleText>
+      <RemoveButton onClick={onDeleteClick}>
+        <StyledSVGCloseIcon />
+      </RemoveButton>
+    </MemberBadgeWrapper>
+  )
+}
