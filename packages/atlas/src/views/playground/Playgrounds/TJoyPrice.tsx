22fcf33262f38df6fdada27d0472d330c361345a
lcoenengithub <96242244+lcoenengithub@users.noreply.github.com>
2022-02-24 11:22:46 +0100

    feat: add tjoy price logic (#2170)
    Co-authored-by: Loic Coenen <lcoenenbwu@gmail.com>

---
+++ packages/atlas/src/views/playground/Playgrounds/TJoyPrice.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+import { useTokenPrice } from '@/providers/joystream'
+
+export const TJoyPrice = () => {
+  const { price, convertToUSD, convertToTJoy } = useTokenPrice()
+  const [toConvert, setToConvert] = useState(0)
+  const [unit, setUnit] = useState('tJoy')
+  const [converted, setConverted] = useState(0)
+  const [showConverted, setShowConverted] = useState(false)
+  const convert = () => {
+    setShowConverted(true)
+    unit === 'tJoy' ? setConverted(convertToUSD(toConvert)) : setConverted(convertToTJoy(toConvert))
+  }
+  const convertedUnit = unit === 'usd' ? 'tJoy' : 'usd'
+  return (
+    <div>
+      <div>tJoy price: {price}$ / tJoy</div>
+      <div>
+        Converter:{' '}
+        <input
+          type="number"
+          value={toConvert}
+          onChange={(v) => {
+            setShowConverted(false)
+            setToConvert(parseInt(v.target.value))
+          }}
+        />
+        <select
+          onChange={(v) => {
+            setShowConverted(false)
+            setUnit(v.target.value)
+          }}
+          value={unit}
+        >
+          <option value="usd">USD</option>
+          <option value="tJoy">tJoy</option>
+        </select>
+      </div>
+      <div>
+        {' '}
+        <button onClick={convert}>Convert</button>
+      </div>
+      {showConverted ? (
+        <div>
+          {toConvert} {unit} = {converted} {convertedUnit}
+        </div>
+      ) : null}
+    </div>
+  )
+}
