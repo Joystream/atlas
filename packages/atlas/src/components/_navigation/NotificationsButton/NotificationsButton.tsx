1069ba1685a58537211bc600b066391ff2394b8c
Rafał Pawłow <pawlow.rafal@gmail.com>
2022-02-25 14:42:40 +0100

    add notification button (#2154)

---
+++ packages/atlas/src/components/_navigation/NotificationsButton/NotificationsButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { SvgActionNotifications } from '@/components/_icons'
+
+import { StyledButton } from './NotificationsButton.styles'
+
+export const NotificationsButton = () => {
+  return <StyledButton variant="secondary" icon={<SvgActionNotifications />} />
+}
