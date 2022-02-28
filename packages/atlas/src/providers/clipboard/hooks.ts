19d66d8e1448b6ba857acfb65378277b71999e49
Loic Coenen <lcoenenbwu@gmail.com>
2022-02-28 13:48:06 +0100

    fix: add default args

--- packages/atlas/src/providers/clipboard/hooks.ts
+++ packages/atlas/src/providers/clipboard/hooks.ts
@@ -1,12 +1,12 @@
-import { useSnackbar } from '@/providers/snackbars'
+import { SnackbarIconType, useSnackbar } from '@/providers/snackbars'
 import { copyToClipboard } from '@/utils/browser'
 
 export const useClipboard = () => {
   const { displaySnackbar } = useSnackbar()
   return {
-    copyToClipboard: (text: string) => {
+    copyToClipboard: (text: string, title = 'Video URL copied to clipboard', iconType: SnackbarIconType = 'info') => {
       copyToClipboard(text)
-      displaySnackbar({ title: 'Video URL have been copied', iconType: 'info' })
+      displaySnackbar({ title, iconType })
     },
   }
 }
