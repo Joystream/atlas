9d42409d28c5d76f4ee97fcd589cbb02d48941d9
Bartosz Dryl <drylbartosz@gmail.com>
2022-03-01 12:47:59 +0100

    add MemberComboBox component (#2179)

---
+++ packages/atlas/src/components/MemberBadge/MemberBadge.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { MemberBadgeWrapper, RemoveButton, StyledHandleText, StyledSVGCloseIcon } from './MemberBadge.styles'
+
+import { Avatar } from '../Avatar'
+
+export type MemberBadgeProps = {
+  avatarUri?: string | null
+  handle?: string | null
+  onDeleteClick?: () => void
+  className?: string
+  isLoadingAvatar?: boolean
+}
+export const MemberBadge: React.FC<MemberBadgeProps> = ({
+  avatarUri,
+  handle,
+  onDeleteClick,
+  className,
+  isLoadingAvatar,
+}) => {
+  return (
+    <MemberBadgeWrapper className={className}>
+      <Avatar size="bid" assetUrl={avatarUri} loading={isLoadingAvatar} />
+      <StyledHandleText variant="t100" as="p">
+        {handle}
+      </StyledHandleText>
+      <RemoveButton onClick={onDeleteClick}>
+        <StyledSVGCloseIcon />
+      </RemoveButton>
+    </MemberBadgeWrapper>
+  )
+}
---
+++ packages/atlas/src/components/_inputs/ComboBox/ComboBox.styles.ts
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled'
+
+import { cVar, zIndex } from '@/styles'
+
+export const ComboBoxWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`
+
+export const ListWrapper = styled.ul<{ topPosition?: number }>`
+  max-height: 300px;
+  overflow-y: auto;
+  background: ${cVar('colorBackgroundStrong')};
+  padding: 0;
+  position: absolute;
+  top: ${({ topPosition }) => (topPosition ? `${topPosition}px` : 'unset')};
+  left: 0;
+  width: 100%;
+  z-index: ${zIndex.globalOverlay};
+  margin: 0;
+`
