c4527aa36162641b817ceb95d2cd5f8a99d7e26f
drillprop <drylbartosz@gmail.com>
2022-03-23 12:35:49 +0100

    fix stories

--- packages/atlas/src/components/OwnerPill/OwnerPill.stories.tsx
+++ packages/atlas/src/components/OwnerPill/OwnerPill.stories.tsx
@@ -10,7 +10,7 @@ export default {
   component: OwnerPill,
   args: {
     handle: 'verylonghandle',
-    avatar: 'https://thispersondoesnotexist.com/image',
+    avatar: { assetUrl: 'https://thispersondoesnotexist.com/image' },
   },
 } as Meta
 
