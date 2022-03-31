62b9135e5afe6a43e4a4a0a4a527bd48f39ac9c1
Rafal Pawlow <pawlow.rafal@gmail.com>
2022-03-31 13:54:31 +0200

    PR FIX

--- packages/atlas/src/hooks/useVideoTileSharedLogic.ts
+++ packages/atlas/src/hooks/useVideoTileSharedLogic.ts
@@ -5,21 +5,16 @@ import { useAsset } from '@/providers/assets'
 type UseVideoSharedLogicOpts = {
   video?: BasicVideoFieldsFragment | VideoFieldsFragment | null
   loading?: boolean
-  isDraft?: boolean
-  onNotFound?: () => void
 }
-export const useVideoTileSharedLogic = ({ video, loading }: UseVideoSharedLogicOpts) => {
+export const useVideoTileSharedLogic = ({ video }: UseVideoSharedLogicOpts) => {
   const { url: thumbnailPhotoUrl, isLoadingAsset: isLoadingThumbnail } = useAsset(video?.thumbnailPhoto)
   const { url: avatarPhotoUrl, isLoadingAsset: isLoadingAvatar } = useAsset(video?.channel?.avatarPhoto)
 
-  const internalIsLoadingState = loading || !video
   const videoHref = video ? absoluteRoutes.viewer.video(video.id) : undefined
 
   return {
-    video,
-    loading: internalIsLoadingState,
-    isLoadingThumbnail: isLoadingThumbnail || internalIsLoadingState,
-    isLoadingAvatar: isLoadingAvatar || internalIsLoadingState,
+    isLoadingThumbnail: isLoadingThumbnail || !video,
+    isLoadingAvatar: isLoadingAvatar || !video,
     thumbnailPhotoUrl,
     avatarPhotoUrl,
     videoHref,
