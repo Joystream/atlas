a34fbc3904848a386d143d7f70947a52e950c0b6
Bartosz Dryl <drylbartosz@gmail.com>
2022-04-08 11:36:33 +0200

    Card interactions (#2412)
    Co-authored-by: eldiegod <cardenasrdiego@gmail.com>

--- packages/atlas/src/components/_nft/NftCard/NftCard.styles.ts
+++ packages/atlas/src/components/_nft/NftCard/NftCard.styles.ts
@@ -5,9 +5,142 @@ import { AvatarGroup } from '@/components/Avatar/AvatarGroup'
 import { Text } from '@/components/Text'
 import { cVar, media, sizes } from '@/styles'
 
+import holo from './assets/holo.png'
+import pattern from './assets/pattern.png'
+
 export const Container = styled.div<{ fullWidth?: boolean }>`
   width: ${({ fullWidth }) => (fullWidth ? '100%' : '360px')};
-  background-color: ${cVar('colorBackgroundStrong')};
+  transform-style: preserve-3d;
+  perspective: 2000px;
+  position: relative;
+`
+
+export const FadeMask = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  mask-position: 50% 50%;
+  transition: all 0.1s ease;
+  mask-image: linear-gradient(
+    104deg,
+    rgb(0 0 0 / 1) 0%,
+    rgb(0 0 0 / 1) 7%,
+    rgb(0 0 0 / 0.5) 10%,
+    rgb(0 0 0 / 0) 40%,
+    rgb(0 0 0 / 0) 60%,
+    rgb(0 0 0 / 0.5) 90%,
+    rgb(0 0 0 / 1) 92%,
+    rgb(0 0 0 / 1) 100%
+  );
+  mask-mode: alpha;
+  mix-blend-mode: hard-light;
+  opacity: 0;
+`
+
+export const Pattern = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-image: url(${holo});
+  background-position: 50% 50%;
+  background-size: 1000%;
+  transition: all 0.1s ease;
+  mask-image: url(${pattern});
+  mask-position: 50% 50%;
+`
+
+export const LightReflection = styled.div`
+  --color1: white;
+  --color2: white;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0.5;
+  transition: all 0.33s ease;
+  mix-blend-mode: overlay;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: 300% 300%;
+  background-image: linear-gradient(
+    115deg,
+    transparent 0%,
+    var(--color1) 25%,
+    transparent 47%,
+    transparent 53%,
+    var(--color2) 75%,
+    transparent 100%
+  );
+  filter: brightness(0.5) contrast(1);
+`
+
+export const ReflectionContent = styled.div`
+  transform: rotateX(0) rotateY(0);
+  transition: transform 300ms, box-shadow 300ms;
+  box-shadow: 0 0 0 0 ${cVar('colorCoreNeutral600')}, 0 55px 35px -60px rgb(0 0 0 / 0.5);
+`
+
+const generateEdges = (num: number) => {
+  if (num > 6) {
+    return -3
+  }
+  if (num < 2) {
+    return 2
+  }
+  if (num < 3) {
+    return 3
+  }
+  return 0
+}
+
+export const ReflectionGridCell = styled.div<{
+  row: number
+  column: number
+  lp: number
+  tp: number
+}>`
+  position: absolute;
+  transform: translateZ(80px);
+  width: 10%;
+  height: 10%;
+  top: calc(${({ row }) => row} * 10%);
+  left: calc(${({ column }) => column} * 10%);
+  :hover ~ ${ReflectionContent} {
+    transform: rotateX(${({ row }) => row * -5 + 25}deg) rotateY(${({ column }) => -25 + column * 5}deg);
+    box-shadow: ${({ row, column }) =>
+        `${generateEdges(column)}px ${generateEdges(row)}px 0 0 ${cVar('colorCoreNeutral600')}`},
+      0 55px 35px -20px rgb(0 0 0 / 0.5);
+  }
+  :hover ~ ${ReflectionContent} ${Pattern} {
+    opacity: 0.65;
+    background-position: ${({ lp, tp }) => `${lp * 0.25}% ${tp * 0.5}%`};
+  }
+  :hover ~ ${ReflectionContent} ${FadeMask} {
+    opacity: 1;
+    mask-position: ${({ lp, tp }) => `${lp * 1.15}% ${tp}%`};
+    mask-image: linear-gradient(
+      104deg,
+      rgb(0 0 0 / 1) 0%,
+      rgb(0 0 0 / 0.8) 10%,
+      rgb(0 0 0 / 0) 35%,
+      rgb(0 0 0 / 0) 65%,
+      rgb(0 0 0 / 0.8) 90%,
+      rgb(0 0 0 / 1) 100%
+    );
+    mask-size: 190%;
+  }
+  :hover ~ ${ReflectionContent} ${LightReflection} {
+    background-position: ${({ lp, tp }) => `${lp}% ${tp}%`};
+    background-image: linear-gradient(110deg, transparent 25%, var(--color1) 48%, var(--color2) 52%, transparent 75%);
+    opacity: 0.5;
+    filter: brightness(1.66) contrast(1.33);
+  }
 `
 
 export const Title = styled(Text)`
@@ -20,6 +153,7 @@ export const Title = styled(Text)`
 
 export const Details = styled.div`
   padding: ${sizes(4)};
+  background-color: ${cVar('colorBackgroundStrong')};
 
   ${media.sm} {
     padding: ${sizes(6)};
--- packages/atlas/src/components/_nft/NftCard/NftCard.tsx
+++ packages/atlas/src/components/_nft/NftCard/NftCard.tsx
@@ -4,7 +4,18 @@ import { SkeletonLoader } from '@/components/_loaders/SkeletonLoader'
 import { VideoThumbnail, VideoThumbnailProps } from '@/components/_video/VideoThumbnail'
 
 import { Member, Members } from './Members'
-import { Container, Content, Details, Separator, Title } from './NftCard.styles'
+import {
+  Container,
+  Content,
+  Details,
+  FadeMask,
+  LightReflection,
+  Pattern,
+  ReflectionContent,
+  ReflectionGridCell,
+  Separator,
+  Title,
+} from './NftCard.styles'
 
 export type NftCardProps = {
   title?: string | null
@@ -25,22 +36,40 @@ export const NftCard: React.FC<NftCardProps> = ({
   fullWidth,
   loading,
 }) => {
+  const reflectionCells = Array.from({ length: 100 })
   return (
     <Container fullWidth={fullWidth}>
-      <VideoThumbnail clickable={false} {...thumbnail} />
-      <Details>
-        {loading ? <SkeletonLoader width="70%" height={24} bottomSpace={24} /> : <Title variant="h400">{title}</Title>}
-        <Content>
-          <Members loading={loading} caption="Creator" members={creator} />
-          {supporters && !!supporters.length && (
-            <>
-              <Members caption="Supporters" members={supporters} />
-              <Separator />
-            </>
+      {reflectionCells.map((_, idx) => {
+        const row = Math.floor(idx / 10)
+        const column = idx % 10
+        const lp = column * 10
+        const tp = row * 10
+        return <ReflectionGridCell row={row} column={column} lp={lp} tp={tp} key={idx} />
+      })}
+      <ReflectionContent>
+        <VideoThumbnail clickable={false} {...thumbnail} />
+        <FadeMask>
+          <Pattern />
+        </FadeMask>
+        <LightReflection />
+        <Details>
+          {loading ? (
+            <SkeletonLoader width="70%" height={24} bottomSpace={24} />
+          ) : (
+            <Title variant="h400">{title}</Title>
           )}
-          <Members loading={loading} caption="Owner" members={owner} />
-        </Content>
-      </Details>
+          <Content>
+            <Members loading={loading} caption="Creator" members={creator} />
+            {supporters && !!supporters.length && (
+              <>
+                <Members caption="Supporters" members={supporters} />
+                <Separator />
+              </>
+            )}
+            <Members loading={loading} caption="Owner" members={owner} />
+          </Content>
+        </Details>
+      </ReflectionContent>
     </Container>
   )
 }
