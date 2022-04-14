2169b23c65640003720d6fa7bbdc688d0624018c
drillprop <drylbartosz@gmail.com>
2022-04-14 20:14:16 +0200

    extract getNftStatus from useNft

--- packages/atlas/src/api/hooks/nfts.ts
+++ packages/atlas/src/api/hooks/nfts.ts
@@ -6,10 +6,10 @@ import {
   BasicMembershipFieldsFragment,
   GetNftQuery,
   GetNftQueryVariables,
+  GetNftsConnectionQuery,
+  GetNftsConnectionQueryVariables,
   GetNftsQuery,
   GetNftsQueryVariables,
-  OwnedNftOrderByInput,
-  OwnedNftWhereInput,
   useGetNftQuery,
   useGetNftsConnectionQuery,
   useGetNftsQuery,
@@ -61,86 +61,73 @@ export type NftStatus = (
 
 export type UseNftData = Omit<QueryResult, 'data'> & { nft?: AllNftFieldsFragment | null; nftStatus?: NftStatus }
 
-export const useNft = (id: string, opts?: QueryHookOptions<GetNftQuery, GetNftQueryVariables>): UseNftData => {
-  const { data, ...rest } = useGetNftQuery({ variables: { id }, skip: !id, ...opts })
-  const nft = data?.ownedNftByUniqueInput
-
+export const getNftStatus = (nft?: AllNftFieldsFragment | null): NftStatus | undefined => {
+  if (!nft) return undefined
   const commonProperties = {
     title: nft?.video?.title,
     duration: nft?.video?.duration,
     views: nft?.video?.views,
   }
 
-  const getNftProperties = (): NftStatus | undefined => {
-    if (!nft) return undefined
-
-    if (nft?.transactionalStatusAuction) {
-      const auction = nft.transactionalStatusAuction
-      const englishAuction = auction.auctionType.__typename === 'AuctionTypeEnglish' && auction.auctionType
-      const openAuction = auction.auctionType.__typename === 'AuctionTypeOpen' && auction.auctionType
-      return {
-        ...commonProperties,
-        status: 'auction',
-        type: openAuction ? 'open' : 'english',
-        startingPrice: Number(auction.startingPrice) || 0,
-        buyNowPrice: Number(auction.buyNowPrice) || undefined,
-        topBid: auction.topBid || undefined,
-        topBidAmount: Number(auction.topBid?.amount) || undefined,
-        topBidder: auction.topBid?.bidder,
-        auctionPlannedEndBlock: englishAuction ? englishAuction.plannedEndAtBlock : undefined,
-        bidLockingTime: openAuction ? openAuction.bidLockDuration : undefined,
-        minimalBidStep: englishAuction ? englishAuction.minimalBidStep : undefined,
-        whitelistedMembers: auction.whitelistedMembers,
-      }
-    }
-
-    if (!nft?.transactionalStatus) {
-      throw new Error('NFT missing transactional status')
-    }
-    switch (nft?.transactionalStatus.__typename) {
-      case 'TransactionalStatusBuyNow':
-        return {
-          ...commonProperties,
-          status: 'buy-now',
-          buyNowPrice: Number(nft.transactionalStatus.price),
-        }
-      case 'TransactionalStatusIdle':
-        return {
-          ...commonProperties,
-          status: 'idle',
-          // TODO: last transaction on iddle
-          lastPrice: undefined,
-          lastTransactionDate: undefined,
-        }
-      default:
-        return undefined
+  if (nft?.transactionalStatusAuction) {
+    const auction = nft.transactionalStatusAuction
+    const englishAuction = auction.auctionType.__typename === 'AuctionTypeEnglish' && auction.auctionType
+    const openAuction = auction.auctionType.__typename === 'AuctionTypeOpen' && auction.auctionType
+    return {
+      ...commonProperties,
+      status: 'auction',
+      type: openAuction ? 'open' : 'english',
+      startingPrice: Number(auction.startingPrice) || 0,
+      buyNowPrice: Number(auction.buyNowPrice) || undefined,
+      topBid: auction.topBid || undefined,
+      topBidAmount: Number(auction.topBid?.amount) || undefined,
+      topBidder: auction.topBid?.bidder,
+      auctionPlannedEndBlock: englishAuction ? englishAuction.plannedEndAtBlock : undefined,
+      bidLockingTime: openAuction ? openAuction.bidLockDuration : undefined,
+      minimalBidStep: englishAuction ? englishAuction.minimalBidStep : undefined,
+      whitelistedMembers: auction.whitelistedMembers,
     }
   }
 
+  if (!nft?.transactionalStatus) {
+    throw new Error('NFT missing transactional status')
+  }
+  switch (nft?.transactionalStatus.__typename) {
+    case 'TransactionalStatusBuyNow':
+      return {
+        ...commonProperties,
+        status: 'buy-now',
+        buyNowPrice: Number(nft.transactionalStatus.price),
+      }
+    case 'TransactionalStatusIdle':
+      return {
+        ...commonProperties,
+        status: 'idle',
+        // TODO: last transaction on iddle
+        lastPrice: undefined,
+        lastTransactionDate: undefined,
+      }
+    default:
+      return undefined
+  }
+}
+
+export const useNft = (id: string, opts?: QueryHookOptions<GetNftQuery, GetNftQueryVariables>): UseNftData => {
+  const { data, ...rest } = useGetNftQuery({ variables: { id }, skip: !id, ...opts })
+  const nft = data?.ownedNftByUniqueInput
+
   return {
     nft,
-    nftStatus: getNftProperties(),
+    nftStatus: getNftStatus(nft),
     ...rest,
   }
 }
 
-type ChannelNftsOpts = {
-  sortNftsBy?: OwnedNftOrderByInput
-  where?: OwnedNftWhereInput
-}
-
-export const useChannelNfts = (channelId: string, opts?: ChannelNftsOpts) => {
-  const { data, ...rest } = useGetNftsConnectionQuery({
-    variables: {
-      orderBy: opts?.sortNftsBy,
-      where: {
-        ...opts?.where,
-        creatorChannel: {
-          id_eq: channelId,
-        },
-      },
-    },
-  })
+export const useNftsConnection = (
+  variables?: GetNftsConnectionQueryVariables,
+  opts?: QueryHookOptions<GetNftsConnectionQuery, GetNftsConnectionQueryVariables>
+) => {
+  const { data, ...rest } = useGetNftsConnectionQuery({ variables, ...opts })
 
   return {
     nfts: data?.ownedNftsConnection.edges.map(({ node }) => node),
