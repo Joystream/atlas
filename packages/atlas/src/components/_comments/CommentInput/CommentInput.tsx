bf1a94e4daa66ff9c1d6d4ba2052e65db45ebcfe
eldiegod <cardenasrdiego@gmail.com>
2022-05-03 11:53:28 +0200

    fix storys

--- packages/atlas/src/components/_comments/CommentInput/CommentInput.tsx
+++ packages/atlas/src/components/_comments/CommentInput/CommentInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import mergeRefs from 'react-merge-refs'
 import useResizeObserver from 'use-resize-observer'
 
--- packages/atlas/src/components/_comments/CommentRow/CommentRow.stories.tsx
+++ packages/atlas/src/components/_comments/CommentRow/CommentRow.stories.tsx
@@ -2,11 +2,14 @@ import { Meta, Story } from '@storybook/react'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
-import { Comment, CommentProps } from '../Comment'
+import { Text } from '@/components/Text'
+import { cVar } from '@/styles'
+
+import { CommentRow, CommentRowProps } from './CommentRow'
 
 export default {
-  title: 'comments/Comment',
-  component: Comment,
+  title: 'comments/CommentRow',
+  component: CommentRow,
   args: {
     memberAvatarUrl: 'https://placedog.net/100/100?random=2',
   },
@@ -22,8 +25,24 @@ export default {
       </BrowserRouter>
     ),
   ],
-} as Meta<CommentProps>
+} as Meta<CommentRowProps>
 
-const Template: Story<CommentProps> = (args) => <Comment {...args} />
+const Template: Story<CommentRowProps> = (args) => (
+  <CommentRow {...args}>
+    {/* children */}
+    <div
+      style={{
+        background: cVar('colorBackgroundPrimaryMuted'),
+      }}
+    >
+      <Text variant="h200">Please ignore styling of this blue box. It's for testing purposes only</Text>
+      <Text variant="t200">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur aut, ipsam sit impedit voluptatem ad libero
+        necessitatibus porro dignissimos voluptatum quis debitis tempora omnis, provident repellat iure accusantium,
+        blanditiis voluptate!{' '}
+      </Text>
+    </div>
+  </CommentRow>
+)
 
 export const Default = Template.bind({})
