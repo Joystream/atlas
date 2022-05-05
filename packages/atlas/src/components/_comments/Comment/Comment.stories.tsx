4c20eb46ebaf19064a0d207eaee89d4b0c78e4ed
Bartosz Dryl <drylbartosz@gmail.com>
2022-05-05 09:43:04 +0200

    Comment reactions (#2563)

--- packages/atlas/src/components/_comments/Comment/Comment.stories.tsx
+++ packages/atlas/src/components/_comments/Comment/Comment.stories.tsx
@@ -13,6 +13,11 @@ export default {
     loading: false,
     type: 'default',
     createdAt: new Date(1649761429792),
+    reactions: [
+      { count: 320, type: 'amusment' },
+      { count: 500, type: 'love', active: true },
+      { count: 100, type: 'shock' },
+    ],
     comment:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, facere molestias, iusto unde harum ducimus reprehenderit iste, magnam delectus aperiam impedit eligendi iure autem repellendus dolorum culpa sed cupiditate illum.',
   },
@@ -28,9 +33,33 @@ export default {
     onDeleteClick: { table: { disable: true } },
     onEditClick: { table: { disable: true } },
     onEditLabelClick: { table: { disable: true } },
+    onReactionClick: { table: { disable: true } },
+    reactions: { table: { disable: true } },
   },
 } as Meta<CommentProps>
 
 const Template: Story<CommentProps> = (args) => <Comment {...args} />
 
 export const Default = Template.bind({})
+export const NoReactions = Template.bind({})
+NoReactions.args = {
+  reactions: [],
+}
+export const ProcessingReaction = Template.bind({})
+ProcessingReaction.args = {
+  reactions: [
+    { count: 320, type: 'amusment' },
+    { count: 500, type: 'love', active: true, state: 'processing' },
+    { count: 100, type: 'shock' },
+  ],
+}
+export const AllReactionsApplied = Template.bind({})
+AllReactionsApplied.args = {
+  reactions: [
+    { count: 320, type: 'amusment' },
+    { count: 500, type: 'love' },
+    { count: 100, type: 'laugh' },
+    { count: 100, type: 'shock' },
+    { count: 100, type: 'anger', active: true },
+  ],
+}
