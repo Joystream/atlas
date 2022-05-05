76a52e639b9447eb174eafbdb5563f05d654971c
eldiegod <cardenasrdiego@gmail.com>
2022-05-05 13:46:29 +0200

    Not found page follow up

--- packages/atlas/src/views/viewer/NotFoundView/NotFoundView.tsx
+++ packages/atlas/src/views/viewer/NotFoundView/NotFoundView.tsx
@@ -14,29 +14,35 @@ export const NotFoundView = () => {
       <StyledPattern1 />
       <StyledPattern2 />
       <Container>
-        <SvgSmallCursor />
-        <Text align="center" variant="h600">
-          You have reached <br /> the end of the Internet
-        </Text>
-        <Text margin={{ top: 4, bottom: 16 }} align="center" variant="t300" secondary>
-          This page does not exist, but you can find lots of other cool content on our platform!
-        </Text>
-        <Button size="large" to="/">
-          Go to homepage
-        </Button>
+        <InnerContainer>
+          <SvgSmallCursor />
+          <Text align="center" variant="h600">
+            You have reached <br /> the end of the Internet
+          </Text>
+          <Text margin={{ top: 4, bottom: 16 }} align="center" variant="t300" secondary>
+            This page does not exist, but you can find lots of other cool content on our platform!
+          </Text>
+          <Button size="large" to="/">
+            Go to homepage
+          </Button>
+        </InnerContainer>
       </Container>
     </>
   )
 }
 
 const Container = styled.div`
-  position: relative;
-  left: 50%;
-  top: 50%;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const InnerContainer = styled.div`
   display: grid;
   justify-items: center;
   justify-content: center;
-  transform: translate(-50%, calc(-1 * (50vh - 50%)));
   max-width: 500px;
 `
 
