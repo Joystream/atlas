c2a0ec95cb8a20b9e5febf30fe97dfd3400b391a
Diego Cardenas <cardenasrdiego@gmail.com>
2022-05-12 11:44:57 +0200

    Not found page follow up (#2609)

--- packages/atlas/src/views/viewer/NotFoundView/NotFoundView.tsx
+++ packages/atlas/src/views/viewer/NotFoundView/NotFoundView.tsx
@@ -3,10 +3,7 @@ import React from 'react'
 
 import { Text } from '@/components/Text'
 import { Button } from '@/components/_buttons/Button'
-import { SvgSmallCursor } from '@/components/_illustrations'
-
-import { Pattern1 } from './Pattern1'
-import { Pattern2 } from './Pattern2'
+import { Svg404PatternBottomLeft, Svg404PatternTopRight, SvgSmallCursor } from '@/components/_illustrations'
 
 export const NotFoundView = () => {
   return (
@@ -14,39 +11,45 @@ export const NotFoundView = () => {
       <StyledPattern1 />
       <StyledPattern2 />
       <Container>
-        <SvgSmallCursor />
-        <Text align="center" variant="h600">
-          You have reached <br /> the end of the Internet
-        </Text>
-        <Text margin={{ top: 4, bottom: 16 }} align="center" variant="t300" secondary>
-          This page does not exist, but you can find lots of other cool content on our platform!
-        </Text>
-        <Button size="large" to="/">
-          Go to homepage
-        </Button>
+        <InnerContainer>
+          <SvgSmallCursor />
+          <Text align="center" variant="h600">
+            You have reached <br /> the end of the Internet
+          </Text>
+          <Text margin={{ top: 4, bottom: 16 }} align="center" variant="t300" secondary>
+            This page does not exist, but you can find lots of other cool content on our platform!
+          </Text>
+          <Button size="large" to="/">
+            Go to homepage
+          </Button>
+        </InnerContainer>
       </Container>
     </>
   )
 }
 
 const Container = styled.div`
-  position: relative;
-  left: 50%;
-  top: 50%;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const InnerContainer = styled.div`
   display: grid;
   justify-items: center;
   justify-content: center;
-  transform: translate(-50%, calc(-1 * (50vh - 50%)));
   max-width: 500px;
 `
 
-const StyledPattern1 = styled(Pattern1)`
+const StyledPattern1 = styled(Svg404PatternTopRight)`
   position: absolute;
   right: 0;
   top: 0;
 `
 
-const StyledPattern2 = styled(Pattern2)`
+const StyledPattern2 = styled(Svg404PatternBottomLeft)`
   position: absolute;
   left: 0;
   bottom: 0;
