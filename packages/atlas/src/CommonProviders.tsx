ed000d1bc7d0bafab6e30ae404f06ff1539f9d4e
Rafal Pawlow <pawlow.rafal@gmail.com>
2022-08-04 16:06:42 +0200

    handle dead Orion

--- packages/atlas/src/CommonProviders.tsx
+++ packages/atlas/src/CommonProviders.tsx
@@ -2,8 +2,12 @@ import { ApolloProvider } from '@apollo/client'
 import { FC, PropsWithChildren } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
+import { Maintenance } from '@/Maintenance'
 import { createApolloClient } from '@/api'
+import { useGetKillSwitch } from '@/api/hooks/admin'
+import { AdminModal } from '@/components/_overlays/AdminModal'
 import { AssetsManager, OperatorsContextProvider } from '@/providers/assets'
+import { OverlayManagerProvider } from '@/providers/overlayManager'
 import { GlobalStyles } from '@/styles'
 
 export const CommonProviders: FC<PropsWithChildren> = ({ children }) => {
@@ -13,12 +17,37 @@ export const CommonProviders: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
       <GlobalStyles />
-      <ApolloProvider client={apolloClient}>
-        <OperatorsContextProvider>
-          <AssetsManager />
-          <BrowserRouter>{children}</BrowserRouter>
-        </OperatorsContextProvider>
-      </ApolloProvider>
+      <OverlayManagerProvider>
+        <ApolloProvider client={apolloClient}>
+          <MaintenanceWrapper>
+            <OperatorsContextProvider>
+              <AssetsManager />
+              <BrowserRouter>{children}</BrowserRouter>
+            </OperatorsContextProvider>
+          </MaintenanceWrapper>
+        </ApolloProvider>
+      </OverlayManagerProvider>
+    </>
+  )
+}
+
+const MaintenanceWrapper: FC<PropsWithChildren> = ({ children }) => {
+  const { isKilled, wasKilledLastTime, error, loading } = useGetKillSwitch()
+
+  return (
+    <>
+      {isKilled || (error && wasKilledLastTime) || (loading && wasKilledLastTime) ? (
+        <>
+          <Maintenance />
+        </>
+      ) : (
+        children
+      )}
+      {(isKilled || (loading && wasKilledLastTime)) && !loading && !error && (
+        <BrowserRouter>
+          <AdminModal />
+        </BrowserRouter>
+      )}
     </>
   )
 }
--- packages/atlas/src/MainLayout.tsx
+++ packages/atlas/src/MainLayout.tsx
@@ -2,8 +2,6 @@ import loadable from '@loadable/component'
 import { FC, useEffect, useRef, useState } from 'react'
 import { Route, Routes, useLocation, useNavigationType } from 'react-router-dom'
 
-import { Maintenance } from '@/Maintenance'
-import { useGetKillSwitch } from '@/api/hooks/admin'
 import { SvgJoystreamLogoStudio } from '@/components/_illustrations'
 import { StudioLoading } from '@/components/_loaders/StudioLoading'
 import { AdminModal } from '@/components/_overlays/AdminModal'
@@ -35,7 +33,6 @@ const LoadablePlaygroundLayout = loadable(() => import('./views/playground/Playg
 })
 
 export const MainLayout: FC = () => {
-  const { isKilled, wasKilledLastTime, loading } = useGetKillSwitch()
   const scrollPosition = useRef<number>(0)
   const location = useLocation()
   const navigationType = useNavigationType()
@@ -81,19 +78,13 @@ export const MainLayout: FC = () => {
 
   return (
     <>
-      {isKilled || (loading && wasKilledLastTime) ? (
-        <Maintenance />
-      ) : (
-        <>
-          <CookiePopover />
-          <Routes>
-            <Route path={BASE_PATHS.viewer + '/*'} element={<ViewerLayout />} />
-            <Route path={BASE_PATHS.legal + '/*'} element={<LegalLayout />} />
-            <Route path={BASE_PATHS.studio + '/*'} element={<LoadableStudioLayout />} />
-            <Route path={BASE_PATHS.playground + '/*'} element={<LoadablePlaygroundLayout />} />
-          </Routes>
-        </>
-      )}
+      <CookiePopover />
+      <Routes>
+        <Route path={BASE_PATHS.viewer + '/*'} element={<ViewerLayout />} />
+        <Route path={BASE_PATHS.legal + '/*'} element={<LegalLayout />} />
+        <Route path={BASE_PATHS.studio + '/*'} element={<LoadableStudioLayout />} />
+        <Route path={BASE_PATHS.playground + '/*'} element={<LoadablePlaygroundLayout />} />
+      </Routes>
       <AdminModal />
     </>
   )
