b24f243c66317fb04ed997836f4f7670bee5aa0a
Rafal Pawlow <pawlow.rafal@gmail.com>
2022-08-12 10:09:55 +0200

    fix channel filtering for videos

--- packages/atlas/src/config/videoFilter.ts
+++ packages/atlas/src/config/videoFilter.ts
@@ -1,5 +1,9 @@
 import { VideoWhereInput } from '@/api/queries'
 
+import { channelFilter } from './channelFilter'
+
+const channelIdFilter = channelFilter.NOT?.find((item) => item.id_in)
+
 export const videoFilter: VideoWhereInput = {
   isCensored_eq: false,
   isPublic_eq: true,
@@ -9,5 +13,10 @@ export const videoFilter: VideoWhereInput = {
   media: {
     isAccepted_eq: true,
   },
-  NOT: [{ id_in: [] }, { thumbnailPhoto: { id_in: [] } }, { media: { id_in: [] } }],
+  NOT: [
+    { id_in: [] },
+    { thumbnailPhoto: { id_in: [] } },
+    { media: { id_in: [] } },
+    { channel: { id_in: channelIdFilter ? channelIdFilter.id_in : [] } },
+  ],
 }
