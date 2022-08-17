447350290e0994424cb31ae9a2de9105bfd867bc
Rafał Pawłow <pawlow.rafal@gmail.com>
2022-08-17 16:10:44 +0200

    Update packages/atlas/src/config/channelFilter.ts
    Co-authored-by: Bartosz Dryl <drylbartosz@gmail.com>

--- packages/atlas/src/config/channelFilter.ts
+++ packages/atlas/src/config/channelFilter.ts
@@ -3,5 +3,5 @@ import { ChannelWhereInput } from '@/api/queries'
 export const channelFilter: ChannelWhereInput = {
   isCensored_eq: false,
   isPublic_eq: true,
-  NOT: [{ id_in: ['3'] }, { avatarPhoto: { id_in: [] } }, { coverPhoto: { id_in: [] } }],
+  NOT: [{ id_in: [] }, { avatarPhoto: { id_in: [] } }, { coverPhoto: { id_in: [] } }],
 }
