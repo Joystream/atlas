5796125148593b9c366be082ee11c8fa061170a2
drillprop <drylbartosz@gmail.com>
2022-08-22 13:54:32 +0200

    bring back dialog in member dropdowns

--- packages/atlas/src/components/_overlays/MemberDropdown/MemberDropdown.tsx
+++ packages/atlas/src/components/_overlays/MemberDropdown/MemberDropdown.tsx
@@ -37,7 +37,8 @@ import {
   BalanceContainer,
   BlurredBG,
   ChannelsSectionTitle,
-  Container, // Divider,
+  Container,
+  Divider,
   Filter,
   InnerContainer,
   MemberHandleText,
@@ -213,18 +214,18 @@ export const MemberDropdown = forwardRef<HTMLDivElement, MemberDropdownProps>(
                             <SkeletonLoader width={30} height={20} />
                           )}
                           <BalanceContainer>
-                            {/*<TextLink*/}
-                            {/*  as="span"*/}
-                            {/*  onClick={() => {*/}
-                            {/*    closeDropdown?.()*/}
-                            {/*    publisher ? setShowWithdrawDialog(true) : setShowSendDialog(true)*/}
-                            {/*  }}*/}
-                            {/*  variant="t100"*/}
-                            {/*  color="colorCoreNeutral200Lighten"*/}
-                            {/*>*/}
-                            {/*  {publisher ? 'Withdraw' : 'Transfer'}*/}
-                            {/*</TextLink>*/}
-                            {/*<Divider />*/}
+                            <TextLink
+                              as="span"
+                              onClick={() => {
+                                closeDropdown?.()
+                                publisher ? setShowWithdrawDialog(true) : setShowSendDialog(true)
+                              }}
+                              variant="t100"
+                              color="colorCoreNeutral200Lighten"
+                            >
+                              {publisher ? 'Withdraw' : 'Transfer'}
+                            </TextLink>
+                            <Divider />
                             <TextLink
                               variant="t100"
                               as="a"
