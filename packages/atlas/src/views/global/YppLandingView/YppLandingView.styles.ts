b04a7b0db36ebf785da15dd526a3c469bbb1e069
drillprop <drylbartosz@gmail.com>
2022-09-08 08:35:22 +0200

    add mor sections on YppLandingView

--- packages/atlas/src/views/global/YppLandingView/YppLandingView.styles.ts
+++ packages/atlas/src/views/global/YppLandingView/YppLandingView.styles.ts
@@ -1,8 +1,13 @@
 import styled from '@emotion/styled'
 
+import bottomLeftPattern from '@/assets/images/ypp-background-pattern.svg'
+import topLeftBannerPattern from '@/assets/images/ypp-banner-pattern.svg'
 import { GridItem, LayoutGrid } from '@/components/LayoutGrid'
 import { LimitedWidthContainer } from '@/components/LimitedWidthContainer'
-import { media, sizes } from '@/styles'
+import { Text } from '@/components/Text'
+import { Button } from '@/components/_buttons/Button'
+import { CallToActionButton } from '@/components/_buttons/CallToActionButton'
+import { cVar, media, sizes } from '@/styles'
 
 export const StyledLimitedWidthContainer = styled(LimitedWidthContainer)`
   margin-top: ${sizes(16)};
@@ -40,7 +45,7 @@ export const BackImage = styled.img`
   top: 0;
 `
 
-export const YppProgramContanerGrid = styled(LayoutGrid)`
+export const CenteredLayoutGrid = styled(LayoutGrid)`
   text-align: center;
 `
 
@@ -48,17 +53,35 @@ export const HeroGridItem = styled(GridItem)`
   margin: ${sizes(16)} 0;
   align-self: center;
   ${media.md} {
-    margin-top: ${sizes(24)} 0;
+    margin: ${sizes(24)} 0;
   }
 `
 
-export const CardImageRow = styled(GridItem)`
+export const BackgroundContainer = styled.div`
+  background-color: ${cVar('colorBackgroundMuted')};
+  margin-left: calc(-1 * var(--size-global-horizontal-padding));
+  margin-right: calc(-1 * var(--size-global-horizontal-padding));
+  padding: 0 var(--size-global-horizontal-padding);
+`
+
+export const CardsWithImagesContainer = styled.div`
+  padding-top: ${sizes(16)};
+  padding-bottom: ${sizes(4)};
   display: grid;
-  gap: ${sizes(6)};
+  gap: ${sizes(16)};
+
   ${media.md} {
-    grid-template-columns: auto auto;
+    padding-top: ${sizes(24)};
+    padding-bottom: ${sizes(8)};
+  }
+`
+
+export const CardImageRow = styled(LayoutGrid)`
+  ${media.sm} {
+    justify-items: center;
+  }
+  ${media.sm} {
     align-items: center;
-    justify-content: center;
   }
 `
 
@@ -68,7 +91,6 @@ export const ImageContainer = styled.div<{
 }>`
   position: relative;
   overflow-x: hidden;
-  order: ${({ reverseOrderOnDesktop }) => (reverseOrderOnDesktop ? '-1' : 'unset')};
   display: flex;
   justify-content: ${({ positionOnMobile = 'unset' }) => positionOnMobile};
   ${media.sm} {
@@ -76,22 +98,72 @@ export const ImageContainer = styled.div<{
   }
 `
 
-export const AbsolutelyPositionedImg = styled.img<{ stickToRightSideOnMobile?: boolean }>`
+export const AbsolutelyPositionedImg = styled.img`
   position: absolute;
-  z-index: -1;
-  width: 480px;
+  z-index: 0;
+  width: 100%;
+  min-width: 480px;
+  max-width: 640px;
+`
+
+export const RelativelyPositionedImg = styled.img`
+  position: relative;
+  z-index: 1;
+  min-width: 480px;
+  width: 100%;
+  max-width: 640px;
+`
+
+export const StyledLimitedContainerWidth = styled(LimitedWidthContainer)`
+  padding-bottom: unset;
+`
+
+export const BannerContainerLayoutGrid = styled(LayoutGrid)`
+  padding: ${sizes(16)} 0;
   ${media.sm} {
-    right: unset;
-    width: 100%;
-    max-width: 640px;
+    padding: ${sizes(24)} 0;
   }
 `
 
-export const RelativelyPositionedImg = styled.img<{ stickToRightSideOnMobile?: boolean }>`
-  width: 480px;
+export const CtaBanner = styled.div`
+  padding: ${sizes(16)} ${sizes(8)};
+  background: ${cVar('colorBackgroundPrimary')};
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   ${media.sm} {
-    float: unset;
-    width: 100%;
-    max-width: 640px;
+    background-image: url(${bottomLeftPattern}), url(${topLeftBannerPattern});
+    background-position: bottom left, top right;
+    background-repeat: no-repeat, no-repeat;
   }
 `
+
+export const StyledBannerText = styled(Text)`
+  max-width: 400px;
+  ${media.md} {
+    max-width: 500px;
+  }
+  ${media.lg} {
+    max-width: unset;
+  }
+`
+
+export const StyledButton = styled(Button)`
+  margin-top: ${sizes(8)};
+  background-color: ${cVar('colorCoreBaseBlack')};
+`
+
+export const CtaCardRow = styled.div`
+  display: grid;
+  gap: ${sizes(4)};
+  padding-bottom: ${sizes(16)};
+
+  ${media.md} {
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: ${sizes(6)};
+    padding-bottom: ${sizes(24)};
+  }
+`
+
+export const StyledCallToActionButton = styled(CallToActionButton)``
--- packages/atlas/src/views/global/YppLandingView/YppLandingView.tsx
+++ packages/atlas/src/views/global/YppLandingView/YppLandingView.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react'
 import { useParallax } from 'react-scroll-parallax'
 
+import crt1 from '@/assets/images/illustration-crt-l1.webp'
+import crt2 from '@/assets/images/illustration-crt-l2.webp'
+import crt3 from '@/assets/images/illustration-crt-l3.webp'
 import myVideosBack from '@/assets/images/illustration-my-videos-page-l1.webp'
 import myVideosFront from '@/assets/images/illustration-my-videos-page-l2.webp'
+import videoNfts1 from '@/assets/images/illustration-video-nfts-l1.webp'
+import videoNfts2 from '@/assets/images/illustration-video-nfts-l2.webp'
+import videoNfts3 from '@/assets/images/illustration-video-nfts-l3.webp'
+import videoNfts4 from '@/assets/images/illustration-video-nfts-l4.webp'
 import dashboardImgBack from '@/assets/images/illustration-ypp-dashboard-l1.webp'
 import dashboardImgFront from '@/assets/images/illustration-ypp-dashboard-l2.webp'
 import youtubeSyncBack from '@/assets/images/illustration-ypp-sync-l1.webp'
@@ -19,20 +26,29 @@ import { ContentCard } from '@/components/ContentCard'
 import { GridItem, LayoutGrid } from '@/components/LayoutGrid'
 import { Text } from '@/components/Text'
 import { Button } from '@/components/_buttons/Button'
-import { SvgActionChevronR } from '@/components/_icons'
+import { SvgActionChevronR, SvgActionInfo, SvgActionSpeech, SvgActionTokensStack } from '@/components/_icons'
 import { useMediaMatch } from '@/hooks/useMediaMatch'
 
 import {
   AbsolutelyPositionedImg,
   BackImage,
+  BackgroundContainer,
+  BannerContainerLayoutGrid,
   CardImageRow,
+  CardsWithImagesContainer,
+  CenteredLayoutGrid,
+  CtaBanner,
+  CtaCardRow,
   FrontImage,
   HeroGridItem,
   HeroImageWrapper,
   ImageContainer,
   RelativelyPositionedImg,
+  StyledBannerText,
+  StyledButton,
+  StyledCallToActionButton,
+  StyledLimitedContainerWidth,
   StyledLimitedWidthContainer,
-  YppProgramContanerGrid,
 } from './YppLandingView.styles'
 
 export const YppLandingView: FC = () => {
@@ -71,73 +87,172 @@ export const YppLandingView: FC = () => {
           />
         </HeroImageWrapper>
       </StyledLimitedWidthContainer>
-      <YppProgramContanerGrid as="section">
-        <HeroGridItem as="header" colStart={{ sm: 3, lg: 4 }} colSpan={{ base: 12, sm: 8, lg: 6 }}>
-          <Text variant={mdMatch ? 'h800' : 'h600'} as="h2">
-            Get started in 3 steps
-          </Text>
-          <Text variant="t300" as="p" margin={{ top: 4, bottom: 8 }} color="colorText">
-            Our fully automated verification process is as simple as 1-2-3. If you don't have an Atlas channel already,
-            you'll be able to create one for free.{' '}
-          </Text>
-          <Button size="large" iconPlacement="right" icon={<SvgActionChevronR />}>
-            Sign up now
-          </Button>
-          <Text variant="t100" as="p" color="colorTextMuted" margin={{ top: 2 }}>
-            It takes 3 minutes and is 100% free.
-          </Text>
-        </HeroGridItem>
-      </YppProgramContanerGrid>
-      <YppProgramContanerGrid as="section">
-        <HeroGridItem as="header" colStart={{ sm: 3, lg: 4 }} colSpan={{ base: 12, sm: 8, lg: 6 }}>
-          <Text variant={mdMatch ? 'h800' : 'h600'} as="h2">
-            There is more to Youtube Partner Program
-          </Text>
-          <Text variant="t300" as="p" margin={{ top: 4 }} color="colorText">
-            New to Atlas? Joining our YouTube Partner Program is an exciting opportunity to try out the future of online
-            video sharing. And we're only getting started.
-          </Text>
-        </HeroGridItem>
-      </YppProgramContanerGrid>
-      <LayoutGrid>
-        <CardImageRow colSpan={{ base: 12 }}>
-          <ImageContainer>
-            <AbsolutelyPositionedImg src={dashboardImgBack} />
-            <RelativelyPositionedImg src={dashboardImgFront} />
-          </ImageContainer>
+      <BackgroundContainer>
+        <StyledLimitedContainerWidth as="section">
+          <CenteredLayoutGrid>
+            <HeroGridItem as="header" colStart={{ sm: 3, lg: 4 }} colSpan={{ base: 12, sm: 8, lg: 6 }}>
+              <Text variant={mdMatch ? 'h800' : 'h600'} as="h2">
+                Get started in 3 steps
+              </Text>
+              <Text variant="t300" as="p" margin={{ top: 4, bottom: 8 }} color="colorText">
+                Our fully automated verification process is as simple as 1-2-3. If you don't have an Atlas channel
+                already, you'll be able to create one for free.{' '}
+              </Text>
+              <Button size="large" iconPlacement="right" icon={<SvgActionChevronR />}>
+                Sign up now
+              </Button>
+              <Text variant="t100" as="p" color="colorTextMuted" margin={{ top: 2 }}>
+                It takes 3 minutes and is 100% free.
+              </Text>
+            </HeroGridItem>
+          </CenteredLayoutGrid>
+        </StyledLimitedContainerWidth>
+      </BackgroundContainer>
+      {/* ypp benefits */}
+      <StyledLimitedContainerWidth as="section">
+        <CardsWithImagesContainer>
+          <CenteredLayoutGrid>
+            <HeroGridItem as="header" colStart={{ sm: 3, lg: 4 }} colSpan={{ base: 12, sm: 8, lg: 6 }}>
+              <Text variant={mdMatch ? 'h800' : 'h600'} as="h2">
+                There is a lot more to YouTube Partner Program
+              </Text>
+              <Text variant="t300" as="p" margin={{ top: 4 }} color="colorText">
+                New to Atlas? Joining our YouTube Partner Program is an exciting opportunity to try out the future of
+                online video sharing. And we're only getting started.
+              </Text>
+            </HeroGridItem>
+          </CenteredLayoutGrid>
 
-          <ContentCard
-            title="Manage your channel on a simple dashboard"
-            subtitle="YPP dashboard"
-            body="There you can check your channel status and get instructions on completing all actions to get rewards"
-          />
-        </CardImageRow>
-        <CardImageRow colSpan={{ base: 12, md: 10 }} colStart={{ md: 2 }}>
-          <ImageContainer positionOnMobile="flex-end">
-            <AbsolutelyPositionedImg src={myVideosBack} stickToRightSideOnMobile />
-            <RelativelyPositionedImg src={myVideosFront} stickToRightSideOnMobile />
-          </ImageContainer>
-          <ContentCard
-            title="Backup your videos and own them forever"
-            subtitle="YouTube backup"
-            body="You can sleep peacefully knowing that your conent is safe and forever stored on blockchain. You can even sell it as NFTs — you own it forever."
-          />
-        </CardImageRow>
-        <CardImageRow colSpan={{ base: 12 }}>
-          <ImageContainer positionOnMobile="center">
-            <AbsolutelyPositionedImg src={youtubeSyncBack} />
-            <RelativelyPositionedImg src={youtubeSyncFront} />
-          </ImageContainer>
-          <ContentCard
-            pill={{
-              label: 'Coming later this year',
-            }}
-            title="Automatic YouTube video sync"
-            subtitle="YouTube sync"
-            body="Set up once and do nothing. Your videos will be automaticly synced from youtube to Atlas and you will still earn for it passively."
-          />
-        </CardImageRow>
-      </LayoutGrid>
+          <CardImageRow>
+            <GridItem colStart={{ sm: 2, md: 1, lg: 2 }} colSpan={{ base: 12, sm: 10, md: 7, lg: 6 }}>
+              <ImageContainer>
+                <AbsolutelyPositionedImg src={dashboardImgBack} />
+                <RelativelyPositionedImg src={dashboardImgFront} />
+              </ImageContainer>
+            </GridItem>
+            <GridItem colStart={{ sm: 3, md: 8 }} colSpan={{ base: 12, sm: 8, md: 5, lg: 4 }}>
+              <ContentCard
+                title="Manage your channel on a simple dashboard"
+                subtitle="YPP dashboard"
+                body="There you can check your channel status and get instructions on completing all actions to get rewards"
+              />
+            </GridItem>
+          </CardImageRow>
+
+          <CardImageRow>
+            <GridItem colStart={{ sm: 2, md: 6, lg: 6 }} colSpan={{ base: 12, sm: 10, md: 7, lg: 6 }}>
+              <ImageContainer positionOnMobile="flex-end">
+                <AbsolutelyPositionedImg src={myVideosBack} />
+                <RelativelyPositionedImg src={myVideosFront} />
+              </ImageContainer>
+            </GridItem>
+            <GridItem
+              colStart={{ sm: 3, md: 1, lg: 2 }}
+              rowStart={{ md: 1 }}
+              colSpan={{ base: 12, sm: 8, md: 5, lg: 4 }}
+            >
+              <ContentCard
+                title="Backup your videos and own them forever"
+                subtitle="YouTube backup"
+                body="You can sleep peacefully knowing that your conent is safe and forever stored on blockchain. You can even sell it as NFTs — you own it forever."
+              />
+            </GridItem>
+          </CardImageRow>
+
+          <CardImageRow>
+            <GridItem colStart={{ sm: 2, md: 1, lg: 2 }} colSpan={{ base: 12, sm: 10, md: 7, lg: 6 }}>
+              <ImageContainer positionOnMobile="center">
+                <AbsolutelyPositionedImg src={youtubeSyncBack} />
+                <RelativelyPositionedImg src={youtubeSyncFront} />
+              </ImageContainer>
+            </GridItem>
+            <GridItem colStart={{ sm: 3, md: 8 }} colSpan={{ base: 12, sm: 8, md: 5, lg: 4 }}>
+              <ContentCard
+                pill={{
+                  label: 'Coming later this year',
+                }}
+                title="Automatic YouTube video sync"
+                subtitle="YouTube sync"
+                body="Set up once and do nothing. Your videos will be automaticly synced from youtube to Atlas and you will still earn for it passively."
+              />
+            </GridItem>
+          </CardImageRow>
+        </CardsWithImagesContainer>
+      </StyledLimitedContainerWidth>
+
+      {/* nfts */}
+      <BackgroundContainer>
+        <StyledLimitedContainerWidth>
+          <CardsWithImagesContainer>
+            <CardImageRow>
+              <GridItem colStart={{ sm: 2, md: 1, lg: 2 }} colSpan={{ base: 12, sm: 10, md: 7, lg: 6 }}>
+                <ImageContainer positionOnMobile="center">
+                  <AbsolutelyPositionedImg src={videoNfts1} />
+                  <AbsolutelyPositionedImg src={videoNfts2} />
+                  <AbsolutelyPositionedImg src={videoNfts3} />
+                  <RelativelyPositionedImg src={videoNfts4} />
+                </ImageContainer>
+              </GridItem>
+              <GridItem colStart={{ sm: 3, md: 8 }} colSpan={{ base: 12, sm: 8, md: 5, lg: 4 }}>
+                <ContentCard
+                  title="Monetise your videos with NFT sales and royalties"
+                  subtitle="Video NFTs"
+                  body="Turn your videos into NFTs and put them up for sale.  Choose between fixed price, open auction, and timed auction. Define royalties to get a cut every time your NFT gets sold again.There you can check your channel status and get instructions on completing all actions to get rewards"
+                />
+              </GridItem>
+            </CardImageRow>
+
+            <CardImageRow>
+              <GridItem colStart={{ sm: 2, md: 6, lg: 6 }} colSpan={{ base: 12, sm: 10, md: 7, lg: 6 }}>
+                <ImageContainer>
+                  <AbsolutelyPositionedImg src={crt1} />
+                  <AbsolutelyPositionedImg src={crt2} />
+                  <RelativelyPositionedImg src={crt3} />
+                </ImageContainer>
+              </GridItem>
+              <GridItem
+                colStart={{ sm: 3, md: 1, lg: 2 }}
+                rowStart={{ md: 1 }}
+                colSpan={{ base: 12, sm: 8, md: 5, lg: 4 }}
+              >
+                <ContentCard
+                  title="New ways to engage and get support from your audience"
+                  subtitle="CREATOR TOKENS"
+                  body="Turn viewers into evengelists [?] by letting them hold a share in your channel when they buy your own channel token."
+                />
+              </GridItem>
+            </CardImageRow>
+          </CardsWithImagesContainer>
+        </StyledLimitedContainerWidth>
+      </BackgroundContainer>
+      <StyledLimitedContainerWidth>
+        <BannerContainerLayoutGrid>
+          <GridItem colStart={{ lg: 2 }} colSpan={{ base: 12, lg: 10 }}>
+            <CtaBanner>
+              <Text variant="h100" as="p" color="colorText">
+                Get started now
+              </Text>
+              <StyledBannerText
+                variant={mdMatch ? 'h700' : 'h600'}
+                as="h2"
+                color="colorCoreBaseWhite"
+                margin={{ top: 1 }}
+              >
+                Get the most out of your YouTube channel
+              </StyledBannerText>
+
+              <StyledButton size="large" icon={<SvgActionChevronR />} iconPlacement="right">
+                Authorize with YouTube
+              </StyledButton>
+            </CtaBanner>
+          </GridItem>
+        </BannerContainerLayoutGrid>
+      </StyledLimitedContainerWidth>
+      <CtaCardRow>
+        <StyledCallToActionButton colorVariant="lightBlue" icon={<SvgActionInfo />} label="Program details" />
+        <StyledCallToActionButton colorVariant="lightBlue" icon={<SvgActionSpeech />} label="Discord" />
+        <StyledCallToActionButton colorVariant="lightBlue" icon={<SvgActionTokensStack />} label="Payments" />
+      </CtaCardRow>
     </>
   )
 }
