097d2b31e9eea06bb7184a0cbd5c9ac85c0cbca1
Bartosz Dryl <drylbartosz@gmail.com>
2022-09-15 20:56:46 +0200

    Ypp landing page (#3230)

---
+++ packages/atlas/src/views/global/YppLandingView/YppFooter.styles.ts
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled'
+
+import bottomLeftPattern from '@/assets/images/ypp-background-pattern.svg'
+import topLeftBannerPattern from '@/assets/images/ypp-banner-pattern.svg'
+import { Text } from '@/components/Text'
+import { Button } from '@/components/_buttons/Button'
+import { cVar, media, sizes } from '@/styles'
+
+export const CtaBanner = styled.div`
+  padding: ${sizes(16)} ${sizes(8)};
+  background: ${cVar('colorBackgroundPrimary')};
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: ${sizes(16)} 0;
+  ${media.sm} {
+    margin: ${sizes(24)} 0;
+    background-image: url(${bottomLeftPattern}), url(${topLeftBannerPattern});
+    background-position: bottom left, top right;
+    background-repeat: no-repeat, no-repeat;
+  }
+  ${media.md} {
+    padding: ${sizes(24)} ${sizes(12)};
+  }
+`
+
+export const StyledBannerText = styled(Text)`
+  max-width: 400px;
+  ${media.md} {
+    max-width: 500px;
+  }
+  ${media.lg} {
+    max-width: unset;
+  }
+`
+
+export const StyledButton = styled(Button)`
+  margin-top: ${sizes(8)};
+  background-color: ${cVar('colorCoreBaseBlack')};
+`
+
+export const CtaCardRow = styled.div`
+  display: grid;
+  gap: ${sizes(4)};
+  padding-bottom: ${sizes(16)};
+
+  ${media.md} {
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: ${sizes(6)};
+    padding-bottom: ${sizes(24)};
+  }
+`
---
+++ packages/atlas/src/views/global/YppLandingView/YppFooter.tsx
@@ -0,0 +1,53 @@
+import { FC } from 'react'
+
+import { GridItem, LayoutGrid } from '@/components/LayoutGrid'
+import { Text } from '@/components/Text'
+import { CallToActionButton } from '@/components/_buttons/CallToActionButton'
+import { SvgActionChevronR, SvgActionInfo, SvgActionSpeech, SvgActionTokensStack } from '@/components/_icons'
+import { JOYSTREAM_DISCORD_URL } from '@/config/env'
+import { useMediaMatch } from '@/hooks/useMediaMatch'
+
+import { CtaBanner, CtaCardRow, StyledBannerText, StyledButton } from './YppFooter.styles'
+import { StyledLimitedWidthContainer } from './YppLandingView.styles'
+
+export const YppFooter: FC = () => {
+  const mdMatch = useMediaMatch('md')
+  return (
+    <>
+      <StyledLimitedWidthContainer centerText>
+        <LayoutGrid>
+          <GridItem colStart={{ lg: 2 }} colSpan={{ base: 12, lg: 10 }}>
+            <CtaBanner>
+              <Text variant="h100" as="p" color="colorText">
+                Get started now
+              </Text>
+              <StyledBannerText
+                variant={mdMatch ? 'h700' : 'h600'}
+                as="h2"
+                color="colorCoreBaseWhite"
+                margin={{ top: 1 }}
+              >
+                Get the most out of your YouTube channel
+              </StyledBannerText>
+
+              <StyledButton size="large" icon={<SvgActionChevronR />} iconPlacement="right">
+                Authorize with YouTube
+              </StyledButton>
+            </CtaBanner>
+          </GridItem>
+        </LayoutGrid>
+      </StyledLimitedWidthContainer>
+      <CtaCardRow>
+        <CallToActionButton external colorVariant="lightBlue" icon={<SvgActionInfo />} label="Program details" />
+        <CallToActionButton
+          to={JOYSTREAM_DISCORD_URL}
+          external
+          colorVariant="lightBlue"
+          icon={<SvgActionSpeech />}
+          label="Discord"
+        />
+        <CallToActionButton external colorVariant="lightBlue" icon={<SvgActionTokensStack />} label="Payments" />
+      </CtaCardRow>
+    </>
+  )
+}
