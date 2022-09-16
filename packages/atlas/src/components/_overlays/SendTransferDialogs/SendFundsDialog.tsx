dfbc11f003c35bcbc9baee4ef5bafde2123dcb9a
Rafal Pawlow <pawlow.rafal@gmail.com>
2022-09-16 14:33:08 +0200

    add tooltip for SendFunds avatar

--- packages/atlas/src/components/_overlays/SendTransferDialogs/SendFundsDialog.tsx
+++ packages/atlas/src/components/_overlays/SendTransferDialogs/SendFundsDialog.tsx
@@ -14,6 +14,7 @@ import { Avatar, AvatarProps } from '@/components/Avatar'
 import { Fee } from '@/components/Fee'
 import { NumberFormat } from '@/components/NumberFormat'
 import { Text } from '@/components/Text'
+import { Tooltip } from '@/components/Tooltip'
 import { JoyTokenIcon } from '@/components/_icons/JoyTokenIcon'
 import { FormField } from '@/components/_inputs/FormField'
 import { Input } from '@/components/_inputs/Input'
@@ -231,9 +232,13 @@ export const SendFundsDialog: FC<SendFundsDialogProps> = ({ onExitClick, account
 }
 
 type ResolvedAvatarProps = {
-  member?: BasicMembershipFieldsFragment
+  member: BasicMembershipFieldsFragment
 } & AvatarProps
 const ResolvedAvatar: FC<ResolvedAvatarProps> = ({ member }) => {
   const { url, isLoadingAsset } = useMemberAvatar(member)
-  return <Avatar assetUrl={url} loading={isLoadingAsset} size="bid" />
+  return (
+    <Tooltip text={member?.handle} placement="top">
+      <Avatar assetUrl={url} loading={isLoadingAsset} size="bid" />
+    </Tooltip>
+  )
 }
