4a65ace80d5c61800f4d1986e9199d079d71b0e5
Bartosz Dryl <drylbartosz@gmail.com>
2022-09-16 10:46:17 +0200

    Reward section (#3242)
    * add reward section
    
    * add BenefitsCardButton
    
    * refactor reward section
    
    * remove redundant container

--- packages/atlas/src/views/global/YppLandingView/YppLandingView.tsx
+++ packages/atlas/src/views/global/YppLandingView/YppLandingView.tsx
@@ -6,6 +6,7 @@ import { useHeadTags } from '@/hooks/useHeadTags'
 import { YppCardsSections } from './YppCardsSections'
 import { YppFooter } from './YppFooter'
 import { YppHero } from './YppHero'
+import { YppRewardSection } from './YppRewardSection'
 import { YppThreeStepsSection } from './YppThreeStepsSection'
 
 export const YppLandingView: FC = () => {
@@ -15,6 +16,7 @@ export const YppLandingView: FC = () => {
       {headTags}
       <ParallaxProvider>
         <YppHero />
+        <YppRewardSection />
         <YppThreeStepsSection />
         <YppCardsSections />
         <YppFooter />
