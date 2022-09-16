52773186940d8dc298fa25443d19303f2684f599
Bartosz Dryl <drylbartosz@gmail.com>
2022-09-16 10:05:33 +0200

    Add YPP missing headtags (#3260)

--- packages/atlas/src/views/global/YppLandingView/YppLandingView.tsx
+++ packages/atlas/src/views/global/YppLandingView/YppLandingView.tsx
@@ -1,18 +1,24 @@
 import { FC } from 'react'
 import { ParallaxProvider } from 'react-scroll-parallax'
 
+import { useHeadTags } from '@/hooks/useHeadTags'
+
 import { YppCardsSections } from './YppCardsSections'
 import { YppFooter } from './YppFooter'
 import { YppHero } from './YppHero'
 import { YppThreeStepsSection } from './YppThreeStepsSection'
 
 export const YppLandingView: FC = () => {
+  const headTags = useHeadTags('Youtube Partner Program')
   return (
-    <ParallaxProvider>
-      <YppHero />
-      <YppThreeStepsSection />
-      <YppCardsSections />
-      <YppFooter />
-    </ParallaxProvider>
+    <>
+      {headTags}
+      <ParallaxProvider>
+        <YppHero />
+        <YppThreeStepsSection />
+        <YppCardsSections />
+        <YppFooter />
+      </ParallaxProvider>
+    </>
   )
 }
