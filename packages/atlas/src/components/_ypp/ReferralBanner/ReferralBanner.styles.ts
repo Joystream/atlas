edef43c270f2afccb40a4f639f32ca9f29d9c80a
Klaudiusz Dembler <accounts@kdembler.com>
2022-09-21 16:25:57 +0200

    update mobile banner padding

--- packages/atlas/src/components/_ypp/ReferralBanner/ReferralBanner.styles.ts
+++ packages/atlas/src/components/_ypp/ReferralBanner/ReferralBanner.styles.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import { Link } from 'react-router-dom'
 
 import { Text } from '@/components/Text'
-import { cVar, sizes } from '@/styles'
+import { cVar, media, sizes } from '@/styles'
 
 export const InformationWrapper = styled.div`
   display: inline-grid;
@@ -13,12 +13,15 @@ export const InformationWrapper = styled.div`
 
 export const ReferralBannerContainer = styled.div`
   width: 100%;
-  padding: ${sizes(4)} ${sizes(8)};
   display: grid;
   grid-template-columns: auto max-content;
   justify-content: space-between;
   column-gap: ${sizes(2)};
   background-color: ${cVar('colorBackgroundMuted')};
+  padding: ${sizes(4)} ${sizes(4)};
+  ${media.md} {
+    padding: ${sizes(4)} ${sizes(8)};
+  }
 `
 
 export const ChannelInfoContainer = styled.div`
