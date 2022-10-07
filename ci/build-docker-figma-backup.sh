1ec28a0e45eb988b1c6073a28ee4adb3f609fde8
Rafal Pawlow <pawlow.rafal@gmail.com>
2022-10-07 14:58:39 +0200

    Figma backup POC

---
+++ ci/build-docker-figma-backup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t joystream/atlas-figma-backup-service -f packages/atlas-figma-backup-service/Dockerfile .
---
+++ packages/atlas-figma-backup-service/index.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+import { spawn } from 'child_process'
+import { differenceInDays } from 'date-fns'
+import { config } from 'dotenv'
+import fs from 'fs'
+import { scheduleJob } from 'node-schedule'
+
+config()
+const BACKUPS_PATH = './figma-backup-root/backups'
+const DOWNLOAD_TIMEOUT = 15
+
+const init = () => {
+  fs.readdir(BACKUPS_PATH, (err, files) => {
+    if (files && files.length) {
+      files.forEach((file) => {
+        if (differenceInDays(new Date(), new Date(file)) > 30) {
+          fs.rm(`${BACKUPS_PATH}/${file}`, { force: true, recursive: true }, (error) => {
+            if (error) {
+              console.error(error)
+            }
+            console.log('Old backups have been removed')
+          })
+        }
+      })
+    }
+
+    const figmaBackup = spawn('figma-backup', [
+      '-e',
+      process.env.FIGMA_EMAIL,
+      '-p',
+      process.env.FIGMA_PASSWORD,
+      '-t',
+      process.env.FIGMA_TOKEN,
+      '--download-timeout',
+      DOWNLOAD_TIMEOUT,
+      '--projects-ids',
+      ...process.env.FIGMA_IDS.split(' '),
+    ])
+
+    figmaBackup.stdout.on('data', (data) => {
+      process.stdout.write(data)
+    })
+  })
+}
+
+scheduleJob({ hour: '00', minute: '00' }, () => {
+  console.info(`[${new Date()}] Starting figma backup`)
+  init()
+})
