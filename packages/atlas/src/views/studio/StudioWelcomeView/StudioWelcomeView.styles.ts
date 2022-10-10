c4caeb2ec1377dd799c5c0393e52dae93e4347d2
drillprop <drylbartosz@gmail.com>
2022-10-10 15:52:14 +0200

    basic setup for mobile view

--- packages/atlas/src/views/studio/StudioWelcomeView/StudioWelcomeView.styles.ts
+++ packages/atlas/src/views/studio/StudioWelcomeView/StudioWelcomeView.styles.ts
@@ -1,34 +1,35 @@
+import { css } from '@emotion/react'
 import styled from '@emotion/styled'
+import { Link } from 'react-router-dom'
 
 import { SvgSigninIllustration } from '@/assets/illustrations'
+import { SvgJoystreamLogoFull } from '@/assets/logos'
+import { GridItem, LayoutGrid } from '@/components/LayoutGrid'
 import { LimitedWidthContainer } from '@/components/LimitedWidthContainer'
 import { Text } from '@/components/Text'
 import { Button } from '@/components/_buttons/Button'
-import { media, sizes } from '@/styles'
+import { cVar, media, sizes } from '@/styles'
+
+const XXS_IMAGE_WIDTH = 360
+const XXS_IMAGE_HEIGHT = 225
+const XS_IMAGE_WIDTH = 480
+const XS_IMAGE_HEIGHT = 300
+const SM_IMAGE_WIDTH = 720
+const SM_IMAGE_HEIGHT = 450
 
 export const StyledContainer = styled(LimitedWidthContainer)`
-  margin-top: 64px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding-bottom: 100px;
-  ${media.md} {
-    flex-direction: row;
-    justify-content: space-between;
-  }
-  ${media.xl} {
-    max-width: 1400px;
-    left: 150px;
-  }
-  ${media.xxl} {
-    max-width: 1700px;
-    margin-top: 128px;
-  }
-`
+  overflow-x: hidden;
+  padding: ${sizes(12)} var(--size-global-horizontal-padding);
 
-export const Header = styled.header`
-  max-width: 550px;
+  /* negative margin for the purpose of overflowing images */
+  margin: 0 calc(-1 * var(--size-global-horizontal-padding)) 0;
+`
+export const ContentLayoutGrid = styled(LayoutGrid)``
+
+export const HeaderGridItem = styled(GridItem)`
+  ${media.sm} {
+    max-width: 550px;
+  }
 `
 
 export const StyledSignInIllustrationSVG = styled(SvgSigninIllustration)`
@@ -52,20 +53,92 @@ export const ButtonGroup = styled.div`
   margin-top: ${sizes(12)};
   display: flex;
   flex-direction: column;
-  ${media.sm} {
-    flex-direction: row;
-  }
 `
 
 export const SignInButton = styled(Button)`
-  margin-bottom: ${sizes(4)};
-  ${media.sm} {
-    margin-bottom: unset;
-    margin-right: ${sizes(4)};
-  }
+  margin-bottom: ${sizes(6)};
 `
 
 export const SubTitle = styled(Text)`
   display: block;
   margin-top: ${sizes(4)};
+`
+
+const linkStyles = css`
+  text-decoration: none;
+  color: ${cVar('colorTextMuted')};
+  align-items: center;
+  display: flex;
+
+  :hover,
+  :focus {
+    color: ${cVar('colorText')};
+  }
+
+  :hover path,
+  :focus path {
+    fill: ${cVar('colorText')};
+  }
+`
+
+export const LinksGroup = styled.div`
+  justify-content: start;
+  display: grid;
+  color: ${cVar('colorTextMuted')};
+  grid-auto-flow: column;
+  align-items: center;
+  gap: ${sizes(2)};
+`
+
+export const StyledAnchor = styled.a`
+  ${linkStyles}
+`
+
+export const StyledSvgJoystreamLogoFull = styled(SvgJoystreamLogoFull)`
+  height: 16px;
+  margin-left: ${sizes(2)};
+  width: unset;
+
+  path {
+    fill: ${cVar('colorTextMuted')};
+  }
+`
+
+export const StyledLink = styled(Link)`
+  ${linkStyles}
+`
+
+export const ImageLayoutGrid = styled(LayoutGrid)`
+  margin-top: ${sizes(12)};
+`
+
+export const IllustrationWrapper = styled.div`
+  position: relative;
+  height: ${XXS_IMAGE_HEIGHT}px;
+  ${media.xs} {
+    height: ${XS_IMAGE_HEIGHT}px;
+  }
+  ${media.sm} {
+    height: ${SM_IMAGE_HEIGHT}px;
+  }
+`
+
+type StyledIllustrationProps = {
+  stickToTheRightEdge?: boolean
+}
+
+export const StyledIllustration = styled.img<StyledIllustrationProps>`
+  position: absolute;
+  top: 0;
+  right: ${({ stickToTheRightEdge }) => (stickToTheRightEdge ? '0' : 'unset')};
+  width: ${XXS_IMAGE_WIDTH}px;
+  height: ${XXS_IMAGE_HEIGHT}px;
+  ${media.xs} {
+    height: ${XS_IMAGE_HEIGHT}px;
+    width: ${XS_IMAGE_WIDTH}px;
+  }
+  ${media.sm} {
+    height: ${SM_IMAGE_HEIGHT}px;
+    width: ${SM_IMAGE_WIDTH}px;
+  }
 `
--- packages/atlas/src/views/studio/StudioWelcomeView/StudioWelcomeView.tsx
+++ packages/atlas/src/views/studio/StudioWelcomeView/StudioWelcomeView.tsx
@@ -1,20 +1,32 @@
 import { FC } from 'react'
 
-import { SvgActionChannel, SvgActionInformative, SvgActionMember } from '@/assets/icons'
+import { SvgActionChannel, SvgActionMember, SvgActionPlay } from '@/assets/icons'
+import downloadsMockup from '@/assets/images/downloads-mockup.webp'
+import myVideosMockup from '@/assets/images/my-videos-mockup.webp'
+import nftWorkspaceMockup from '@/assets/images/nft-workspace-mockup.webp'
+import videoWorkspaceMockup from '@/assets/images/video-workspace-mockup.webp'
+import { GridItem } from '@/components/LayoutGrid'
 import { Text } from '@/components/Text'
-import { Button } from '@/components/_buttons/Button'
+import { TextButton } from '@/components/_buttons/Button'
 import { atlasConfig } from '@/config'
 import { absoluteRoutes } from '@/config/routes'
 import { useDisplaySignInDialog } from '@/hooks/useDisplaySignInDialog'
 import { useHeadTags } from '@/hooks/useHeadTags'
+import { useMediaMatch } from '@/hooks/useMediaMatch'
 import { useUser } from '@/providers/user/user.hooks'
 
 import {
   ButtonGroup,
-  Header,
+  ContentLayoutGrid,
+  HeaderGridItem,
+  IllustrationWrapper,
+  ImageLayoutGrid,
+  LinksGroup,
   SignInButton,
+  StyledAnchor,
   StyledContainer,
-  StyledSignInIllustrationSVG,
+  StyledIllustration,
+  StyledSvgJoystreamLogoFull,
   SubTitle,
 } from './StudioWelcomeView.styles'
 
@@ -29,35 +41,70 @@ export const StudioWelcomeView: FC = () => {
   const { signIn, isLoggedIn } = useUser()
   const headTags = useHeadTags('Studio')
   const { openSignInDialog } = useDisplaySignInDialog()
+  const mdMatch = useMediaMatch('md')
 
   return (
     <>
       {headTags}
       <StyledContainer>
-        <Header>
-          <Text as="h1" variant="h800" margin={{ top: 8 }}>
-            Welcome to {atlasConfig.general.appName} Studio
-          </Text>
-          <SubTitle as="p" variant="t300" color="colorText">
-            Start your journey as a {atlasConfig.general.appName} content creator. Manage your channels, publish video
-            content, issue NFTs, and more!
-          </SubTitle>
-          <ButtonGroup>
-            {isLoggedIn ? (
-              <SignInButton icon={<SvgActionChannel />} size="large" to={absoluteRoutes.studio.newChannel()}>
-                Create first channel
-              </SignInButton>
-            ) : (
-              <SignInButton icon={<SvgActionMember />} size="large" onClick={() => signIn(undefined, openSignInDialog)}>
-                Connect wallet
-              </SignInButton>
-            )}
-            <Button variant="secondary" icon={<SvgActionInformative />} size="large" to="https://www.joystream.org/">
-              How it works?
-            </Button>
-          </ButtonGroup>
-        </Header>
-        <StyledSignInIllustrationSVG />
+        <ContentLayoutGrid>
+          <HeaderGridItem colSpan={{ xxs: 12, sm: 8 }} colStart={{ sm: 3 }}>
+            <Text as="h1" variant="h100" margin={{ top: 8 }} color="colorTextPrimary">
+              {atlasConfig.general.appName} studio
+            </Text>
+            <Text as="h1" variant={mdMatch ? 'h700' : 'h600'} margin={{ top: 2 }}>
+              Your creator journey starts here
+            </Text>
+            <SubTitle as="p" variant="t300" color="colorText">
+              To create a channel, first set up a free Joystream membership with our simple step-by-step wizard.
+            </SubTitle>
+            <ButtonGroup>
+              {isLoggedIn ? (
+                <SignInButton icon={<SvgActionChannel />} size="large" to={absoluteRoutes.studio.newChannel()}>
+                  Create first channel
+                </SignInButton>
+              ) : (
+                <SignInButton
+                  icon={<SvgActionMember />}
+                  size="large"
+                  onClick={() => signIn(undefined, openSignInDialog)}
+                >
+                  Connect wallet
+                </SignInButton>
+              )}
+              <TextButton variant="tertiary" icon={<SvgActionPlay />} size="large" to={absoluteRoutes.viewer.index()}>
+                Go to {atlasConfig.general.appName}
+              </TextButton>
+            </ButtonGroup>
+            <LinksGroup>
+              <StyledAnchor href={atlasConfig.general.joystreamLandingPageUrl} target="_blank">
+                <Text as="span" variant="t100" color="inherit">
+                  Powered by
+                </Text>
+                <StyledSvgJoystreamLogoFull />
+              </StyledAnchor>
+              <Text as="span" variant="t100" color="inherit">
+                •
+              </Text>
+            </LinksGroup>
+          </HeaderGridItem>
+        </ContentLayoutGrid>
+        <ImageLayoutGrid>
+          <GridItem rowStart={1} colSpan={{ xxs: 12, sm: 8 }} colStart={{ sm: 3 }}>
+            <IllustrationWrapper>
+              <StyledIllustration src={isLoggedIn ? downloadsMockup : myVideosMockup} alt="" />
+            </IllustrationWrapper>
+          </GridItem>
+          <GridItem rowStart={2} colSpan={{ xxs: 12, sm: 10 }} colStart={{ sm: 3 }}>
+            <IllustrationWrapper>
+              <StyledIllustration
+                src={isLoggedIn ? nftWorkspaceMockup : videoWorkspaceMockup}
+                alt=""
+                stickToTheRightEdge
+              />
+            </IllustrationWrapper>
+          </GridItem>
+        </ImageLayoutGrid>
       </StyledContainer>
     </>
   )
