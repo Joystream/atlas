ef4169a292f7288b1c675b567e8d4ee1fe762767
drillprop <drylbartosz@gmail.com>
2022-11-16 14:01:44 +0100

    revert prev changes

--- packages/atlas/vite.config.ts
+++ packages/atlas/vite.config.ts
@@ -81,7 +81,7 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
-    include: ['blake3/browser-async', 'multihashes', 'buffer', 'crypto-js'],
+    include: ['blake3/browser-async', 'multihashes', 'buffer'],
     esbuildOptions: {
       define: {
         global: 'globalThis',
