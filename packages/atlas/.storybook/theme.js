58de398a76c7491c169fc4c04b2cbfc65e9087a4
drillprop <drylbartosz@gmail.com>
2022-11-23 15:30:31 +0100

    bring back joystream logo in storybook

--- packages/atlas/.storybook/theme.js
+++ packages/atlas/.storybook/theme.js
@@ -2,9 +2,7 @@ import { create } from '@storybook/theming'
 
 export default create({
   base: 'dark',
-
   brandTitle: '@joystream/atlas',
   brandUrl: 'https://joystream.org',
-  brandImage:
-    'https://raw.githubusercontent.com/Joystream/design/master/logo/logo/PNG/Logo-horisontal-basic-white-1x.png',
+  brandImage: './assets/logos/svgs/joystream-logo-full.svg',
 })
