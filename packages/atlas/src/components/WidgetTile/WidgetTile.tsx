117f7e152f4f68e6e8fd7347676b89a0b6688b51
drillprop <drylbartosz@gmail.com>
2022-11-24 10:15:16 +0100

    add conditional props to WidgetTile

--- packages/atlas/src/components/WidgetTile/WidgetTile.tsx
+++ packages/atlas/src/components/WidgetTile/WidgetTile.tsx
@@ -6,87 +6,100 @@ import { TooltipProps } from '@/components/Tooltip'
 import { ButtonProps } from '@/components/_buttons/Button'
 import { SkeletonLoader } from '@/components/_loaders/SkeletonLoader'
 import { useMediaMatch } from '@/hooks/useMediaMatch'
+import { ConsoleLogger } from '@/utils/logs'
 
 import { Content, StyledButton, TextWrapper, Title, Wrapper } from './WidgetTile.styles'
 
 type TopRightElement =
   | {
-      type: 'custom'
-      node: ReactNode
+      customTopRightNode: ReactNode
+      tooltip?: never
     }
   | {
-      type: 'tooltip'
-      tooltipProps: TooltipProps
+      customTopRightNode?: never
+      tooltip?: TooltipProps
     }
 
 type ContentElement =
   | {
-      type: 'custom'
-      node: ReactNode
+      customNode: ReactNode
+      caption?: never
+      icon?: never
+      text?: never
     }
-  | { type: 'text'; text: string; caption?: ReactNode }
+  | { customNode?: never; text: string | number; caption?: ReactNode; icon?: ReactNode }
 
 export type WidgetTileProps = {
   loading?: boolean
   title: string | number
-  text: string | number
-  caption?: ReactNode
-  icon?: ReactNode
   button: { text: string } & Omit<ButtonProps, 'children'>
-  tooltip?: TooltipProps
   customNode?: ReactNode
-  topRightElement?: TopRightElement
-  contentElement?: ContentElement
-}
+} & TopRightElement &
+  ContentElement
 
 export const WidgetTile: FC<WidgetTileProps> = ({
   loading,
   title,
+  button,
+  customNode,
+  tooltip,
+  customTopRightNode,
   text,
   caption,
   icon,
-  button,
-  tooltip,
-  customNode,
-  topRightElement,
-  contentElement,
 }) => {
   const mdMatch = useMediaMatch('md')
   const lgMatch = useMediaMatch('lg')
+  const withCustomNode = customNode && !text
+  const withText = !customNode && text
+  const withCustomTopRightNode = customTopRightNode && !tooltip
+  const withTooltip = !customTopRightNode && tooltip
+
+  if (customNode && text) {
+    ConsoleLogger.error('WidgetTile: text and customNode are mutually exclusive. customNode will be ignored.')
+  }
+
+  if (tooltip && customTopRightNode) {
+    ConsoleLogger.error(
+      'WidgetTile: tooltip and customTopRightNode are mutually exclusive. customTopRightNode will be ignored.'
+    )
+  }
+
   return (
     <Wrapper>
-      <Title hasTooltip={!!tooltip}>
+      <Title hasTooltip={!!tooltip || !!customTopRightNode}>
         <Text variant="h100" as="p" color="colorText">
           {title}
         </Text>
-        {tooltip && <Information {...tooltip} />}
+        {withTooltip && <Information {...tooltip} />}
+        {withCustomTopRightNode && customTopRightNode}
       </Title>
       <Content>
-        {loading && !customNode ? (
-          <>
+        {withCustomNode && customNode}
+        {withText &&
+          (loading ? (
+            <>
+              <div>
+                <SkeletonLoader height={mdMatch ? 32 : 24} width={128} bottomSpace={caption ? 4 : 0} />
+                {caption && <SkeletonLoader width={56} height={16} />}
+              </div>
+              <SkeletonLoader height={40} width={lgMatch ? 120 : '100%'} />
+            </>
+          ) : (
             <div>
-              <SkeletonLoader height={mdMatch ? 32 : 24} width={128} bottomSpace={caption ? 4 : 0} />
-              {caption && <SkeletonLoader width={56} height={16} />}
+              <TextWrapper>
+                {icon}
+                <Text variant={mdMatch ? 'h500' : 'h400'} as="p">
+                  {text}
+                </Text>
+              </TextWrapper>
+              {caption && (
+                <Text variant="t100" as="p" color="colorText" margin={{ top: 1 }}>
+                  {caption}
+                </Text>
+              )}
             </div>
-            <SkeletonLoader height={40} width={lgMatch ? 120 : '100%'} />
-          </>
-        ) : customNode ? (
-          customNode
-        ) : (
-          <div>
-            <TextWrapper>
-              {icon}
-              <Text variant={mdMatch ? 'h500' : 'h400'} as="p">
-                {text}
-              </Text>
-            </TextWrapper>
-            {caption && (
-              <Text variant="t100" as="p" color="colorText" margin={{ top: 1 }}>
-                {caption}
-              </Text>
-            )}
-          </div>
-        )}
+          ))}
         {!loading && <StyledButton {...button}>{button.text}</StyledButton>}
       </Content>
     </Wrapper>
