78efebcbd6bc5e3ae2be3e708acad5eb27cb9a78
drillprop <drylbartosz@gmail.com>
2022-11-24 10:25:54 +0100

    fix path

--- packages/atlas/.storybook/theme.js
+++ packages/atlas/.storybook/theme.js
@@ -1,8 +1,10 @@
 import { create } from '@storybook/theming'
 
+import logo from '../src/assets/logos/svgs/joystream-logo-full.svg'
+
 export default create({
   base: 'dark',
   brandTitle: '@joystream/atlas',
   brandUrl: 'https://joystream.org',
-  brandImage: './assets/logos/svgs/joystream-logo-full.svg',
+  brandImage: logo,
 })
