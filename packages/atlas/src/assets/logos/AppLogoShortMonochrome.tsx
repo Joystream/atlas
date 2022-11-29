9ad7b7658da202c8115534c352407648b0ffa758
Bartosz Dryl <drylbartosz@gmail.com>
2022-11-29 12:14:41 +0100

    Update gleev assets (#1)
    Fix https://github.com/Joystream/atlas/issues/3320

--- packages/atlas/src/assets/logos/AppLogoShortMonochrome.tsx
+++ packages/atlas/src/assets/logos/AppLogoShortMonochrome.tsx
@@ -2,15 +2,16 @@
 import { Ref, SVGProps, forwardRef, memo } from 'react'
 
 const SvgAppLogoShortMonochrome = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
-  <svg width={98} height={88} viewBox="0 0 98 88" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}>
-    <path
-      d="M8.246 88H0l11.455-45.82a23.99 23.99 0 0 1 12.101-15.417L8.246 88ZM22.675 88h-8.243l15.402-61.608a19 19 0 0 1 11.516-13.09L22.675 88ZM37.106 88H28.86L47.8 12.246a16.167 16.167 0 0 1 31.37 0L98 88h-8.14L71.407 14.186a8.167 8.167 0 0 0-15.847 0L37.106 88Z"
-      fill="#fff"
-    />
-    <path
-      d="M69.246 88H61l-8.855-35.418 4.123-16.492L69.246 88ZM83.675 88h-8.243l-16.07-64.281 2.018-8.077a2.167 2.167 0 0 1 4.206 0L83.676 88Z"
-      fill="#fff"
-    />
+  <svg width={104} height={88} viewBox="0 0 104 88" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}>
+    <g clipPath="url(#clip0_1541_25189)" fill="#fff">
+      <path d="M64 88h.131a12 12 0 0 0 9.472-4.632l9.245-11.885a12 12 0 0 0 .025-14.705L73 44h31L73.603 4.663A12 12 0 0 0 64.108 0H64L51.46 16.124 73 44H42l9.873 12.778a12 12 0 0 1-.025 14.705l-.347.447L64 88Z" />
+      <path d="M64.125 0H33.869a12 12 0 0 0-9.472 4.633L4.912 29.685a23.316 23.316 0 0 0 0 28.63l19.485 25.052A12 12 0 0 0 33.869 88H64.13l-.045-.026a12 12 0 0 1-8.688-4.607L35.91 58.315a23.317 23.317 0 0 1 0-28.63L55.397 4.633A12 12 0 0 1 64.022.03c.037-.003.103.012.103-.03Z" />
+    </g>
+    <defs>
+      <clipPath id="clip0_1541_25189">
+        <path fill="#fff" d="M0 0h104v88H0z" />
+      </clipPath>
+    </defs>
   </svg>
 ))
 SvgAppLogoShortMonochrome.displayName = 'SvgAppLogoShortMonochrome'
--- packages/atlas/src/components/AppLogo/AppLogo.tsx
+++ packages/atlas/src/components/AppLogo/AppLogo.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react'
 
-import {
-  SvgAppLogoFull,
-  SvgAppLogoFullMonochrome,
-  SvgAppLogoShort,
-  SvgAppLogoShortMonochrome,
-  SvgAppLogoStudio,
-} from '@/assets/logos'
+import fullLogo1x from '@/assets/images/app-logo-full-1x.webp'
+import fullLogo2x from '@/assets/images/app-logo-full-2x.webp'
+import shortLogo1x from '@/assets/images/app-logo-short-1x.webp'
+import shortLogo2x from '@/assets/images/app-logo-short-2x.webp'
+import studioLogo1x from '@/assets/images/app-logo-studio-1x.webp'
+import studioLogo2x from '@/assets/images/app-logo-studio-2x.webp'
+import { SvgAppLogoFullMonochrome, SvgAppLogoShortMonochrome } from '@/assets/logos'
 
 type LogoVariant = 'short' | 'short-monochrome' | 'full' | 'full-monochrome' | 'studio'
 
@@ -20,17 +20,17 @@ export type AppLogoProps = {
 export const AppLogo: FC<AppLogoProps> = ({ variant, ...restProps }) => {
   switch (variant) {
     case 'full':
-      return <SvgAppLogoFull {...restProps} />
+      return <img srcSet={`${fullLogo1x} 1x, ${fullLogo2x} 2x`} {...restProps} />
     case 'full-monochrome':
       return <SvgAppLogoFullMonochrome {...restProps} />
     case 'studio':
-      return <SvgAppLogoStudio {...restProps} />
+      return <img srcSet={`${studioLogo1x} 1x, ${studioLogo2x} 2x`} {...restProps} />
     case 'short':
-      return <SvgAppLogoShort {...restProps} />
+      return <img srcSet={`${shortLogo1x} 1x, ${shortLogo2x} 2x`} {...restProps} />
     case 'short-monochrome':
       return <SvgAppLogoShortMonochrome {...restProps} />
     default:
-      return <SvgAppLogoFull {...restProps} />
+      return <img srcSet={`${fullLogo1x} 1x, ${fullLogo2x} 2x`} {...restProps} />
   }
 }
 
