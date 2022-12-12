d86060f0bac5c447177fc25f993b8ea6e42bb5d9
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2022-12-12 12:38:11 +0100

    🔦 Flashing ypp routes (#3513)
    * Introduce loader on route when loading auth data
    
    * Introduce loader when fetching ypp details

--- packages/atlas/src/components/_navigation/PrivateRoute/PrivateRoute.tsx
+++ packages/atlas/src/components/_navigation/PrivateRoute/PrivateRoute.tsx
@@ -1,13 +1,26 @@
 import { FC, ReactElement } from 'react'
 import { Navigate } from 'react-router-dom'
 
+import { Loader } from '@/components/_loaders/Loader'
+
+import { LoaderContainer } from './PrivteRoute.styles'
+
 type PrivateRouteProps = {
   element: ReactElement
   redirectTo?: string
   isAuth?: boolean
+  isLoadingAuthData?: boolean
 }
 
-export const PrivateRoute: FC<PrivateRouteProps> = ({ redirectTo, isAuth, element }) => {
+export const PrivateRoute: FC<PrivateRouteProps> = ({ redirectTo, isAuth, element, isLoadingAuthData }) => {
+  if (isLoadingAuthData) {
+    return (
+      <LoaderContainer>
+        <Loader variant="xlarge" />
+      </LoaderContainer>
+    )
+  }
+
   if (!isAuth && redirectTo) {
     return <Navigate to={redirectTo} replace />
   }
