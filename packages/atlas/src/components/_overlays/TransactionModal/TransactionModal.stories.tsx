e16ccc62fd7f61cfe4540b314bc330c4c7451f91
Klaudiusz Dembler <accounts@kdembler.com>
2022-12-13 18:18:16 +0100

    fix TransactionModal story

--- packages/atlas/src/components/_overlays/TransactionModal/TransactionModal.stories.tsx
+++ packages/atlas/src/components/_overlays/TransactionModal/TransactionModal.stories.tsx
@@ -3,6 +3,7 @@ import { Meta, StoryFn } from '@storybook/react'
 import { BrowserRouter } from 'react-router-dom'
 
 import { createApolloClient } from '@/api'
+import { ExtrinsicStatus } from '@/joystream-lib/types'
 import { ConfirmationModalProvider } from '@/providers/confirmationModal'
 import { OverlayManagerProvider } from '@/providers/overlayManager'
 import { UserProvider } from '@/providers/user/user.provider'
@@ -12,6 +13,25 @@ import { TransactionModal, TransactionModalProps } from './TransactionModal'
 export default {
   title: 'overlays/TransactionModal',
   component: TransactionModal,
+  argTypes: {
+    status: {
+      options: Object.values(ExtrinsicStatus),
+      control: {
+        type: 'select',
+        labels: {
+          [ExtrinsicStatus.ProcessingAssets]: 'ProcessingAssets',
+          [ExtrinsicStatus.Unsigned]: 'Unsigned',
+          [ExtrinsicStatus.Syncing]: 'Syncing',
+          [ExtrinsicStatus.Signed]: 'Signed',
+          [ExtrinsicStatus.Error]: 'Error',
+          [ExtrinsicStatus.Completed]: 'Completed',
+        },
+      },
+    },
+  },
+  args: {
+    status: ExtrinsicStatus.ProcessingAssets,
+  },
   decorators: [
     (Story) => {
       const apolloClient = createApolloClient()
