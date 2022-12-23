2927379483a9de1db3bbb3b9a30426bf675d1ef0
WRadoslaw <r.wyszynski00@gmail.com>
2022-12-23 09:14:56 +0100

    Remove tabs from view

--- packages/atlas/src/views/studio/MyPaymentsView/MyPaymentsView.tsx
+++ packages/atlas/src/views/studio/MyPaymentsView/MyPaymentsView.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-
 import { LimitedWidthContainer } from '@/components/LimitedWidthContainer'
-import { Tabs } from '@/components/Tabs'
 import { Text } from '@/components/Text'
 import { PayoutsWelcomeDialogContent } from '@/components/_overlays/PayoutsWelcomeDialogContent'
 import { useHeadTags } from '@/hooks/useHeadTags'
@@ -9,18 +6,13 @@ import { useMountEffect } from '@/hooks/useMountEffect'
 import { useConfirmationModal } from '@/providers/confirmationModal'
 import { usePersonalDataStore } from '@/providers/personalData'
 
-import { TabsContainer } from './MyPayments.styles'
-import { PaymentsOverViewTab } from './PaymentsOverviewTab'
-import { PaymentTransactionsTab } from './PaymentsTransactionTab'
+import { PaymentsOverView } from './PaymentsOverview'
+import { PaymentTransactions } from './PaymentsTransactions'
 
 const PAYOUTS_WELCOME_MESSAGE = 'payouts-welcome'
 
-const TABS = ['Overview', 'Transactions'] as const
-
 export const MyPaymentsView = () => {
   const headTags = useHeadTags('My payments')
-  const mappedTabs = TABS.map((tab) => ({ name: tab }))
-  const [selectedTab, setSelectedTab] = useState<typeof TABS[number]>('Overview')
   const isDismissedMessage = usePersonalDataStore((state) =>
     state.dismissedMessages.some((message) => message.id === PAYOUTS_WELCOME_MESSAGE)
   )
@@ -49,11 +41,8 @@ export const MyPaymentsView = () => {
       <Text as="h1" variant="h700" margin={{ top: 12, bottom: 12 }}>
         My payments
       </Text>
-      <TabsContainer>
-        <Tabs initialIndex={0} tabs={mappedTabs} onSelectTab={(tabIdx) => setSelectedTab(TABS[tabIdx])} />
-      </TabsContainer>
-      {selectedTab === 'Overview' && <PaymentsOverViewTab />}
-      {selectedTab === 'Transactions' && <PaymentTransactionsTab />}
+      <PaymentsOverView />
+      <PaymentTransactions />
     </LimitedWidthContainer>
   )
 }
