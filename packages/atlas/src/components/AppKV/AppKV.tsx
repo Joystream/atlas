9359451776b71420c0e5f8e709dafcae07cefa9a
Bartosz Dryl <drylbartosz@gmail.com>
2023-01-12 13:53:44 +0100

    Misc fixes (#3615)
    * fix issue with date validation
    
    * fix app logo size, show cookies banner in development
    
    * fix bid and create english auction issue
    
    * change locked funds to invitation lock

--- packages/atlas/src/components/AppKV/AppKV.tsx
+++ packages/atlas/src/components/AppKV/AppKV.tsx
@@ -1,15 +1,16 @@
 import appKv from '@/assets/images/app-kv.webp'
-import { AppLogo } from '@/components/AppLogo'
 import { StyledSvgJoystreamLogoFull } from '@/components/_navigation/SidenavBase'
 
-import { Fade, Image, LogoWrapper, StyledText, Wrapper } from './AppKV.styles'
+import { AppLogoContainer, Fade, Image, LogoWrapper, StyledAppLogo, StyledText, Wrapper } from './AppKV.styles'
 
 export const AppKV = () => {
   return (
     <Wrapper>
       <Image src={appKv} />
       <LogoWrapper>
-        <AppLogo variant="full" />
+        <AppLogoContainer>
+          <StyledAppLogo variant="full" />
+        </AppLogoContainer>
         <StyledText as="span" variant="t100" color="colorTextMuted">
           Powered by
           <StyledSvgJoystreamLogoFull />
