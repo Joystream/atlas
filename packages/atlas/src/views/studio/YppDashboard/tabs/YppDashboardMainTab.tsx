89ff1a12401bfa0fb2bf12deba8119311e3bdcb7
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-02-01 16:10:25 +0100

    ‼️ Banner for YPP payout info (#3690)
    * Update config property
    
    * Add banner
    
    * CR fixes

--- packages/atlas/src/views/studio/YppDashboard/tabs/YppDashboardMainTab.tsx
+++ packages/atlas/src/views/studio/YppDashboard/tabs/YppDashboardMainTab.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react'
 
-import { SvgActionNewTab, SvgAlertsError24 } from '@/assets/icons'
+import { SvgActionNewTab, SvgAlertsError24, SvgAlertsInformative24 } from '@/assets/icons'
 import { Banner } from '@/components/Banner'
+import { NumberFormat } from '@/components/NumberFormat'
 import { Text } from '@/components/Text'
 import { WidgetTile } from '@/components/WidgetTile'
 import { Button } from '@/components/_buttons/Button'
@@ -46,6 +47,24 @@ export const YppDashboardMainTab: FC<YppDashboardMainTabProps> = ({ currentTier 
           }
         />
       )}
+      <StyledBanner
+        title="When rewards are paid out?"
+        icon={<SvgAlertsInformative24 />}
+        description={
+          <>
+            Payouts are made on a weekly basis, every Friday, for the previous calendar week. Your first payment will
+            involve the reward for the sign up of{' '}
+            <NumberFormat
+              value={(atlasConfig.features.ypp.enrollmentReward ?? 0) * multiplier}
+              format="short"
+              as="p"
+              withToken
+            />{' '}
+            after your channel is verified by the program operators.
+          </>
+        }
+        dismissibleId="ypp-first-reward-information"
+      />
       {atlasConfig.features.ypp.widgets && (
         <WidgetsWrapper>
           {atlasConfig.features.ypp.widgets.map((widget) => (
