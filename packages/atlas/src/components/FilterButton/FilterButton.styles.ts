4e05bb3fa6e0697eb87abafc06ad68b314e3348d
WRadoslaw <r.wyszynski00@gmail.com>
2023-02-09 11:21:51 +0100

    Linter

--- packages/atlas/src/components/FilterButton/FilterButton.styles.ts
+++ packages/atlas/src/components/FilterButton/FilterButton.styles.ts
@@ -3,7 +3,6 @@ import styled from '@emotion/styled'
 import { cVar } from '@/styles'
 
 export const Counter = styled.div`
-  line-height: 16px;
   height: 16px;
   width: 16px;
   background-color: ${cVar('colorBackgroundPrimary')};
