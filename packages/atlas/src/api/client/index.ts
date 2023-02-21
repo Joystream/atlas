1cf98fb360c09883620fff582d6c0f520956bfd7
drillprop <drylbartosz@gmail.com>
2023-02-21 13:46:04 +0100

    remove batching comments

--- packages/atlas/src/api/client/index.ts
+++ packages/atlas/src/api/client/index.ts
@@ -1,5 +1,4 @@
 import { ApolloClient, ApolloLink, FetchResult, HttpLink, Observable, split } from '@apollo/client'
-// import { BatchHttpLink } from '@apollo/client/link/batch-http'
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
 import { getMainDefinition } from '@apollo/client/utilities'
 import { createClient } from 'graphql-ws'
@@ -7,8 +6,6 @@ import { createClient } from 'graphql-ws'
 import { ORION_GRAPHQL_URL, QUERY_NODE_GRAPHQL_SUBSCRIPTION_URL } from '@/config/env'
 
 import cache from './cache'
-
-// const BATCHED_QUERIES = ['GetBasicVideos', 'GetDistributionBucketsWithBags', 'GetStorageBucketsWithBags']
 
 const delayLink = new ApolloLink((operation, forward) => {
   const ctx = operation.getContext()
@@ -34,19 +31,6 @@ const createApolloClient = () => {
   )
 
   const orionLink = ApolloLink.from([delayLink, new HttpLink({ uri: ORION_GRAPHQL_URL })])
-  // todo batching not working with new orion?
-  // const batchedOrionLink = ApolloLink.from([
-  //   delayLink,
-  //   new BatchHttpLink({ uri: ORION_GRAPHQL_URL, batchMax: 10, batchInterval: 300 }),
-  // ])
-
-  // const orionSplitLink = split(
-  //   ({ operationName }) => {
-  //     return BATCHED_QUERIES.includes(operationName)
-  //   },
-  //   batchedOrionLink,
-  //   orionLink
-  // )
 
   const operationSplitLink = split(
     ({ query }) => {
@@ -54,7 +38,6 @@ const createApolloClient = () => {
       return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
     },
     subscriptionLink,
-    // orionSplitLink
     orionLink
   )
 
