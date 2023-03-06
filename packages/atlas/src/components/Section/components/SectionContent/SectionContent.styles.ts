0c7dc88945ecb38d2bcd957aca2ba647918003ab
WRadoslaw <r.wyszynski00@gmail.com>
2023-03-06 16:04:40 +0100

    Section content component

---
+++ packages/atlas/src/components/Section/components/SectionContent/SectionContent.styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+import { media, sizes } from '@/styles'
+
+export const GridWrapper = styled.div<{ minWidth: number }>`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(${(props) => `${props.minWidth}px`}, 1fr));
+  gap: ${sizes(4)};
+
+  ${media.md} {
+    gap: ${sizes(6)};
+  }
+`
