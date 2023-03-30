45a4419ed4e48472ab4e65114fadc29c402763a1
WRadoslaw <r.wyszynski00@gmail.com>
2023-03-30 14:21:24 +0200

    Progress drawer styles

--- packages/atlas/src/components/ProgressDrawer/ProgressDrawer.tsx
+++ packages/atlas/src/components/ProgressDrawer/ProgressDrawer.tsx
@@ -1,46 +1,61 @@
-import { FC, useState } from 'react'
-import { CSSTransition } from 'react-transition-group'
+import { FC } from 'react'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 
 import { SvgActionCheck, SvgActionChevronR } from '@/assets/icons'
 import { Text } from '@/components/Text'
-import { Button } from '@/components/_buttons/Button'
 import { transitions } from '@/styles'
 
 import {
-  BottomRowContainer,
-  CircularProgresaBarContainer,
   Container,
+  RequiredText,
   Step,
   StepInnerContainer,
   StepState,
-  StepsCompletedText,
   StepsContainer,
-  StepsProgressContainer,
-  StyledCircularProgress,
-  StyledSvgGlyphChevronDown,
+  TitleContainer,
 } from './ProgressDrawer.styles'
 
-export type Step = { title: string; onClick: () => void; completed: boolean }
+export type Step = { title: string; onClick: () => void; completed: boolean; required?: boolean }
 export type ProgressDrawerProps = {
   steps: Step[]
   className?: string
 }
 export const ProgressDrawer: FC<ProgressDrawerProps> = ({ steps, className }) => {
   const stepsCompletedNumber = steps.filter(({ completed }) => completed).length
-  const [isHidden, setIsHidden] = useState(false)
+
   return (
     <Container className={className}>
-      <StepsContainer isHidden={isHidden}>
+      <TitleContainer>
+        <Text variant="h200" as="p">
+          Progress
+        </Text>
+        <Text variant="t200" as="p">
+          {stepsCompletedNumber}/{steps.length}
+        </Text>
+      </TitleContainer>
+      <StepsContainer>
         {steps.map((step, idx) => (
           <Step completed={step.completed} key={step.title + idx} onClick={() => !step.completed && step.onClick()}>
             <StepInnerContainer>
               <StepState completed={step.completed}>
-                <CSSTransition in={step.completed} timeout={100} classNames={transitions.names.fade} unmountOnExit>
-                  <SvgActionCheck />
-                </CSSTransition>
+                <SwitchTransition mode="out-in">
+                  <CSSTransition
+                    key={step.completed ? 'done' : 'not-yet'}
+                    in={step.completed}
+                    timeout={100}
+                    classNames={transitions.names.fade}
+                  >
+                    {step.completed ? <SvgActionCheck /> : <span>{step.completed ? null : idx + 1}</span>}
+                  </CSSTransition>
+                </SwitchTransition>
               </StepState>
               <Text as="span" variant="t200">
                 {step.title}
+                {step.required && (
+                  <RequiredText as="span" variant="t300" color="colorTextError">
+                    *
+                  </RequiredText>
+                )}
               </Text>
             </StepInnerContainer>
             <CSSTransition in={!step.completed} timeout={100} classNames={transitions.names.fade} unmountOnExit>
@@ -49,22 +64,6 @@ export const ProgressDrawer: FC<ProgressDrawerProps> = ({ steps, className }) =>
           </Step>
         ))}
       </StepsContainer>
-      <BottomRowContainer>
-        <StepsProgressContainer>
-          <CircularProgresaBarContainer>
-            <StyledCircularProgress value={stepsCompletedNumber} maxValue={steps.length} />
-            <StepsCompletedText as="span" variant="t200">
-              {stepsCompletedNumber}/{steps.length}
-            </StepsCompletedText>
-          </CircularProgresaBarContainer>
-          <Button
-            icon={<StyledSvgGlyphChevronDown isHidden={isHidden} />}
-            variant="secondary"
-            onClick={() => setIsHidden(!isHidden)}
-            size="small"
-          />
-        </StepsProgressContainer>
-      </BottomRowContainer>
     </Container>
   )
 }
