36b6706b773b9898910f0b771cebb866ff926383
WRadoslaw <r.wyszynski00@gmail.com>
2023-03-30 15:53:14 +0200

    Fix story

--- packages/atlas/src/components/TopSellingChannelsTable/TopSellingChannelsTable.stories.tsx
+++ packages/atlas/src/components/TopSellingChannelsTable/TopSellingChannelsTable.stories.tsx
@@ -1,9 +1,29 @@
+import { ApolloProvider } from '@apollo/client'
+import { Meta } from '@storybook/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { createApolloClient } from '@/api'
 import { TopSellingChannelsTable } from '@/components/TopSellingChannelsTable/TopSellingChannelsTable'
+import { OverlayManagerProvider } from '@/providers/overlayManager'
 
 export default {
   title: 'other/TopSellingChannelsTable',
   component: TopSellingChannelsTable,
-}
+  decorators: [
+    (Story) => {
+      const apolloClient = createApolloClient()
+      return (
+        <ApolloProvider client={apolloClient}>
+          <OverlayManagerProvider>
+            <MemoryRouter>
+              <Story />
+            </MemoryRouter>
+          </OverlayManagerProvider>
+        </ApolloProvider>
+      )
+    },
+  ],
+} as Meta
 
 const Template = () => <TopSellingChannelsTable />
 
