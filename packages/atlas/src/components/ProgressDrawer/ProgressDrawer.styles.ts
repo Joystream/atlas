6498609f35b6d4594ea15e978aeef199a60107bc
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-04-04 17:53:10 +0200

    🥨 Channel creation progression (#4000)
    * Progress drawer styles
    
    * Channel creation view fixes

--- packages/atlas/src/components/ProgressDrawer/ProgressDrawer.styles.ts
+++ packages/atlas/src/components/ProgressDrawer/ProgressDrawer.styles.ts
@@ -1,64 +1,27 @@
-import isPropValid from '@emotion/is-prop-valid'
 import styled from '@emotion/styled'
 
-import { SvgActionChevronB } from '@/assets/icons'
-import { CircularProgress } from '@/components/CircularProgress'
 import { Text } from '@/components/Text'
-import { cVar, sizes, transitions } from '@/styles'
+import { cVar, sizes } from '@/styles'
 
 export const Container = styled.div`
-  width: 280px;
+  width: 240px;
   overflow-y: hidden;
-`
-export const CircularProgresaBarContainer = styled.div`
-  display: flex;
-  align-items: center;
-`
-
-export const StyledCircularProgress = styled(CircularProgress)`
-  margin-left: ${sizes(2)};
-  margin-right: ${sizes(2)};
-  width: ${sizes(6)};
-  height: ${sizes(6)};
-`
-
-export const StepsProgressContainer = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-`
-
-export const BottomRowContainer = styled.div`
-  padding: ${sizes(4)};
   background-color: ${cVar('colorCoreNeutral800')};
+`
+export const TitleContainer = styled.div`
+  padding: ${sizes(4)};
+  width: 100%;
+  background: ${cVar('colorBackgroundMutedAlpha')};
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  position: relative;
-  z-index: 1;
 `
 
-type StepsContainerProps = {
-  isHidden?: boolean
-}
-
-export const StepsContainer = styled.div<StepsContainerProps>`
+export const StepsContainer = styled.div`
   display: grid;
   grid-auto-flow: row;
-  padding: ${sizes(4)};
-  transform: translateY(${({ isHidden }) => (isHidden ? '150%' : '0%')});
   background-color: ${cVar('colorCoreNeutral800')};
-  transition: transform ${transitions.timings.regular} ${transitions.easing};
   position: relative;
   z-index: 1;
-`
-
-export const StyledSvgGlyphChevronDown = styled(SvgActionChevronB, {
-  shouldForwardProp: isPropValid,
-})<StepsContainerProps>`
-  transform: rotate(${({ isHidden }) => (isHidden ? '-180deg' : '0deg')});
-  transition: transform ${transitions.timings.regular} ${transitions.easing};
 `
 
 type StepStateProps = {
@@ -73,10 +36,8 @@ export const StepState = styled.div<StepStateProps>`
   justify-content: center;
   border-radius: 100%;
   ${({ completed }) => [
-    completed &&
-      `background-color: ${cVar('colorCoreBlue500')};
-    border: 2px solid transparent;`,
-    completed === false && `border: 2px solid ${cVar('colorCoreNeutral300')}`,
+    `background-color:${cVar('colorBackgroundAlpha')}`,
+    completed && `background-color: ${cVar('colorCoreBlue500')};`,
   ]};
 `
 
@@ -86,34 +47,19 @@ export const Step = styled.div<StepStateProps>`
   grid-auto-flow: column;
   justify-content: space-between;
   align-items: center;
-  padding: ${sizes(2)};
-  margin-bottom: ${sizes(2)};
-
-  :last-of-type {
-    position: relative;
-
-    ::after {
-      bottom: -${sizes(4)};
-      position: absolute;
-      content: '';
-      width: 100%;
-      height: 1px;
-      background-color: ${cVar('colorCoreNeutral300')};
-    }
-  }
+  padding: ${sizes(4)};
 
   :hover {
-    background-color: ${({ completed }) => !completed && cVar('colorCoreNeutral700Lighten')};
+    background-color: ${({ completed }) => !completed && cVar('colorBackgroundMutedAlpha')};
   }
-`
-
-export const StepsCompletedText = styled(Text)`
-  min-width: 25px;
-  text-align: right;
 `
 export const StepInnerContainer = styled.div`
   display: grid;
   grid-auto-flow: column;
   grid-gap: ${sizes(3)};
   align-items: center;
+`
+
+export const RequiredText = styled(Text)`
+  margin-left: ${sizes(1)};
 `
