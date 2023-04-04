0f635a2599f105f3e02080976c42872b3aa8f53b
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-04-04 14:14:01 +0200

    📗 Correct pagination button styles (#4001)
    * Correct pagination button styles

--- packages/atlas/src/components/Pagination/Pagination.styles.ts
+++ packages/atlas/src/components/Pagination/Pagination.styles.ts
@@ -60,9 +60,13 @@ export const PaginationButton = styled(Button)<PaginationButtonProps>`
   user-select: none;
 
   :hover,
-  :focus,
-  :active {
+  :focus-visible {
+    background-color: ${cVar('colorBackgroundStrongAlpha')};
     color: ${cVar('colorCoreNeutral50')};
+  }
+
+  :active {
+    ${buttonActiveState};
   }
 
   ${({ isActive }) => isActive && buttonActiveState};
