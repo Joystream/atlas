d4f400dacf223d1638af99875a03ec2106a38796
Artem <Artem Slugin>
2023-04-12 16:03:38 +0300

    Restored CRT preview screen

--- packages/atlas/src/views/studio/CrtWelcomeView/CrtWelcomeView.tsx
+++ packages/atlas/src/views/studio/CrtWelcomeView/CrtWelcomeView.tsx
@@ -1,26 +1,26 @@
-import { SvgActionPlay } from '@/assets/icons'
-import { WelcomeView } from '@/components/WelcomeView'
-import { absoluteRoutes } from '@/config/routes'
+import { CrtView } from '@/views/studio/CrtView'
 
 export const CrtWelcomeView = () => {
   return (
-    <WelcomeView
-      headTagTitle="Creator tokens"
-      title="Ready to create your channel token?"
-      pageTitle="Creator tokens"
-      subtitle="Create your very own channel token, sell it on your own terms and share your success with your token holders."
-      type="crt"
-      buttons={[
-        { children: 'Create token', size: 'large', to: absoluteRoutes.studio.crtTokenPreview() },
-        {
-          children: 'Learn more',
-          size: 'large',
-          variant: 'tertiary',
-          to: '',
-          _textOnly: true,
-          icon: <SvgActionPlay />,
-        },
-      ]}
-    />
+    <CrtView />
+    // todo: uncomment when CRT are ready
+    // <WelcomeView
+    //   headTagTitle="Creator tokens"
+    //   title="Ready to create your channel token?"
+    //   pageTitle="Creator tokens"
+    //   subtitle="Create your very own channel token, sell it on your own terms and share your success with your token holders."
+    //   type="crt"
+    //   buttons={[
+    //     { children: 'Create token', size: 'large', to: absoluteRoutes.studio.crtTokenPreview() },
+    //     {
+    //       children: 'Learn more',
+    //       size: 'large',
+    //       variant: 'tertiary',
+    //       to: '',
+    //       _textOnly: true,
+    //       icon: <SvgActionPlay />,
+    //     },
+    //   ]}
+    // />
   )
 }
