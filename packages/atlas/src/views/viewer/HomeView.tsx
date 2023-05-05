3cf62d64767d01f261c4f9bb64c4e9a3db26f67e
WRadoslaw <r.wyszynski00@gmail.com>
2023-05-05 11:52:53 +0200

    Use section and remove sorting on cache

--- packages/atlas/src/views/viewer/HomeView.tsx
+++ packages/atlas/src/views/viewer/HomeView.tsx
@@ -3,38 +3,49 @@ import { FC } from 'react'
 
 import { VideoOrderByInput } from '@/api/queries/__generated__/baseTypes.generated'
 import { GetBasicVideosConnectionDocument } from '@/api/queries/__generated__/videos.generated'
-import { InfiniteVideoGrid } from '@/components/InfiniteGrids'
+import { Section } from '@/components/Section/Section'
 import { VideoContentTemplate } from '@/components/_templates/VideoContentTemplate'
+import { VideoTileViewer } from '@/components/_video/VideoTileViewer'
 import { useHeadTags } from '@/hooks/useHeadTags'
-import { sizes, transitions } from '@/styles'
+import { useInfiniteVideoGrid } from '@/hooks/useInfiniteVideoGrid'
+import { DEFAULT_VIDEO_GRID, sizes } from '@/styles'
 
 export const HomeView: FC = () => {
   const headTags = useHeadTags()
+  const { columns, fetchMore, pageInfo, tiles } = useInfiniteVideoGrid({
+    query: GetBasicVideosConnectionDocument,
+    variables: {
+      limit: 200,
+      orderBy: [VideoOrderByInput.VideoRelevanceDesc, VideoOrderByInput.CreatedAtDesc, VideoOrderByInput.ViewsNumDesc],
+    },
+  })
 
   return (
     <VideoContentTemplate>
       {headTags}
-      <Container className={transitions.names.slide}>
-        <InfiniteVideoGrid
-          orderBy={[
-            VideoOrderByInput.VideoRelevanceDesc,
-            VideoOrderByInput.CreatedAtDesc,
-            VideoOrderByInput.ViewsNumDesc,
-          ]}
-          query={GetBasicVideosConnectionDocument}
-        />
-      </Container>
+      <StyledSection
+        contentProps={{
+          type: 'grid',
+          grid: DEFAULT_VIDEO_GRID,
+          children: tiles?.map((video, idx) => <VideoTileViewer id={video.id} key={idx} />),
+        }}
+        footerProps={{
+          reachedEnd: !pageInfo?.hasNextPage,
+          fetchMore: async () => {
+            if (pageInfo?.hasNextPage) {
+              await fetchMore({
+                variables: { first: columns * 4, after: pageInfo.endCursor },
+              })
+            }
+            return
+          },
+          type: 'infinite',
+        }}
+      />
     </VideoContentTemplate>
   )
 }
 
-const Container = styled.div`
-  position: relative;
+const StyledSection = styled(Section)`
   padding: ${sizes(8)} 0;
-
-  > section {
-    :not(:first-of-type) {
-      margin-top: ${sizes(32)};
-    }
-  }
 `
