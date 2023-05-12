5bcfbcf81d156969c0fe14fbff3ace50490e7e8a
Bartosz Dryl <drylbartosz@gmail.com>
2023-05-12 09:35:03 +0200

    remove marketplace heading (#4169)

--- packages/atlas/src/views/viewer/MarketplaceView/MarketplaceView.tsx
+++ packages/atlas/src/views/viewer/MarketplaceView/MarketplaceView.tsx
@@ -3,10 +3,8 @@ import { FC } from 'react'
 import { useFeaturedNftsVideos } from '@/api/hooks/nfts'
 import { AllNftSection } from '@/components/AllNftSection/AllNftSection'
 import { MarketplaceCarousel } from '@/components/NftCarousel/MarketplaceCarousel'
-import { Text } from '@/components/Text'
 import { TopSellingChannelsTable } from '@/components/TopSellingChannelsTable'
 import { useHeadTags } from '@/hooks/useHeadTags'
-import { useMediaMatch } from '@/hooks/useMediaMatch'
 
 import { FeaturedNftsSection } from './FeaturedNftsSection/FeaturedNftsSection'
 import { StyledLimitedWidth } from './MarketplaceView.styles'
@@ -14,14 +12,10 @@ import { StyledLimitedWidth } from './MarketplaceView.styles'
 export const MarketplaceView: FC = () => {
   const headTags = useHeadTags('Marketplace')
   const { nfts, loading } = useFeaturedNftsVideos()
-  const lgMatch = useMediaMatch('lg')
 
   return (
     <StyledLimitedWidth big>
       {headTags}
-      <Text as="h1" variant={lgMatch ? 'h700' : 'h600'}>
-        Marketplace
-      </Text>
       <MarketplaceCarousel type="nft" nfts={nfts} isLoading={loading} />
       <FeaturedNftsSection />
       <TopSellingChannelsTable />
