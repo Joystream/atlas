e44d8c0d432e3d3209b340fda435cbfd63bdb712
WRadoslaw <r.wyszynski00@gmail.com>
2023-05-17 11:18:38 +0200

    Adjusting logic

--- packages/atlas/src/hooks/useLogIn.ts
+++ packages/atlas/src/hooks/useLogIn.ts
@@ -1,13 +1,18 @@
+import { JOYSTREAM_ADDRESS_PREFIX } from '@joystream/types'
 import { ScryptOpts, scrypt } from '@noble/hashes/scrypt'
-import { cryptoWaitReady, sr25519PairFromSeed, sr25519Sign } from '@polkadot/util-crypto'
+import { Keyring } from '@polkadot/keyring'
+import { u8aToHex } from '@polkadot/util'
+import { cryptoWaitReady } from '@polkadot/util-crypto'
 import axios from 'axios'
+import { entropyToMnemonic } from 'bip39'
 import { Buffer } from 'buffer'
-import { AES, enc } from 'crypto-js'
+import { AES, enc, lib, mode } from 'crypto-js'
 import { useCallback } from 'react'
 
-import { atlasConfig } from '@/config'
 import { ORION_AUTH_URL } from '@/config/env'
 import { SentryLogger } from '@/utils/logs'
+
+export const keyring = new Keyring({ type: 'sr25519', ss58Format: JOYSTREAM_ADDRESS_PREFIX })
 
 // todo extract these 3 Fn to `packages/atlas/src/utils/user.ts` after #4168 is merged
 export async function scryptHash(
@@ -21,15 +26,18 @@ export async function scryptHash(
 }
 
 function aes256CbcDecrypt(encryptedData: string, key: Buffer, iv: Buffer): string {
-  const keyHex = enc.Hex.parse(key.toString('hex'))
-  const ivHex = enc.Hex.parse(iv.toString('hex'))
-  const decrypted = AES.decrypt(encryptedData, keyHex, { iv: ivHex })
-  return decrypted.toString(enc.Utf8)
+  const keyWA = enc.Hex.parse(key.toString('hex'))
+  const ivWA = enc.Hex.parse(iv.toString('hex'))
+  const decrypted = AES.decrypt(lib.CipherParams.create({ ciphertext: enc.Hex.parse(encryptedData) }), keyWA, {
+    iv: ivWA,
+    mode: mode.CBC,
+  })
+  return decrypted.toString(enc.Hex)
 }
 
 const getArtifacts = async (id: string) => {
   try {
-    const res = await axios.get<{ cipherIv: any; encryptedSeed: any }>(`${ORION_AUTH_URL}/artifacts?id=${id}`)
+    const res = await axios.get<{ cipherIv: string; encryptedSeed: string }>(`${ORION_AUTH_URL}/artifacts?id=${id}`)
 
     return res.data
   } catch (error) {
@@ -52,7 +60,8 @@ type LogInHandler = {
 export const useLogIn = () => {
   return useCallback(async (email: string, password: string): Promise<LogInHandler> => {
     await cryptoWaitReady()
-    const id = (await scryptHash(`${email}:${password}`, '0x4f7242b39969c3ac4c6712524d633ce9')).toString('hex')
+    const time = Date.now() - 1000
+    const id = (await scryptHash(`${email}:${password}`, '0x0818ee04c541716831bdd0f598fa4bbb')).toString('hex')
     const data = await getArtifacts(id)
     if (!data) {
       return {
@@ -60,22 +69,34 @@ export const useLogIn = () => {
         error: LogInErrors.ArtifactsNotFound,
       }
     }
+
     const { cipherIv, encryptedSeed } = data
     const cipherKey = await scryptHash(`${email}:${password}`, Buffer.from(cipherIv, 'hex'))
     const decryptedSeed = aes256CbcDecrypt(encryptedSeed, cipherKey, Buffer.from(cipherIv, 'hex'))
-    const keypair = sr25519PairFromSeed(decryptedSeed)
+    const keypair = keyring.addFromMnemonic(
+      entropyToMnemonic(Buffer.from(decryptedSeed.slice(2, decryptedSeed.length), 'hex'))
+    )
     const payload = {
-      joystreamAccountId: keypair.publicKey,
-      gatewayName: atlasConfig.general.appName,
-      timestamp: Date.now(),
+      joystreamAccountId: keypair.address,
+      gatewayName: 'Gleev',
+      timestamp: time,
       action: 'login',
     }
-    const signatureOverPayload = sr25519Sign(JSON.stringify(payload), keypair)
+    const signatureOverPayload = u8aToHex(keypair.sign(JSON.stringify(payload)))
     try {
-      const response = await axios.post<{ accountId: string }>(`${ORION_AUTH_URL}/login`, {
-        signature: signatureOverPayload,
-        payload,
-      })
+      const response = await axios.post<{ accountId: string }>(
+        `${ORION_AUTH_URL}/login`,
+        {
+          signature: signatureOverPayload,
+          payload,
+        },
+        {
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
 
       return {
         data: response.data,
