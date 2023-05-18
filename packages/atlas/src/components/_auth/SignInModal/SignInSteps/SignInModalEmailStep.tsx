158f31d5af6cc7d8235c8dc512b2fbe656672399
WRadoslaw <r.wyszynski00@gmail.com>
2023-05-18 16:40:08 +0200

    Add email field

--- packages/atlas/src/components/_auth/SignInModal/SignInSteps/SignInModalEmailStep.tsx
+++ packages/atlas/src/components/_auth/SignInModal/SignInSteps/SignInModalEmailStep.tsx
@@ -1,4 +1,10 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { FC, useCallback, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { FormField } from '@/components/_inputs/FormField'
+import { Input } from '@/components/_inputs/Input'
 
 import { SignInModalStepTemplate } from './SignInModalStepTemplate'
 import { SignInStepProps } from './SignInSteps.types'
@@ -8,7 +14,21 @@ type SignInModalEmailStepProps = SignInStepProps & {
 }
 
 export const SignInModalEmailStep: FC<SignInModalEmailStepProps> = ({ setPrimaryButtonProps, onConfirm }) => {
-  const handleConfirm = useCallback(() => {}, [])
+  const { register, formState, handleSubmit } = useForm<{ email: string }>({
+    resolver: (data, ctx, options) => {
+      return zodResolver(
+        z.object({
+          email: z.string().email(),
+        })
+      )(data, ctx, options)
+    },
+  })
+
+  const handleConfirm = useCallback(() => {
+    handleSubmit((data) => {
+      console.log(data)
+    })()
+  }, [handleSubmit])
 
   // send updates to SignInModal on state of primary button
   useEffect(() => {
@@ -23,6 +43,10 @@ export const SignInModalEmailStep: FC<SignInModalEmailStepProps> = ({ setPrimary
       title="Add your email"
       subtitle="Get notified about important events and stay updated. You can change all notifications permissions in your profile settings."
       hasNavigatedBack={false}
-    />
+    >
+      <FormField label="Email" error={formState.errors.email?.message}>
+        <Input {...register('email')} placeholder="Email" />
+      </FormField>
+    </SignInModalStepTemplate>
   )
 }
