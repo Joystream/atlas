c4c44cd06e5fcb35d4d2a952ba7378a10278a555
Bartosz Dryl <drylbartosz@gmail.com>
2023-05-22 12:34:41 +0200

    Fix weird carousel animation (#4217)
    * fix
    
    * unrelated adjustments

--- packages/atlas/src/components/LimitedWidthContainer/LimitedWidthContainer.tsx
+++ packages/atlas/src/components/LimitedWidthContainer/LimitedWidthContainer.tsx
@@ -2,12 +2,13 @@ import styled from '@emotion/styled'
 
 import { sizes } from '@/styles'
 
-type LimitedWidthContainerProps = { big?: boolean; noBottomPadding?: boolean }
+type LimitedWidthContainerProps = { big?: boolean; noBottomPadding?: boolean; fullWidth?: boolean }
 
 export const LimitedWidthContainer = styled.div<LimitedWidthContainerProps>`
   --max-inner-width: calc(${({ big }) => (big ? '2284' : '1440')}px - calc(2 * var(--size-global-horizontal-padding)));
 
   max-width: var(--max-inner-width);
+  width: ${({ fullWidth }) => (fullWidth ? '100%' : 'unset')};
   position: relative;
   margin: 0 auto;
   padding-bottom: ${({ noBottomPadding }) => (noBottomPadding ? 0 : sizes(16))};
