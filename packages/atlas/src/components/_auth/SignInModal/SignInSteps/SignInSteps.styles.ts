3d3fba720ec96185595d7df236f4b313fae1d8dd
drillprop <drylbartosz@gmail.com>
2023-05-23 14:34:05 +0200

    remove redundant styles

--- packages/atlas/src/components/_auth/SignInModal/SignInSteps/SignInSteps.styles.ts
+++ packages/atlas/src/components/_auth/SignInModal/SignInSteps/SignInSteps.styles.ts
@@ -48,21 +48,3 @@ export const Anchor = styled.a`
   cursor: pointer;
   color: ${cVar('colorTextPrimary')};
 `
-
-export const StyledSignUpForm = styled.form`
-  display: grid;
-  gap: ${sizes(6)};
-`
-
-export const CheckboxWrapper = styled.div<{ isAccepted: boolean }>`
-  margin: 0 calc(-1 * var(--local-size-dialog-padding));
-  display: flex;
-  align-items: center;
-  background-color: ${({ isAccepted }) => (isAccepted ? cVar('colorBackground') : cVar('colorBackgroundElevated'))};
-  padding: ${sizes(4)} var(--local-size-dialog-padding);
-`
-
-export const StyledLink = styled.a`
-  color: ${cVar('colorTextPrimary')};
-  text-decoration: underline;
-`
