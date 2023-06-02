ffe8b5ddbd71b75fc44a6e4309630408b414b503
drillprop <drylbartosz@gmail.com>
2023-06-02 12:30:58 +0200

    create page tabs component

---
+++ packages/atlas/src/components/PageTabs/PageTabs.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+import { PageTabsWrapper } from './PageTabs.styles'
+
+import { Tabs, TabsProps } from '../Tabs'
+
+type PageTabsProps = Omit<TabsProps, 'underline'>
+
+export const PageTabs: FC<PageTabsProps> = ({ className, ...tabsProps }) => {
+  return (
+    <PageTabsWrapper className={className}>
+      <Tabs {...tabsProps} />
+    </PageTabsWrapper>
+  )
+}
