b69518190cae386c612a59803193536ea973dc85
WRadoslaw <r.wyszynski00@gmail.com>
2023-06-12 10:08:30 +0200

    Try to bring back modals animations

--- packages/atlas/src/components/_auth/AuthModals/AuthModals.tsx
+++ packages/atlas/src/components/_auth/AuthModals/AuthModals.tsx
@@ -1,18 +1,22 @@
 import { ExternalSignInModal } from '@/components/_auth/ExternalSignInModal'
 import { LogInModal } from '@/components/_auth/LogInModal'
 import { SignUpModal } from '@/components/_auth/SignUpModal'
+import { useDebounceValue } from '@/hooks/useDebounceValue'
 import { useAuthStore } from '@/providers/auth/auth.store'
 
 export const AuthModals = () => {
   const { authModalOpenName } = useAuthStore()
-  switch (authModalOpenName) {
-    case 'logIn':
-      return <LogInModal />
-    case 'externalLogIn':
-      return <ExternalSignInModal />
-    case 'signUp':
-      return <SignUpModal />
-    default:
-      return null
+  const debouncedAuthModalOpenName = useDebounceValue(authModalOpenName, 2_000)
+
+  if (debouncedAuthModalOpenName ?? authModalOpenName) {
+    return (
+      <>
+        <LogInModal />
+        <ExternalSignInModal />
+        <SignUpModal />
+      </>
+    )
   }
+
+  return null
 }
