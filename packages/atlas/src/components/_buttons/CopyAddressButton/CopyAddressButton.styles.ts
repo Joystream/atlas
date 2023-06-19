f04eeee6181567cd1c5ad8de11ac136ed043fe42
WRadoslaw <r.wyszynski00@gmail.com>
2023-06-19 15:10:14 +0200

    Rework CopyAddressButton.tsx

--- packages/atlas/src/components/_buttons/CopyAddressButton/CopyAddressButton.styles.ts
+++ packages/atlas/src/components/_buttons/CopyAddressButton/CopyAddressButton.styles.ts
@@ -4,15 +4,25 @@ import { SvgActionCheck, SvgActionCopy } from '@/assets/icons'
 import { Text } from '@/components/Text'
 import { cVar, sizes } from '@/styles'
 
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  align-items: center;
+  width: fit-content;
+`
+
 export const StyledText = styled(Text)`
+  min-width: 0;
+  width: auto;
+  padding: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
   border: none;
   background: none;
-  display: flex;
-  align-items: center;
   cursor: pointer;
 `
 export const StyledSvgActionCopy = styled(SvgActionCopy)`
-  margin-left: ${sizes(2)};
+  margin-left: ${sizes(1)};
 
   path {
     fill: ${cVar('colorCoreNeutral300')};
