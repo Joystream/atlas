40f768797dd164a83c7d2296c7f64f358af468c9
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-06-27 10:13:44 +0200

    🐉 Dropdown rework (#4337)
    * Import icon
    
    * Rework CopyAddressButton.tsx
    
    * Rework dropdown
    
    * Rework studio topbar
    
    * Remove unused styles
    
    * Styling fixes
    
    * Linter
    
    * Try to replace spring
    
    * Linter plus timing fix
    
    * Fix scroll on animation
    
    * Fix scroll on animation v2
    
    * Fix small bug
    
    * Final fixes

--- packages/atlas/src/components/_buttons/CopyAddressButton/CopyAddressButton.styles.ts
+++ packages/atlas/src/components/_buttons/CopyAddressButton/CopyAddressButton.styles.ts
@@ -4,15 +4,26 @@ import { SvgActionCheck, SvgActionCopy } from '@/assets/icons'
 import { Text } from '@/components/Text'
 import { cVar, sizes } from '@/styles'
 
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  align-items: center;
+  width: fit-content;
+`
+
 export const StyledText = styled(Text)`
+  min-width: 0;
+  width: auto;
+  padding: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
   border: none;
   background: none;
-  display: flex;
-  align-items: center;
+  max-height: 20px;
   cursor: pointer;
 `
 export const StyledSvgActionCopy = styled(SvgActionCopy)`
-  margin-left: ${sizes(2)};
+  margin-left: ${sizes(1)};
 
   path {
     fill: ${cVar('colorCoreNeutral300')};
