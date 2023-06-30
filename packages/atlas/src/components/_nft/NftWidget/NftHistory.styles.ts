98d92c49860be3837c577c2b2eff6e1de4d12f0f
Bartosz Dryl <drylbartosz@gmail.com>
2023-06-30 15:07:35 +0200

    Make nft widget collapsible (#4140)
    * make nft widget collapsible
    
    * add chevron and pill
    
    * introduce StatusContainer
    
    * introduce NftWidgetContent, improve widget performance
    
    * nft history adjustments
    
    * show not collapsed if it's clicked from nft tile
    
    * fix circular dependency
    
    * fixes after merge
    
    * cr fixes

--- packages/atlas/src/components/_nft/NftWidget/NftHistory.styles.ts
+++ packages/atlas/src/components/_nft/NftWidget/NftHistory.styles.ts
@@ -1,19 +1,15 @@
 import styled from '@emotion/styled'
 
-import { Button } from '@/components/_buttons/Button'
 import { cVar, sizes } from '@/styles'
 
 import { SizeProps, sizeObj } from './NftWidget.styles'
 
-type OpenProps = { 'data-open': boolean }
-
-export const NftHistoryHeader = styled.div<SizeProps & OpenProps>`
+export const NftHistoryHeader = styled.div<SizeProps>`
   display: grid;
   grid-template-columns: 1fr auto;
   align-items: center;
   padding: ${sizes(6)};
   user-select: none;
-  cursor: pointer;
 
   &[data-size=${sizeObj.small}] {
     padding: ${sizes(4)};
@@ -24,57 +20,20 @@ export const NftHistoryHeader = styled.div<SizeProps & OpenProps>`
   }
 `
 
-export const StyledChevronButton = styled(Button)<OpenProps>`
-  transform: rotate(0);
-  transform-origin: center;
-  transition: ${cVar('animationTransitionFast')};
-
-  &[data-open='true'] {
-    transform: rotate(180deg);
-  }
-`
-
-type FadingBlockProps = { width: number; 'data-bottom'?: boolean } & SizeProps
-export const FadingBlock = styled.div<FadingBlockProps>`
-  height: ${sizes(6)};
-  background: linear-gradient(0deg, rgb(11 12 15 / 0) 0%, ${cVar('colorCoreNeutral900')} 100%);
-  position: absolute;
-  width: ${({ width }) => width}px;
-  z-index: 1;
-
-  &[data-size=${sizeObj.small}] {
-    height: ${sizes(4)};
-  }
-
-  &[data-bottom] {
-    transform: rotate(180deg);
-    bottom: 0;
-  }
-`
-
-type HistoryPanelProps = SizeProps & OpenProps
+type HistoryPanelProps = SizeProps
 export const HistoryPanel = styled.div<HistoryPanelProps>`
   background-color: ${cVar('colorBackgroundMuted')};
   position: relative;
   display: grid;
   gap: ${sizes(6)};
-  max-height: 280px;
-  padding: ${sizes(6)};
+  padding: ${sizes(2)} ${sizes(6)} ${sizes(6)} ${sizes(6)};
   overflow: hidden auto;
-  transition: transform ${cVar('animationTransitionFast')};
-  will-change: transform;
+  max-height: 376px;
 
   &[data-size=${sizeObj.small}] {
+    max-height: 400px;
     gap: ${sizes(4)};
     padding: ${sizes(4)};
-  }
-
-  transform: translateY(-100%);
-  z-index: -1;
-
-  &[data-open='true'] {
-    transform: translateY(0);
-    z-index: unset;
   }
 `
 
