8deb72051b63c4a94858a4ddb374007dbbf80953
WRadoslaw <r.wyszynski00@gmail.com>
2023-07-04 20:18:02 +0200

    Unify hide password UI and logic

--- packages/atlas/src/components/_auth/ForgotPasswordModal/steps/NewPasswordStep.tsx
+++ packages/atlas/src/components/_auth/ForgotPasswordModal/steps/NewPasswordStep.tsx
@@ -1,51 +1,37 @@
-import { useCallback, useState } from 'react'
 import { useFormContext } from 'react-hook-form'
 
-import { SvgActionHide, SvgActionShow } from '@/assets/icons'
 import { AuthenticationModalStepTemplate } from '@/components/_auth/AuthenticationModalStepTemplate'
 import {
   ForgotPasswordModalForm,
   ForgotPasswordStep,
 } from '@/components/_auth/ForgotPasswordModal/ForgotPasswordModal.types'
 import { FormField } from '@/components/_inputs/FormField'
-import { Input, InputProps } from '@/components/_inputs/Input'
+import { Input } from '@/components/_inputs/Input'
+import { useHidePasswordInInput } from '@/hooks/useHidePasswordInInput'
 
 import { PasswordCriterias } from '../../PasswordCriterias'
 import { GapContainer } from '../ForgotPasswordModal.styles'
 
 export const NewPasswordStep = () => {
   const { register, formState } = useFormContext<ForgotPasswordModalForm>()
-  const [isFieldVisible, setIsFieldVisible] = useState<Record<string, boolean>>({
-    password: false,
-    confirmPassword: false,
-  })
-
-  const getInputProps = useCallback(
-    (name: string): Partial<InputProps> => {
-      return {
-        ...register(name as keyof ForgotPasswordModalForm),
-        type: isFieldVisible[name] ? 'text' : 'password',
-        actionButton: {
-          children: isFieldVisible[name] ? 'Hide' : 'Show',
-          dontFocusOnClick: true,
-          icon: isFieldVisible[name] ? <SvgActionHide /> : <SvgActionShow />,
-          onClick: () => setIsFieldVisible((fields) => ({ ...fields, [name]: !fields[name] })),
-        },
-      }
-    },
-    [isFieldVisible, register]
-  )
+  const [hidePasswordProps] = useHidePasswordInInput()
+  const [hideConfirmPasswordProps] = useHidePasswordInInput()
 
   return (
     <AuthenticationModalStepTemplate hasNavigatedBack={false} title="Change password" subtitle="Set your new password.">
       <GapContainer>
         <FormField label="New password" error={formState.errors.NewPasswordStep?.password?.message}>
-          <Input placeholder="Password" {...getInputProps(`${ForgotPasswordStep.NewPasswordStep}.password`)} />
+          <Input
+            placeholder="Password"
+            {...register(`${ForgotPasswordStep.NewPasswordStep}.password`)}
+            {...hidePasswordProps}
+          />
         </FormField>
         <FormField label="Repeat password" error={formState.errors.NewPasswordStep?.confirmPassword?.message}>
           <Input
             placeholder="Confirm password"
-            {...getInputProps(`${ForgotPasswordStep.NewPasswordStep}.confirmPassword`)}
+            {...register(`${ForgotPasswordStep.NewPasswordStep}.confirmPassword`)}
+            {...hideConfirmPasswordProps}
           />
         </FormField>
         <PasswordCriterias path={`${ForgotPasswordStep.NewPasswordStep}.password`} />
--- packages/atlas/src/components/_auth/SignUpModal/SignUpSteps/SignUpPasswordStep/SignUpPasswordStep.tsx
+++ packages/atlas/src/components/_auth/SignUpModal/SignUpSteps/SignUpPasswordStep/SignUpPasswordStep.tsx
@@ -1,13 +1,13 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { FC, RefObject, useCallback, useEffect, useRef, useState } from 'react'
+import { FC, RefObject, useCallback, useEffect, useRef } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 
-import { SvgActionHide, SvgActionShow } from '@/assets/icons'
 import { AuthenticationModalStepTemplate } from '@/components/_auth/AuthenticationModalStepTemplate'
 import { PasswordCriterias } from '@/components/_auth/PasswordCriterias'
 import { FormField } from '@/components/_inputs/FormField'
-import { Input, InputProps } from '@/components/_inputs/Input'
+import { Input } from '@/components/_inputs/Input'
 import { AccountFormData } from '@/hooks/useCreateMember'
+import { useHidePasswordInInput } from '@/hooks/useHidePasswordInInput'
 import { passwordAndRepeatPasswordSchema } from '@/utils/formValidationOptions'
 
 import { StyledSignUpForm } from '../SignUpSteps.styles'
@@ -17,7 +17,6 @@ type PasswordStepForm = {
   password: string
   confirmPassword: string
 }
-type PasswordInputNames = keyof PasswordStepForm
 
 type SignUpPasswordStepProps = {
   onPasswordSubmit: (password: string) => void
@@ -46,10 +45,8 @@ export const SignUpPasswordStep: FC<SignUpPasswordStepProps> = ({
     register,
     formState: { errors },
   } = form
-  const [isFieldVisible, setIsFieldVisible] = useState<Record<PasswordInputNames, boolean>>({
-    password: false,
-    confirmPassword: false,
-  })
+  const [hidePasswordProps] = useHidePasswordInInput()
+  const [hideConfirmPasswordProps] = useHidePasswordInInput()
 
   const handleGoToNextStep = useCallback(() => {
     handleSubmit((data) => {
@@ -67,26 +64,6 @@ export const SignUpPasswordStep: FC<SignUpPasswordStepProps> = ({
   // used to scroll the form to the bottom upon first handle field focus - this is done to let the user see password requirements
   const hasDoneInitialScroll = useRef(false)
 
-  const handleTogglePassword = (name: PasswordInputNames) => {
-    setIsFieldVisible((fields) => ({ ...fields, [name]: !fields[name] }))
-  }
-
-  const getInputProps = useCallback(
-    (name: PasswordInputNames): InputProps => {
-      return {
-        ...register(name),
-        type: isFieldVisible[name] ? 'text' : 'password',
-        actionButton: {
-          children: isFieldVisible[name] ? 'Hide' : 'Show',
-          dontFocusOnClick: true,
-          icon: isFieldVisible[name] ? <SvgActionHide /> : <SvgActionShow />,
-          onClick: () => handleTogglePassword(name),
-        },
-      }
-    },
-    [isFieldVisible, register]
-  )
-
   return (
     <FormProvider {...form}>
       <AuthenticationModalStepTemplate
@@ -98,7 +75,8 @@ export const SignUpPasswordStep: FC<SignUpPasswordStepProps> = ({
           <FormField label="Password" error={errors.password?.message}>
             <Input
               placeholder="Password"
-              {...getInputProps('password')}
+              {...register('password')}
+              {...hidePasswordProps}
               autoComplete="off"
               onClick={() => {
                 if (hasDoneInitialScroll.current || !dialogContentRef?.current) return
@@ -108,7 +86,12 @@ export const SignUpPasswordStep: FC<SignUpPasswordStepProps> = ({
             />
           </FormField>
           <FormField label="Repeat Password" error={errors.confirmPassword?.message}>
-            <Input placeholder="Repeat password" {...getInputProps('confirmPassword')} autoComplete="off" />
+            <Input
+              placeholder="Repeat password"
+              {...register('confirmPassword')}
+              {...hideConfirmPasswordProps}
+              autoComplete="off"
+            />
           </FormField>
           <PasswordCriterias />
         </StyledSignUpForm>
