2ecc897348d95f28e1517a7e2a335121633d2fb3
Artem <Artem Slugin>
2023-07-13 18:04:10 +0400

    fixed image path for twitterBot

--- packages/atlas-meta-server/src/tags.ts
+++ packages/atlas-meta-server/src/tags.ts
@@ -39,7 +39,7 @@ export const generateCommonMetaTags = (
     }),
     ...(image && {
       'og:image': image,
-      'twitter:image': image,
+      'twitter:image': appUrl + image,
       'og:image:alt': title,
       'twitter:image:alt': title,
       'og:image:type': 'image/webp',
