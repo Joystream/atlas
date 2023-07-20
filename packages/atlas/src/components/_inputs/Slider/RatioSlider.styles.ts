b2d971c08ebf0e9e7243458e67a12a57444a6d1e
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-07-20 17:55:26 +0200

    Add the RatioSlider component

---
+++ packages/atlas/src/components/_inputs/Slider/RatioSlider.styles.ts
@@ -0,0 +1,87 @@
+import styled from '@emotion/styled'
+
+import { cVar, sizes } from '@/styles'
+
+export const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+`
+
+export const Track = styled.svg`
+  pointer-events: none;
+  overflow: visible;
+  height: ${sizes(6)};
+  width: calc(100% - ${sizes(5)});
+  margin: 0 ${sizes(5 / 2)} ${sizes(6)};
+
+  .rail {
+    stroke-width: ${sizes(1 / 2)};
+    stroke: ${cVar('colorCoreNeutral600')};
+
+    &-left {
+      stroke: ${cVar('colorBackgroundPrimary')};
+    }
+
+    .steps .step--active {
+      stroke: ${cVar('colorBackgroundPrimary')};
+    }
+  }
+
+  .knob {
+    paint-order: stroke;
+    stroke-width: 0;
+    stroke: ${cVar('colorBackgroundPrimary')};
+    fill: ${cVar('colorBackgroundPrimary')};
+  }
+
+  .cutout-mask {
+    paint-order: stroke;
+    stroke-width: 0;
+    stroke: #000;
+    fill: #000;
+  }
+
+  text {
+    font-size: ${sizes(3)};
+    user-select: none;
+
+    &:nth-of-type(1) {
+      fill: ${cVar('colorTextMuted')};
+      text-anchor: start;
+    }
+
+    &:nth-of-type(2) {
+      fill: ${cVar('colorTextStrong')};
+      text-anchor: middle;
+    }
+
+    &:nth-of-type(3) {
+      fill: ${cVar('colorTextMuted')};
+      text-anchor: end;
+    }
+  }
+`
+
+export const Range = styled.input`
+  position: absolute;
+  width: 100%;
+  opacity: 0;
+  height: ${sizes(8)};
+  cursor: grab;
+
+  &:active {
+    cursor: grabbing;
+
+    & + svg .knob {
+      fill: ${cVar('colorTextStrong')};
+    }
+  }
+
+  &:active,
+  &:hover {
+    & + svg .knob,
+    & + svg .cutout-mask {
+      stroke-width: ${sizes(2)};
+    }
+  }
+`
