d4e1397009b42a35fdc53c197120f470ad41a302
WRadoslaw <r.wyszynski00@gmail.com>
2023-07-25 10:00:21 +0200

    Add line chart

---
+++ packages/atlas/src/components/_charts/LineChart/LineChart.tsx
@@ -0,0 +1,85 @@
+import styled from '@emotion/styled'
+import { LineSvgProps, Point, ResponsiveLine } from '@nivo/line'
+
+import { Text } from '@/components/Text'
+import { cVar, sizes } from '@/styles'
+
+const defaultJoystreamProps: Omit<LineSvgProps, 'data'> = {
+  isInteractive: true,
+  useMesh: true,
+  enablePoints: false,
+  lineWidth: 2,
+  margin: { top: 50, right: 110, bottom: 50, left: 60 },
+  yScale: {
+    type: 'linear',
+    min: 'auto',
+    max: 'auto',
+    stacked: true,
+    reverse: false,
+  },
+  tooltip: (value) => (
+    <ChartTooltip>
+      <Text variant="t100" as="p" color="colorTextStrong">
+        {value.point.data.yStacked}
+      </Text>
+    </ChartTooltip>
+  ),
+  axisTop: null,
+  axisRight: null,
+  axisBottom: {
+    tickSize: 10,
+    tickPadding: 10,
+  },
+  axisLeft: {
+    tickSize: 5,
+    tickPadding: 5,
+    tickValues: 5,
+  },
+  colors: (d) => d.color,
+  theme: {
+    tooltip: {
+      container: {
+        background: cVar('colorBackgroundStrong'),
+      },
+    },
+    axis: {
+      ticks: {
+        line: {
+          stroke: cVar('colorBackgroundAlpha'),
+        },
+        text: {
+          color: cVar('colorTextMuted'),
+          fontSize: 12,
+        },
+      },
+    },
+    grid: {
+      line: {
+        stroke: cVar('colorBackgroundAlpha'),
+      },
+    },
+  },
+}
+export type LineChartProps = {
+  tooltip?: (point: Point) => string
+} & Omit<LineSvgProps, 'tooltip'>
+export const LineChart = (props: LineChartProps) => {
+  return (
+    <ResponsiveLine
+      {...defaultJoystreamProps}
+      {...props}
+      tooltip={(point) => (
+        <ChartTooltip>
+          <Text variant="t100" as="p" color="colorTextStrong">
+            {props.tooltip ? props.tooltip(point.point) : point.point.data.yStacked}
+          </Text>
+        </ChartTooltip>
+      )}
+    />
+  )
+}
+
+const ChartTooltip = styled.div`
+  background-color: ${cVar('colorBackgroundStrong')};
+  padding: ${sizes(1)} ${sizes(2)};
+`
