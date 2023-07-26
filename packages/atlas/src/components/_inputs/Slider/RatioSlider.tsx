4341cd3f125ff2c9fba025e1fea3b4e1a4e4ccf1
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-07-26 15:05:02 +0200

    🛝 CRT Ratio Slider (#4470)
    * Add the RatioSlider component
    
    * Add a story
    
    * Annimate the transitions
    
    * Rename the svg mask
    
    * Fix the RatioSliderProps type name
    
    * Fix the active steps class name

---
+++ packages/atlas/src/components/_inputs/Slider/RatioSlider.tsx
@@ -0,0 +1,78 @@
+import { ChangeEventHandler, forwardRef, useMemo, useState } from 'react'
+
+import { sizes } from '@/styles'
+
+import { Range, Track, Wrapper } from './RatioSlider.styles'
+
+type RatioSliderProps = {
+  min?: number
+  max?: number
+  step?: number
+  defaultValue?: number
+  value?: number
+  onChange?: (value: number) => void
+}
+
+export const RatioSlider = forwardRef<HTMLInputElement, RatioSliderProps>(
+  ({ min = 0, max = 100, step = 10, defaultValue = 50, value: controlledValue, onChange }, ref) => {
+    const [internalValue, setInternalValue] = useState<number>(defaultValue)
+    const value = controlledValue ?? internalValue
+
+    const handleChange: ChangeEventHandler<HTMLInputElement> = useMemo(
+      () =>
+        typeof controlledValue === 'undefined'
+          ? (evt) => setInternalValue(Number(evt.target.value))
+          : (evt) => onChange?.(Number(evt.target.value)),
+      [controlledValue, onChange]
+    )
+
+    const length = max - min
+    const valuePercent = `${(value / length) * 100}%`
+
+    const steps = useMemo(() => {
+      const stepPercent = (step / length) * 100
+      return Array.from({ length: Math.ceil(length / step) + 1 }).map(
+        (_, index) => `${Math.min(index * stepPercent, 100)}%`
+      )
+    }, [step, length])
+
+    return (
+      <Wrapper>
+        <Range ref={ref} type="range" min={min} max={max} step={step} value={value} onChange={handleChange} />
+
+        <Track xmlns="http://www.w3.org/2000/svg">
+          <circle className="knob" cx={valuePercent} cy={sizes(3)} r={sizes(2)} />
+
+          <mask id="cutout-mask">
+            <rect x="-5%" y="0%" width="110%" height="100%" fill="#fff" />
+            <circle className="cutout-mask" cx={valuePercent} cy={sizes(3)} r={sizes(3)} />
+          </mask>
+
+          <g className="rail" mask="url(#cutout-mask)">
+            <line className="rail-left" x1="0%" x2={valuePercent} y1={sizes(3)} y2={sizes(3)} />
+            <line className="rail-rigth" x1={valuePercent} x2="100%" y1={sizes(3)} y2={sizes(3)} />
+
+            <g className="steps">
+              {steps.map((x, index) => {
+                const cls = Math.min(index * step, max) <= internalValue ? 'active' : ''
+                return <line key={index} className={cls} x1={x} x2={x} y1={sizes(3 / 2)} y2={sizes(9 / 2)} radius={4} />
+              })}
+            </g>
+          </g>
+
+          <text x="0%" y={sizes(10)}>
+            {min}%
+          </text>
+          <text x="50%" y={sizes(10)}>
+            {internalValue}%
+          </text>
+          <text x="100%" y={sizes(10)}>
+            {max}%
+          </text>
+        </Track>
+      </Wrapper>
+    )
+  }
+)
+
+RatioSlider.displayName = 'RatioSlider'
