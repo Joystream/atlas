ebc325773651eb05959e3180d22a51ce298d624e
WRadoslaw <r.wyszynski00@gmail.com>
2023-07-27 14:42:22 +0200

    Setup second step

--- packages/atlas/src/components/_crt/CreateTokenDrawer/CreateTokenDrawer.tsx
+++ packages/atlas/src/components/_crt/CreateTokenDrawer/CreateTokenDrawer.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import { CrtDrawer, CrtDrawerProps } from '@/components/CrtDrawer'
 import { SetupTokenStep } from '@/components/_crt/CreateTokenDrawer/steps/SetupTokenStep'
 
+import { TokenIssuanceStep } from './steps/TokenIssuanceStep'
+
 enum CREATE_TOKEN_STEPS {
   setup,
   issuance,
@@ -16,11 +18,12 @@ export type CommonStepProps = {
 }
 
 export const CreateTokenDrawer = () => {
-  const [activeStep, setActiveStep] = useState(CREATE_TOKEN_STEPS.setup)
+  const [activeStep, setActiveStep] = useState(CREATE_TOKEN_STEPS.issuance)
   const [actionBarProps, setActionBarProps] = useState<CrtDrawerProps['actionBar']>()
   return (
     <CrtDrawer steps={steps} activeStep={activeStep} isOpen={true} onClose={() => undefined} actionBar={actionBarProps}>
       {activeStep === CREATE_TOKEN_STEPS.setup && <SetupTokenStep setActionBarProps={setActionBarProps} />}
+      {activeStep === CREATE_TOKEN_STEPS.issuance && <TokenIssuanceStep setActionBarProps={setActionBarProps} />}
     </CrtDrawer>
   )
 }
