1b44584ff4bf0cc048eee2ac75979380b04a8365
Artem <Artem Slugin>
2023-07-28 17:05:25 +0400

    custom fetcher for schema

---
+++ packages/atlas/codegen.config.ts
@@ -0,0 +1,57 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+
+import { customSchemaLoader } from './scripts/customSchemaLoader'
+
+const schemaUrl = 'https://orion.gleev.xyz/graphql'
+console.log(schemaUrl)
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: [
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    {
+      [schemaUrl]: {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        loader: customSchemaLoader,
+      },
+    },
+  ],
+  documents: ['./src/api/queries/**/*.graphql'],
+  config: {
+    scalars: {
+      DateTime: 'Date',
+      BigInt: 'string',
+    },
+    preResolveTypes: true, // avoid using Pick
+  },
+  generates: {
+    'src/api/schemas/orion.json': {
+      plugins: ['introspection'],
+      config: {
+        minify: true,
+      },
+    },
+    'src/api/queries/__generated__/baseTypes.generated.ts': {
+      hooks: {
+        afterOneFileWrite: ['prettier --write', 'eslint --fix'],
+      },
+      plugins: ['typescript'],
+    },
+    'src/api/queries/__generated__/': {
+      preset: 'near-operation-file',
+      presetConfig: {
+        baseTypesPath: 'baseTypes.generated.ts',
+        folder: '__generated__',
+        extension: '.generated.tsx',
+      },
+      hooks: {
+        afterOneFileWrite: ['prettier --write', 'eslint --fix'],
+      },
+      plugins: ['typescript-operations', 'typescript-react-apollo'],
+    },
+  },
+}
+
+export default config
