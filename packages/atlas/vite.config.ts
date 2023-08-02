e611c85b277842b31f494852f405b9b86b7f2c4b
WRadoslaw <r.wyszynski00@gmail.com>
2023-08-02 14:09:14 +0200

    Transfer token to correct .env file

--- packages/atlas/vite.config.ts
+++ packages/atlas/vite.config.ts
@@ -18,8 +18,7 @@ import {
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
-
+  const env = loadEnv(mode, `${process.cwd()}/src`)
   return {
     root: './src',
     build: {
