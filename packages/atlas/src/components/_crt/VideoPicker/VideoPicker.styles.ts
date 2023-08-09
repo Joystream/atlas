d1cdb24660a64159f99e1880fd832684d172b36e
WRadoslaw <r.wyszynski00@gmail.com>
2023-08-09 17:56:53 +0200

    Further work on video selection

--- packages/atlas/src/components/_crt/VideoPicker/VideoPicker.styles.ts
+++ packages/atlas/src/components/_crt/VideoPicker/VideoPicker.styles.ts
@@ -24,5 +24,31 @@ export const TextBox = styled.div`
 
 export const DialogContent = styled.div`
   display: grid;
+  padding: ${sizes(6)};
+`
+
+export const VideoBox = styled.div`
+  margin-bottom: ${sizes(6)};
+`
+
+export const ThumbnailContainer = styled.div`
+  position: relative;
+  height: 400px;
+`
+
+export const ThumbnailOverlay = styled.div`
+  display: grid;
+  place-items: center;
+  align-content: center;
   gap: ${sizes(2)};
+  position: absolute;
+  inset: 0;
+  background-color: #101214bf;
+  opacity: 0;
+  cursor: pointer;
+  transition: opacity 250ms ease-in-out;
+
+  :hover {
+    opacity: 1;
+  }
 `
--- packages/atlas/src/components/_crt/VideoPicker/VideoPicker.tsx
+++ packages/atlas/src/components/_crt/VideoPicker/VideoPicker.tsx
@@ -1,62 +1,129 @@
+import { useState } from 'react'
+
 import { useGetBasicVideosQuery } from '@/api/queries/__generated__/videos.generated'
-import { SvgActionPlus, SvgActionSearch, SvgIllustrativeVideo } from '@/assets/icons'
+import { SvgActionPlus, SvgActionSearch, SvgActionTrash, SvgIllustrativeVideo } from '@/assets/icons'
 import { Text } from '@/components/Text'
 import { Button } from '@/components/_buttons/Button'
-import { DialogContent, PlaceholderBox, TextBox } from '@/components/_crt/VideoPicker/VideoPicker.styles'
+import {
+  DialogContent,
+  PlaceholderBox,
+  TextBox,
+  ThumbnailContainer,
+  ThumbnailOverlay,
+  VideoBox,
+} from '@/components/_crt/VideoPicker/VideoPicker.styles'
 import { Input } from '@/components/_inputs/Input'
 import { DialogModal } from '@/components/_overlays/DialogModal'
-import { VideoListItem } from '@/components/_video/VideoListItem'
+import { VideoListItem, VideoListItemLoader } from '@/components/_video/VideoListItem'
+import { ThumbnailImage } from '@/components/_video/VideoThumbnail/VideoThumbnail.styles'
+import { useDebounceValue } from '@/hooks/useDebounceValue'
+import { useUser } from '@/providers/user/user.hooks'
 
 export const VideoPicker = () => {
+  const [showPicker, setShowPicker] = useState(false)
+  const [selectedVideo, setSelectedVideo] = useState<string | null>(null)
+  const { memberId } = useUser()
+  const { data } = useGetBasicVideosQuery({
+    variables: {
+      where: {
+        id_eq: selectedVideo,
+      },
+    },
+    skip: !selectedVideo,
+  })
+  console.log('selected video: ', data?.videos)
   return (
     <>
-      <SelectVideoDialog />
-      <PlaceholderBox>
-        <SvgIllustrativeVideo />
-        <TextBox>
-          <Text variant="h400" as="h4" color="colorTextStrong">
-            Token video trailer
-          </Text>
-          <Text variant="t200" as="p" color="colorText">
-            Present yourself, your idea and your project. Tell people why they should invest in you.
-          </Text>
-        </TextBox>
-        <Button variant="secondary" icon={<SvgActionPlus />} size="large">
-          Select video trailer
-        </Button>
-      </PlaceholderBox>
+      <SelectVideoDialog
+        show={showPicker}
+        onClose={() => setShowPicker(false)}
+        onVideoSelection={(id) => {
+          setSelectedVideo(id)
+          setShowPicker(false)
+        }}
+        memberId={memberId || undefined}
+      />
+      {data?.videos[0] ? (
+        <ThumbnailContainer>
+          <ThumbnailImage resolvedUrls={data?.videos[0].thumbnailPhoto?.resolvedUrls} />
+          <ThumbnailOverlay onClick={() => setSelectedVideo(null)}>
+            <SvgActionTrash />
+            <Text variant="t300" as="p">
+              Clear selection
+            </Text>
+          </ThumbnailOverlay>
+        </ThumbnailContainer>
+      ) : (
+        <PlaceholderBox>
+          <SvgIllustrativeVideo />
+          <TextBox>
+            <Text variant="h400" as="h4" color="colorTextStrong">
+              Token video trailer
+            </Text>
+            <Text variant="t200" as="p" color="colorText">
+              Present yourself, your idea and your project. Tell people why they should invest in you.
+            </Text>
+          </TextBox>
+          <Button variant="secondary" icon={<SvgActionPlus />} onClick={() => setShowPicker(true)} size="large">
+            Select video trailer
+          </Button>
+        </PlaceholderBox>
+      )}
     </>
   )
 }
 
-const SelectVideoDialog = ({ id }: { id?: string }) => {
-  const { data } = useGetBasicVideosQuery({
+type SelectVideoDialogProps = {
+  memberId?: string
+  onVideoSelection: (id: string) => void
+  show: boolean
+  onClose: () => void
+}
+
+const SelectVideoDialog = ({ memberId, onVideoSelection, show, onClose }: SelectVideoDialogProps) => {
+  const [search, setSearch] = useState('')
+  const debouncedSearch = useDebounceValue(search)
+  const { data, loading } = useGetBasicVideosQuery({
+    notifyOnNetworkStatusChange: true,
     variables: {
       where: {
+        title_containsInsensitive: debouncedSearch,
         channel: {
-          id_eq: '229',
-          // ownerMember: {
-          //   id_eq: '21',
-          // },
+          ownerMember: {
+            id_eq: memberId,
+          },
         },
       },
+      limit: 5,
     },
-    // skip: !id,
+    skip: !memberId,
   })
 
   return (
-    <DialogModal show>
+    <DialogModal title="Select video trailer" onExitClick={onClose} show={show} noContentPadding>
       <DialogContent>
-        <Text variant="h500" as="h5">
-          Select video trailer
-        </Text>
-        <Input nodeStart={<SvgActionSearch />} placeholder="Search for video" />
-        <div>
-          {data?.videos.map((video) => (
-            <VideoListItem key={video.id} variant="small" id={video.id} />
-          ))}
-        </div>
+        <Input
+          size="large"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          nodeStart={<SvgActionSearch />}
+          placeholder="Search for video"
+        />
       </DialogContent>
+
+      <VideoBox>
+        {loading
+          ? Array.from({ length: 5 }, (_, idx) => <VideoListItemLoader key={idx} variant="small" />)
+          : data?.videos.map((video) => (
+              <VideoListItem
+                key={video.id}
+                isInteractive
+                onClick={() => onVideoSelection(video.id)}
+                variant="small"
+                id={video.id}
+              />
+            ))}
+      </VideoBox>
     </DialogModal>
   )
 }
