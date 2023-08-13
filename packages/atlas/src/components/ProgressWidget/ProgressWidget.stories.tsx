6a88173cad454174f9cdd20afb76a5361536e869
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-08-13 18:53:16 +0200

    🌳 CRT progress widget (#4569)
    * Initial setup
    
    * Finishing touches for UI
    
    * Introduce data into widget
    
    * Create story
    
    * Lint fix
    
    * CR fixes

---
+++ packages/atlas/src/components/ProgressWidget/ProgressWidget.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryFn } from '@storybook/react'
+
+import { ProgressWidget, ProgressWidgetProps } from '@/components/ProgressWidget/ProgressWidget'
+import { Text } from '@/components/Text'
+
+import { TextButton } from '../_buttons/Button'
+
+export default {
+  title: 'CRT/ProgressWidget',
+  component: ProgressWidget,
+} as Meta<ProgressWidgetProps>
+
+const steps: ProgressWidgetProps['steps'] = [
+  {
+    title: 'Create token',
+    description: 'Create own token and share it with your viewers!',
+    primaryButton: {
+      text: 'Create token',
+    },
+  },
+  {
+    title: 'Share the good news',
+    description: 'Share base information about your token with your viewers',
+    primaryButton: {
+      text: 'Share now',
+    },
+  },
+  {
+    title: 'Take your profits',
+    description: 'Visit dashboard and withdraw first revenue from your token',
+    primaryButton: {
+      text: 'To the moon!',
+    },
+  },
+]
+
+const Template: StoryFn<ProgressWidgetProps> = (args) => <ProgressWidget {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  activeStep: 2,
+  steps,
+  goalComponent: (
+    <Text variant="t200" as="p">
+      Complete 1 more step to achieve <TextButton>Token master</TextButton>
+    </Text>
+  ),
+}
