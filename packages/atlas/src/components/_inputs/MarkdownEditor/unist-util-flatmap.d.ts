359c866db602c360de334d47ec9d0ecdca2dd2af
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-08-17 13:57:28 +0200

    Clean unused code

--- packages/atlas/src/components/_inputs/MarkdownEditor/unist-util-flatmap.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'unist-util-flatmap' {
-  type UnistNode = import('unist').Node
-  declare function flatMap<F extends UnistNode, T extends UnistNode>(from: F, tr: (v: F) => T[]): T
-  export default flatMap
-}
