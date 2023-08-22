bb9bd7b285a382b464f31ad15aeaaf7db08edfab
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-08-22 13:02:17 +0200

    😄 Benefit list item (#4644)
    * UI work
    
    * Add logic to single input
    
    * Disable move button on borders
    
    * Create multiple benefit input
    
    * Create story for multiple benefit input
    
    * Build fix
    
    * CR

---
+++ packages/atlas/src/components/_inputs/BenefitInput/BenefitInput.tsx
@@ -0,0 +1,53 @@
+import { ChangeEvent } from 'react'
+
+import { SvgActionChevronB, SvgActionChevronT } from '@/assets/icons'
+import { Button } from '@/components/_buttons/Button'
+import {
+  Container,
+  EmojiPlaceholder,
+  FlexBox,
+  StyledSvgActionTrash,
+} from '@/components/_inputs/BenefitInput/BenefitInput.styles'
+import { Input } from '@/components/_inputs/Input'
+import { TextArea } from '@/components/_inputs/TextArea'
+import { useMediaMatch } from '@/hooks/useMediaMatch'
+
+export type BenefitInputProps = {
+  position: 'last' | 'first' | 'middle'
+  onMoveUp: () => void
+  onMoveDown: () => void
+  onRemove: () => void
+  title?: string
+  description?: string
+  onTitleChange?: (e: ChangeEvent<HTMLInputElement>) => void
+  onDescriptionChange?: (e: ChangeEvent<HTMLTextAreaElement>) => void
+}
+
+export const BenefitInput = ({
+  onRemove,
+  onMoveUp,
+  onMoveDown,
+  title,
+  description,
+  onDescriptionChange,
+  onTitleChange,
+  position,
+}: BenefitInputProps) => {
+  const smMatch = useMediaMatch('sm')
+  return (
+    <Container>
+      <FlexBox>
+        <EmojiPlaceholder />
+      </FlexBox>
+      <FlexBox dir="column">
+        <Input placeholder="Add benefit title" value={title} onChange={onTitleChange} />
+        <TextArea placeholder="Add benefit description" value={description} onChange={onDescriptionChange} />
+      </FlexBox>
+      <FlexBox dir={smMatch ? 'column' : 'row'}>
+        <Button variant="tertiary" icon={<SvgActionChevronT />} onClick={onMoveUp} disabled={position === 'first'} />
+        <Button variant="tertiary" icon={<SvgActionChevronB />} onClick={onMoveDown} disabled={position === 'last'} />
+        <Button variant="tertiary" icon={<StyledSvgActionTrash />} onClick={onRemove} />
+      </FlexBox>
+    </Container>
+  )
+}
