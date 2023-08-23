b47033990672bf179cc6b9174e64a8eef958a80d
WRadoslaw <r.wyszynski00@gmail.com>
2023-08-23 10:02:54 +0200

    Terms step - chart

--- packages/atlas/src/components/_crt/BuySaleTokenModal/BuySaleTokenModal.tsx
+++ packages/atlas/src/components/_crt/BuySaleTokenModal/BuySaleTokenModal.tsx
@@ -1,12 +1,9 @@
-import { useMemo, useRef, useState } from 'react'
-import { flushSync } from 'react-dom'
-import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import { useMemo, useState } from 'react'
 
 import { BuySaleTokenForm } from '@/components/_crt/BuySaleTokenModal/steps/BuySaleTokenForm'
 import { BuySaleTokenTerms } from '@/components/_crt/BuySaleTokenModal/steps/BuySaleTokenTerms'
 import { DialogProps } from '@/components/_overlays/Dialog'
 import { DialogModal } from '@/components/_overlays/DialogModal'
-import { transitions } from '@/styles'
 
 type BuySaleTokenModalProps = {
   tokenId: string
@@ -21,8 +18,6 @@ enum BUY_SALE_TOKEN_STEPS {
 export const BuySaleTokenModal = ({ tokenId, onClose }: BuySaleTokenModalProps) => {
   const [activeStep, setActiveStep] = useState(BUY_SALE_TOKEN_STEPS.form)
   const [primaryButtonProps, setPrimaryButtonProps] = useState<DialogProps['primaryButton']>()
-  const nodeRef = useRef<HTMLDivElement>(null)
-  const [isGoingBack, setIsGoingBack] = useState(false)
 
   const secondaryButton = useMemo(
     () =>
@@ -33,9 +28,6 @@ export const BuySaleTokenModal = ({ tokenId, onClose }: BuySaleTokenModalProps) 
         : {
             text: 'Back',
             onClick: () => {
-              flushSync(() => {
-                setIsGoingBack(true)
-              })
               setActiveStep(BUY_SALE_TOKEN_STEPS.form)
             },
           },
@@ -55,35 +47,21 @@ export const BuySaleTokenModal = ({ tokenId, onClose }: BuySaleTokenModalProps) 
       primaryButton={primaryButtonProps}
       secondaryButton={secondaryButton}
     >
-      <SwitchTransition mode="out-in">
-        <CSSTransition
-          key={activeStep}
-          nodeRef={nodeRef}
-          timeout={100}
-          addEndListener={(done) => {
-            nodeRef.current?.addEventListener('transitionend', done, false)
-          }}
-          onEntered={() => setIsGoingBack(false)}
-          classNames={isGoingBack ? transitions.names.backwardSlideSwitch : transitions.names.forwardSlideSwitch}
-        >
-          <div ref={nodeRef}>
-            {activeStep === BUY_SALE_TOKEN_STEPS.form && (
-              <BuySaleTokenForm
-                {...commonProps}
-                onSubmit={() => setActiveStep(BUY_SALE_TOKEN_STEPS.terms)}
-                tokenId={tokenId}
-              />
-            )}
-            {activeStep === BUY_SALE_TOKEN_STEPS.terms && (
-              <BuySaleTokenTerms
-                {...commonProps}
-                onSubmit={() => setActiveStep(BUY_SALE_TOKEN_STEPS.terms)}
-                tokenId={tokenId}
-              />
-            )}
-          </div>
-        </CSSTransition>
-      </SwitchTransition>
+      {activeStep === BUY_SALE_TOKEN_STEPS.form && (
+        <BuySaleTokenForm
+          {...commonProps}
+          onSubmit={() => setActiveStep(BUY_SALE_TOKEN_STEPS.terms)}
+          tokenId={tokenId}
+        />
+      )}
+      {activeStep === BUY_SALE_TOKEN_STEPS.terms && (
+        <BuySaleTokenTerms
+          {...commonProps}
+          onSubmit={() => setActiveStep(BUY_SALE_TOKEN_STEPS.terms)}
+          tokenId={tokenId}
+          tokenAmount={1000}
+        />
+      )}
     </DialogModal>
   )
 }
--- packages/atlas/src/components/_crt/BuySaleTokenModal/steps/BuySaleTokenTerms.tsx
+++ packages/atlas/src/components/_crt/BuySaleTokenModal/steps/BuySaleTokenTerms.tsx
@@ -1,11 +1,17 @@
+import styled from '@emotion/styled'
 import { useMemo } from 'react'
 
 import { FlexBox } from '@/components/FlexBox/FlexBox'
 import { Information } from '@/components/Information'
 import { NumberFormat } from '@/components/NumberFormat'
 import { Text } from '@/components/Text'
+import { LineChart } from '@/components/_charts/LineChart'
 import { CommonProps } from '@/components/_crt/BuySaleTokenModal/steps/types'
+import { generateChartData } from '@/components/_crt/CreateTokenDrawer/steps/TokenIssuanceStep/TokenIssuanceStep.utils'
+import { TooltipBox } from '@/components/_crt/CreateTokenDrawer/steps/styles'
 import { useMountEffect } from '@/hooks/useMountEffect'
+import { cVar, media } from '@/styles'
+import { formatNumber } from '@/utils/number'
 import { formatDate } from '@/utils/time'
 
 const getTokenDetails = (_: string) => ({
@@ -13,23 +19,33 @@ const getTokenDetails = (_: string) => ({
   pricePerUnit: 1000,
   tokensOnSale: 67773,
   userBalance: 100000,
+  cliffTime: 1,
+  vestingTime: 6,
+  firstPayout: 25,
 })
 
 type BuySaleTokenTermsProps = {
   tokenId: string
+  tokenAmount: number
   onSubmit: () => void
 } & CommonProps
 
-export const BuySaleTokenTerms = ({ tokenId, setPrimaryButtonProps, onSubmit }: BuySaleTokenTermsProps) => {
-  const { tokensOnSale } = getTokenDetails(tokenId)
+export const BuySaleTokenTerms = ({
+  tokenId,
+  setPrimaryButtonProps,
+  onSubmit,
+  tokenAmount,
+}: BuySaleTokenTermsProps) => {
+  const { title, cliffTime, vestingTime, firstPayout } = getTokenDetails(tokenId)
 
-  const details = useMemo(
-    () => [
+  const details = useMemo(() => {
+    const currentDate = new Date()
+    return [
       {
         title: 'Your cliff',
         content: (
           <Text variant="h300" as="h1">
-            1 month
+            {cliffTime} month{cliffTime > 1 ? 's' : ''}
           </Text>
         ),
         date: new Date(),
@@ -38,29 +54,43 @@ export const BuySaleTokenTerms = ({ tokenId, setPrimaryButtonProps, onSubmit }: 
       {
         title: 'First payout',
         content: (
-          <NumberFormat value={0} as="p" variant="t200" withDenomination="before" withToken customTicker="$JBC" />
+          <NumberFormat
+            value={(tokenAmount * firstPayout) / 100}
+            as="p"
+            variant="t200"
+            withDenomination="before"
+            withToken
+            customTicker="$JBC"
+          />
         ),
-        date: new Date(),
+        date: new Date(new Date(currentDate.setMonth(currentDate.getMonth() + cliffTime))),
 
         tooltipText: 'Lorem ipsum',
       },
       {
         title: 'Your vesting',
-        content: <NumberFormat value={tokensOnSale} as="p" variant="t200" withDenomination="before" withToken />,
-        date: new Date(),
+        content: (
+          <Text variant="h300" as="h1">
+            {vestingTime} month{vestingTime > 1 ? 's' : ''}
+          </Text>
+        ),
+        date: new Date(new Date(currentDate.setMonth(currentDate.getMonth() + cliffTime + vestingTime))),
 
         tooltipText: 'Lorem ipsum',
       },
-    ],
-    [tokensOnSale]
-  )
+    ]
+  }, [cliffTime, firstPayout, tokenAmount, vestingTime])
 
   useMountEffect(() => {
     setPrimaryButtonProps({
-      text: 'Continue',
+      text: 'Buy tokens',
       onClick: onSubmit,
     })
   })
+
+  const chartData = useMemo(() => {
+    return generateChartData(cliffTime, vestingTime, firstPayout)
+  }, [cliffTime, firstPayout, vestingTime])
 
   return (
     <>
@@ -68,29 +98,80 @@ export const BuySaleTokenTerms = ({ tokenId, setPrimaryButtonProps, onSubmit }: 
         <Text variant="h400" as="h4">
           Holders terms
         </Text>
-        <FlexBox gap={4}>
-          <div>chart</div>
-        </FlexBox>
+        <FlexBox flow="column" gap={4}>
+          <ChartBox>
+            <LineChart
+              enablePointLabel
+              tooltip={(point) => {
+                const currentDate = new Date()
+                const timeInMonths = point.data.x === 'Now' ? 0 : +(point.data.x as string).split('M')[0]
+                return (
+                  <TooltipBox>
+                    <Text variant="t300" as="p">
+                      {formatNumber(((tokenAmount ?? 0) * (point.data.y as number)) / 100)} ${title}
+                    </Text>
+                    <Text variant="t100" as="p" color="colorTextMuted">
+                      {point.data.x !== 'Now'
+                        ? formatDate(new Date(currentDate.setMonth(currentDate.getMonth() + timeInMonths)))
+                        : 'Now'}
+                    </Text>
+                  </TooltipBox>
+                )
+              }}
+              yScale={{
+                type: 'linear',
+                min: 0,
+                max: 'auto',
+                stacked: false,
+                reverse: false,
+              }}
+              axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickValues: [0, 25, 50, 75, 100],
+                format: (tick) => `${tick}%`,
+              }}
+              gridYValues={[0, 25, 50, 75, 100]}
+              data={[
+                {
+                  id: 1,
+                  color: cVar('colorTextPrimary'),
+                  data: chartData,
+                },
+              ]}
+            />
+          </ChartBox>
 
-        <FlexBox flow="column" gap={2}>
-          {details.map((row) => (
-            <FlexBox key={row.title} justifyContent="space-between" equalChildren>
-              <FlexBox width="fit-content" alignItems="center">
-                <Text variant="h300" as="p" color="colorText">
-                  {row.title}
-                </Text>
-                <Information text={row.tooltipText} />
+          <FlexBox flow="column" gap={2}>
+            {details.map((row) => (
+              <FlexBox key={row.title} justifyContent="space-between" equalChildren>
+                <FlexBox width="fit-content" alignItems="center">
+                  <Text variant="h300" as="p" color="colorText">
+                    {row.title}
+                  </Text>
+                  <Information text={row.tooltipText} />
+                </FlexBox>
+                <FlexBox flow="column" alignItems="end">
+                  {row.content}
+                  <Text variant="t100" as="p" color="colorText">
+                    {formatDate(row.date)}
+                  </Text>
+                </FlexBox>
               </FlexBox>
-              <FlexBox flow="column" alignItems="end">
-                {row.content}
-                <Text variant="t100" as="p" color="colorText">
-                  {formatDate(row.date)}
-                </Text>
-              </FlexBox>
-            </FlexBox>
-          ))}
+            ))}
+          </FlexBox>
         </FlexBox>
       </FlexBox>
     </>
   )
 }
+
+const ChartBox = styled.div`
+  width: calc(100% + 125px);
+  height: 300px;
+  margin-left: -25px;
+
+  ${media.sm} {
+    width: calc(100% + 125px);
+  }
+`
