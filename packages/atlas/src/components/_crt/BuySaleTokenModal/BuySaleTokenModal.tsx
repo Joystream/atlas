ae88e7f3ff58a731dc67e8b100116124fba082aa
WRadoslaw <r.wyszynski00@gmail.com>
2023-08-25 10:00:04 +0200

    Success modal

--- packages/atlas/src/components/_crt/BuySaleTokenModal/BuySaleTokenModal.tsx
+++ packages/atlas/src/components/_crt/BuySaleTokenModal/BuySaleTokenModal.tsx
@@ -1,9 +1,11 @@
 import { useMemo, useState } from 'react'
 
 import { BuySaleTokenForm } from '@/components/_crt/BuySaleTokenModal/steps/BuySaleTokenForm'
+import { BuySaleTokenSuccess } from '@/components/_crt/BuySaleTokenModal/steps/BuySaleTokenSuccess'
 import { BuySaleTokenTerms } from '@/components/_crt/BuySaleTokenModal/steps/BuySaleTokenTerms'
 import { DialogProps } from '@/components/_overlays/Dialog'
 import { DialogModal } from '@/components/_overlays/DialogModal'
+import { useMediaMatch } from '@/hooks/useMediaMatch'
 
 export type BuySaleTokenModalProps = {
   tokenId: string
@@ -13,26 +15,31 @@ export type BuySaleTokenModalProps = {
 enum BUY_SALE_TOKEN_STEPS {
   form,
   terms,
+  success,
 }
 
 export const BuySaleTokenModal = ({ tokenId, onClose }: BuySaleTokenModalProps) => {
   const [activeStep, setActiveStep] = useState(BUY_SALE_TOKEN_STEPS.form)
   const [primaryButtonProps, setPrimaryButtonProps] = useState<DialogProps['primaryButton']>()
+  const smMatch = useMediaMatch('sm')
 
-  const secondaryButton = useMemo(
-    () =>
-      activeStep === BUY_SALE_TOKEN_STEPS.form
-        ? {
-            text: 'Cancel',
-          }
-        : {
-            text: 'Back',
-            onClick: () => {
-              setActiveStep(BUY_SALE_TOKEN_STEPS.form)
-            },
+  const secondaryButton = useMemo(() => {
+    switch (activeStep) {
+      case BUY_SALE_TOKEN_STEPS.terms:
+        return {
+          text: 'Back',
+          onClick: () => {
+            setActiveStep(BUY_SALE_TOKEN_STEPS.form)
           },
-    [activeStep]
-  )
+        }
+      case BUY_SALE_TOKEN_STEPS.form:
+        return {
+          text: 'Cancel',
+        }
+      default:
+        return undefined
+    }
+  }, [activeStep])
 
   const commonProps = {
     setPrimaryButtonProps,
@@ -40,13 +47,14 @@ export const BuySaleTokenModal = ({ tokenId, onClose }: BuySaleTokenModalProps) 
 
   return (
     <DialogModal
-      title="Buy $JBC"
+      title={activeStep !== BUY_SALE_TOKEN_STEPS.success ? 'Buy $JBC' : undefined}
+      onExitClick={activeStep !== BUY_SALE_TOKEN_STEPS.success ? onClose : undefined}
       dividers={activeStep === BUY_SALE_TOKEN_STEPS.terms}
       show
-      onExitClick={onClose}
       primaryButton={primaryButtonProps}
       secondaryButton={secondaryButton}
       noContentPadding={activeStep === BUY_SALE_TOKEN_STEPS.terms}
+      confetti={activeStep === BUY_SALE_TOKEN_STEPS.success && smMatch}
     >
       {activeStep === BUY_SALE_TOKEN_STEPS.form && (
         <BuySaleTokenForm
@@ -58,10 +66,13 @@ export const BuySaleTokenModal = ({ tokenId, onClose }: BuySaleTokenModalProps) 
       {activeStep === BUY_SALE_TOKEN_STEPS.terms && (
         <BuySaleTokenTerms
           {...commonProps}
-          onSubmit={() => setActiveStep(BUY_SALE_TOKEN_STEPS.terms)}
+          onSubmit={() => setActiveStep(BUY_SALE_TOKEN_STEPS.success)}
           tokenId={tokenId}
           tokenAmount={1000}
         />
+      )}
+      {activeStep === BUY_SALE_TOKEN_STEPS.success && (
+        <BuySaleTokenSuccess {...commonProps} onClose={onClose} tokenName="JBC" />
       )}
     </DialogModal>
   )
