6d4a732883a290fdab11dada2271ecccb86d70bb
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-08-28 13:06:56 +0200

    🪙 Token details tab (#4735)
    * Add flexbox
    
    * Adjust layout
    
    * Make it possible to hide video ending overlay
    
    * Introduce TokenDetails.tsx
    
    * Linter
    
    * CR fixes

--- packages/atlas/src/components/CrtPreviewLayout/CrtPreviewLayout.styles.ts
+++ packages/atlas/src/components/CrtPreviewLayout/CrtPreviewLayout.styles.ts
@@ -10,7 +10,7 @@ export const Wrapper = styled(LimitedWidthContainer)`
   margin-top: ${sizes(8)};
 
   ${media.md} {
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 2fr 1fr;
     column-gap: ${sizes(6)};
   }
 `
