0ec1715c2910f01ed29acea1ddbf111cd3e7428a
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-08-28 13:30:09 +0200

    💱 CRT transaction table component (#4752)
    * Extract a DateTimeBlock component from TablePaymentsHistory
    
    * Add the CrtTransactionsTable component
    
    * Fix stories globaly

---
+++ packages/atlas/src/components/DateTimeBlock/DateTimeBlock.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta } from '@storybook/react'
+
+import { DateTimeBlockProps, DateTimeBlock as _DateTimeBlock } from './DateTimeBlock'
+
+export default {
+  title: 'crt/DateTimeBlock',
+  component: _DateTimeBlock,
+  args: {
+    date: new Date(),
+  },
+} as Meta<DateTimeBlockProps>
+
+export const DateTimeBlock = {}
