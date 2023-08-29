e1d54caf729822c92a0f4f92d13553a6acafaeaf
WRadoslaw <r.wyszynski00@gmail.com>
2023-08-29 09:56:58 +0200

    Switch single pause into polling for timer test

--- packages/atlas/src/hooks/useTimer/useTimer.test.tsx
+++ packages/atlas/src/hooks/useTimer/useTimer.test.tsx
@@ -1,10 +1,6 @@
-import { act, renderHook } from '@testing-library/react'
+import { renderHook, waitFor } from '@testing-library/react'
 
 import { DIFF_THRESHOLD, useTimer } from './useTimer'
-
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
 
 describe('useTimer', () => {
   const diffTresholdMs = DIFF_THRESHOLD * 1000
@@ -12,34 +8,48 @@ describe('useTimer', () => {
   it('Should display date format', async () => {
     const bigDate = new Date(Date.now() + diffTresholdMs * 2)
     const { result, unmount } = renderHook(() => useTimer(bigDate))
-    await sleep(1000)
-    expect(result.current[0]?.includes('at')).toBe(true)
-    expect(result.current[1] === 'date').toBe(true)
+    await waitFor(
+      () => {
+        expect(result.current[0]?.includes('at')).toBe(true)
+        expect(result.current[1] === 'date').toBe(true)
+      },
+      {
+        timeout: 5_000,
+      }
+    )
+
     unmount()
   })
 
   it('Should display countdown format', async () => {
     const bigDate = new Date(Date.now() + (diffTresholdMs - 1000))
     const { result, unmount } = renderHook(() => useTimer(bigDate))
-    await act(async () => {
-      await sleep(2000)
-    })
-    expect(result.current[0]?.includes('at')).toBe(false)
-    expect(result.current[1] === 'countdown').toBe(true)
+    await waitFor(
+      () => {
+        expect(result.current[0]?.includes('at')).toBe(false)
+        expect(result.current[1] === 'countdown').toBe(true)
+      },
+      {
+        timeout: 5_000,
+      }
+    )
+
     unmount()
   })
 
   it('Should convert into date type after reaching zero', async () => {
-    const bigDate = new Date(Date.now() + 1000 * 2)
+    const bigDate = new Date(Date.now() + 3000)
     const { result, unmount } = renderHook(() => useTimer(bigDate))
-    await act(async () => {
-      await sleep(1000)
-    })
+
     expect(result.current[1] === 'countdown').toBe(true)
-    await act(async () => {
-      await sleep(2000)
-    })
-    expect(result.current[1] === 'date').toBe(true)
+    await waitFor(
+      () => {
+        expect(result.current[1] === 'date').toBe(true)
+      },
+      {
+        timeout: 10_000,
+      }
+    )
     unmount()
   })
 })
