c09af2b9cdfd7fd91961c28533a3d94c6d4c17f1
WRadoslaw <r.wyszynski00@gmail.com>
2023-08-30 14:17:37 +0200

    Story

---
+++ packages/atlas/src/components/_crt/HoldersWidget/HoldersWidget.stories.tsx
@@ -0,0 +1,44 @@
+import { ApolloProvider } from '@apollo/client'
+import { Meta, StoryFn } from '@storybook/react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { MemoryRouter } from 'react-router-dom'
+
+import { createApolloClient } from '@/api'
+import { HoldersWidget, HoldersWidgetProps } from '@/components/_crt/HoldersWidget/HoldersWidget'
+import { OperatorsContextProvider } from '@/providers/assets/assets.provider'
+import { JoystreamProvider } from '@/providers/joystream/joystream.provider'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+export default {
+  title: 'crt/HoldersWidget',
+  component: HoldersWidget,
+  decorators: [
+    (Story) => {
+      const apolloClient = createApolloClient()
+      return (
+        <MemoryRouter>
+          <QueryClientProvider client={queryClient}>
+            <ApolloProvider client={apolloClient}>
+              <OperatorsContextProvider>
+                <JoystreamProvider>
+                  <Story />
+                </JoystreamProvider>
+              </OperatorsContextProvider>
+            </ApolloProvider>
+          </QueryClientProvider>
+        </MemoryRouter>
+      )
+    },
+  ],
+} as Meta<HoldersWidgetProps>
+
+const Template: StoryFn<HoldersWidgetProps> = (args) => <HoldersWidget {...args} />
+
+export const Default = Template.bind({})
