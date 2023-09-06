bcec8202a23e1d7f2c0ee3d27860010ac72790a4
attemka <attemka@gmail.com>
2023-09-06 11:08:03 +0200

    reduce data for homePage queries (#4811)

--- packages/atlas/src/views/viewer/HomeView.tsx
+++ packages/atlas/src/views/viewer/HomeView.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import { FC } from 'react'
 
 import { VideoOrderByInput } from '@/api/queries/__generated__/baseTypes.generated'
-import { GetBasicVideosConnectionDocument } from '@/api/queries/__generated__/videos.generated'
+import { GetBasicVideosConnectionLightweightDocument } from '@/api/queries/__generated__/videos.generated'
 import { Section } from '@/components/Section/Section'
 import { VideoContentTemplate } from '@/components/_templates/VideoContentTemplate'
 import { VideoTileViewer } from '@/components/_video/VideoTileViewer'
@@ -15,7 +15,7 @@ import { InfiniteLoadingOffsets } from '@/utils/loading.contants'
 export const HomeView: FC = () => {
   const headTags = useHeadTags()
   const { columns, fetchMore, pageInfo, tiles } = useInfiniteVideoGrid({
-    query: GetBasicVideosConnectionDocument,
+    query: GetBasicVideosConnectionLightweightDocument,
     variables: {
       where: publicVideoFilter,
       orderBy: VideoOrderByInput.VideoRelevanceDesc,
