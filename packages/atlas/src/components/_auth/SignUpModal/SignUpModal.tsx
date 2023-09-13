0b32d0d072012f372d6bda53a9b23efb09649573
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-09-13 10:20:10 +0200

    🔥 Hotfix for account creation (#4855)
    * Add new error
    
    * Try to create session if its not present on request

--- packages/atlas/src/components/_auth/SignUpModal/SignUpModal.tsx
+++ packages/atlas/src/components/_auth/SignUpModal/SignUpModal.tsx
@@ -11,6 +11,7 @@ import { AccountFormData, FaucetError, MemberFormData, RegisterError, useCreateM
 import { useMediaMatch } from '@/hooks/useMediaMatch'
 import { useSegmentAnalytics } from '@/hooks/useSegmentAnalytics'
 import { useUniqueMemberHandle } from '@/hooks/useUniqueMemberHandle'
+import { handleAnonymousAuth } from '@/providers/auth/auth.helpers'
 import { useAuthStore } from '@/providers/auth/auth.store'
 import { useSnackbar } from '@/providers/snackbars'
 import { useYppStore } from '@/providers/ypp/ypp.store'
@@ -59,9 +60,11 @@ export const SignUpModal = () => {
   const [amountOfTokens, setAmountofTokens] = useState<number>()
   const memberRef = useRef<string | null>(null)
   const memberPollingTries = useRef(0)
+  const haveTriedCreateSession = useRef(false)
   const ytResponseData = useYppStore((state) => state.ytResponseData)
   const setYppModalOpenName = useYppStore((state) => state.actions.setYppModalOpenName)
   const setYtResponseData = useYppStore((state) => state.actions.setYtResponseData)
+  const { anonymousUserId } = useAuthStore()
   const { displaySnackbar } = useSnackbar()
   const ytEmailIsValid = Boolean(ytResponseData?.email && !ytResponseData.email.includes('@pages.plusgoogle.com'))
 
@@ -131,6 +134,21 @@ export const SignUpModal = () => {
           })
           setAuthModalOpenName(undefined)
         }
+        if (error === RegisterError.SessionRequired) {
+          if (!haveTriedCreateSession.current) {
+            haveTriedCreateSession.current = true
+            handleAnonymousAuth(anonymousUserId).then(() => {
+              handleOrionAccountCreation()
+            })
+          } else {
+            displaySnackbar({
+              title: 'Something went wrong',
+              description: 'We could not create or find session. Please contact support.',
+              iconType: 'error',
+            })
+            setAuthModalOpenName(undefined)
+          }
+        }
       },
       onStart: () => {
         goToStep(SignUpSteps.Creating)
@@ -148,6 +166,7 @@ export const SignUpModal = () => {
       },
     })
   }, [
+    anonymousUserId,
     createNewOrionAccount,
     displaySnackbar,
     goToNextStep,
