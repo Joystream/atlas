da9bb3233d1225d2b287211cc98286661fb878dd
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-09-14 10:07:17 +0200

    👨 CRT header widget (#4693)
    * Component
    
    * Story
    
    * Correct styling
    
    * CR fixes
    
    * Conflict fixes
    
    * Replace useRef

---
+++ packages/atlas/src/components/_crt/CrtBasicInfoWidget/CrtBasicInfoWidget.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, StoryFn } from '@storybook/react'
+
+import { JoyTokenIcon } from '@/components/JoyTokenIcon'
+import { CrtBasicInfoWidget, CrtBasicInfoWidgetProps } from '@/components/_crt/CrtBasicInfoWidget/CrtBasicInfoWidget'
+import { JoystreamProvider } from '@/providers/joystream/joystream.provider'
+
+export default {
+  title: 'crt/CrtBasicInfoWidget',
+  component: CrtBasicInfoWidget,
+  decorators: [
+    (Story) => (
+      <JoystreamProvider>
+        <Story />
+      </JoystreamProvider>
+    ),
+  ],
+} as Meta<CrtBasicInfoWidgetProps>
+
+const Template: StoryFn<CrtBasicInfoWidgetProps> = (args) => <CrtBasicInfoWidget {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  name: 'CRT',
+  details: [
+    {
+      caption: 'TOTAL REV.',
+      content: 65656,
+      icon: <JoyTokenIcon size={16} variant="silver" />,
+      tooltipText: 'Lorem ipsum',
+    },
+    {
+      caption: 'REV. SHARE',
+      content: '80%',
+      tooltipText: 'Lorem ipsum',
+    },
+    {
+      caption: 'AN. REWARD',
+      content: '20%',
+      icon: <JoyTokenIcon size={16} variant="silver" />,
+      tooltipText: 'Lorem ipsum',
+    },
+  ],
+}
