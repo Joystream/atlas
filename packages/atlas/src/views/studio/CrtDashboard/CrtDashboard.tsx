32efcc926cea9a7637417131fcde991e366c36a5
WRadoslaw <r.wyszynski00@gmail.com>
2023-09-20 13:30:00 +0200

    Add transaction to finalize revenue split

--- packages/atlas/src/views/studio/CrtDashboard/CrtDashboard.tsx
+++ packages/atlas/src/views/studio/CrtDashboard/CrtDashboard.tsx
@@ -5,6 +5,11 @@ import { LimitedWidthContainer } from '@/components/LimitedWidthContainer'
 import { Tabs } from '@/components/Tabs'
 import { Text } from '@/components/Text'
 import { Button } from '@/components/_buttons/Button'
+import { atlasConfig } from '@/config'
+import { useJoystream } from '@/providers/joystream'
+import { useSnackbar } from '@/providers/snackbars'
+import { useTransaction } from '@/providers/transactions/transactions.hooks'
+import { useUser } from '@/providers/user/user.hooks'
 import { HeaderContainer, MainContainer, TabsContainer } from '@/views/studio/CrtDashboard/CrtDashboard.styles'
 import { CrtDashboardMainTab } from '@/views/studio/CrtDashboard/tabs/CrtDashboardMainTab'
 import { CrtHoldersTab } from '@/views/studio/CrtDashboard/tabs/CrtHoldersTab'
@@ -14,11 +19,32 @@ const TABS = ['Dashboard', 'Holders', 'Revenue share', 'Settings'] as const
 
 export const CrtDashboard = () => {
   const [currentTab, setCurrentTab] = useState<number>(0)
+  const { joystream, proxyCallback } = useJoystream()
+  const { channelId, memberId } = useUser()
+  const { displaySnackbar } = useSnackbar()
+  const handleTransaction = useTransaction()
   const handleChangeTab = useCallback((idx: number) => {
     setCurrentTab(idx)
   }, [])
 
   const mappedTabs = TABS.map((tab) => ({ name: tab }))
+
+  const finalizeRevenueShare = useCallback(() => {
+    if (!joystream || !memberId || !channelId) {
+      return
+    }
+    handleTransaction({
+      txFactory: async (updateStatus) =>
+        (await joystream.extrinsics).finalizeRevenueSplit(memberId, channelId, proxyCallback(updateStatus)),
+      onTxSync: async (data) => {
+        displaySnackbar({
+          title: 'Revenue share is closed',
+          description: `Remaining unclaimed ${data.amount} ${atlasConfig.joystream.tokenTicker} was transfered back to your channel balance`,
+          iconType: 'info',
+        })
+      },
+    })
+  }, [channelId, displaySnackbar, handleTransaction, joystream, memberId])
 
   return (
     <LimitedWidthContainer>
@@ -34,9 +60,12 @@ export const CrtDashboard = () => {
 
         <TabsContainer>
           <Tabs initialIndex={0} selected={currentTab} tabs={mappedTabs} onSelectTab={handleChangeTab} />
-          <Button variant="secondary" icon={<SvgActionEdit />}>
-            Edit token page
-          </Button>
+          {currentTab === 0 && (
+            <Button variant="secondary" icon={<SvgActionEdit />}>
+              Edit token page
+            </Button>
+          )}
+          {currentTab === 2 && <Button onClick={finalizeRevenueShare}>Close revenue share</Button>}
           <Button icon={<SvgActionSell />}>Start sale or market</Button>
         </TabsContainer>
         {currentTab === 0 && <CrtDashboardMainTab />}
