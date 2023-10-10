b1ef030509277df4d375ba937ee3f0fe7b01f6b5
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-10-10 12:03:06 +0200

    😮‍💨 Start revenue share modal (#4869)
    * Add start revenue share modal
    
    * CR fixes
    
    * CR fixes v2
    
    * Lint

--- packages/atlas/src/views/studio/CrtDashboard/CrtDashboard.tsx
+++ packages/atlas/src/views/studio/CrtDashboard/CrtDashboard.tsx
@@ -1,10 +1,11 @@
 import { useCallback, useState } from 'react'
 
-import { SvgActionEdit, SvgActionLinkUrl, SvgActionSell } from '@/assets/icons'
+import { SvgActionEdit, SvgActionLinkUrl, SvgActionRevenueShare, SvgActionSell } from '@/assets/icons'
 import { LimitedWidthContainer } from '@/components/LimitedWidthContainer'
 import { Tabs } from '@/components/Tabs'
 import { Text } from '@/components/Text'
 import { Button } from '@/components/_buttons/Button'
+import { StartRevenueShare } from '@/components/_crt/StartRevenueShareModal/StartRevenueShareModal'
 import { HeaderContainer, MainContainer, TabsContainer } from '@/views/studio/CrtDashboard/CrtDashboard.styles'
 import { CrtDashboardMainTab } from '@/views/studio/CrtDashboard/tabs/CrtDashboardMainTab'
 import { CrtHoldersTab } from '@/views/studio/CrtDashboard/tabs/CrtHoldersTab'
@@ -14,6 +15,7 @@ const TABS = ['Dashboard', 'Holders', 'Revenue share', 'Settings'] as const
 
 export const CrtDashboard = () => {
   const [currentTab, setCurrentTab] = useState<number>(0)
+  const [openRevenueShareModal, setOpenRevenueShareModal] = useState(false)
   const handleChangeTab = useCallback((idx: number) => {
     setCurrentTab(idx)
   }, [])
@@ -22,6 +24,7 @@ export const CrtDashboard = () => {
 
   return (
     <LimitedWidthContainer>
+      <StartRevenueShare show={openRevenueShareModal} tokenId="1" onClose={() => setOpenRevenueShareModal(false)} />
       <MainContainer>
         <HeaderContainer>
           <Text variant="h700" as="h1">
@@ -34,10 +37,19 @@ export const CrtDashboard = () => {
 
         <TabsContainer>
           <Tabs initialIndex={0} selected={currentTab} tabs={mappedTabs} onSelectTab={handleChangeTab} />
-          <Button variant="secondary" icon={<SvgActionEdit />}>
-            Edit token page
-          </Button>
-          <Button icon={<SvgActionSell />}>Start sale or market</Button>
+          {currentTab === 0 && (
+            <>
+              <Button variant="secondary" icon={<SvgActionEdit />}>
+                Edit token page
+              </Button>
+              <Button icon={<SvgActionSell />}>Start sale or market</Button>
+            </>
+          )}
+          {currentTab === 2 && (
+            <Button onClick={() => setOpenRevenueShareModal(true)} icon={<SvgActionRevenueShare />}>
+              Start revenue share
+            </Button>
+          )}
         </TabsContainer>
         {currentTab === 0 && <CrtDashboardMainTab />}
         {currentTab === 1 && <CrtHoldersTab />}
