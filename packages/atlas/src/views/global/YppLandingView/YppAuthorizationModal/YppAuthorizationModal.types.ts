aa77fa1f5e6e25845f7e5132cc1014799700cf2b
WRadoslaw <r.wyszynski00@gmail.com>
2023-10-25 16:41:31 +0200

    Rework hook

--- packages/atlas/src/views/global/YppLandingView/YppAuthorizationModal/YppAuthorizationModal.types.ts
+++ packages/atlas/src/views/global/YppLandingView/YppAuthorizationModal/YppAuthorizationModal.types.ts
@@ -36,15 +36,11 @@ export type ChannelVerificationSuccessResponse = {
 }
 
 export type ChannelRequirements = {
-  MINIMUM_SUBSCRIBERS_COUNT: number
-  MINIMUM_TOTAL_VIDEOS_COUNT: number
-  MINIMUM_VIDEO_AGE_HOURS: number
-  MINIMUM_CHANNEL_AGE_HOURS: number
-  MINIMUM_VIDEOS_PER_MONTH: number
-  MONTHS_TO_CONSIDER: number
+  requirements: {
+    errorCode: YppRequirementsErrorCode
+    text: string
+  }[]
 }
-
-export type Requirements = Record<keyof ChannelRequirements, number | undefined>
 
 type ChannelRequirementsFailedError = {
   code: YppRequirementsErrorCode
