7d2c661de0e81e0e386d76ac4bc4c7555f98dafa
WRadoslaw <r.wyszynski00@gmail.com>
2023-11-08 12:25:44 +0100

    Claim revenue share

--- packages/atlas/src/views/studio/CrtDashboard/tabs/CrtRevenueTab.tsx
+++ packages/atlas/src/views/studio/CrtDashboard/tabs/CrtRevenueTab.tsx
@@ -7,6 +7,7 @@ import { GridItem, LayoutGrid } from '@/components/LayoutGrid'
 import { NumberFormat } from '@/components/NumberFormat'
 import { RatioPreview } from '@/components/RatioPreview/RatioPreview'
 import { WidgetTile } from '@/components/WidgetTile'
+import { ClaimShareModal } from '@/components/_crt/ClaimShareModal'
 import { RevenueShareHistoryTable } from '@/components/_crt/RevenueShareHistoryTable'
 import { RevenueShareParticipationWidget } from '@/components/_crt/RevenueShareParticipationWidget'
 import { RevenueShareStakersTable } from '@/components/_crt/RevenueShareStakersTable'
@@ -31,72 +32,75 @@ export const CrtRevenueTab = ({ token }: CrtRevenueTabProps) => {
   const finalizedRevenueShares = token.revenueShares.filter((revenueShare) => revenueShare.finalized)
 
   return (
-    <LayoutGrid>
-      <GridItem colSpan={{ base: 12, sm: 4 }}>
-        <RevenueShareStateWidget endsAtBlock={activeRevenueShare?.endsAt} />
-      </GridItem>
-      <GridItem colSpan={{ base: 12, sm: 4 }}>
-        <WidgetTile
-          title="CHANNEL BALANCE"
-          customNode={
-            <NumberFormat
-              value={channelBalance ?? 0}
-              icon={<SvgJoyTokenMonochrome24 />}
-              variant="h500"
-              as="p"
-              withDenomination
-            />
-          }
-          tooltip={{
-            text: 'Lorem ipsum',
-          }}
-        />
-      </GridItem>
-      <GridItem colSpan={{ base: 12, sm: 4 }}>
-        <WidgetTile
-          title="REVENUE SHARE RATIO"
-          customNode={
-            <RatioPreview
-              ratios={[
-                [100 - permillToPercentage(token.revenueShareRatioPermill), 'Holders'],
-                [permillToPercentage(token.revenueShareRatioPermill), 'Channel'],
-              ]}
-            />
-          }
-        />
-      </GridItem>
-      {activeRevenueShare && (
-        <GridItem colSpan={{ base: 12 }}>
-          <RevenueShareParticipationWidget revenueShare={activeRevenueShare} />
+    <>
+      <ClaimShareModal onClose={() => undefined} show={true} token={token} />
+      <LayoutGrid>
+        <GridItem colSpan={{ base: 12, sm: 4 }}>
+          <RevenueShareStateWidget endsAtBlock={activeRevenueShare?.endsAt} />
         </GridItem>
-      )}
-
-      {activeRevenueShare ? (
-        <GridItem colSpan={{ base: 12 }}>
-          <RevenueShareStakersTable
-            data={activeRevenueShare.stakers.map((staker) => ({
-              memberId: staker.account.member.id,
-              stakedAtBlock: staker.createdIn,
-              staked: +(staker.stakedAmount ?? 0),
-              earnings: +(staker.earnings ?? 0),
-            }))}
-            tokenSymbol={token.symbol}
+        <GridItem colSpan={{ base: 12, sm: 4 }}>
+          <WidgetTile
+            title="CHANNEL BALANCE"
+            customNode={
+              <NumberFormat
+                value={channelBalance ?? 0}
+                icon={<SvgJoyTokenMonochrome24 />}
+                variant="h500"
+                as="p"
+                withDenomination
+              />
+            }
+            tooltip={{
+              text: 'Lorem ipsum',
+            }}
           />
         </GridItem>
-      ) : null}
-
-      {finalizedRevenueShares.length ? (
-        <GridItem colSpan={{ base: 12 }}>
-          <RevenueShareHistoryTable
-            data={finalizedRevenueShares.map((revenueShare) => ({
-              claimed: +(revenueShare.claimed ?? 0),
-              stakers: revenueShare.stakers,
-              totalParticipants: revenueShare.participantsNum,
-              endsAtBlock: revenueShare.endsAt,
-            }))}
+        <GridItem colSpan={{ base: 12, sm: 4 }}>
+          <WidgetTile
+            title="REVENUE SHARE RATIO"
+            customNode={
+              <RatioPreview
+                ratios={[
+                  [100 - permillToPercentage(token.revenueShareRatioPermill), 'Holders'],
+                  [permillToPercentage(token.revenueShareRatioPermill), 'Channel'],
+                ]}
+              />
+            }
           />
         </GridItem>
-      ) : null}
-    </LayoutGrid>
+        {activeRevenueShare && (
+          <GridItem colSpan={{ base: 12 }}>
+            <RevenueShareParticipationWidget revenueShare={activeRevenueShare} />
+          </GridItem>
+        )}
+
+        {activeRevenueShare ? (
+          <GridItem colSpan={{ base: 12 }}>
+            <RevenueShareStakersTable
+              data={activeRevenueShare.stakers.map((staker) => ({
+                memberId: staker.account.member.id,
+                stakedAtBlock: staker.createdIn,
+                staked: +(staker.stakedAmount ?? 0),
+                earnings: +(staker.earnings ?? 0),
+              }))}
+              tokenSymbol={token.symbol}
+            />
+          </GridItem>
+        ) : null}
+
+        {finalizedRevenueShares.length ? (
+          <GridItem colSpan={{ base: 12 }}>
+            <RevenueShareHistoryTable
+              data={finalizedRevenueShares.map((revenueShare) => ({
+                claimed: +(revenueShare.claimed ?? 0),
+                stakers: revenueShare.stakers,
+                totalParticipants: revenueShare.participantsNum,
+                endsAtBlock: revenueShare.endsAt,
+              }))}
+            />
+          </GridItem>
+        ) : null}
+      </LayoutGrid>
+    </>
   )
 }
