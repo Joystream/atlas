970e2fe81feccac4bb9f5fbbd5ca6eb4df0f7ddb
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-11-13 13:28:45 +0100

    🤑 Buy sell amm (#5135)
    * Update schema and fragment
    
    * Add market type for CrtStatusWidget
    
    * Update fragment
    
    * Update transaction
    
    * Change on modal
    
    * Create button for modal and place it in widget
    
    * Linter
    
    * Interate modal UI with orion data
    
    * Create sell token button and place it in widget
    
    * Small logic improvements
    
    * Clean up
    
    * Initial correction of buy amm formulas
    
    * Further fixes on token calculations
    
    * Add templates for amm modals
    
    * Rework sell amm modal
    
    * Adjust buy amm modal
    
    * Build fix

---
+++ packages/atlas/src/components/_crt/AmmModalTemplates/AmmModalSummaryTemplate.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+
+import { FlexBox } from '@/components/FlexBox'
+import { Information } from '@/components/Information'
+import { Text } from '@/components/Text'
+
+type AmmModalSummaryTemplateProps = {
+  details?: {
+    title: string
+    content: ReactNode
+    tooltipText?: string
+  }[]
+}
+
+export const AmmModalSummaryTemplate = ({ details }: AmmModalSummaryTemplateProps) => {
+  return (
+    <FlexBox flow="column" gap={5}>
+      {details?.map((row, i) => (
+        <FlexBox key={row.title} alignItems="center" justifyContent="space-between">
+          <FlexBox width="auto" alignItems="center">
+            <Text variant={i + 1 === details.length ? 't200-strong' : 't200'} as="span" color="colorText">
+              {row.title}
+            </Text>
+            {row.tooltipText ? <Information text={row.tooltipText} /> : null}
+          </FlexBox>
+          {row.content}
+        </FlexBox>
+      ))}
+    </FlexBox>
+  )
+}
