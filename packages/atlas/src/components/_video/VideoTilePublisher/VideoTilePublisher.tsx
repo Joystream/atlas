321c6db2bb9b7ed72591fc71f3fb53ec4df9919e
WRadoslaw <r.wyszynski00@gmail.com>
2023-11-21 13:53:47 +0100

    Remove views from viewer video tile

--- packages/atlas/src/components/_video/VideoTilePublisher/VideoTilePublisher.tsx
+++ packages/atlas/src/components/_video/VideoTilePublisher/VideoTilePublisher.tsx
@@ -331,6 +331,7 @@ export const VideoTilePublisher: FC<VideoTilePublisherProps> = memo(
         videoTitle={video?.title}
         views={video?.viewsNum}
         kebabMenuItems={getPublisherKebabMenuItems()}
+        isPublisher
       />
     )
   }
