4e5b35b6909606bddd1317d0a119f09379f74dee
WRadoslaw <r.wyszynski00@gmail.com>
2023-12-08 13:17:18 +0100

    Fix close revenue share button on main dashboard

--- packages/atlas/src/components/_crt/CloseRevenueShareButton/CloseRevenueShareButton.tsx
+++ packages/atlas/src/components/_crt/CloseRevenueShareButton/CloseRevenueShareButton.tsx
@@ -1,7 +1,8 @@
+import { useApolloClient } from '@apollo/client'
 import { useCallback } from 'react'
 
 import { FullCreatorTokenFragment } from '@/api/queries/__generated__/fragments.generated'
-import { Button } from '@/components/_buttons/Button'
+import { Button, ButtonProps } from '@/components/_buttons/Button'
 import { atlasConfig } from '@/config'
 import { useJoystream } from '@/providers/joystream'
 import { useJoystreamStore } from '@/providers/joystream/joystream.store'
@@ -11,14 +12,15 @@ import { useUser } from '@/providers/user/user.hooks'
 
 type CloseRevenueShareButtonProps = {
   revenueShare: FullCreatorTokenFragment['revenueShares'][number]
-}
+} & Pick<ButtonProps, 'variant' | 'disabled'>
 
-export const CloseRevenueShareButton = ({ revenueShare }: CloseRevenueShareButtonProps) => {
+export const CloseRevenueShareButton = ({ revenueShare, variant }: CloseRevenueShareButtonProps) => {
   const { joystream, proxyCallback } = useJoystream()
   const { channelId, memberId } = useUser()
   const handleTransaction = useTransaction()
   const { displaySnackbar } = useSnackbar()
   const { currentBlock } = useJoystreamStore()
+  const client = useApolloClient()
 
   const finalizeRevenueShare = useCallback(() => {
     if (!joystream || !memberId || !channelId) {
@@ -28,18 +30,23 @@ export const CloseRevenueShareButton = ({ revenueShare }: CloseRevenueShareButto
       txFactory: async (updateStatus) =>
         (await joystream.extrinsics).finalizeRevenueSplit(memberId, channelId, proxyCallback(updateStatus)),
       onTxSync: async (data) => {
+        client.refetchQueries({ include: 'active' })
         displaySnackbar({
           title: 'Revenue share is closed',
-          description: `Remaining unclaimed ${data.amount} ${atlasConfig.joystream.tokenTicker} was transfered back to your channel balance`,
+          description: `Remaining unclaimed ${data.amount} ${atlasConfig.joystream.tokenTicker} was transfered back to your channel balance.`,
           iconType: 'info',
         })
       },
     })
-  }, [channelId, displaySnackbar, handleTransaction, joystream, memberId, proxyCallback])
+  }, [channelId, client, displaySnackbar, handleTransaction, joystream, memberId, proxyCallback])
 
   if (currentBlock < revenueShare.endsAt) {
     return null
   }
 
-  return <Button onClick={finalizeRevenueShare}>Close revenue share</Button>
+  return (
+    <Button onClick={finalizeRevenueShare} variant={variant}>
+      Close revenue share
+    </Button>
+  )
 }
--- packages/atlas/src/components/_crt/CrtRevenueShareWidget/CrtRevenueShareWidget.tsx
+++ packages/atlas/src/components/_crt/CrtRevenueShareWidget/CrtRevenueShareWidget.tsx
@@ -8,6 +8,7 @@ import { Avatar } from '@/components/Avatar'
 import { FlexBox } from '@/components/FlexBox'
 import { Text } from '@/components/Text'
 import { Button, TextButton } from '@/components/_buttons/Button'
+import { CloseRevenueShareButton } from '@/components/_crt/CloseRevenueShareButton'
 import { Widget } from '@/components/_crt/CrtStatusWidget/CrtStatusWidget.styles'
 import { RevenueShareProgress } from '@/components/_crt/RevenueShareParticipationWidget'
 import { SkeletonLoader } from '@/components/_loaders/SkeletonLoader'
@@ -16,7 +17,7 @@ import { getMemberAvatar } from '@/providers/assets/assets.helpers'
 import { permillToPercentage } from '@/utils/number'
 import { formatDateTime, formatDurationShort } from '@/utils/time'
 
-import { CloseRevenueButton, CustomPill, EmptyStateBox, StakersBox, StyledPill } from './CrtRevenueShareWidget.styles'
+import { CustomPill, EmptyStateBox, StakersBox, StyledPill } from './CrtRevenueShareWidget.styles'
 
 export type CrtHoldersWidgetProps = {
   token: FullCreatorTokenFragment
@@ -106,10 +107,12 @@ export const CrtRevenueShareWidget = ({ token, onTabSwitch }: CrtHoldersWidgetPr
             <RevenueShareProgress token={token} revenueShare={activeRevenueShare} hasEnded={status === 'past'} />
           )}
 
-          {status !== 'inactive' && (
-            <CloseRevenueButton disabled={status === 'active'} variant="secondary">
-              Close revenue share
-            </CloseRevenueButton>
+          {activeRevenueShare && status !== 'inactive' && (
+            <CloseRevenueShareButton
+              disabled={status === 'active'}
+              variant="secondary"
+              revenueShare={activeRevenueShare}
+            />
           )}
         </FlexBox>
       }
