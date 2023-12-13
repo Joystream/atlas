ec3d9b0889a2081af541322b2f37606233b3eeff
WRadoslaw <r.wyszynski00@gmail.com>
2023-12-13 11:12:15 +0100

    Market tab loading

--- packages/atlas/src/hooks/useGetTokenBalance.ts
+++ packages/atlas/src/hooks/useGetTokenBalance.ts
@@ -15,7 +15,6 @@ export const useGetTokenBalance = (tokenId?: string, memberId?: string) => {
       memberId: memberId ?? currentMemberId ?? '',
       currentBlockHeight: currentBlock,
     },
-    notifyOnNetworkStatusChange: true,
     fetchPolicy: 'no-cache',
     skip: !!tokenBalance || !tokenId || !(memberId || currentMemberId),
     onCompleted: (data) => {
