892cac78638de6d5b7a4374355859e8d5cee1b3b
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-12-19 10:20:42 +0100

    🎓 CRT Marketplace scaffold (#5674)

--- packages/atlas/src/views/viewer/MarketplaceView/MarketplaceView.tsx
+++ packages/atlas/src/views/viewer/MarketplaceView/MarketplaceView.tsx
@@ -1,32 +1,43 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
-import { useFeaturedNftsVideos } from '@/api/hooks/nfts'
-import { AllNftSection } from '@/components/AllNftSection/AllNftSection'
-import { LimitedWidthContainer } from '@/components/LimitedWidthContainer'
-import { MarketplaceCarousel } from '@/components/NftCarousel/MarketplaceCarousel'
-import { TopSellingChannelsTable } from '@/components/TopSellingChannelsTable'
-import { useHeadTags } from '@/hooks/useHeadTags'
+import { SvgActionCreatorToken, SvgActionPlay } from '@/assets/icons'
+import { useMediaMatch } from '@/hooks/useMediaMatch'
+import { StyledPageTabs } from '@/views/viewer/PortfolioView'
 
-import { FeaturedNftsSection } from './FeaturedNftsSection/FeaturedNftsSection'
-import { MarketplaceWrapper, TableFullWitdhtWrapper } from './MarketplaceView.styles'
+import { MarketplaceWrapper } from './MarketplaceView.styles'
+import { MarketplaceCrtTab } from './tabs/MarketplaceCrtTab'
+import { MarketplaceNftTab } from './tabs/MarketplaceNftTab'
+
+const TABS = [
+  {
+    name: 'Video NFTs',
+    description: 'Explore offers of non-fungible tokens for popular videos',
+    icon: <SvgActionPlay />,
+  },
+  {
+    name: 'Creator Tokens',
+    description: 'Discover channels you can invest in',
+    icon: <SvgActionCreatorToken />,
+  },
+]
 
 export const MarketplaceView: FC = () => {
-  const headTags = useHeadTags('Marketplace')
-  const { nfts, loading } = useFeaturedNftsVideos()
+  const [tab, setTab] = useState(0)
+  const smMatch = useMediaMatch('sm')
 
   return (
-    <MarketplaceWrapper>
-      {headTags}
-      <MarketplaceCarousel type="nft" nfts={nfts} isLoading={loading} />
-      <FeaturedNftsSection />
-      <TableFullWitdhtWrapper>
-        <LimitedWidthContainer big noBottomPadding>
-          <TopSellingChannelsTable />
-        </LimitedWidthContainer>
-      </TableFullWitdhtWrapper>
-      <LimitedWidthContainer big noBottomPadding fullWidth>
-        <AllNftSection />
-      </LimitedWidthContainer>
-    </MarketplaceWrapper>
+    <>
+      <StyledPageTabs
+        isBig
+        tabs={TABS.map((tab) => (smMatch ? tab : { ...tab, description: '' }))}
+        onSelectTab={setTab}
+        selected={tab}
+      />
+
+      <MarketplaceWrapper>
+        {tab === 0 && <MarketplaceNftTab />}
+        {tab === 1 && <MarketplaceCrtTab />}
+      </MarketplaceWrapper>
+    </>
   )
 }
