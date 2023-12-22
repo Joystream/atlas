2d8ed13696a7ab462e767c65bf355e17b69025cd
WRadoslaw <r.wyszynski00@gmail.com>
2023-12-22 16:42:27 +0100

    Top selling channel with token

--- packages/atlas/src/views/viewer/MarketplaceView/tabs/MarketplaceCrtTab.tsx
+++ packages/atlas/src/views/viewer/MarketplaceView/tabs/MarketplaceCrtTab.tsx
@@ -3,10 +3,13 @@ import BN from 'bn.js'
 import { useGetBasicCreatorTokenQuery } from '@/api/queries/__generated__/creatorTokens.generated'
 import { FallbackContainer } from '@/components/AllNftSection'
 import { EmptyFallback } from '@/components/EmptyFallback'
+import { LimitedWidthContainer } from '@/components/LimitedWidthContainer'
 import { Section } from '@/components/Section/Section'
+import { TopSellingChannelsTable } from '@/components/TopSellingChannelsTable'
 import { CrtCard, CrtSaleTypes } from '@/components/_crt/CrtCard/CrtCard'
 import { hapiBnToTokenNumber } from '@/joystream-lib/utils'
 import { DEFAULT_NFTS_GRID } from '@/styles'
+import { TableFullWitdhtWrapper } from '@/views/viewer/MarketplaceView/MarketplaceView.styles'
 
 export const MarketplaceCrtTab = () => {
   const { data } = useGetBasicCreatorTokenQuery({})
@@ -46,24 +49,31 @@ export const MarketplaceCrtTab = () => {
   )
 
   return (
-    <Section
-      headerProps={{
-        start: {
-          type: 'title',
-          title: 'All NFTs',
-        },
-      }}
-      contentProps={{
-        type: 'grid',
-        grid: DEFAULT_NFTS_GRID,
-        children: children?.length
-          ? children
-          : [
-              <FallbackContainer key="fallback">
-                <EmptyFallback title="No NFTs found" subtitle="Please, try changing your filtering criteria." />
-              </FallbackContainer>,
-            ],
-      }}
-    />
+    <>
+      <Section
+        headerProps={{
+          start: {
+            type: 'title',
+            title: 'All NFTs',
+          },
+        }}
+        contentProps={{
+          type: 'grid',
+          grid: DEFAULT_NFTS_GRID,
+          children: children?.length
+            ? children
+            : [
+                <FallbackContainer key="fallback">
+                  <EmptyFallback title="No NFTs found" subtitle="Please, try changing your filtering criteria." />
+                </FallbackContainer>,
+              ],
+        }}
+      />
+      <TableFullWitdhtWrapper>
+        <LimitedWidthContainer big noBottomPadding>
+          <TopSellingChannelsTable withCrtOnly />
+        </LimitedWidthContainer>
+      </TableFullWitdhtWrapper>
+    </>
   )
 }
