5dbedda8da77195326de6f0ee6741c14ad4718ff
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2023-12-23 19:13:20 +0100

    🪙 Marketplace tokens table (#5685)
    * Create table component
    
    * Create hook to handle filtering and sorting logic
    
    * Create section component to encaplusate table logic
    
    * Minor improvements
    
    * Add total count to the header
    
    * Build fixes

---
+++ packages/atlas/src/components/_crt/AllTokensSection/AllTokensSection.tsx
@@ -0,0 +1,100 @@
+import BN from 'bn.js'
+
+import {
+  useGetBasicCreatorTokensQuery,
+  useGetCreatorTokensCountQuery,
+} from '@/api/queries/__generated__/creatorTokens.generated'
+import { FallbackContainer } from '@/components/AllNftSection'
+import { EmptyFallback } from '@/components/EmptyFallback'
+import { NumberFormat } from '@/components/NumberFormat'
+import { Section } from '@/components/Section/Section'
+import { Button } from '@/components/_buttons/Button'
+import { MarketplaceCrtTable } from '@/components/_crt/MarketplaceCrtTable'
+import { SORTING_FILTERS, useCrtSectionFilters } from '@/hooks/useCrtSectionFilters'
+import { useMediaMatch } from '@/hooks/useMediaMatch'
+import { hapiBnToTokenNumber } from '@/joystream-lib/utils'
+
+export const AllTokensSection = () => {
+  const smMatch = useMediaMatch('sm')
+  const {
+    creatorTokenWhereInput,
+    order,
+    hasAppliedFilters,
+    rawFilters,
+    actions: { onApplyFilters, setOrder, clearFilters },
+  } = useCrtSectionFilters()
+
+  const { data, loading } = useGetBasicCreatorTokensQuery({
+    variables: {
+      where: creatorTokenWhereInput,
+      orderBy: order,
+    },
+  })
+  const { data: countData } = useGetCreatorTokensCountQuery({
+    variables: {
+      where: creatorTokenWhereInput,
+    },
+  })
+  const totalCount = countData?.creatorTokensConnection.totalCount
+
+  const tableData =
+    data?.creatorTokens.map(({ createdAt, accountsNum, lastPrice, totalSupply, status, symbol, channel }) => ({
+      createdAt: new Date(createdAt),
+      totalRevenue: 0,
+      holdersNum: accountsNum,
+      isVerified: true,
+      marketCap: lastPrice && totalSupply ? hapiBnToTokenNumber(new BN(lastPrice).mul(new BN(totalSupply))) ?? 0 : 0,
+      status,
+      channelId: channel?.channel.id ?? '',
+      tokenName: symbol ?? 'N/A',
+      tokenTitle: symbol ?? 'N/A',
+    })) ?? []
+
+  return (
+    <Section
+      headerProps={{
+        onApplyFilters,
+        start: {
+          type: 'title',
+          title: 'All Creator Tokens',
+          nodeEnd:
+            typeof totalCount === 'number' ? (
+              <NumberFormat value={totalCount} as="p" variant={smMatch ? 'h500' : 'h400'} color="colorTextMuted" />
+            ) : undefined,
+        },
+        filters: rawFilters,
+        sort: {
+          type: 'toggle-button',
+          toggleButtonOptionTypeProps: {
+            type: 'options',
+            options: SORTING_FILTERS,
+            value: order,
+            onChange: setOrder,
+          },
+        },
+      }}
+      contentProps={{
+        type: 'grid',
+        grid: { xxs: { columns: 1 } },
+        children:
+          tableData.length || loading
+            ? [<MarketplaceCrtTable key="single-table" data={tableData} isLoading={loading} />]
+            : [
+                <FallbackContainer key="fallback">
+                  <EmptyFallback
+                    title="No CRTs found"
+                    subtitle="Please, try changing your filtering criteria."
+                    button={
+                      hasAppliedFilters && (
+                        <Button variant="secondary" onClick={() => clearFilters()}>
+                          Clear all filters
+                        </Button>
+                      )
+                    }
+                  />
+                </FallbackContainer>,
+              ],
+      }}
+    />
+  )
+}
---
+++ packages/atlas/src/components/_crt/MarketplaceCrtTable/MarketplaceCrtTable.tsx
@@ -0,0 +1,140 @@
+import styled from '@emotion/styled'
+import { useMemo } from 'react'
+
+import { TokenStatus } from '@/api/queries/__generated__/baseTypes.generated'
+import { JoyTokenIcon } from '@/components/JoyTokenIcon'
+import { NumberFormat } from '@/components/NumberFormat'
+import { Table, TableProps } from '@/components/Table'
+import { ColumnBox } from '@/components/Table/Table.styles'
+import { Text } from '@/components/Text'
+import { SkeletonLoader } from '@/components/_loaders/SkeletonLoader'
+import { cVar } from '@/styles'
+import { pluralizeNoun } from '@/utils/misc'
+import { formatDate } from '@/utils/time'
+
+import { CrtStatus, TokenInfo } from '../CrtPortfolioTable'
+
+export const tableLoadingData = Array.from({ length: 10 }, () => ({
+  token: (
+    <ColumnBox>
+      <SkeletonLoader rounded height={32} width={32} />
+      <SkeletonLoader height={20} width="40%" />
+    </ColumnBox>
+  ),
+  status: <SkeletonLoader height={20} width="60%" />,
+  createdAt: <SkeletonLoader height={20} width="50%" />,
+  marketCap: <SkeletonLoader height={20} width="50%" />,
+  totalRevenue: <SkeletonLoader height={20} width="50%" />,
+  holders: <SkeletonLoader height={20} width="50%" />,
+}))
+
+const COLUMNS: TableProps['columns'] = [
+  { Header: 'Token', accessor: 'token', width: 300 },
+  { Header: 'Status', accessor: 'status', width: 100 },
+  { Header: 'Date created', accessor: 'createdAt', width: 100 },
+  { Header: 'Market cap', accessor: 'marketCap', width: 100 },
+  { Header: 'Total rev.', accessor: 'totalRevenue', width: 100 },
+  { Header: 'Holders', accessor: 'holders', width: 100 },
+]
+
+export type MarketplaceToken = {
+  tokenTitle: string
+  tokenName: string
+  isVerified: boolean
+  status: TokenStatus
+  createdAt: Date
+  marketCap: number
+  totalRevenue: number
+  holdersNum: number
+  channelId: string
+}
+
+export type MarketplaceCrtTableProps = {
+  data: MarketplaceToken[]
+  isLoading: boolean
+  emptyState?: TableProps['emptyState']
+}
+
+export const MarketplaceCrtTable = ({ data, emptyState, isLoading }: MarketplaceCrtTableProps) => {
+  const mappingData = useMemo(() => {
+    return data.map((row) => ({
+      token: <TokenInfo {...row} />,
+      status: <CrtStatus status={row.status} />,
+      createdAt: (
+        <Text variant="t100" as="p">
+          {formatDate(row.createdAt)}
+        </Text>
+      ),
+      marketCap: (
+        <NumberFormat
+          icon={<JoyTokenIcon size={16} variant="gray" />}
+          format="short"
+          withDenomination
+          value={row.marketCap}
+          as="p"
+        />
+      ),
+      totalRevenue: (
+        <NumberFormat
+          icon={<JoyTokenIcon size={16} variant="gray" />}
+          format="short"
+          withDenomination
+          value={row.totalRevenue}
+          as="p"
+        />
+      ),
+      holders: (
+        <Text variant="t100" as="p">
+          {pluralizeNoun(row.holdersNum, 'holder')}
+        </Text>
+      ),
+    }))
+  }, [data])
+  // todo add pagination after qa-v6 is merged
+  return (
+    <>
+      <StyledTable
+        minWidth={730}
+        isEmpty={!mappingData.length}
+        columns={COLUMNS}
+        data={isLoading ? tableLoadingData : mappingData}
+        emptyState={emptyState}
+        pageSize={10}
+      />
+    </>
+  )
+}
+
+const StyledTable = styled(Table)<{ isEmpty?: boolean }>`
+  width: 100%;
+  background: transparent;
+
+  .table-base {
+    height: fit-content;
+  }
+
+  .table-header {
+    background-color: transparent;
+    box-shadow: ${cVar('effectDividersBottom')};
+  }
+
+  .table-row {
+    background-color: transparent;
+  }
+
+  th:nth-child(n + 3),
+  th:nth-child(n + 4),
+  th:nth-child(n + 5) {
+    justify-content: end;
+  }
+
+  td:nth-child(n + 3),
+  td:nth-child(n + 4),
+  td:nth-child(n + 5) {
+    align-items: end;
+
+    > div {
+      align-items: end;
+    }
+  }
+`
