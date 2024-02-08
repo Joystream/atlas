4fbf72d3038d0b9167a29ad560d38c94ae327ce6
Artem <Artem Slugin>
2024-02-08 21:54:23 +0530

    fixed empty class error

--- packages/atlas/src/providers/wallet/tmpwallet/core/walletAggregator.ts
+++ packages/atlas/src/providers/wallet/tmpwallet/core/walletAggregator.ts
@@ -1,21 +1,23 @@
 // import type { BaseWallet, BaseWalletProvider } from './types.js'
 // import { InjectedWallet } from '@polkadot-onboard/injected-wallets'
 //
-// export class WalletAggregator {
-//   walletProviders: BaseWalletProvider[]
-//
-//   constructor(providers: BaseWalletProvider[]) {
-//     this.walletProviders = providers
-//   }
-//
-//   async getWallets(): Promise<BaseWallet[]> {
-//     const wallets: (BaseWallet | InjectedWallet)[] = []
-//
-//     for (const provider of this.walletProviders) {
-//       const providedWallets = await provider.getWallets()
-//
-//       wallets.push(...providedWallets)
-//     }
-//     return wallets
-//   }
-// }
+export class WalletAggregator {
+    constructor() {
+    }
+  // walletProviders: BaseWalletProvider[]
+  //
+  // constructor(providers: BaseWalletProvider[]) {
+  //   this.walletProviders = providers
+  // }
+  //
+  // async getWallets(): Promise<BaseWallet[]> {
+  //   const wallets: (BaseWallet | InjectedWallet)[] = []
+  //
+  //   for (const provider of this.walletProviders) {
+  //     const providedWallets = await provider.getWallets()
+  //
+  //     wallets.push(...providedWallets)
+  //   }
+  //   return wallets
+  // }
+}
