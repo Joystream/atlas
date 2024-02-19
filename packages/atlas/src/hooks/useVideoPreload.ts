67741bba785b2ed6ecc330d82fb3fe4a4db3c132
WRadoslaw <r.wyszynski00@gmail.com>
2024-02-19 12:45:10 +0100

    Improve asset fetching logic

--- packages/atlas/src/hooks/useVideoPreload.ts
+++ packages/atlas/src/hooks/useVideoPreload.ts
@@ -1,25 +1,4 @@
-import { useEffect } from 'react'
+import { useGetAssetUrl } from './useGetAssetUrl'
 
-import { useGetAssetUrl } from '@/hooks/useGetAssetUrl'
-
-export const useVideoPreload = (mediaUrls?: string[]) => {
-  const { url } = useGetAssetUrl(mediaUrls, 'video')
-
-  useEffect(() => {
-    if (url) {
-      const videoElement = document.createElement('video')
-      videoElement.src = url
-      videoElement.preload = 'auto'
-      videoElement.style.display = 'none'
-      document.body.appendChild(videoElement)
-      videoElement.oncanplaythrough = () => {
-        // after video reaches playtrough disable preloading to save resources
-        videoElement.preload = 'metadata'
-      }
-
-      return () => {
-        document.body.removeChild(videoElement)
-      }
-    }
-  }, [url])
-}
+export const useVideoPreload = (mediaUrls?: string[]) =>
+  !!useGetAssetUrl(mediaUrls, 'video', { resolveOnlyOnEvents: ['canplay'] })
