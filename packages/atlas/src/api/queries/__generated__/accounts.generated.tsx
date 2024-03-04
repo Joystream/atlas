545bc8be585e2adfa01462651b9a35d81f2427ca
WRadoslaw <r.wyszynski00@gmail.com>
2024-03-04 16:18:58 +0100

    New query generation

--- packages/atlas/src/api/queries/__generated__/accounts.generated.tsx
+++ packages/atlas/src/api/queries/__generated__/accounts.generated.tsx
@@ -18,6 +18,22 @@ export type GetCurrentAccountQuery = {
   }
 }
 
+export type GetChannelFollowsQueryVariables = Types.Exact<{
+  where?: Types.InputMaybe<Types.ChannelFollowWhereInput>
+  limit?: Types.InputMaybe<Types.Scalars['Int']>
+}>
+
+export type GetChannelFollowsQuery = {
+  __typename?: 'Query'
+  channelFollows: Array<{
+    __typename?: 'ChannelFollow'
+    id: string
+    timestamp: Date
+    channelId: string
+    user: { __typename?: 'User'; id: string }
+  }>
+}
+
 export const GetCurrentAccountDocument = gql`
   query GetCurrentAccount {
     accountData {
@@ -63,3 +79,51 @@ export function useGetCurrentAccountLazyQuery(
 export type GetCurrentAccountQueryHookResult = ReturnType<typeof useGetCurrentAccountQuery>
 export type GetCurrentAccountLazyQueryHookResult = ReturnType<typeof useGetCurrentAccountLazyQuery>
 export type GetCurrentAccountQueryResult = Apollo.QueryResult<GetCurrentAccountQuery, GetCurrentAccountQueryVariables>
+export const GetChannelFollowsDocument = gql`
+  query GetChannelFollows($where: ChannelFollowWhereInput, $limit: Int) {
+    channelFollows(where: $where, limit: $limit) {
+      id
+      timestamp
+      channelId
+      user {
+        id
+      }
+    }
+  }
+`
+
+/**
+ * __useGetChannelFollowsQuery__
+ *
+ * To run a query within a React component, call `useGetChannelFollowsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetChannelFollowsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetChannelFollowsQuery({
+ *   variables: {
+ *      where: // value for 'where'
+ *      limit: // value for 'limit'
+ *   },
+ * });
+ */
+export function useGetChannelFollowsQuery(
+  baseOptions?: Apollo.QueryHookOptions<GetChannelFollowsQuery, GetChannelFollowsQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useQuery<GetChannelFollowsQuery, GetChannelFollowsQueryVariables>(GetChannelFollowsDocument, options)
+}
+export function useGetChannelFollowsLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<GetChannelFollowsQuery, GetChannelFollowsQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useLazyQuery<GetChannelFollowsQuery, GetChannelFollowsQueryVariables>(
+    GetChannelFollowsDocument,
+    options
+  )
+}
+export type GetChannelFollowsQueryHookResult = ReturnType<typeof useGetChannelFollowsQuery>
+export type GetChannelFollowsLazyQueryHookResult = ReturnType<typeof useGetChannelFollowsLazyQuery>
+export type GetChannelFollowsQueryResult = Apollo.QueryResult<GetChannelFollowsQuery, GetChannelFollowsQueryVariables>
