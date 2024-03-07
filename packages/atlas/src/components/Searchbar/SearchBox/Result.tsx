2469d94de07a61a55d5882f43fa1f859341f9017
WRadoslaw <r.wyszynski00@gmail.com>
2024-03-07 10:19:42 +0100

    Fix search results width on wide screens

--- packages/atlas/src/components/Searchbar/SearchBox/Result.tsx
+++ packages/atlas/src/components/Searchbar/SearchBox/Result.tsx
@@ -67,7 +67,7 @@ export const Result: FC<ResultProps> = ({
   return (
     <ResultWrapper to={to} selected={selected} handleSelectedItem={onSelected} selectedItem={selectedItem}>
       <ResultContent>
-        {loading && (video ? isLoadingThumbnail : isLoadingAvatar) ? (
+        {loading || (video ? isLoadingThumbnail : isLoadingAvatar) ? (
           <StyledSkeletonLoader width={video ? '64px' : '32px'} height={video ? '40px' : '32px'} rounded={!!channel} />
         ) : channel && !thumbnailUrl ? (
           <StyledSvgAvatarSilhouette width={32} height={32} />
