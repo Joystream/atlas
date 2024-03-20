98b432e146f328b9f38b053db456e99b851eabd7
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2024-03-20 13:47:54 +0100

    ✊ Powered by Joystream (#6031)

--- packages/atlas/src/components/_navigation/TopbarBase/TopbarBase.tsx
+++ packages/atlas/src/components/_navigation/TopbarBase/TopbarBase.tsx
@@ -1,9 +1,10 @@
 import { FC, PropsWithChildren, ReactNode } from 'react'
 
 import { AppLogo } from '@/components/AppLogo'
+import { Text } from '@/components/Text'
 import { useMediaMatch } from '@/hooks/useMediaMatch'
 
-import { Header, LogoLink } from './TopbarBase.styles'
+import { Header, LogoDivider, LogoLink } from './TopbarBase.styles'
 
 export type TopbarBaseProps = PropsWithChildren<{
   fullLogoNode: ReactNode
@@ -14,12 +15,20 @@ export type TopbarBaseProps = PropsWithChildren<{
 
 export const TopbarBase: FC<TopbarBaseProps> = ({ children, fullLogoNode, logoLinkUrl, noLogo, className }) => {
   const mdMatch = useMediaMatch('md')
+  const smMatch = useMediaMatch('sm')
+  const xsMatch = useMediaMatch('xs')
 
   return (
     <Header className={className} data-scroll-lock-fill-gap>
       {!noLogo && (
         <LogoLink to={logoLinkUrl}>
           {mdMatch ? fullLogoNode : <AppLogo variant="short" height={32} width={undefined} />}
+          {xsMatch && <LogoDivider />}
+          {xsMatch && (
+            <Text variant="h200" as="h4" color="colorTextMuted" margin={{ right: 2 }}>
+              {smMatch ? 'Powered' : ''} by Joystream
+            </Text>
+          )}
         </LogoLink>
       )}
       {children}
