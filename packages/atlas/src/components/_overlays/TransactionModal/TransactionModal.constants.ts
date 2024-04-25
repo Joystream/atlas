02887cc1fcfc3096c39b82a52254319a4d919b2b
WRadoslaw <r.wyszynski00@gmail.com>
2024-04-25 10:22:58 +0200

    Add runtime error handling

--- packages/atlas/src/components/_overlays/TransactionModal/TransactionModal.constants.ts
+++ packages/atlas/src/components/_overlays/TransactionModal/TransactionModal.constants.ts
@@ -5,6 +5,7 @@ import {
   propagatingChangesAnimation,
   signatureAnimation,
 } from '@/assets/animations'
+import { atlasConfig } from '@/config'
 import '@/config/config'
 import { ErrorCode } from '@/joystream-lib/errors'
 import { ExtrinsicStatus } from '@/joystream-lib/types'
@@ -192,6 +193,142 @@ export const getExtrinsicStatusDetails = (
           return {
             title: sharedTitle,
             description: 'Insufficient balance to create a video. Add tokens to your membership balance and try again.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.TokenDoesNotExist:
+          return {
+            title: sharedTitle,
+            description:
+              "We couldn't find token that was requested in your transaction, refresh the page and try again. If the problem persists please contact support.",
+            animation: sharedAnimation,
+          }
+        case ErrorCode.NoActiveSale:
+          return {
+            title: sharedTitle,
+            description: 'There is no active sale for this token, refresh the page and try again.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.InsufficientBalanceForTokenPurchase:
+          return {
+            title: sharedTitle,
+            description: `Looks like your balance was insufficient for token purchase. Decrease the amount or buy more ${atlasConfig.joystream.tokenTicker} in Portfolio.`,
+            animation: sharedAnimation,
+          }
+        case ErrorCode.NotEnoughTokensOnSale:
+          return {
+            title: sharedTitle,
+            description: `Amount of tokens to purchase on sale exceeds the quantity of tokens still available on the sale.`,
+            animation: sharedAnimation,
+          }
+        case ErrorCode.SaleStartingBlockInThePast:
+          return {
+            title: sharedTitle,
+            description: 'Specified sale starting block is in the past. Pick future date and try again',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.SaleAccessProofRequired:
+          return {
+            title: sharedTitle,
+            description: 'Only whitelisted participants are allowed to access the sale.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.PreviousSaleNotFinalized:
+          return {
+            title: sharedTitle,
+            description: 'There can be only one sale active at the same time. Finalize the old one to start new.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.SaleDurationTooShort:
+          return {
+            title: sharedTitle,
+            description: 'Sale duration is too short. Please pick longer time frame.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.CannotInitSaleIfAmmIsActive:
+          return {
+            title: sharedTitle,
+            description: 'Cannot initialize a sale when the market is active.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.RevenueSplitDurationTooShort:
+          return {
+            title: sharedTitle,
+            description: 'Revenue split duration is too short. Pick longer time frame.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.RevenueSplitAlreadyActiveForToken:
+          return {
+            title: sharedTitle,
+            description: 'There is already active revenue share for the token. Finalize it to start a new one.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.RevenueSplitNotActiveForToken:
+          return {
+            title: sharedTitle,
+            description: 'Revenue share already ended. Wait for a new one to claim your share.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.RevenueSplitDidNotEnd:
+          return {
+            title: sharedTitle,
+            description: 'Revenue Split has not ended yet, refresh the page and try again.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.RevenueSplitNotOngoing:
+          return {
+            title: sharedTitle,
+            description: 'Revenue share for token is not yet ongoing, refresh the page and try again.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.UserAlreadyParticipating:
+          return {
+            title: sharedTitle,
+            description: 'You are already participating in the revenue share.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.InsufficientBalanceForSplitParticipation:
+          return {
+            title: sharedTitle,
+            description: "You don't have enough balance to participate in the revenue share.",
+            animation: sharedAnimation,
+          }
+        case ErrorCode.CannotModifySupplyWhenRevenueSplitsAreActive:
+          return {
+            title: sharedTitle,
+            description: 'Transaction was blocked to avoid token supply changes during the revenue share.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.NotInAmmState:
+          return {
+            title: sharedTitle,
+            description: 'There is no open market for the token.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.SlippageToleranceExceeded:
+          return {
+            title: sharedTitle,
+            description:
+              'Your order failed to succeed due to slippage exceed. This means that other user just executed their own order which resulted in the price change, to prevent you from losing money your order was cancelled.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.OutstandingAmmProvidedSupplyTooLarge:
+          return {
+            title: sharedTitle,
+            description:
+              'Outstanding market supply is too large. Sell more tokens to the market before trying to close it.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.NotEnoughTokenMintedByAmmForThisSale:
+          return {
+            title: sharedTitle,
+            description:
+              'There are not enough tokens on the market to execute your order. Decrease the amount and try again.',
+            animation: sharedAnimation,
+          }
+        case ErrorCode.CurveSlopeParametersTooLow:
+          return {
+            title: sharedTitle,
+            description: 'Curve slope parameters below minimum allowed.',
             animation: sharedAnimation,
           }
         default:
