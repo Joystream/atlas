bd9e463ae0131ad6523c1ba284ee83bba7d9a4ce
ikprk <i.koprek6@gmail.com>
2024-06-04 13:31:19 +0200

    Bring back joystream total earnings query

--- packages/atlas/src/api/queries/__generated__/baseTypes.generated.ts
+++ packages/atlas/src/api/queries/__generated__/baseTypes.generated.ts
@@ -8384,6 +8384,7 @@ export type Query = {
   tokens: Array<Token>
   tokensConnection: TokensConnection
   topSellingChannels: Array<TopSellingChannelsResult>
+  totalJoystreamEarnings: EarningStatsOutput
   trailerVideoById?: Maybe<TrailerVideo>
   /** @deprecated Use trailerVideoById */
   trailerVideoByUniqueInput?: Maybe<TrailerVideo>
