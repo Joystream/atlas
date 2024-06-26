503a550edd4fea34337f2b1cdfd9ddb1dfce8c46
ikprk <168457495+ikprk@users.noreply.github.com>
2024-06-26 12:53:30 +0200

    :fire: Hotfix: Remove dropped rewards tasks  (#55)
    * Remove some tasks from ypp dashboard
    
    * Remove extra tooltip from membership dropdown
    
    * Add nft and crt marketplace to segment

--- packages/atlas/src/components/_overlays/MemberDropdown/BalanceTooltip.tsx
+++ packages/atlas/src/components/_overlays/MemberDropdown/BalanceTooltip.tsx
@@ -1,13 +1,10 @@
 import BN from 'bn.js'
 import { FC, PropsWithChildren } from 'react'
 
-import { SvgActionInformative } from '@/assets/icons'
-import { JoyTokenIcon } from '@/components/JoyTokenIcon'
-import { NumberFormat } from '@/components/NumberFormat'
-import { Text } from '@/components/Text'
 import { Tooltip } from '@/components/Tooltip'
-
-import { TextWithIcon, TooltipFooter, TooltipRow, TooltipWrapper, UpperRow } from './BalanceTooltip.styles'
+import { TooltipText } from '@/components/Tooltip/Tooltip.styles'
+import { atlasConfig } from '@/config'
+import { ColorAnchor } from '@/views/global/YppLandingView/sections/YppRewardSection.styles'
 
 type BalanceTooltipProps = PropsWithChildren<{
   accountBalance: BN | undefined
@@ -17,7 +14,7 @@ type BalanceTooltipProps = PropsWithChildren<{
 
 export const BalanceTooltip: FC<BalanceTooltipProps> = ({
   accountBalance,
-  lockedAccountBalance = new BN(0),
+  // lockedAccountBalance = new BN(0),
   containerRefElement,
   children,
 }) => {
@@ -33,34 +30,43 @@ export const BalanceTooltip: FC<BalanceTooltipProps> = ({
       placement="bottom"
       delay={[250, 0]}
       customContent={
-        <TooltipWrapper>
-          <UpperRow>
-            <TooltipRow>
-              <Text as="span" variant="t100" color="colorText">
-                Spendable
-              </Text>
-              <TextWithIcon>
-                <JoyTokenIcon size={16} variant="gray" />
-                <NumberFormat
-                  as="span"
-                  variant="t200-strong"
-                  value={lockedAccountBalance}
-                  format="full"
-                  color="colorText"
-                />
-              </TextWithIcon>
-            </TooltipRow>
-          </UpperRow>
-          <TooltipFooter>
-            <SvgActionInformative />
-            <Text as="span" variant="t100" color="colorText">
-              Your transferable balance is zero, meaning you cannot buy NFTs or transfer tokens. There is a spendable
-              balance from faucet, which you can still spend on paying transaction fees for creating channels, posting
-              videos, adding comments and reactions. This balance needs to be repaid before accumulating transferable
-              balance.
-            </Text>
-          </TooltipFooter>
-        </TooltipWrapper>
+        <TooltipText as="span" variant="t100">
+          {atlasConfig.joystream.tokenTicker} token is a native crypto asset of Joystream blockchain which powers
+          {atlasConfig.general.appName}. It is used for trading Creator Tokens, NFTs and covering blockchain processing
+          fees. It is also used for voting on proposals and partaking in council elections.{' '}
+          <ColorAnchor href="https://www.joystream.org/token/" target="_blank">
+            Purchase {atlasConfig.joystream.tokenTicker}
+          </ColorAnchor>
+        </TooltipText>
+
+        // <TooltipWrapper>
+        //   <UpperRow>
+        //     <TooltipRow>
+        //       <Text as="span" variant="t100" color="colorText">
+        //         Spendable
+        //       </Text>
+        //       <TextWithIcon>
+        //         <JoyTokenIcon size={16} variant="gray" />
+        //         <NumberFormat
+        //           as="span"
+        //           variant="t200-strong"
+        //           value={lockedAccountBalance}
+        //           format="full"
+        //           color="colorText"
+        //         />
+        //       </TextWithIcon>
+        //     </TooltipRow>
+        //   </UpperRow>
+        //   <TooltipFooter>
+        //     <SvgActionInformative />
+        //     <Text as="span" variant="t100" color="colorText">
+        //       Your transferable balance is zero, meaning you cannot buy NFTs or transfer tokens. There is a spendable
+        //       balance from faucet, which you can still spend on paying transaction fees for creating channels, posting
+        //       videos, adding comments and reactions. This balance needs to be repaid before accumulating transferable
+        //       balance.
+        //     </Text>
+        //   </TooltipFooter>
+        // </TooltipWrapper>
       }
     >
       {children}
