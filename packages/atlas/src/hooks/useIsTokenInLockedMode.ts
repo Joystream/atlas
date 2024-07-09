c00bf0ae3b747d4b967c745bf3f39f94166a744e
ikprk <i.koprek6@gmail.com>
2024-07-09 08:03:32 +0200

    Correct logic to follow runtime

--- packages/atlas/src/hooks/useIsTokenInLockedMode.ts
+++ packages/atlas/src/hooks/useIsTokenInLockedMode.ts
@@ -1,15 +1,16 @@
-import { useRef } from 'react'
-
+// import { useRef } from 'react'
 import { FullCreatorTokenFragment } from '@/api/queries/__generated__/fragments.generated'
-import { useJoystreamStore } from '@/providers/joystream/joystream.store'
+
+// import { useJoystreamStore } from '@/providers/joystream/joystream.store'
 
 export const useIsTokenInLockedMode = (token?: FullCreatorTokenFragment) => {
-  const currentBlockRef = useRef(useJoystreamStore((store) => store.currentBlock))
+  // const currentBlockRef = useRef(useJoystreamStore((store) => store.currentBlock))
   // 1. Check if there is any unfinalized revenue share
   const activeRevenueShare = token?.revenueShares.find((rS) => !rS.finalized)
 
   // 2. If ending block is not yet came for a user, consider that the token is locked
-  const hasActiveRevenueShare = (activeRevenueShare?.endsAt ?? 0) > currentBlockRef.current
+  // BUG: looks like runtime doesnt allow to make tx if the revenue share not finalized
+  // const hasActiveRevenueShare = (activeRevenueShare?.endsAt ?? 0) > currentBlockRef.current
 
-  return hasActiveRevenueShare
+  return !!activeRevenueShare
 }
