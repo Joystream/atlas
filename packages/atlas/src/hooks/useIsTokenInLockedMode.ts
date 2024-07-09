bebff1161e7be31ba44226ac867949bd9b202507
ikprk <i.koprek6@gmail.com>
2024-07-09 07:51:31 +0200

    Add new hook to unify the token lock logic

---
+++ packages/atlas/src/hooks/useIsTokenInLockedMode.ts
@@ -0,0 +1,15 @@
+import { useRef } from 'react'
+
+import { FullCreatorTokenFragment } from '@/api/queries/__generated__/fragments.generated'
+import { useJoystreamStore } from '@/providers/joystream/joystream.store'
+
+export const useIsTokenInLockedMode = (token?: FullCreatorTokenFragment) => {
+  const currentBlockRef = useRef(useJoystreamStore((store) => store.currentBlock))
+  // 1. Check if there is any unfinalized revenue share
+  const activeRevenueShare = token?.revenueShares.find((rS) => !rS.finalized)
+
+  // 2. If ending block is not yet came for a user, consider that the token is locked
+  const hasActiveRevenueShare = (activeRevenueShare?.endsAt ?? 0) > currentBlockRef.current
+
+  return hasActiveRevenueShare
+}
