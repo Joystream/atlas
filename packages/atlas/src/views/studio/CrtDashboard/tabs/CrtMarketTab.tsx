a7027e2b4cf663da316feb2ccadd4bfc2ad793f0
Edvin Dzidic <edvindzidic2000@gmail.com>
2025-05-09 14:15:20 +0200

    Add market transactions pagination (#6505)
    * Move fetching functionality and add pagination within useMarketTransactionsPagination hook, accomodate code to integrate pagination
    
    * Remove unnecessary comment
    
    * Update packages/atlas/src/hooks/useMarketTransactionsPagination.ts
    
    ---------
    
    Co-authored-by: Leszek Wiesner <leszek@jsgenesis.com>

--- packages/atlas/src/views/studio/CrtDashboard/tabs/CrtMarketTab.tsx
+++ packages/atlas/src/views/studio/CrtDashboard/tabs/CrtMarketTab.tsx
@@ -1,7 +1,6 @@
 import BN from 'bn.js'
 import { useMemo } from 'react'
 
-import { useGetFullAmmCurveQuery } from '@/api/queries/__generated__/creatorTokens.generated'
 import { FullCreatorTokenFragment } from '@/api/queries/__generated__/fragments.generated'
 import { SvgJoyTokenMonochrome24 } from '@/assets/icons'
 import { FlexBox } from '@/components/FlexBox'
@@ -10,29 +9,23 @@ import { WidgetTile } from '@/components/WidgetTile'
 import { AmmTransactionsTable } from '@/components/_crt/AmmTransactionsTable/AmmTransactionsTable'
 import { SkeletonLoader } from '@/components/_loaders/SkeletonLoader'
 import { atlasConfig } from '@/config'
+import { useMarketTransactionsPagination } from '@/hooks/useMarketTransactionsPagination'
 import { useMediaMatch } from '@/hooks/useMediaMatch'
 import { HAPI_TO_JOY_RATE } from '@/joystream-lib/config'
 import { calcBuyMarketPricePerToken } from '@/utils/crts'
-import { SentryLogger } from '@/utils/logs'
 
 type CrtMarketTabProps = {
   token: FullCreatorTokenFragment
 }
 
+const TILES_PER_PAGE = 10
+
 export const CrtMarketTab = ({ token }: CrtMarketTabProps) => {
   const mdMatch = useMediaMatch('md')
   const currentAmm = token.ammCurves.find((curve) => !curve.finalized)
-  const { data, loading } = useGetFullAmmCurveQuery({
-    variables: {
-      where: {
-        id_eq: currentAmm?.id,
-      },
-    },
-    skip: !currentAmm,
-    onError: (error) => {
-      SentryLogger.error('Failed to fetch AMM curve', 'CrtMarketTab', error)
-    },
-  })
+  const { loading, marketTransactions, currentPage, setCurrentPage, perPage, setPerPage, totalCount } =
+    useMarketTransactionsPagination({ ammId: currentAmm?.id, initialPageSize: TILES_PER_PAGE })
+
   const pricePerUnit = useMemo(
     () => calcBuyMarketPricePerToken(currentAmm?.mintedByAmm, currentAmm?.ammSlopeParameter, currentAmm?.ammInitPrice),
     [currentAmm?.ammInitPrice, currentAmm?.ammSlopeParameter, currentAmm?.mintedByAmm]
@@ -103,7 +96,8 @@ export const CrtMarketTab = ({ token }: CrtMarketTabProps) => {
       <AmmTransactionsTable
         symbol={token.symbol ?? 'N/A'}
         loading={loading}
-        data={data?.ammCurves[0].transactions ?? []}
+        data={marketTransactions}
+        pagination={{ page: currentPage, setPerPage, totalCount, itemsPerPage: perPage, onChangePage: setCurrentPage }}
       />
     </>
   )
